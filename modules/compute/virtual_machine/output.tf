13d1b7113287da9a51e034b36c5da8bb45a9f9dc
lolorol <lalesle@microsoft.com>
2020-09-10 02:58:28 +0000

    Fix vm output id for windows

--- modules/compute/virtual_machine/output.tf
+++ modules/compute/virtual_machine/output.tf
@@ -1,4 +1,4 @@
 output id {
-  value     = local.os_type == "linux" ? azurerm_linux_virtual_machine.vm["linux"].id : null
+  value     = local.os_type == "linux" ? azurerm_linux_virtual_machine.vm["linux"].id : azurerm_windows_virtual_machine.vm["windows"].id
   sensitive = true
 }
