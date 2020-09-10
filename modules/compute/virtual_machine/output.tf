83cb38bb3483ccf38dec521d88fb35941280bd1a
lolorol <lalesle@microsoft.com>
2020-09-10 03:10:05 +0000

    Fix vm output null condition

--- modules/compute/virtual_machine/output.tf
+++ modules/compute/virtual_machine/output.tf
@@ -1,4 +1,4 @@
 output id {
-  value     = local.os_type == "linux" ? azurerm_linux_virtual_machine.vm["linux"].id : azurerm_windows_virtual_machine.vm["windows"].id
+  value     = local.os_type == "linux" ? try(azurerm_linux_virtual_machine.vm["linux"].id, null) : try(azurerm_windows_virtual_machine.vm["windows"].id, null)
   sensitive = true
 }
