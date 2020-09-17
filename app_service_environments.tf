b7eadc8cef879006042585c8f99745eb7addff3c
lolorol <lalesle@microsoft.com>
2020-09-17 03:58:18 +0000

    Add Remote vnet support for ASE isolated

---
+++ app_service_environments.tf
@@ -0,0 +1,42 @@
+
+module "app_service_environments" {
+  source = "./modules/webapps/ase"
+
+  for_each = local.webapp.app_service_environments
+
+  resource_group_name       = module.resource_groups[each.value.resource_group_key].name
+  location                  = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : var.global_settings.regions[each.value.region]
+  tags                      = try(each.value.tags, null)
+  name                      = each.value.name
+  kind                      = try(each.value.kind, "ASEV2")
+  zone                      = try(each.value.zone, null)
+  subnet_id                 = lookup(each.value, "remote_networking", null) == null ? module.networking[each.value.vnet_key].subnets[each.value.subnet_key].id : data.terraform_remote_state.ase_vnets[each.key].outputs[each.value.remote_networking.output_key][each.value.remote_networking.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id
+  subnet_name               = lookup(each.value, "remote_networking", null) == null ? module.networking[each.value.vnet_key].subnets[each.value.subnet_key].name :data.terraform_remote_state.ase_vnets[each.key].outputs[each.value.remote_networking.output_key][each.value.remote_networking.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].name
+  internalLoadBalancingMode = each.value.internalLoadBalancingMode
+  front_end_size            = try(each.value.front_end_size, "Standard_D1_V2")
+  diagnostic_profiles       = try(each.value.diagnostic_profiles, null)
+  diagnostics               = local.diagnostics
+  global_settings           = local.global_settings
+
+}
+
+#
+# Get remote vnet to deploy the ase
+#
+data "terraform_remote_state" "ase_vnets" {
+  for_each = {
+    for key, value in local.webapp.app_service_environments : key => value
+    if try(value.remote_networking, null) != null
+  }
+
+  backend = "azurerm"
+  config = {
+    storage_account_name = var.tfstates[each.value.remote_networking.tfstate_key].storage_account_name
+    container_name       = var.tfstates[each.value.remote_networking.tfstate_key].container_name
+    resource_group_name  = var.tfstates[each.value.remote_networking.tfstate_key].resource_group_name
+    key                  = var.tfstates[each.value.remote_networking.tfstate_key].key
+    use_msi              = var.use_msi
+    subscription_id      = var.use_msi ? var.tfstates[each.value.remote_networking.tfstate_key].subscription_id : null
+    tenant_id            = var.use_msi ? var.tfstates[each.value.remote_networking.tfstate_key].tenant_id : null
+  }
+}
--- app_services.tf
+++ app_services.tf
@@ -1,41 +1,4 @@
 
-module "app_service_environments" {
-  source = "./modules/webapps/ase"
-
-  for_each = local.webapp.app_service_environments
-
-  resource_group_name       = module.resource_groups[each.value.resource_group_key].name
-  location                  = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : var.global_settings.regions[each.value.region]
-  tags                      = try(each.value.tags, null)
-  name                      = each.value.name
-  kind                      = try(each.value.kind, "ASEV2")
-  zone                      = try(each.value.zone, null)
-  subnet_id                 = module.networking[each.value.vnet_key].subnets[each.value.subnet_key].id
-  subnet_name               = module.networking[each.value.vnet_key].subnets[each.value.subnet_key].name
-  internalLoadBalancingMode = each.value.internalLoadBalancingMode
-  front_end_size            = try(each.value.front_end_size, "Standard_D1_V2")
-  diagnostic_profiles       = try(each.value.diagnostic_profiles, null)
-  diagnostics               = local.diagnostics
-  global_settings           = local.global_settings
-
-}
-
-module "app_service_plans" {
-  source = "./modules/webapps/asp"
-
-  for_each = local.webapp.app_service_plans
-
-  resource_group_name        = module.resource_groups[each.value.resource_group_key].name
-  location                   = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  app_service_environment_id = lookup(each.value, "app_service_environment_key", null) == null ? null : module.app_service_environments[each.value.app_service_environment_key].id
-  tags                       = try(each.value.tags, null)
-  kind                       = try(each.value.kind, null)
-  settings                   = each.value
-  global_settings            = local.global_settings
-}
-
-
-
 module "app_services" {
   source = "./modules/webapps/appservice"
 
