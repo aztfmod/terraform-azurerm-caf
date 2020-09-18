6896938909d291fed6f8b96c981867c792d26c7b
lolorol <lalesle@microsoft.com>
2020-09-18 08:20:17 +0000

    Update the remote_state attribute to get the vnet and subnet

--- modules/databases/mssql_server/private_endpoints.tf
+++ modules/databases/mssql_server/private_endpoints.tf
@@ -9,10 +9,10 @@ module private_endpoint {
   for_each = var.private_endpoints
 
   resource_id         = azurerm_mssql_server.mssql.id
-  name                = try(format("%s-to-%s-%s", each.value.name, each.value.vnet_key, each.value.subnet_key), format("%s-to-%s-%s-%s", each.value.name, each.value.remote_tfstate.lz_key, each.value.remote_tfstate.vnet_key, each.value.remote_tfstate.subnet_key))
+  name                = try(format("%s-to-%s-%s", each.value.name, each.value.vnet_key, each.value.subnet_key), format("%s-to-%s-%s-%s", each.value.name, each.value.remote_tfstate.lz_key, each.value.vnet_key, each.value.subnet_key))
   location            = var.resource_groups[each.value.resource_group_key].location
   resource_group_name = var.resource_groups[each.value.resource_group_key].name
-  subnet_id           = try(var.vnets[each.value.vnet_key].subnets[each.value.subnet_key].id, data.terraform_remote_state.vnets[each.key].outputs[each.value.remote_tfstate.output_key][each.value.remote_tfstate.lz_key][each.value.remote_tfstate.vnet_key].subnets[each.value.remote_tfstate.subnet_key].id)
+  subnet_id           = try(var.vnets[each.value.vnet_key].subnets[each.value.subnet_key].id, data.terraform_remote_state.vnets[each.key].outputs[each.value.remote_tfstate.output_key][each.value.remote_tfstate.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id)
   settings            = each.value
   global_settings     = var.global_settings
 }
