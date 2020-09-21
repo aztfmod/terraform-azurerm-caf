e3f05ffc708f1176dff7b48b9427c4104386b997
lolorol <LaurentLesle@users.noreply.github.com>
2020-09-21 18:33:19 +0800

    Remove objects

--- modules/recovery_vault/outputs.tf
+++ modules/recovery_vault/outputs.tf
@@ -9,10 +9,4 @@ output "name" {
   value       = azurerm_recovery_services_vault.asr_rg_vault.name
 }
 
-output "object" {
-  description = "Output the full object"
-  value       = azurerm_recovery_services_vault.asr_rg_vault
-}
-
-
 ## need to export all backup policies, asr policies, etc.
