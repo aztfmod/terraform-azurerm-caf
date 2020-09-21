826ae4eaf29de355a2c04a59c1c7879f1b67ee2a
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2020-09-21 11:43:01 +0800

    Revert "Fixed issue executing AIRS launchpad" (#18)

--- modules/analytics/synapse/workspace.tf
+++ modules/analytics/synapse/workspace.tf
@@ -1,7 +1,7 @@
 # naming convention
 resource "azurecaf_name" "wp" {
   name          = var.settings.name
-  resource_type = "azurerm_synapse_workspace"
+  resource_type = "azurerm_machine_learning_workspace"
   prefixes      = [var.global_settings.prefix]
   random_length = var.global_settings.random_length
   clean_input   = true
@@ -9,7 +9,7 @@ resource "azurecaf_name" "wp" {
 }
 
 # synapse workspace
-resource "azurerm_synapse_workspace" "ws" {
+resource "azurerm_synapse_workspace" "wp" {
   name                                 = azurecaf_name.wp.result
   resource_group_name                  = var.resource_group_name
   location                             = var.location
