87bb6294f49d46a861c2f2edf57a6d349838c108
lolorol <LaurentLesle@users.noreply.github.com>
2020-09-21 18:29:09 +0800

    Remove object output

--- modules/automation/output.tf
+++ modules/automation/output.tf
@@ -1,15 +1,9 @@
-output "id" {
+output id {
   description = "Output the object ID"
   value       = azurerm_automation_account.auto_account.id
 }
 
-output "name" {
+output name {
   description = "Output the object name"
   value       = azurerm_automation_account.auto_account.name
-}
-
-output "object" {
-  description = "Output the full object"
-  value       = azurerm_automation_account.auto_account
-  sensitive   = true
 }
