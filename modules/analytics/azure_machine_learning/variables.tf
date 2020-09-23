9bc2f1e8dd3197c7dcdbcbd6f14144328331b6f5
Rahul <39583459+singh-rahul@users.noreply.github.com>
2020-09-23 14:49:11 +0800

    azure ml module added (#29)
    * added "azure machine learning"
    
    * machine learning module
    
    * Update readme
    
    * Update README.md
    
    * FMT
    
    * Delete aml_configuration.tfvars
    
    Co-authored-by: Arnaud Lheureux <arnaudlh@users.noreply.github.com>

---
+++ modules/analytics/azure_machine_learning/variables.tf
@@ -0,0 +1,12 @@
+variable settings {}
+variable global_settings {}
+variable location {}
+variable resource_group_name {}
+variable keyvault_id {}
+variable storage_account_id {}
+variable application_insights_id {}
+
+/* variable virtual_network_id {}
+variable private_subnet_name {}
+variable public_subnet_name {} */
+
