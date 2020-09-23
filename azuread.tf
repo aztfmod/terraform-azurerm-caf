fb4fad65380e3265d3d729df78e37ad38fa02860
lolorol <LaurentLesle@users.noreply.github.com>
2020-09-23 14:55:17 +0800

    Ll integration pipeline (#30)
    * Update keyvault secrets lifecycles
    
    * Update keyvault secrets
    Fix formating
    
    * Azure AD Role - Add msi support

--- azuread.tf
+++ azuread.tf
@@ -22,15 +22,6 @@ output aad_apps {
   sensitive = true
 }
 
-
-module azuread_app_roles {
-  source   = "./modules/azuread/roles"
-  for_each = var.azuread_app_roles
-
-  object_id         = module.azuread_applications[each.key].azuread_service_principal.object_id
-  azuread_app_roles = each.value.roles
-}
-
 #
 # Azure Active Directory Groups
 #
