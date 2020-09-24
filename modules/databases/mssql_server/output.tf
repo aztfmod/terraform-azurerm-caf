b96c655602ef91e892fbaddda3aeda78ec1f9c81
Abdullah Khairi Kamarul Zaman <v-abkama@microsoft.com>
2020-09-24 12:16:05 +0000

    add mssql db module

---
+++ modules/databases/mssql_server/output.tf
@@ -0,0 +1,3 @@
+output id {
+  value = azurerm_mssql_server.mssql.id
+}
---
+++ mssql_databases.tf
@@ -0,0 +1,37 @@
+
+output mssql_databases {
+  value     = module.mssql_databases
+  sensitive = true
+}
+
+module "mssql_databases" {
+  source     = "./modules/databases/mssql_database"
+  depends_on = [module.mssql_servers]
+  for_each   = local.database.mssql_databases
+
+  global_settings  = local.global_settings
+  settings         = each.value
+  server_id        = try(each.value.remote_tfstate, null) == null ? module.mssql_servers[each.value.mssql_server_key].id : data.terraform_remote_state.mssql_remote_server[each.key].outputs[each.value.remote_tfstate.output_key][each.value.mssql_server_key].id
+  storage_accounts = module.storage_accounts
+}
+
+#
+# Get remote mssql server to deploy the database
+#
+data "terraform_remote_state" "mssql_remote_server" {
+  for_each = {
+    for key, value in local.database.mssql_databases : key => value
+    if try(value.remote_tfstate, null) != null
+  }
+
+  backend = "azurerm"
+  config = {
+    storage_account_name = var.tfstates[each.value.remote_tfstate.tfstate_key].storage_account_name
+    container_name       = var.tfstates[each.value.remote_tfstate.tfstate_key].container_name
+    resource_group_name  = var.tfstates[each.value.remote_tfstate.tfstate_key].resource_group_name
+    key                  = var.tfstates[each.value.remote_tfstate.tfstate_key].key
+    use_msi              = var.use_msi
+    subscription_id      = var.use_msi ? var.tfstates[each.value.remote_tfstate.tfstate_key].subscription_id : null
+    tenant_id            = var.use_msi ? var.tfstates[each.value.remote_tfstate.tfstate_key].tenant_id : null
+  }
+}
