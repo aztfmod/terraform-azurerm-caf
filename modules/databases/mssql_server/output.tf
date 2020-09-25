d51a2e3516e0cf759446778026fa3b119b65022e
Abdullah Khairi <iriahk89@gmail.com>
2020-09-25 12:27:42 +0800

    Added identity property for mssql_server for IAM (#33)
    * Added identity property for mssql_server for IAM
    
    * Added missing porperty for mssql_server
    
    * Add identity in mssql_server output
    
    Co-authored-by: Abdullah Khairi Kamarul Zaman <v-abkama@microsoft.com>

--- modules/databases/mssql_server/output.tf
+++ modules/databases/mssql_server/output.tf
@@ -3,5 +3,13 @@ output id {
 }
 
 output rbac_id {
-  value = azurerm_mssql_server.mssql.identity[0].principal_id
+  value = try(azurerm_mssql_server.mssql.identity[0].principal_id, null)
+}
+
+output identity {
+  value = try(azurerm_mssql_server.mssql.identity, null)
+}
+
+output azuread_administrator  {
+  value = try(azurerm_mssql_server.mssql.azuread_administrator , null)
 }
