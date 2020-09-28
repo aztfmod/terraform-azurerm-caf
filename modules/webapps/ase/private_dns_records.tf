a001712cb975c71394a54a1273b469904146a4bc
Abdullah Khairi <iriahk89@gmail.com>
2020-09-28 14:00:45 +0800

    Ak hotfix ase privatedns record name (#39)
    * concate asename into private dns record name
    * Update private dns a record ase name logic

--- modules/webapps/ase/private_dns_records.tf
+++ modules/webapps/ase/private_dns_records.tf
@@ -4,7 +4,7 @@ resource "azurerm_private_dns_a_record" "a_records" {
   ]
   for_each = try(var.settings.private_dns_records.a_records, {})
 
-  name                = format("%s.%s", each.value.name, azurecaf_name.ase.result)
+  name                = each.value.name == "" ? azurecaf_name.ase.result : format("%s.%s", each.value.name, azurecaf_name.ase.result)
   resource_group_name = lookup(each.value, "remote_tfstate", null) == null ? var.private_dns[each.value.private_dns_key].resource_group_name : data.terraform_remote_state.ase_vnet_dns[each.key].outputs[each.value.remote_tfstate.output_key][each.value.private_dns_key].resource_group_name
   zone_name           = lookup(each.value, "remote_tfstate", null) == null ? var.private_dns[each.value.private_dns_key].name : data.terraform_remote_state.ase_vnet_dns[each.key].outputs[each.value.remote_tfstate.output_key][each.value.private_dns_key].name
   ttl                 = each.value.ttl
