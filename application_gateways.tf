af1f1ba8468bae60e50bca62df2384d85001e0d2
lolorol <LaurentLesle@users.noreply.github.com>
2020-10-01 05:06:32 +0000

    Update merge

--- application_gateways.tf
+++ application_gateways.tf
@@ -2,7 +2,6 @@ module application_gateways {
   source   = "./modules/networking/application_gateway"
   for_each = local.networking.application_gateways
 
-<<<<<<< HEAD
   global_settings     = local.global_settings
   diagnostics         = local.diagnostics
   tfstates            = var.tfstates
@@ -29,16 +28,3 @@ output application_gateway_applications {
   value     = local.networking.application_gateway_applications
   sensitive = true
 }
-=======
-  global_settings                  = local.global_settings
-  diagnostics                      = local.diagnostics
-  resource_group_name              = module.resource_groups[each.value.resource_group_key].name
-  location                         = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  settings                         = each.value
-  sku_name                         = each.value.sku_name
-  sku_tier                         = each.value.sku_tier
-  vnets                            = module.networking
-  public_ip_addresses              = module.public_ip_addresses
-  application_gateway_applications = local.networking.application_gateway_applications[each.key]
-}
->>>>>>> 0117fb71b38a3fd9f6e8df24b52418e3dc799062
