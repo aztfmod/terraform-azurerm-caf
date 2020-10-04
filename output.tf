d0e13463dec179b4719c3221e47a3d6822263957
lolorol <LaurentLesle@users.noreply.github.com>
2020-10-04 15:54:11 +0000

    Add tenant details

--- output.tf
+++ output.tf
@@ -2,3 +2,11 @@ output diagnostics {
   value     = local.diagnostics
   sensitive = true
 }
+
+output tenant {
+  value = {
+    tenant_id       = local.client_config.tenant_id
+    subscription_id = local.client_config.subscription_id
+  }
+  sensitive = true
+}
