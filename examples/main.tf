36cbaac4a0065d4f2be2494f7df95f436f5a3c72
lolorol <LaurentLesle@users.noreply.github.com>
2020-10-06 13:45:22 +0000

    CI Update level3 examples

--- examples/main.tf
+++ examples/main.tf
@@ -43,57 +43,85 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-data "terraform_remote_state" "caf_foundations" {
-  backend = "azurerm"
+data "terraform_remote_state" "lower_networking" {
+  for_each = try(var.landingzone.lower.networking, {})
+
+  backend = var.landingzone.backend_type
   config = {
     storage_account_name = var.lower_storage_account_name
     container_name       = var.lower_container_name
-    key                  = var.tfstates.caf_foundations.tfstate
     resource_group_name  = var.lower_resource_group_name
+    key                  = each.value.tfstate
   }
 }
 
-data "terraform_remote_state" "networking" {
-  backend = "azurerm"
+data "terraform_remote_state" "shared_services" {
+  backend = var.landingzone.backend_type
   config = {
     storage_account_name = var.lower_storage_account_name
     container_name       = var.lower_container_name
-    key                  = var.tfstates.networking.tfstate
     resource_group_name  = var.lower_resource_group_name
+    key                  = var.landingzone.lower.shared_services.tfstate
   }
 }
 
 locals {
-  tags = merge(var.tags, { "level" = var.level }, { "environment" = var.environment }, { "rover_version" = var.rover_version })
+  landingzone_tag = {
+    "landingzone" = basename(abspath(path.module))
+  }
+  tags = merge(local.landingzone_tag, { "level" = var.landingzone.current.level }, { "environment" = local.global_settings.environment }, { "rover_version" = var.rover_version }, var.tags)
 
   global_settings = {
-    prefix         = data.terraform_remote_state.caf_foundations.outputs.global_settings.prefix
-    default_region = try(var.global_settings.default_region, data.terraform_remote_state.caf_foundations.outputs.global_settings.default_region)
-    environment    = data.terraform_remote_state.caf_foundations.outputs.global_settings.environment
-    regions        = try(var.global_settings.regions, data.terraform_remote_state.caf_foundations.outputs.global_settings.regions)
+    prefix         = try(var.global_settings.prefix, data.terraform_remote_state.shared_services.outputs.global_settings.prefix)
+    default_region = try(var.global_settings.default_region, data.terraform_remote_state.shared_services.outputs.global_settings.default_region)
+    regions        = try(var.global_settings.regions, null) == null ? data.terraform_remote_state.shared_services.outputs.global_settings.regions : merge(data.terraform_remote_state.shared_services.outputs.global_settings.regions, var.global_settings.regions)
+    environment    = data.terraform_remote_state.shared_services.outputs.global_settings.environment
+    random_length  = try(var.global_settings.random_length, data.terraform_remote_state.shared_services.outputs.global_settings.random_length)
+    passthrough    = try(var.global_settings.passthrough, false)
   }
 
   diagnostics = {
-    diagnostics_definition   = merge(data.terraform_remote_state.caf_foundations.outputs.diagnostics.diagnostics_definition, var.diagnostics_definition)
-    diagnostics_destinations = data.terraform_remote_state.caf_foundations.outputs.diagnostics.diagnostics_destinations
-    storage_accounts         = data.terraform_remote_state.caf_foundations.outputs.diagnostics.storage_accounts
-    log_analytics            = data.terraform_remote_state.caf_foundations.outputs.diagnostics.log_analytics
+    diagnostics_definition   = merge(data.terraform_remote_state.shared_services.outputs.diagnostics.diagnostics_definition, var.diagnostics_definition)
+    diagnostics_destinations = data.terraform_remote_state.shared_services.outputs.diagnostics.diagnostics_destinations
+    storage_accounts         = data.terraform_remote_state.shared_services.outputs.diagnostics.storage_accounts
+    log_analytics            = data.terraform_remote_state.shared_services.outputs.diagnostics.log_analytics
   }
 
+  # Merge all remote networking objects
+  lower_networking = {
+    for key, networking in try(var.landingzone.lower.networking, {}) : key => merge(data.terraform_remote_state.lower_networking[key].outputs.networking[key])
+  }
+
+  # Merge all remote private_dns objects
+  lower_private_dns = {
+    for key, networking in try(var.landingzone.lower.networking, {}) : key => merge(data.terraform_remote_state.lower_networking[key].outputs.private_dns)
+  }
+
+  # Combine them with the local networking objects
+  networking = merge(
+    local.lower_networking,
+    map(
+      var.landingzone.current.key,
+      map(
+        "vnets", try(module.caf.vnets, {})
+      )
+    )
+  )
+
   tfstates = merge(
-    map(var.landingzone_name,
+    map(var.landingzone.current.key,
       map(
         "storage_account_name", var.tfstate_storage_account_name,
         "container_name", var.tfstate_container_name,
         "resource_group_name", var.tfstate_resource_group_name,
         "key", var.tfstate_key,
-        "level", var.level,
+        "level", var.landingzone.current.level,
         "tenant_id", data.azurerm_client_config.current.tenant_id,
         "subscription_id", data.azurerm_client_config.current.subscription_id
       )
     )
     ,
-    data.terraform_remote_state.networking.outputs.tfstates
+    data.terraform_remote_state.shared_services.outputs.tfstates
   )
 
 }
