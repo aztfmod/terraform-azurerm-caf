094417be265c916d49e41289bba709d26c09b3bf
lolorol <LaurentLesle@users.noreply.github.com>
2020-10-06 07:03:18 +0000

    Update virtual wan vhub on remote networking

--- networking_virtual_wan.tf
+++ networking_virtual_wan.tf
@@ -23,25 +23,6 @@ module virtual_wans {
 #
 #
 
-# Support for peering with vnet in another state/landing zone
-data "terraform_remote_state" "peering_vhub" {
-  for_each = {
-    for key, peering in local.networking.vhub_peerings : key => peering
-    if try(peering.from.tfstate_key, null) != null
-  }
-
-  backend = "azurerm"
-  config = {
-    storage_account_name = var.tfstates[each.value.from.tfstate_key].storage_account_name
-    container_name       = var.tfstates[each.value.from.tfstate_key].container_name
-    resource_group_name  = var.tfstates[each.value.from.tfstate_key].resource_group_name
-    key                  = var.tfstates[each.value.from.tfstate_key].key
-    use_msi              = var.use_msi
-    subscription_id      = var.use_msi ? var.tfstates[each.value.from.tfstate_key].subscription_id : null
-    tenant_id            = var.use_msi ? var.tfstates[each.value.from.tfstate_key].tenant_id : null
-  }
-}
-
 # Peering
 resource "azurerm_virtual_hub_connection" "vhub_connection" {
   depends_on = [module.networking, module.virtual_wans]
@@ -49,11 +30,8 @@ resource "azurerm_virtual_hub_connection" "vhub_connection" {
 
   name                      = each.value.name
   virtual_hub_id            = try(module.virtual_wans[each.value.vhub.virtual_wan_key].virtual_hubs[each.value.vhub.virtual_hub_key].id, null)
-  remote_virtual_network_id = try(module.networking[each.value.vnet.vnet_key].id, data.terraform_remote_state.peering_vhub[each.key].outputs[each.value.vnet.output_key][each.value.vnet.lz_key][each.value.vnet.vnet_key].id)
-
-  hub_to_vitual_network_traffic_allowed          = try(each.value.hub_to_vitual_network_traffic_allowed, null)
-  vitual_network_to_hub_gateways_traffic_allowed = try(each.value.vitual_network_to_hub_gateways_traffic_allowed, null)
-  internet_security_enabled                      = try(each.value.internet_security_enabled, null)
+  remote_virtual_network_id = lookup(each.value.vnet, "lz_key", null) == null ? local.combined_objects_networking[each.value.vnet.vnet_key].id : local.combined_objects_networking[each.value.vnet.output_key].vnets[each.value.vnet.vnet_key].id
+  internet_security_enabled = try(each.value.internet_security_enabled, null)
 }
 
 # Outputs
