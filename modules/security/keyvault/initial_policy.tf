a910554ec72d10dc8579c178877a46d8bcbb4e40
lolorol <LaurentLesle@users.noreply.github.com>
2020-10-22 23:28:35 +0800

    0.4.3 patch (#80)
    * Add keyvault RBAC support
    
    * Role_mapping structure update. Require to adjust the config structure
    
    * Add landingzone key to example caf module
    
    * Update pipeline and default settings
    
    * Change examples pipelines
    
    * Fix modules for examples
    
    * Fix example policy dependencies
    
    * Update README
    
    * Update remote resources support
    
    * Update timeout for role propagation
    Update peering for remote vnet support
    
    * Update rover for ci
    
    * Update azure caf name for msi, private vnet ling and ase
    
    * Fix examples for app_services to remote remote lz
    
    * Update remote
    
    * Fix AzureAD for remote lz support
    
    * Add remote lz for Azure AD Groups
    Add Azure AD groups to initial keyvault policies
    
    Co-authored-by: Arnaud Lheureux <arnaudlh@users.noreply.github.com>

--- modules/security/keyvault/initial_policy.tf
+++ modules/security/keyvault/initial_policy.tf
@@ -3,7 +3,8 @@ module "initial_policy" {
   source = "../keyvault_access_policies"
   count  = try(var.settings.creation_policies, null) == null ? 0 : 1
 
-  keyvault_id     = azurerm_key_vault.keyvault.id
-  access_policies = var.settings.creation_policies
-  client_config   = var.client_config
+  keyvault_id             = azurerm_key_vault.keyvault.id
+  access_policies         = var.settings.creation_policies
+  client_config           = var.client_config
+  azuread_groups          = var.azuread_groups
 }
