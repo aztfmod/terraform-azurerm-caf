5a5d0f977ca69cfe1ffc9526fca2e6c89ddfa660
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2020-10-22 07:58:54 +0000

    Update for empty var.settings.tags

--- modules/analytics/synapse/sql_pool/main.tf
+++ modules/analytics/synapse/sql_pool/main.tf
@@ -2,7 +2,7 @@ locals {
   module_tag = {
     "module" = basename(abspath(path.module))
   }
-  tags = merge(local.module_tag, var.tags, var.settings.tags)
+  tags = merge(local.module_tag, var.tags, try(var.settings.tags, null))
 }
 
 
