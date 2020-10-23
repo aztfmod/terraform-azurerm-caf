7d4a5b7f846722429de02eed761029afbd44294e
Abdullah Khairi <akamarulzama@microsoft.com>
2020-10-23 05:06:10 +0000

    implement combine object to mssqlserver

--- mssql_servers.tf
+++ mssql_servers.tf
@@ -13,9 +13,9 @@ module "mssql_servers" {
   settings            = each.value
   resource_group_name = module.resource_groups[each.value.resource_group_key].name
   location            = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  keyvault_id         = try(each.value.administrator_login_password, null) == null ? module.keyvaults[each.value.keyvault_key].id : null
+  keyvault_id         = try(each.value.administrator_login_password, null) != null ? null : try(each.value.lz_key, null) == null ? local.combined_objects_keyvaults[local.client_config.landingzone_key][each.value.keyvault_key].id : local.combined_objects_keyvaults[each.value.lz_key][each.value.keyvault_key].id
   storage_accounts    = module.storage_accounts
-  azuread_groups      = module.azuread_groups
+  azuread_groups      = local.combined_objects_azuread_groups
   vnets               = local.combined_objects_networking
   private_endpoints   = try(each.value.private_endpoints, {})
   resource_groups     = try(each.value.private_endpoints, {}) == {} ? null : module.resource_groups
