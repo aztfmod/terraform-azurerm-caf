e44ce1ccf92775ae3170d95e9c63b8c8a93c8e32
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2020-10-30 12:24:39 +0000

    Diagnostics optional

--- modules/compute/virtual_machine/diagnostics.tf
+++ modules/compute/virtual_machine/diagnostics.tf
@@ -6,5 +6,5 @@ module nics {
   resource_id       = azurerm_network_interface.nic[each.key].id
   resource_location = azurerm_network_interface.nic[each.key].location
   diagnostics       = var.diagnostics
-  profiles          = each.value.diagnostic_profiles
+  profiles          = try(each.value.diagnostic_profiles, {})
 }
