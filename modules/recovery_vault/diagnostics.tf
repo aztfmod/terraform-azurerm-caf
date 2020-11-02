38348f01f806b06148e92c838f53231606879779
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2020-11-02 08:29:37 +0000

    ASR and Automation optional diags

--- modules/recovery_vault/diagnostics.tf
+++ modules/recovery_vault/diagnostics.tf
@@ -5,5 +5,5 @@ module diagnostics {
   resource_id       = azurerm_recovery_services_vault.asr_rg_vault.id
   resource_location = var.location
   diagnostics       = var.diagnostics
-  profiles          = var.settings.diagnostic_profiles
+  profiles          = try(var.settings.diagnostic_profiles, {})
 }
