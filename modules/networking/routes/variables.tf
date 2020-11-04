fb965aeaa796b87476808f1bee895cecb90df345
Abdullah Khairi <akamarulzama@microsoft.com>
2020-11-04 06:22:05 +0000

    refactor next_hop_in_ip_address implementation

--- modules/networking/routes/variables.tf
+++ modules/networking/routes/variables.tf
@@ -3,6 +3,12 @@ variable resource_group_name {}
 variable route_table_name {}
 variable address_prefix {}
 variable next_hop_type {}
+variable next_hop_in_ip_address_fw {
+  default = null
+}
+variable next_hop_in_ip_address_vm {
+  default = null
+}
 variable next_hop_in_ip_address {
   default = null
 }
