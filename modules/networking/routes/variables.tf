bbad27bd296082c77cb53ce69db8877fd4ed9ffc
Abdullah Khairi <akamarulzama@microsoft.com>
2020-11-04 04:17:33 +0000

    Add support for 3rd party NVA (Palo Alto etc) in route table on next_hop_in_ip_address

--- modules/networking/routes/variables.tf
+++ modules/networking/routes/variables.tf
@@ -3,9 +3,6 @@ variable resource_group_name {}
 variable route_table_name {}
 variable address_prefix {}
 variable next_hop_type {}
-variable next_hop_in_ip_address_fw {
-  default = null
-}
-variable next_hop_in_ip_address_vm {
+variable next_hop_in_ip_address {
   default = null
 }
