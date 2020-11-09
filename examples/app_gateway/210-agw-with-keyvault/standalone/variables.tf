111d07180f97fe264f037c1918176c51aa1d9e5f
lolorol <LaurentLesle@users.noreply.github.com>
2020-11-09 06:38:05 +0000

    AGW - Add example 210

---
+++ examples/app_gateway/210-agw-with-keyvault/standalone/variables.tf
@@ -0,0 +1,78 @@
+
+variable global_settings {
+  default = {}
+}
+
+variable tags {
+  default = null
+  type    = map
+}
+variable app_service_environments {
+  default = {}
+}
+variable app_service_plans {
+  default = {}
+}
+variable app_services {
+  default = {}
+}
+variable diagnostics_definition {
+  default = null
+}
+variable resource_groups {
+  default = null
+}
+variable network_security_group_definition {
+  default = null
+}
+variable vnets {
+  default = {}
+}
+variable storage_accounts {
+  default = {}
+}
+variable azuread_groups {
+  default = {}
+}
+variable keyvaults {
+  default = {}
+}
+variable keyvault_access_policies {
+  default = {}
+}
+variable keyvault_certificates {
+  default = {}
+}
+variable public_ip_addresses {
+  default = {}
+}
+variable diagnostic_storage_accounts {
+  default = {}
+}
+variable managed_identities {
+  default = {}
+}
+variable private_dns {
+  default = {}
+}
+variable synapse_workspaces {
+  default = {}
+}
+variable azurerm_application_insights {
+  default = {}
+}
+variable role_mapping {
+  default = {}
+}
+variable monitoring {
+  default = {}
+}
+variable event_hub_namespaces {
+  default = {}
+}
+variable application_gateways {
+  default = {}
+}
+variable application_gateway_applications {
+  default = {}
+}
