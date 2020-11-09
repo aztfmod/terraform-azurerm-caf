b2251744e40b37cebcd4bce6dfdc0eac50e00216
Abdullah Khairi <iriahk89@gmail.com>
2020-11-09 17:34:03 +0800

    implement data and lifecycle to avoid force replacement on other ase … (#114)
    * implement data and lifecycle to avoid force replacement on other ase dependent resources
    
    * adde comment on lifecycle in dns
    
    * update azure LZ repo branch in workflow
    
    Co-authored-by: Abdullah Khairi <akamarulzama@microsoft.com>

--- app_service_plans.tf
+++ app_service_plans.tf
@@ -7,7 +7,7 @@ module "app_service_plans" {
   resource_group_name        = module.resource_groups[each.value.resource_group_key].name
   location                   = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
   base_tags                  = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
-  app_service_environment_id = try(each.value.lz_key, null) == null ? (try(each.value.app_service_environment_key, null) == null ? null : local.combined_objects_app_service_environments[local.client_config.landingzone_key][each.value.app_service_environment_key].id) : local.combined_objects_app_service_environments[each.value.lz_key][each.value.app_service_environment_key].id
+  app_service_environment_id = try(each.value.app_service_environment_key, null) == null ? null : try(local.combined_objects_app_service_environments[local.client_config.landingzone_key][each.value.app_service_environment_key].id, local.combined_objects_app_service_environments[each.value.lz_key][each.value.app_service_environment_key].id)
   tags                       = try(each.value.tags, null)
   kind                       = try(each.value.kind, null)
   settings                   = each.value
