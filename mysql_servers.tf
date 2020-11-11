59a8b8a9eedccbeab54f5c4f4f82cc805e5e1c37
abhilash-keloth <abhi2620@gmail.com>
2020-11-11 10:40:14 +0000

    correct

--- mysql_servers.tf
+++ mysql_servers.tf
@@ -6,16 +6,18 @@ output mysql_servers {
 
 module "mysql_servers" {
   source     = "./modules/databases/mysql_server"
-  depends_on = [module.keyvault_access_policies]
+  depends_on = [module.keyvault_access_policies, module.keyvault_access_policies_azuread_apps]
   for_each   = local.database.mysql_servers
 
   global_settings     = local.global_settings
+  client_config       = local.client_config
   settings            = each.value
   resource_group_name = module.resource_groups[each.value.resource_group_key].name
   location            = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
   keyvault_id         = try(each.value.administrator_login_password, null) == null ? module.keyvaults[each.value.keyvault_key].id : null
   storage_accounts    = module.storage_accounts
-  azuread_groups      = module.azuread_groups
+  #azuread_groups      = module.azuread_groups
+  azuread_groups      = local.combined_objects_azuread_groups
   vnets               = local.combined_objects_networking
   private_endpoints   = try(each.value.private_endpoints, {})
   resource_groups     = try(each.value.private_endpoints, {}) == {} ? null : module.resource_groups
