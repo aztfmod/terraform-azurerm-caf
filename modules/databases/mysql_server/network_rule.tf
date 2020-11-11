6557b3073e824cb0b7443fdcfe5a12136ac1ccb1
abhilash-keloth <abhi2620@gmail.com>
2020-11-11 14:21:49 +0000

    commented network rule.tf

--- modules/databases/mysql_server/network_rule.tf
+++ modules/databases/mysql_server/network_rule.tf
@@ -1,22 +1,22 @@
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  address_space       = ["10.7.30.0/29"]
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
+# resource "azurerm_virtual_network" "example" {
+#   name                = "example-vnet"
+#   address_space       = ["10.7.30.0/29"]
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
+# }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.7.30.0/29"]
-  service_endpoints    = ["Microsoft.Sql"]
-}
+# resource "azurerm_subnet" "internal" {
+#   name                 = "internal"
+#   resource_group_name  = var.resource_group_name
+#   virtual_network_name = azurerm_virtual_network.example.name
+#   address_prefixes     = ["10.7.30.0/29"]
+#   service_endpoints    = ["Microsoft.Sql"]
+# }
 
-resource "azurerm_mysql_virtual_network_rule" "mysql_network" {
-  
-  name                = "virtual-network-rule"
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_mysql_server.mysql.name
-  subnet_id           = azurerm_subnet.internal.id
-}
+# resource "azurerm_mysql_virtual_network_rule" "mysql_network" {
+#   depends_on = [azurerm_mysql_server.mysql]
+#   name                = "virtual-network-rule"
+#   resource_group_name = var.resource_group_name
+#   server_name         = azurerm_mysql_server.mysql.name
+#   subnet_id           = azurerm_subnet.internal.id
+# }
