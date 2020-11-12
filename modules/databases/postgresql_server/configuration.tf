fd31e60d1cb45c7d85877dde14ca5db74687ee8e
abhilash-keloth <abhi2620@gmail.com>
2020-11-12 02:03:38 +0000

    commented configuration

--- modules/databases/postgresql_server/configuration.tf
+++ modules/databases/postgresql_server/configuration.tf
@@ -3,12 +3,12 @@
 # Configuration
 #
 
-resource "azurerm_postgresql_configuration" "postgresql_configuration" {
+# resource "azurerm_postgresql_configuration" "postgresql_configuration" {
 
-  for_each = var.settings.postgresql_configuration
+#   for_each = var.settings.postgresql_configuration
 
-  name                = each.value.name
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_postgresql_server.postgresql.name
-  value               = each.value.value
-}
+#   name                = each.value.name
+#   resource_group_name = var.resource_group_name
+#   server_name         = azurerm_postgresql_server.postgresql.name
+#   value               = each.value.value
+# }
