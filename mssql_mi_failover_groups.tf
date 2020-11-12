0f2eb30aa5436a36a2000b592951731b3cc8f8cc
Abdullah Khairi <akamarulzama@microsoft.com>
2020-11-12 16:53:57 +0000

    update failover mi implementation to use secondari mi module

--- mssql_mi_failover_groups.tf
+++ mssql_mi_failover_groups.tf
@@ -6,7 +6,7 @@ module "mssql_mi_failover_groups" {
   settings                  = each.value
   resource_group_name       = module.resource_groups[each.value.resource_group_key].name
   primaryManagedInstanceId  = local.combined_objects_mssql_managed_instances[try(each.value.primary_server.lz_key, local.client_config.landingzone_key)][each.value.primary_server.mi_server_key].id
-  partnerManagedInstanceId  = local.combined_objects_mssql_managed_instances[try(each.value.secondary_server.lz_key, local.client_config.landingzone_key)][each.value.secondary_server.mi_server_key].id
-  partnerRegion             = local.combined_objects_mssql_managed_instances[try(each.value.secondary_server.lz_key, local.client_config.landingzone_key)][each.value.secondary_server.mi_server_key].location
+  partnerManagedInstanceId  = module.mssql_managed_instances_secondary[each.value.secondary_server.mi_server_key].id
+  partnerRegion             = module.mssql_managed_instances_secondary[each.value.secondary_server.mi_server_key].location
   base_tags                 = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
 }
