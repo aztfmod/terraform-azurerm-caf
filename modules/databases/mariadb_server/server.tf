c90eff85f4695371101a70e957df6cabf2f0015b
abhilash-keloth <abhi2620@gmail.com>
2020-11-16 02:57:54 +0000

    adding pass

--- modules/databases/mariadb_server/server.tf
+++ modules/databases/mariadb_server/server.tf
@@ -36,7 +36,7 @@ resource "random_password" "mariadb_admin" {
 resource "azurerm_key_vault_secret" "mariadb_admin_password" {
   count = try(var.settings.administrator_login_password, null) == null ? 1 : 0
 
-  name         = format("%s-passwd", azurecaf_name.mariadb.result)
+  name         = format("%s-password", azurecaf_name.mariadb.result)
   value        = random_password.mariadb_admin.0.result
   key_vault_id = var.keyvault_id
 
@@ -50,7 +50,7 @@ resource "azurerm_key_vault_secret" "mariadb_admin_password" {
 resource "azurerm_key_vault_secret" "mariadb_admin" {
   count = try(var.settings.administrator_login_password, null) == null ? 1 : 0
 
-  name         = format("%s-user", azurecaf_name.mariadb.result)
+  name         = format("%s-username", azurecaf_name.mariadb.result)
   value        = var.settings.administrator_login
   key_vault_id = var.keyvault_id
 }
