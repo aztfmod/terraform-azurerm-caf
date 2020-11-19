7e4e650b60d0197a161826eb7809a617619ca921
Abdullah Khairi <akamarulzama@microsoft.com>
2020-11-19 10:02:36 +0000

    remove commented code

--- modules/databases/mssql_managed_instance/administrator/scripts/set_mi_ad_admin.sh
+++ modules/databases/mssql_managed_instance/administrator/scripts/set_mi_ad_admin.sh
@@ -2,15 +2,3 @@
 
 az sql mi ad-admin create -u ${DISPLAY_NAME} --mi ${MI_NAME} -i ${OBJECT_ID} -g ${RG_NAME}
 echo "${DISPLAY_NAME} assigned as ${MI_NAME} admin"
-
-# case "${METHOD}" in
-#     POST)       
-#         az sql mi ad-admin create -u ${DISPLAY_NAME} --mi ${MI_NAME} -i ${OBJECT_ID} -g ${RG_NAME}
-
-#         echo "${DISPLAY_NAME} assigned as ${MI_NAME} admin"
-#         ;;
-#     DELETE)
-#         az sql mi ad-admin delete --mi ${MI_NAME} -g ${RG_NAME}
-#         echo "'Unassigned ${MI_NAME} admin"
-#         ;;
-# esac
