f00e7de1fa30b4ff178d50e502d5c8222d6b9448
lolorol <LaurentLesle@users.noreply.github.com>
2020-11-20 06:19:33 +0000

    Patch example

--- examples/compute/virtual_machine/211-vm-bastion-winrm-agents/standalone/vm_extensions.tf
+++ examples/compute/virtual_machine/211-vm-bastion-winrm-agents/standalone/vm_extensions.tf
@@ -1,15 +1,20 @@
+#
+# microsoft_enterprise_cloud_monitoring - Install the monitoring agent in the virtual machine
+#
+
 module "vm_extension_monitoring_agent" {
   source = "../../../../../modules/compute/virtual_machine_extensions"
   depends_on = [module.caf]
 
   for_each = {
     for key, value in try(var.virtual_machines, {}) : key => value
-    if try(value.virtual_machine_extensions.microsoft_enterprisecloud_monitoring, null) != null
+    if try(value.virtual_machine_extensions.microsoft_enterprise_cloud_monitoring, null) != null
   }
 
   client_config      = module.caf.client_config
   virtual_machine_id = module.caf.virtual_machines[each.key].id
-  extensions         = each.value.virtual_machine_extensions
+  extension          = each.value.virtual_machine_extensions.microsoft_enterprise_cloud_monitoring
+  extension_name     = "microsoft_enterprise_cloud_monitoring"
   settings = {
     log_analytics = map(
       module.caf.client_config.landingzone_key,
