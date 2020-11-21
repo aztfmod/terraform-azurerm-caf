cd8ceb35d1d149291abd6975bf266e5047e39c65
Rahul <39583459+singh-rahul@users.noreply.github.com>
2020-11-21 14:08:06 +0000

    optional arguments for sql databases

--- modules/databases/cosmos_db/sql_database/sql_database.tf
+++ modules/databases/cosmos_db/sql_database/sql_database.tf
@@ -11,6 +11,15 @@ resource "azurerm_cosmosdb_sql_database" "database" {
   resource_group_name = var.resource_group_name
   account_name        = var.cosmosdb_account_name
   throughput          = var.settings.throughput
+
+  # Note : throughput & autoscaling is conflicting properties, Therefore, set any one of them
+  dynamic "autoscale_settings" {
+    for_each = lookup(var.settings, "autoscale_settings", {}) == {} ? [] : [1]
+
+    content {
+      max_throughput = try(autoscale_settings.value.max_throughput, {})
+    }
+  }
 }
 
 # Create container
@@ -27,5 +36,37 @@ resource "azurerm_cosmosdb_sql_container" "container" {
   unique_key {
     paths = each.value.unique_key_path
   }
+
+  default_ttl = try(each.value.default_ttl, -1)
+
+  # Note : throughput & autoscaling is conflicting properties, Therefore, set any one of them
+  dynamic "autoscale_settings" {
+    for_each = lookup(var.settings, "autoscale_settings", {}) == {} ? [] : [1]
+
+    content {
+      max_throughput = try(autoscale_settings.value.max_throughput, {})
+    }
+  }
+
+  dynamic "indexing_policy" {
+    for_each = lookup(var.settings, "indexing_policy", {}) == {} ? [] : [1]
+
+    content {
+      indexing_mode = try(indexing_policy.value.indexing_mode, {})
+      included_path {
+        path = try(indexing_policy.value.included_path, {})
+      }
+      excluded_path {
+        path = try(indexing_policy.value.excluded_path, {})
+      }
+      composite_index {
+        index {
+          path  = try(indexing_policy.value.path, {})
+          order = try(indexing_policy.value.order, {})
+        }
+      }
+    }
+  }
+
 }
 
