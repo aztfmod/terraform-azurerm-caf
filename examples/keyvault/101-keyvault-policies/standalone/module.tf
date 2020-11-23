59b0e35f5e0bc046d9d859b4943297acbe17a83b
lolorol <LaurentLesle@users.noreply.github.com>
2020-11-23 08:40:59 +0000

    Keyvault - Add private endpoint and firewall

---
+++ examples/keyvault/101-keyvault-policies/standalone/module.tf
@@ -0,0 +1,22 @@
+module "caf" {
+  source = "../../../../"
+
+  global_settings             = var.global_settings
+  diagnostic_storage_accounts = var.diagnostic_storage_accounts
+  resource_groups             = var.resource_groups
+  storage_accounts            = var.storage_accounts
+  log_analytics               = var.log_analytics
+  keyvaults                   = var.keyvaults
+  managed_identities          = var.managed_identities
+  role_mapping                = var.role_mapping
+
+  compute = {
+    virtual_machines = var.virtual_machines
+  }
+
+  networking = {
+    vnets                             = var.vnets
+    network_security_group_definition = var.network_security_group_definition
+    public_ip_addresses               = var.public_ip_addresses
+  }
+}
