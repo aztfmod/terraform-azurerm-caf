add404084eb3203af6ea9e943e60009209a62bfc
Ben <v-beprak@microsoft.com>
2020-11-24 22:48:47 +0800

    set standard module arguments

--- networking_virtual_gateway.tf
+++ networking_virtual_gateway.tf
@@ -4,8 +4,8 @@ module virtual_gateways {
 
   settings            = each.value
   resource_group_name = module.resource_groups[each.value.resource_group_key].name
-  resource_groups     = module.resource_groups
   location            = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  public_ip_id        = module.public_ip_addresses[each.value.public_ip_key].id
   diagnostics         = local.diagnostics
   global_settings     = local.global_settings
 }
