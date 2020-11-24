04d5d4f2e2bdc913ee87c6c6851855d9a08af820
Ben <v-beprak@microsoft.com>
2020-11-24 22:48:47 +0800

    set standard module arguments

--- modules/networking/virtual_gateways/module.tf
+++ modules/networking/virtual_gateways/module.tf
@@ -1,7 +1,15 @@
-
+resource "azurecaf_name" "vgw" {
+  name          = var.name
+  resource_type = "azurerm_firewall"
+  prefixes      = [var.global_settings.prefix]
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
 
 resource "azurerm_virtual_network_gateway" "vgw" {
-  name                = "test"
+  name                = azurecaf_name.vgw.result
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -11,13 +19,13 @@ resource "azurerm_virtual_network_gateway" "vgw" {
 
   ip_configuration {
     name                          = "vnetGatewayConfig"
-    public_ip_address_id          = azurerm_public_ip.example.id
+    public_ip_address_id          = var.public_ip_address_id
     private_ip_address_allocation = "Dynamic"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = var.subnet_id
   }
 
 
-#### in development ####
+#### In development. VPN Type will be coming soon ####
 #   enable_bgp    = false
 #   vpn_type = " "
 
