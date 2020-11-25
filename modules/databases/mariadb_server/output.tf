0663e2e8d63be85090baef90975aaebfdf97487e
lolorol <LaurentLesle@users.noreply.github.com>
2020-11-25 11:58:59 +0800

    Cleanup

--- modules/databases/mariadb_server/output.tf
+++ modules/databases/mariadb_server/output.tf
@@ -2,14 +2,6 @@ output id {
   value = azurerm_mariadb_server.mariadb.id
 }
 
-# output rbac_id {
-#   value = try(azurerm_mysql_server.mysql.identity[0].principal_id, null)
-# }
-
-# output identity {
-#   value = try(azurerm_mysql_server.mysql.identity, null)
-# }
-
 output name {
   value = azurecaf_name.mariadb.result
 }
