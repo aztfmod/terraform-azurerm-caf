cb964f1e67aecbb48873018ca60935c90b9d20a4
Arnaud Lheureux <arnaud@msft.com>
2020-11-25 07:13:05 +0000

    Adding Vnet peering and NSG examples

--- examples/variables.tf
+++ examples/variables.tf
@@ -13,7 +13,7 @@ variable global_settings {
     default_region = "region1"
     regions = {
       region1 = "southeastasia"
-  }
+    }
   }
 }
 
@@ -150,5 +150,11 @@ variable log_analytics {
   default = {}
 }
 variable recovery_vaults {
+  default = {}
+}
+variable diagnostics_destinations {
+  default = {}
+}
+variable vnet_peerings {
   default = {}
 }
