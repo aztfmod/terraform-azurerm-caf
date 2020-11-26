e617a11c7c1f8b0048ef80947ae1546532ce8f3b
Ben <benhur.joel@outlook.com>
2020-11-26 08:48:40 +0000

    post-commit check

--- networking_vngw.tf
+++ networking_vngw.tf
@@ -16,17 +16,17 @@ module virtual_network_gateways {
 }
 
 module virtual_network_gateway_connections {
-  source ="./modules/networking/virtual_network_gateway_connections"
+  source   = "./modules/networking/virtual_network_gateway_connections"
   for_each = try(local.networking.virtual_network_gateway_connections, {})
 
-  resource_group_name = module.resource_groups[each.value.resource_group_key].name
-  location            = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  global_settings     = local.global_settings
-  settings            = each.value
-  diagnostics         = local.diagnostics
-  client_config       = local.client_config
+  resource_group_name        = module.resource_groups[each.value.resource_group_key].name
+  location                   = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  global_settings            = local.global_settings
+  settings                   = each.value
+  diagnostics                = local.diagnostics
+  client_config              = local.client_config
   virtual_network_gateway_id = module.virtual_network_gateways[each.value.virtual_network_gateway_key].id
-  express_route_circuit_id = module.express_route_circuits[each.value.express_route_circuit_key].id
-  authorization_key = module.express_route_circuit_authorizations[each.value.authorization_key].authorization_key
-  base_tags           = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
+  express_route_circuit_id   = module.express_route_circuits[each.value.express_route_circuit_key].id
+  authorization_key          = module.express_route_circuit_authorizations[each.value.authorization_key].authorization_key
+  base_tags                  = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
 }
