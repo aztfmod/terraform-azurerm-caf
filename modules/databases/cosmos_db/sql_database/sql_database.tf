0e988407637bf1324a8afc95bdaf8d9eb84b7d6c
Rahul <39583459+singh-rahul@users.noreply.github.com>
2020-11-26 06:52:11 +0000

    unique key dynamic block

--- modules/databases/cosmos_db/sql_database/sql_database.tf
+++ modules/databases/cosmos_db/sql_database/sql_database.tf
@@ -32,13 +32,17 @@ resource "azurerm_cosmosdb_sql_container" "container" {
   database_name       = azurerm_cosmosdb_sql_database.database.name
   partition_key_path  = each.value.partition_key_path
   throughput          = each.value.throughput
-
-  unique_key {
-    paths = each.value.unique_key_path
-  }
-
+  
   default_ttl = try(each.value.default_ttl, -1)
 
+  dynamic "unique_key" {
+    for_each = lookup(var.settings, "unique_key", {}) == {} ? [] : [1]
+
+    content {
+      paths = try(unique_key.each.value.path, {})
+    }
+  }
+  
   # Note : throughput & autoscaling is conflicting properties, Therefore, set any one of them
   dynamic "autoscale_settings" {
     for_each = lookup(var.settings, "autoscale_settings", {}) == {} ? [] : [1]
