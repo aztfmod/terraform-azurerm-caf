9789b8cdaa71d6581206e591fefd6850fa5bfbbf
Ben <benhur.joel@outlook.com>
2020-11-26 08:00:55 +0000

    fix typo in authorization_key argument add variables to Examples to test

--- examples/module.tf
+++ examples/module.tf
@@ -37,6 +37,8 @@ module "caf" {
     application_gateway_applications  = var.application_gateway_applications
     virtual_network_gateways          = var.virtual_network_gateways
     virtual_network_gateway_connections = var.virtual_network_gateway_connections
+    express_route_circuits = var.express_route_circuits
+    express_route_circuit_authorizations = var.express_route_circuit_authorizations
   }
   database = {
     azurerm_redis_caches        = var.azurerm_redis_caches
