705281e8c38e529d24458cb5e1ddc0515df94581
abhilash-keloth <abhi2620@gmail.com>
2020-11-30 10:24:16 +0000

    added eventhub and asr private ep

--- modules/event_hub_namespaces/variables.tf
+++ modules/event_hub_namespaces/variables.tf
@@ -2,3 +2,7 @@ variable global_settings {}
 variable settings {}
 variable resource_groups {}
 variable base_tags {}
+variable private_endpoints {}
+variable vnets {}
+variable client_config {}
+variable subnet_id {}
