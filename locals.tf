371e4bcffc79b00c21236a62e9a8e56703d0c678
lolorol <LaurentLesle@users.noreply.github.com>
2020-12-01 16:15:30 +0000

    Update diagnostics to support inheritence

--- locals.tf
+++ locals.tf
@@ -1,11 +1,12 @@
 locals {
   diagnostics = {
-    diagnostics_definition   = try(var.diagnostics.diagnostics_definition, var.diagnostics_definition)
-    diagnostics_destinations = try(var.diagnostics.diagnostics_destinations, var.diagnostics_destinations)
-    storage_accounts         = try(var.diagnostics.storage_accounts, module.diagnostic_storage_accounts)
-    log_analytics            = try(var.diagnostics.log_analytics, module.log_analytics)
-    event_hub_namespaces     = try(var.diagnostics.event_hub_namespaces, module.event_hub_namespaces)
+    diagnostics_definition   = var.diagnostics.diagnostics_definition
+    diagnostics_destinations = var.diagnostics.diagnostics_destinations
+    storage_accounts         = merge(var.diagnostics.storage_accounts, module.diagnostic_storage_accounts)
+    log_analytics            = merge(var.diagnostics.log_analytics, module.log_analytics)
+    event_hub_namespaces     = merge(var.diagnostics.event_hub_namespaces, module.event_hub_namespaces)
   }
+
 
   prefix = lookup(var.global_settings, "prefix", null) == null ? random_string.prefix.result : var.global_settings.prefix
 
