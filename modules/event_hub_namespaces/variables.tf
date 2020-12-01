febc948fc84d8298825948b99af65492de4f1a2e
lolorol <LaurentLesle@users.noreply.github.com>
2020-12-01 07:03:23 +0000

    cleanup

--- modules/event_hub_namespaces/variables.tf
+++ modules/event_hub_namespaces/variables.tf
@@ -2,7 +2,4 @@ variable global_settings {}
 variable settings {}
 variable resource_groups {}
 variable base_tags {}
-# variable private_endpoints {}
-# variable vnets {}
 variable client_config {}
-# variable subnet_id {}
