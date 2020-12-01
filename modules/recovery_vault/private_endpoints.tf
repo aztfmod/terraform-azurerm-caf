1d42f03f98b3b43a0e2b7fa2470398768b2c3bf9
lolorol <LaurentLesle@users.noreply.github.com>
2020-12-01 16:44:44 +0000

    Patch private link for remote LZ

--- modules/recovery_vault/private_endpoints.tf
+++ modules/recovery_vault/private_endpoints.tf
@@ -14,7 +14,7 @@ module private_endpoint {
   name                = each.value.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_id
+  subnet_id           = try(each.value.lz_key, null) == null ? var.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id : var.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id
   settings            = each.value
   global_settings     = var.global_settings
   base_tags           = local.tags
