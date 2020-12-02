ba6c2141298153e47da60500c8f627e4af4a2b04
Ben Hummerstone <bhummerstone@users.noreply.github.com>
2020-12-02 11:36:01 +0000

    adjusting variables for iteration

--- cosmos_db.tf
+++ cosmos_db.tf
@@ -1,6 +1,6 @@
 module cosmos_db {
   source   = "./modules/databases/cosmos_db"
-  for_each = local.database.cosmos_db
+  for_each = local.database.cosmos_dbs
 
   location            = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
   resource_group_name = module.resource_groups[each.value.resource_group_key].name
