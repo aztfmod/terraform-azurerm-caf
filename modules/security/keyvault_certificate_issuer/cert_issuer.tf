c578b8fd65bb020a837b02e4bd8bee302f23c504
abhilash-keloth <abhi2620@gmail.com>
2020-12-03 06:44:32 +0000

    initial setup

--- modules/security/keyvault_certificate_issuer/cert_issuer.tf
+++ modules/security/keyvault_certificate_issuer/cert_issuer.tf
@@ -1,18 +1,18 @@
-resource "azurecaf_name" "keycertisr" {
-  name          = var.settings.name
-  resource_type = "azurerm_key_vault_certificate_issuer"
-  prefixes      = [var.global_settings.prefix]
-  random_length = var.global_settings.random_length
-  clean_input   = true
-  passthrough   = var.global_settings.passthrough
-  use_slug      = var.global_settings.use_slug
-}
+# resource "azurecaf_name" "keycertisr" {
+#   name          = var.settings.name
+#   resource_type = "azurerm_key_vault_certificate_issuer"
+#   prefixes      = [var.global_settings.prefix]
+#   random_length = var.global_settings.random_length
+#   clean_input   = true
+#   passthrough   = var.global_settings.passthrough
+#   use_slug      = var.global_settings.use_slug
+# }
 
 
 resource "azurerm_key_vault_certificate_issuer" "keycertisr" {
-  name          = azurecaf_name.keycertisr.result
+  name          =  "modname"
   org_id        =  var.org_id
-  key_vault_id  =  var.kayvault_id
+  key_vault_id  =  var.keyvault_id
   provider_name = "DigiCert"
   account_id    = "0000"
   password      = "example-password"
