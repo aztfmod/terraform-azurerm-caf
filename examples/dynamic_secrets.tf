fa3fa30dd3efe6fd9f9d0f2ba2201f5a60cb8e69
lolorol <LaurentLesle@users.noreply.github.com>
2020-12-04 10:34:38 +0000

    Update examples dynamic secrets

--- examples/dynamic_secrets.tf
+++ examples/dynamic_secrets.tf
@@ -6,6 +6,6 @@ module dynamic_keyvault_secrets {
   for_each = try(var.dynamic_keyvault_secrets, {})
 
   settings    = each.value
-  keyvault_id = module.foundations.keyvaults[each.key].id
-  objects     = module.foundations
+  keyvault_id = module.caf.keyvaults[each.key].id
+  objects     = module.caf
 }
