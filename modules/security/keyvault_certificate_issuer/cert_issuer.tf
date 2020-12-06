00dcb61d9000f3faf4538b65d81a2889611556e9
abhilash-keloth <abhi2620@gmail.com>
2020-12-06 09:46:02 +0000

    added dynamic values

--- modules/security/keyvault_certificate_issuer/cert_issuer.tf
+++ modules/security/keyvault_certificate_issuer/cert_issuer.tf
@@ -11,10 +11,25 @@
 
 resource "azurerm_key_vault_certificate_issuer" "keycertisr" {
   name          =  var.issuer_name
-  org_id        =  var.org_id
   key_vault_id  =  var.keyvault_id
   provider_name =  var.provider_name
-  account_id    =  var.account_id
+  org_id        = try(var.settings.organization_id, null)
+  account_id    = try(var.settings.account_id, null)
   password      = "example-password"
+
+  
+    
+
+  dynamic "admin" {
+    for_each = try(var.settings.admin_settings, {})
+    content {
+      email_address                 = admin.value.email_address
+      first_name                    = admin.value.first_name
+      last_name                     = admin.value.last_name
+      phone                         = admin.value.phone_number
+    }
+
+  }
+  
 }
 
