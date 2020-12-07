2459975eb98afe5b34d7850441bea8d6caec7650
lolorol <LaurentLesle@users.noreply.github.com>
2020-12-07 00:50:49 +0000

    Fix time sleep

--- modules/recovery_vault/backup_policies_file_share.tf
+++ modules/recovery_vault/backup_policies_file_share.tf
@@ -28,16 +28,3 @@ resource "azurerm_backup_policy_file_share" "fs" {
 
 # TODO: SAP HANA in Azure VM when available
 # TODO: SQL Server in Azure VM when available
-
-# resource "time_sleep" "delay" {
-#   depends_on = [azurerm_recovery_services_vault.asr_rg_vault]
-
-#   destroy_duration = "15s"
-#   create_duration  = "15s"
-# }
-
-resource "time_sleep" "delay_create" {
-  depends_on = [azurerm_resource_group_template_deployment.asr]
-
-  create_duration = "60s"
-}
