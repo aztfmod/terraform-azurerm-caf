d283d0c9700450b9f15fcc5f908bbba28e620052
lolorol <LaurentLesle@users.noreply.github.com>
2020-12-11 04:52:54 +0000

    Recovery vault service - add msi - private link

--- modules/networking/private_links/endpoints/subnet/recovery_vaults.tf
+++ modules/networking/private_links/endpoints/subnet/recovery_vaults.tf
@@ -1,12 +1,12 @@
-module "diagnostic_event_hub_namespaces" {
+module "recovery_vault" {
   source   = "../private_endpoint"
-  for_each = try(var.private_endpoints.diagnostic_event_hub_namespaces, {})
+  for_each = try(var.private_endpoints.recovery_vaults, {})
 
   global_settings     = var.global_settings
   client_config       = var.client_config
   settings            = each.value
-  resource_id         = var.remote_objects.diagnostic_event_hub_namespaces[each.key].id
-  subresource_names   = ["namespace"]
+  resource_id         = try(var.remote_objects.recovery_vaults[each.value.lz_key][each.key].id, var.remote_objects.recovery_vaults[var.client_config.landingzone_key][each.key].id)
+  subresource_names   = ["AzureSiteRecovery"]
   subnet_id           = var.subnet_id
   private_dns         = var.private_dns
   name                = try(each.value.name, each.key)
