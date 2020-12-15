ff03fd30f66bbb5df5895fb5022068a712d9b447
abhilash-keloth <abhi2620@gmail.com>
2020-12-15 11:36:54 +0000

    added frontdoor and dnszone modules

--- examples/networking/front_door/100-simple-front_door/configuration.tfvars
+++ examples/networking/front_door/100-simple-front_door/configuration.tfvars
@@ -19,13 +19,40 @@ front_doors = {
     resource_group_key = "front_door_region1"
     certificate_name_check = false
 
-    routing_rule = {
-      name                    = "ExampleRouteRule1"
-      accepted_protocols      = ["Http", "Https"]
-      frontend_endpoints      = ["exampleFrontendEndpoint1"]
-      forwarding_protocol = "MatchRequest"
-      backend_pool_name   = "exampleBackendBing"
+    # routing_rule = {
+    #   name                    = "ExampleRouteRule1"
+    #   accepted_protocols      = ["Http", "Https"]
+    #   frontend_endpoints      = ["exampleFrontendEndpoint1"]
+    #   forwarding_protocol = "MatchRequest"
+    #   backend_pool_name   = "exampleBackendBing"
       
+    # }
+
+    routing_rule = {
+      rr1 = {
+        name               = "exampleRoutingRule1"
+        frontend_endpoints = ["exampleFrontendEndpoint1"]
+        accepted_protocols = ["Http", "Https"] 
+        patterns_to_match  = ["/*"]           
+        enabled            = true              
+        configuration      = "Forwarding"        
+        forwarding_configuration = {
+          backend_pool_name                     = "exampleBackendBing1"
+          cache_enabled                         = false       
+          cache_use_dynamic_compression         = false       
+          cache_query_parameter_strip_directive = "StripNone" 
+          custom_forwarding_path                = ""
+          forwarding_protocol                   = "MatchRequest"   
+        }
+        redirect_configuration = {
+          custom_host         = ""             
+          redirect_protocol   = "MatchRequest"   
+          redirect_type       = "Found"        
+          custom_fragment     = ""
+          custom_path         = ""
+          custom_query_string = ""
+        }
+      } 
     }
     
     # Following optional argument can be used to set a time out value between 0-240. If not passed, by default it will be set to 60
@@ -38,29 +65,95 @@ front_doors = {
     # friendly_name          =  "ExampleFriendDoor"
 
     backend_pool_load_balancing = {
-      name = "exampleLoadBalancingSettings1"
+      lb1 = {
+        name                            = "exampleLoadBalancingSettings1"
+        sample_size                     = 4
+        successful_samples_required     = 2
+        additional_latency_milliseconds = 0 
+      }                                     
+    }
 
-    }
-    
     backend_pool_health_probe = {
-      name = "exampleHealthProbeSetting1"
+      hp1 = {
+        name                = "exampleHealthProbeSetting1"
+        path                = "/"
+        protocol            = "Http"
+        interval_in_seconds = 120    
+      }                             
     }
+
+    # backend_pool_load_balancing = {
+    #   name = "exampleLoadBalancingSettings1"
+
+    # }
+    
+    # backend_pool_health_probe = {
+    #   name = "exampleHealthProbeSetting1"
+    # }
+
+    # backend_pool = {
+    #   name = "exampleBackendBing"
+    #   host_header = "www.bing.com"
+    #   address     = "www.bing.com"
+    #   http_port   = 80
+    #   https_port  = 443
+    #   load_balancing_name = "exampleLoadBalancingSettings1"
+    #   health_probe_name   = "exampleHealthProbeSetting1"
+    # }
 
     backend_pool = {
-      name = "exampleBackendBing"
-      host_header = "www.bing.com"
-      address     = "www.bing.com"
-      http_port   = 80
-      https_port  = 443
-      load_balancing_name = "exampleLoadBalancingSettings1"
-      health_probe_name   = "exampleHealthProbeSetting1"
+      bp1 = {
+        name = "exampleBackendBing1"
+        load_balancing_name = "exampleLoadBalancingSettings1"
+        health_probe_name   = "exampleHealthProbeSetting1"
+        backend = {
+          be1 = {
+            enabled     = true
+            address     = "www.bing.com"
+            host_header = "www.bing.com"
+            http_port   = 80
+            https_port  = 443
+            priority    = 1  
+            weight      = 50
+          },
+          be2 = {
+            enabled     = true
+            address     = "www.bing.co.uk"
+            host_header = "www.bing.co.uk"
+            http_port   = 80
+            https_port  = 443
+            priority    = 1 
+            weight      = 50
+          }
+        }
+        
+      } 
     }
 
+    # frontend_endpoint = {
+    #   name                              = "exampleFrontendEndpoint1"
+    #   host_name                         = "randomabcxyz-FrontDoor.azurefd.net"
+    #   custom_https_provisioning_enabled = false
+    # } 
+
     frontend_endpoint = {
-      name                              = "exampleFrontendEndpoint1"
-      host_name                         = "randomabcxyz-FrontDoor.azurefd.net"
-      custom_https_provisioning_enabled = false
-    }  
+      fe1 = {
+        name                              = "exampleFrontendEndpoint1"
+        host_name                         = "randomabcxyz-FrontDoor.azurefd.net"
+        session_affinity_enabled          = false 
+        session_affinity_ttl_seconds      = 0     
+        custom_https_provisioning_enabled = false
+        #Required if custom_https_provisioning_enabled is true
+        custom_https_configuration = {
+          certificate_source = "FrontDoor" 
+          #If certificate source is AzureKeyVault the below are required:
+          azure_key_vault_certificate_vault_id       = ""
+          azure_key_vault_certificate_secret_name    = ""
+          azure_key_vault_certificate_secret_version = ""
+        }
+        web_application_firewall_policy_link_name = ""  
+      }
+    } 
     
   }
 }
--- modules/networking/front_door/front_door.tf
+++ modules/networking/front_door/front_door.tf
@@ -1,4 +1,3 @@
-
 resource "azurecaf_name" "frontdoor" {
   name          = var.settings.name
   resource_type = "azurerm_frontdoor"
@@ -16,14 +15,35 @@ resource "azurerm_frontdoor" "frontdoor" {
   enforce_backend_pools_certificate_name_check = try(var.settings.certificate_name_check, false)
   tags                = local.tags
 
-
-  routing_rule {
-    name                  =   var.settings.routing_rule.name
-    frontend_endpoints    =   var.settings.routing_rule.frontend_endpoints
-    accepted_protocols    =   try(var.settings.routing_rule.accepted_protocols, null)
-    patterns_to_match     =   try(var.settings.routing_rule.patterns_to_match, ["/*"])
-    forwarding_configuration {
-      backend_pool_name  =    try(var.settings.backend_pool.name, null)
+  dynamic "routing_rule" {
+    for_each = var.settings.routing_rule
+    content {
+      name               = routing_rule.value.name
+      accepted_protocols = routing_rule.value.accepted_protocols
+      patterns_to_match  = routing_rule.value.patterns_to_match
+      frontend_endpoints = routing_rule.value.frontend_endpoints
+      dynamic "forwarding_configuration" {
+        for_each = routing_rule.value.configuration == "Forwarding" ? [routing_rule.value.forwarding_configuration] : []
+        content {
+          backend_pool_name                     = routing_rule.value.forwarding_configuration.backend_pool_name
+          cache_enabled                         = routing_rule.value.forwarding_configuration.cache_enabled                           
+          cache_use_dynamic_compression         = routing_rule.value.forwarding_configuration.cache_use_dynamic_compression #default: false
+          cache_query_parameter_strip_directive = routing_rule.value.forwarding_configuration.cache_query_parameter_strip_directive
+          custom_forwarding_path                = routing_rule.value.forwarding_configuration.custom_forwarding_path
+          forwarding_protocol                   = routing_rule.value.forwarding_configuration.forwarding_protocol
+        }
+      }
+      dynamic "redirect_configuration" {
+        for_each = routing_rule.value.configuration == "Redirecting" ? [routing_rule.value.redirect_configuration] : []
+        content {
+          custom_host         = routing_rule.value.redirect_configuration.custom_host
+          redirect_protocol   = routing_rule.value.redirect_configuration.redirect_protocol
+          redirect_type       = routing_rule.value.redirect_configuration.redirect_type
+          custom_fragment     = routing_rule.value.redirect_configuration.custom_fragment
+          custom_path         = routing_rule.value.redirect_configuration.custom_path
+          custom_query_string = routing_rule.value.redirect_configuration.custom_query_string
+        }
+      }
     }
   }
 
@@ -31,31 +51,68 @@ resource "azurerm_frontdoor" "frontdoor" {
   load_balancer_enabled  =  try(var.settings.load_balancer_enabled, true)
   friendly_name          =  try(var.settings.backend_pool.name, null)
   
-  backend_pool_load_balancing {
-    name = var.settings.backend_pool_load_balancing.name
-  }
-
-  backend_pool_health_probe {
-    name = var.settings.backend_pool_health_probe.name
-  } 
-    
-  backend_pool {
-    name = var.settings.backend_pool.name
-    backend {
-      host_header = var.settings.backend_pool.host_header
-      address     = var.settings.backend_pool.address
-      http_port   = var.settings.backend_pool.http_port
-      https_port  = var.settings.backend_pool.https_port
+  dynamic "backend_pool_load_balancing" {
+    for_each = var.settings.backend_pool_load_balancing
+    content {
+      name                            = backend_pool_load_balancing.value.name
+      sample_size                     = backend_pool_load_balancing.value.sample_size
+      successful_samples_required     = backend_pool_load_balancing.value.successful_samples_required
+      additional_latency_milliseconds = backend_pool_load_balancing.value.additional_latency_milliseconds
     }
-
-    load_balancing_name = var.settings.backend_pool.load_balancing_name
-    health_probe_name   = var.settings.backend_pool.health_probe_name
   }
 
-  frontend_endpoint {
-    name                              = var.settings.frontend_endpoint.name
-    host_name                         = format("%s.azurefd.net", azurecaf_name.frontdoor.result)
-    custom_https_provisioning_enabled = var.settings.frontend_endpoint.custom_https_provisioning_enabled
+  dynamic "backend_pool_health_probe" {
+    for_each = var.settings.backend_pool_health_probe
+    content {
+      name                = backend_pool_health_probe.value.name
+      path                = backend_pool_health_probe.value.path
+      protocol            = backend_pool_health_probe.value.protocol
+      interval_in_seconds = backend_pool_health_probe.value.interval_in_seconds
+    }
   }
+
+  dynamic "backend_pool" {
+    for_each = var.settings.backend_pool
+    content {
+      name                = backend_pool.value.name
+      load_balancing_name = backend_pool.value.load_balancing_name
+      health_probe_name   = backend_pool.value.health_probe_name
+
+      dynamic "backend" {
+        for_each = backend_pool.value.backend
+        content {
+          enabled     = backend.value.enabled
+          address     = backend.value.address
+          host_header = backend.value.host_header
+          http_port   = backend.value.http_port
+          https_port  = backend.value.https_port
+          priority    = backend.value.priority
+          weight      = backend.value.weight
+        }
+      }
+    }
+  }
+  
+  dynamic "frontend_endpoint" {
+    for_each = var.settings.frontend_endpoint
+    content {
+      name                              = frontend_endpoint.value.name
+      host_name                         = format("%s.azurefd.net", azurecaf_name.frontdoor.result)
+      session_affinity_enabled          = frontend_endpoint.value.session_affinity_enabled
+      session_affinity_ttl_seconds      = frontend_endpoint.value.session_affinity_ttl_seconds
+      custom_https_provisioning_enabled = frontend_endpoint.value.custom_https_provisioning_enabled
+      dynamic "custom_https_configuration" {
+        for_each = frontend_endpoint.value.custom_https_provisioning_enabled == true ? [frontend_endpoint.value.custom_https_configuration] : []
+        content {
+          certificate_source                         = custom_https_configuration.value.certificate_source 
+          azure_key_vault_certificate_vault_id       = custom_https_configuration.value.azure_key_vault_certificate_vault_id
+          azure_key_vault_certificate_secret_name    = custom_https_configuration.value.azure_key_vault_certificate_secret_name
+          azure_key_vault_certificate_secret_version = custom_https_configuration.value.azure_key_vault_certificate_secret_version
+        }
+      }
+      
+    }
+  }
+
 }
   
