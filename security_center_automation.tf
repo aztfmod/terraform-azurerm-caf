fa92e6efa23240878d432e71b4263ab3e5475fb5
Ben <benhur.joel@outlook.com>
2020-12-15 03:18:35 +0000

    format code

--- security_center_automation.tf
+++ security_center_automation.tf
@@ -2,15 +2,15 @@ module security_center_automation {
   source   = "./modules/security_center/security_center_automation"
   for_each = try(local.shared_services.security_center_automation, {})
 
-  resource_group_name    = module.resource_groups[each.value.resource_group_key].name
-  location               = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  diagnostics            = local.combined_diagnostics
-  client_config          = local.client_config
-  global_settings        = local.global_settings
-  subscription_id        = data.azurerm_client_config.current.subscription_id
-  eventhub_namespace_id     = try(module.event_hub_namespaces[each.value.eventhub_namespace_key].id, null)
+  resource_group_name           = module.resource_groups[each.value.resource_group_key].name
+  location                      = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  diagnostics                   = local.combined_diagnostics
+  client_config                 = local.client_config
+  global_settings               = local.global_settings
+  subscription_id               = data.azurerm_client_config.current.subscription_id
+  eventhub_namespace_id         = try(module.event_hub_namespaces[each.value.eventhub_namespace_key].id, null)
   eventhub_namespace_connection = try(module.event_hub_namespaces[each.value.eventhub_namespace_key].connection_string, null)
-  loganalytics_workspace = try(module.log_analytics[each.value.loganalytics_workspace_key].name, null)
-  settings               = each.value
-  base_tags              = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
+  loganalytics_workspace        = try(module.log_analytics[each.value.loganalytics_workspace_key].name, null)
+  settings                      = each.value
+  base_tags                     = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
 }
