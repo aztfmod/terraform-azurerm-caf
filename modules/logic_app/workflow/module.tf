2abf68daf86d2adef643acbb262cf2f4d06aff36
Malcolm Miller <malcolmjmil@gmail.com>
2020-12-15 16:38:44 +0000

    added azurecaf name to logic workflow

--- modules/logic_app/workflow/module.tf
+++ modules/logic_app/workflow/module.tf
@@ -1,16 +1,15 @@
-# Resorce type doesn't "azurerm_logic_app_workflow" doesn't exist in azurecaf!!
-# resource "azurecaf_name" "logic_app" {
-#   name          = var.name
-#   resource_type = "azurerm_logic_app_workflow"
-#   prefixes      = [var.global_settings.prefix]
-#   random_length = var.global_settings.random_length
-#   clean_input   = true
-#   passthrough   = var.global_settings.passthrough
-#   use_slug      = var.global_settings.use_slug
-# }
+resource "azurecaf_name" "logic_app" {
+  name          = var.name
+  resource_type = "azurerm_logic_app_workflow"
+  prefixes      = [var.global_settings.prefix]
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
 
 resource "azurerm_logic_app_workflow" "la" {
-  name                               = var.name
+  name                               = azurecaf_name.logic_app.result
   location                           = var.location
   resource_group_name                = var.resource_group_name
   integration_service_environment_id = var.integration_service_environment_id
