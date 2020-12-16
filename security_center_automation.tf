cc196bac771c1986f062a6b039c08070275e2978
Ben <57881441+benhurjoel@users.noreply.github.com>
2020-12-16 10:09:52 +0800

    Update LogAnalytics Workspace argument

--- security_center_automation.tf
+++ security_center_automation.tf
@@ -10,7 +10,7 @@ module security_center_automation {
   subscription_id               = data.azurerm_client_config.current.subscription_id
   eventhub_namespace_id         = try(module.event_hub_namespaces[each.value.eventhub_namespace_key].id, null)
   eventhub_namespace_connection = try(module.event_hub_namespaces[each.value.eventhub_namespace_key].connection_string_primary, null)
-  loganalytics_workspace        = try(module.log_analytics[each.value.loganalytics_workspace_key].name, null)
+  loganalytics_workspace        = try(module.log_analytics[each.value.loganalytics_workspace_key].id, null)
   settings                      = each.value
   base_tags                     = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
 }
