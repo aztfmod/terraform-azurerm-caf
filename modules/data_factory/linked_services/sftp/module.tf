c0091afc2a8e32ad4ba04b1acaed10dbcd1bfa02
Kristin Ottofy <krottofy@microsoft.com>
2020-12-17 21:09:23 +0000

    corrections

--- modules/data_factory/linked_services/sftp/module.tf
+++ modules/data_factory/linked_services/sftp/module.tf
@@ -6,7 +6,7 @@ resource "azurerm_data_factory_linked_service_sftp" "linked_service_sftp" {
   integration_runtime_name = try(var.integration_runtime_name, null)
   annotations              = try(var.annotations, null)
   parameters               = try(var.parameters, null)
-  additional_properties    = try(var.account_endpoint, null)
+  additional_properties    = try(var.additional_properties, null)
   authentication_type      = var.authentication_type
   host                     = var.host
   port                     = var.port
