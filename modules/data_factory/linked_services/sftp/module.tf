6458dda5a0ae9eef737c5b9a276a04448eb2c857
Kristin Ottofy <krottofy@microsoft.com>
2020-12-17 18:45:35 +0000

    add linked_service_sftp

--- modules/data_factory/linked_services/sftp/module.tf
+++ modules/data_factory/linked_services/sftp/module.tf
@@ -1,23 +1,15 @@
-resource "azurerm_data_factory_dataset_azure_blob" "dataset" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  data_factory_name   = var.data_factory_name
-  linked_service_name = var.linked_service_name
-  folder = var.folder
-  description = var.description 
-  annotations = var.annotations
-  parameters = var.parameters
-  additional_properties = var.additional_properties
-  path     = var.path
-  filename = var.filename
-
-  dynamic "schema_column" {
-    for_each = lookup(var.settings, "schema_column", {}) == {} ? [] : [1]
-
-    content {
-      name = var.settings.schema_column.name
-      type = var.settings.schema_column.type
-      description = var.settings.schema_column.description
-    }
-  }
+resource "azurerm_data_factory_linked_service_sftp" "linked_service_sftp" {
+  name                     = var.name
+  resource_group_name      = var.resource_group_name
+  data_factory_name        = var.data_factory_name
+  description              = try(var.description, null)
+  integration_runtime_name = try(var.integration_runtime_name, null)
+  annotations              = try(var.annotations, null)
+  parameters               = try(var.parameters, null)
+  additional_properties    = try(var.account_endpoint, null)
+  authentication_type      = var.authentication_type
+  host                     = var.host
+  port                     = var.port
+  username                 = var.username
+  password                 = var.password
 }
