dc943adec615529c86acbe661b14599232a4f7dc
lolorol <LaurentLesle@users.noreply.github.com>
2020-12-17 15:22:08 +0000

    CI - DNS_ZONE - random generate the domain name in CI

--- modules/networking/dns_zone/variables.tf
+++ modules/networking/dns_zone/variables.tf
@@ -3,27 +3,3 @@ variable location {}
 variable settings {}
 variable global_settings {}
 variable base_tags {}
-variable contract {
-  default = {}
-}
-variable name {
-  description = "(Required) Name of the Domain to be created"
-  type        = string
-  default     = "terra.com"
-}
-
-variable lock_zone {
-  description = "(Required) Determines to put a Azure lock after creating the zone"
-  type        = bool
-  default     = false
-}
-
-variable lock_domain {
-  description = "(Required) Determines to put a Azure lock after create the domain"
-  type        = bool
-  default     = false
-}
-
-variable records {
-  default = {}
-}
