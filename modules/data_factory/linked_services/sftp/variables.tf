f14abe4e400f22161077c57854bffc02b2c1c3ad
Kristin Ottofy <krottofy@microsoft.com>
2020-12-17 18:45:35 +0000

    add linked_service_sftp

--- modules/data_factory/linked_services/sftp/variables.tf
+++ modules/data_factory/linked_services/sftp/variables.tf
@@ -1,47 +1,51 @@
 variable name {
-  description = "(Required) Specifies the name of the Data Factory Dataset"
+  description = "(Required) Specifies the name of the Data Factory Linked Service. Changing this forces a new resource to be created. Must be globally unique."
 }
 
 variable resource_group_name {
-  description = "(Required) The name of the resource group in which to create the Data Factory Dataset"
+  description = "(Required) The name of the resource group in which to create the Data Factory Linked Service. Changing this forces a new resource"
 }
 
 variable data_factory_name {
-  description = "(Required) The Data Factory name in which to associate the Dataset with"
-}
-
-variable linked_service_name {
-  description = "(Required) The Data Factory Linked Service name in which to associate the Dataset with"
-}
-
-variable folder {
-  description = "(Optional) The folder that this Dataset is in. If not specified, the Dataset will appear at the root level"
-}
-
-variable schema_column {
-  description = "(Optional) A schema_column block"
+  description = "(Required) The Data Factory name in which to associate the Linked Service with. Changing this forces a new resource."
 }
 
 variable description {
-  description = "(Optional) The description for the Data Factory Dataset"
+  description = "(Optional) The description for the Data Factory Linked Service."
+}
+
+variable integration_runtime_name {
+  description = "(Optional) The integration runtime reference to associate with the Data Factory Linked Service."
 }
 
 variable annotations {
-  description = "(Optional) List of tags that can be used for describing the Data Factory Dataset"
+  description = "(Optional) List of tags that can be used for describing the Data Factory Linked Service."
 }
 
 variable parameters {
-  description = "(Optional) A map of parameters to associate with the Data Factory Dataset"
+  description = "(Optional) A map of parameters to associate with the Data Factory Linked Service."
 }
 
 variable additional_properties {
-  description = "(Optional) A map of additional properties to associate with the Data Factory Dataset"
+  description = "(Optional) A map of additional properties to associate with the Data Factory Linked Service."
 }
 
-variable path {
-  description = "(Required) The path of the Azure Blob"
+variable authentication_type {
+  description = "(Required) The type of authentication used to connect to the web table source. Valid options are Anonymous, Basic and ClientCertificate."
 }
 
-variable filename {
-  description = "(Required) The filename of the Azure Blob"
+variable host {
+  description = "(Required) The SFTP server hostname."
+}
+
+variable port {
+  description = "(Required) The TCP port number that the SFTP server uses to lsiten for client connection. Default value is 22."
+}
+
+variable username {
+  description = "(Required) The username used to log on to the SFTP server."
+}
+
+variable password {
+  description = "(Required) Password to logon to the SFTP Server for Basic Authentication."
 }
