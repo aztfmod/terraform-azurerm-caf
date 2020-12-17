7025ababe950a6c58e5a2e700c423c2a604f621f
lolorol <LaurentLesle@users.noreply.github.com>
2020-12-17 15:22:08 +0000

    CI - DNS_ZONE - random generate the domain name in CI

--- modules/networking/dns_zone/dns_zone.tf
+++ modules/networking/dns_zone/dns_zone.tf
@@ -1,18 +1,30 @@
+resource "random_string" "domain_zone_name" {
+  count = var.settings.name == "" ? 1 : 0
+  length  = 16
+  special = false
+  upper   = false
+}
+
+locals {
+  domain_zone_name = var.settings.name == "" ? format("%s.com", random_string.domain_zone_name[0].result) : var.settings.name
+}
+
+
 resource "azurerm_dns_zone" "domain_zone" {
-  name                = var.settings.name
+  name                = local.domain_zone_name
   resource_group_name = var.resource_group_name
   tags                = local.tags
 }
 
 resource "azurerm_template_deployment" "domain" {
-  name                = var.settings.name
+  name                = local.domain_zone_name
   resource_group_name = var.resource_group_name
   template_body       = file(local.arm_filename)
   lifecycle {
     ignore_changes = [parameters]
   }
   parameters = {
-    "Name"          = var.settings.name 
+    "Name"          = local.domain_zone_name 
     "key1"          = lookup(var.settings.contract, "agreement_key1", "DNRA"),
     "key2"          = lookup(var.settings.contract, "agreement_key2", "DNRA"),
     "AgreedBy"      = lookup(var.settings.contract, "agree_by", "100.5.150.200:52212"), #Change to DevOps Agent IP
@@ -41,16 +53,16 @@ resource "azurerm_template_deployment" "domain" {
 }
 
 resource "azurerm_management_lock" "lock_zone" {
-  count      = var.lock_zone ? 1 : 0
-  name       = "${var.settings.name}-lock-zone"
+  count      = try(var.settings.lock_zone, false) ? 1 : 0
+  name       = "${local.domain_zone_name}-lock-zone"
   scope      = azurerm_dns_zone.domain_zone.id
   lock_level = "CanNotDelete"
   notes      = "Deleting a domain will make it unavailable to purchase for 60 days. Please remove the lock before deleting this domain."
 }
 
 resource "azurerm_management_lock" "lock_domain" {
-  count      = var.lock_domain ? 1 : 0
-  name       = "${var.settings.name}-lock-domain"
+  count      = try(var.settings.lock_domain, false) ? 1 : 0
+  name       = "${local.domain_zone_name}-lock-domain"
   scope      = azurerm_template_deployment.domain.outputs.resourceID
   lock_level = "CanNotDelete"
   notes      = "Deleting a domain will make it unavailable to purchase for 60 days. Please remove the lock before deleting this domain."
