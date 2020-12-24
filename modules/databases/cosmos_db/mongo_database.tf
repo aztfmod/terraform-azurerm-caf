5efeb91cd68d4f4a9dd54a77033879b802d1927b
Ben Hummerstone <bhummerstone@users.noreply.github.com>
2020-12-24 11:25:40 +0000

    refactoring cosmos mongo api for hierarchy

--- modules/databases/cosmos_db/mongo_database.tf
+++ modules/databases/cosmos_db/mongo_database.tf
@@ -5,7 +5,6 @@ module mongo_databases {
   global_settings       = var.global_settings
   settings              = each.value
   resource_group_name   = azurerm_cosmosdb_account.cosmos_account.resource_group_name
-  location              = azurerm_cosmosdb_account.cosmos_account.location
   cosmosdb_account_name = azurerm_cosmosdb_account.cosmos_account.name
 }
 
