c345d38de5b05d4fec1fe1ec711b51db8c9a7a21
Ben <benhur.joel@outlook.com>
2021-01-04 19:07:07 +0000

    standalone -  app insights

---
+++ examples/app_insights/100-all-attributes/standalone/variables.tf
@@ -0,0 +1,15 @@
+variable global_settings {
+  default = {}
+}
+
+variable resource_groups {
+  default = null
+}
+
+variable azurerm_application_insights {
+  default = {}
+}
+
+variable tags {
+  default = {}
+}
