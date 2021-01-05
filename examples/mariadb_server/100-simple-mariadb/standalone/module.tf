0143f45037b2fd21cecf749235de3fe18c9d01ae
Ben <benhur.joel@outlook.com>
2021-01-05 02:24:02 +0000

    standalone - Maria DB

---
+++ examples/mariadb_server/100-simple-mariadb/standalone/module.tf
@@ -0,0 +1,13 @@
+module "caf" {
+  source = "../../../../../caf"
+  global_settings    = var.global_settings
+  tags               = var.tags
+  resource_groups    = var.resource_groups
+  storage_accounts  = var.storage_accounts
+  keyvaults  = var.keyvaults
+
+  database = {
+    mariadb_servers  = var.mariadb_servers
+  }
+}
+  
