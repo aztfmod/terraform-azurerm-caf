b317292922234a4a84f0a560917690d0234e6d47
Ben <benhur.joel@outlook.com>
2021-01-05 11:30:30 +0000

    standalone - storage accounts

---
+++ examples/storage_accounts/100-simple-storage-account-blob-container/standalone/module.tf
@@ -0,0 +1,9 @@
+module "caf" {
+  source = "../../../../../caf"
+  global_settings    = var.global_settings
+  tags               = var.tags
+  resource_groups    = var.resource_groups
+  storage_accounts  = var.storage_accounts
+
+}
+  
