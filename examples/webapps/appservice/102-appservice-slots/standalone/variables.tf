868f480a7eb9e23dce4c35cb4feee03ea3cfbf77
Ben <benhur.joel@outlook.com>
2021-01-05 11:58:59 +0000

    standalone - web apps

---
+++ examples/webapps/appservice/102-appservice-slots/standalone/variables.tf
@@ -0,0 +1,21 @@
+variable global_settings {
+  default = {}
+}
+
+variable resource_groups {
+  default = null
+}
+
+variable tags {
+  default = {}
+}
+
+variable app_service_plans {
+  default = {}
+}
+
+variable app_services  {
+  default = {}
+}
+
+
---
+++ examples/webapps/appservice/104-appservice-appinsight/standalone/variables.tf
@@ -0,0 +1,25 @@
+variable global_settings {
+  default = {}
+}
+
+variable resource_groups {
+  default = null
+}
+
+variable tags {
+  default = {}
+}
+
+variable app_service_plans {
+  default = {}
+}
+
+variable app_services  {
+  default = {}
+}
+
+variable azurerm_application_insights {
+  default = {}
+}
+
+
