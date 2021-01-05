d414437d83e975682a1cad7e6c059a5580a81e75
Ben <benhur.joel@outlook.com>
2021-01-05 11:18:36 +0000

    standalone Redis Cache

---
+++ examples/redis_cache/100-redis-standard/standalone/variables.tf
@@ -0,0 +1,21 @@
+variable global_settings {
+  default = {}
+}
+
+variable resource_groups {
+  default = null
+}
+
+
+variable azurerm_redis_caches  {
+  default = {}
+}
+
+
+
+variable tags {
+  default = {}
+}
+
+
+
