2a4b5370974139e2e4050b33b6f948c394d61497
Ben <benhur.joel@outlook.com>
2021-01-05 02:24:02 +0000

    standalone - Maria DB

--- examples/mariadb_server/101-vnet-rule-mariadb/configuration.tfvars
+++ examples/mariadb_server/101-vnet-rule-mariadb/configuration.tfvars
@@ -1,3 +1,11 @@
+global_settings = {
+  default_region = "region1"
+  regions = {
+    region1 = "southeastasia"
+  }
+}
+
+
 resource_groups = {
   mariadb_region1 = {
     name   = "mariadb-re1"
---
+++ examples/mariadb_server/101-vnet-rule-mariadb/standalone/variables.tf
@@ -0,0 +1,30 @@
+variable global_settings {
+  default = {}
+}
+
+variable resource_groups {
+  default = null
+}
+
+variable storage_accounts {
+  default = {}
+}
+
+variable keyvaults {
+  default = {}
+}
+
+variable mariadb_servers {
+  default = {}
+}
+
+variable vnets {
+  default = {}
+}
+
+variable tags {
+  default = {}
+}
+
+
+
