a8b846a8774a0d055d100ae958e9e483714f2d5f
Ben <benhur.joel@outlook.com>
2021-01-06 08:33:52 +0000

    standalone availability set

---
+++ examples/compute/availability_set/100-simple-availabilityset/standalone/variables.tf
@@ -0,0 +1,28 @@
+variable global_settings {
+  default = {}
+}
+
+variable resource_groups {
+  default = null
+}
+
+variable vnets {
+  default = {}
+}
+
+variable keyvaults {
+  default = {}
+}
+
+variable tags {
+  default = {}
+}
+
+variable availability_sets  {
+  default = {}
+}
+
+variable virtual_machines {
+  default = {}
+}
+
