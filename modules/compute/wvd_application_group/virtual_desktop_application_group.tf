703764db994708494b6858cc3d3b3393a8fd5696
abhilash-keloth <abhi2620@gmail.com>
2021-01-06 09:06:05 +0000

    groupid resource change

--- modules/compute/wvd_application_group/virtual_desktop_application_group.tf
+++ modules/compute/wvd_application_group/virtual_desktop_application_group.tf
@@ -24,6 +24,6 @@ resource "azurerm_virtual_desktop_application_group" "dag" {
 
 resource "azurerm_virtual_desktop_workspace_application_group_association" "workspaceremoteapp" {
   workspace_id         = var.workspace_id
-  application_group_id = azurerm_virtual_desktop_application_group.appgroup.id
+  application_group_id = azurerm_virtual_desktop_application_group.dag.id
 }
 
