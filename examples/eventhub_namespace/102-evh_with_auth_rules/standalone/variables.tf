c31e890bf530fac81546e0a91358cf994b5c4ee1
Ben <benhur.joel@outlook.com>
2021-01-08 05:11:53 +0000

    add standalone module

---
+++ examples/eventhub_namespace/102-evh_with_auth_rules/standalone/variables.tf
@@ -0,0 +1,18 @@
+variable global_settings {
+  default = {}
+}
+
+variable resource_groups {
+  default = null
+}
+
+variable storage_accounts {
+  default = {}
+}
+
+variable tags {
+  default = {}
+}
+
+
+
