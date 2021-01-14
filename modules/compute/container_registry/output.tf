ca3a4991b0e3f1ec692c1ee3fde7bbcfe7923866
Erik Schlegel <erisch@microsoft.com>
2021-01-14 00:21:23 +0000

    Adding dynamic secrets support

--- modules/compute/container_registry/output.tf
+++ modules/compute/container_registry/output.tf
@@ -7,3 +7,13 @@ output login_server {
   value     = azurerm_container_registry.acr.login_server
   sensitive = true
 }
+
+output admin_username {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
+
+output admin_password {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
