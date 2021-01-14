7bf3d67960657c154d14db0ebebf9415520099fd
Arnaud Lheureux <arnaud@microsoft.com>
2021-01-14 09:38:13 +0000

    Revert "Upgrade to azurerm 2.42"
    This reverts commit 6cae711ef9c027722c15f6309d646e818e94163e.

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "~> 2.40.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
