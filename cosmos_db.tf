f245c81095aa6657c80a95d8298ea648225fab32
erikschlegel <erisch@microsoft.com>
2021-01-15 15:57:55 +0000

    changing the naming conventions for the cosmos module so they're more consistent ie cosmos_db vs cosmos_dbs

--- cosmos_db.tf
+++ cosmos_db.tf
@@ -1,5 +1,5 @@
-module cosmos_db {
-  source   = "./modules/databases/cosmos_db"
+module cosmos_dbs {
+  source   = "./modules/databases/cosmos_dbs"
   for_each = local.database.cosmos_dbs
 
   location            = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
@@ -9,7 +9,7 @@ module cosmos_db {
   base_tags           = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
 }
 
-output cosmos_db_id {
-  value     = module.cosmos_db
+output cosmos_dbs {
+  value     = module.cosmos_dbs
   sensitive = true
 }
--- modules/databases/cosmos_db/output.tf
+++
@@ -1,4 +0,0 @@
-output cosmos_account {
-  value     = azurerm_cosmosdb_account.cosmos_account.id
-  sensitive = true
-}
