fb34d1690b0da080bc070329a58d015e2c4d9594
lolorol <LaurentLesle@users.noreply.github.com>
2021-01-25 07:45:47 +0000

    Update for dns_zone_records

--- networking_dns_zones.tf
+++ networking_dns_zones.tf
@@ -13,3 +13,19 @@ output dns_zones {
   value = module.dns_zones
 }
 
+module dns_zone_records {
+  source     = "./modules/networking/dns_zone/records"
+  for_each   = try(local.networking.dns_zone_records, {})
+  depends_on = [module.dns_zones]
+
+  base_tags           = {}
+  client_config       = local.client_config
+  resource_group_name = try(each.value.dns_zone.resource_group_name, null) == null ? local.combined_objects_dns_zones[lookup(each.value.dns_zone, "lz_key", local.client_config.landingzone_key)][each.value.dns_zone.key].resource_group_name : each.value.dns_zone.resource_group_name
+  records             = each.value.records
+  zone_name           = try(each.value.dns_zone.name, null) == null ? local.combined_objects_dns_zones[lookup(each.value.dns_zone, "lz_key", local.client_config.landingzone_key)][each.value.dns_zone.key].name : each.value.dns_zone.name
+}
+
+
+output dns_zone_records {
+  value = module.dns_zone_records
+}
--- networking_domain_name_registrations.tf
+++ networking_domain_name_registrations.tf
@@ -3,10 +3,10 @@ module domain_name_registrations {
   for_each = try(local.networking.domain_name_registrations, {})
 
   base_tags           = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
-  dns_zone_id         = try(each.value.dns_zone.lz_key, null) == null ? local.combined_object_dns_zones[local.client_config.landingzone_key][each.value.dns_zone.key].id : local.combined_object_dns_zones[each.value.dns_zone.lz_key][each.value.dns_zone.key].id
+  dns_zone_id         = try(each.value.dns_zone.lz_key, null) == null ? local.combined_objects_dns_zones[local.client_config.landingzone_key][each.value.dns_zone.key].id : local.combined_objects_dns_zones[each.value.dns_zone.lz_key][each.value.dns_zone.key].id
   global_settings     = local.global_settings
   location            = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  name                = try(each.value.name, "") == "" ? try(local.combined_object_dns_zones[local.client_config.landingzone_key][each.value.dns_zone.key].name, local.combined_object_dns_zones[each.value.dns_zone.lz_key][each.value.dns_zone.key].name) : ""
+  name                = try(each.value.name, "") == "" ? try(local.combined_objects_dns_zones[local.client_config.landingzone_key][each.value.dns_zone.key].name, local.combined_objects_dns_zones[each.value.dns_zone.lz_key][each.value.dns_zone.key].name) : ""
   resource_group_name = module.resource_groups[each.value.resource_group_key].name
   settings            = each.value
 }
