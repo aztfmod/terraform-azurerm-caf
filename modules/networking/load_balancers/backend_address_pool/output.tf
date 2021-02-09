da769ad96dd23031149b4a373cbaaf204301e075
Ben <benhur.joel@outlook.com>
2021-02-09 10:18:04 +0000

    add output values

--- modules/networking/load_balancers/backend_address_pool/output.tf
+++ modules/networking/load_balancers/backend_address_pool/output.tf
@@ -1,3 +1,19 @@
 output id {
   value = azurerm_lb_backend_address_pool.backend_address_pool.id
 }
+
+output backend_address {
+  value = azurerm_lb_backend_address_pool.backend_address_pool.backend_address
+}
+
+output backend_ip_configurations {
+  value = azurerm_lb_backend_address_pool.backend_address_pool.backend_ip_configurations
+}
+
+output load_balancing_rules {
+  value = azurerm_lb_backend_address_pool.backend_address_pool.load_balancing_rules
+}
+
+output outbound_rules {
+  value = azurerm_lb_backend_address_pool.backend_address_pool.outbound_rules
+}
---
+++ modules/networking/load_balancers/nat_pool/output.tf
@@ -0,0 +1,3 @@
+output id {
+  value = azurerm_lb_nat_pool.nat_pool.id
+}
