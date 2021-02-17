4b81d8ab9d08abc92953fa337a4a494a19ca5358
Malcolm Miller <58534574+MalcolmJMiller@users.noreply.github.com>
2021-02-17 09:23:14 -0500

    Malmiller/malmiller/2000 create managed identity for aml (#10)
    * added managed identities to app_config_combined_objects in locals.tf
    
    * added acr and client config to dynamic appsettings, added acr login server url to output, added missing os_type to function app

--- modules/webapps/function_app/module.tf
+++ modules/webapps/function_app/module.tf
@@ -17,6 +17,7 @@ resource "azurerm_function_app" "function_app" {
   client_affinity_enabled     = lookup(var.settings, "client_affinity_enabled", null)
   enabled                     = lookup(var.settings, "enabled", null)
   https_only                  = lookup(var.settings, "https_only", null)
+  os_type                     = lookup(var.settings, "os_type", "linux")
   storage_account_name        = var.storage_account_name
   storage_account_access_key  = var.storage_account_access_key
   tags                        = local.tags
