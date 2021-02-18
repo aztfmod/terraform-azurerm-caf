436528f3afe650183be69800d49c4aa847ea983d
Hieu Nguyen Nhu <5441003+hieumoscow@users.noreply.github.com>
2021-02-18 09:31:47 +0800

    add support for IP Groups

--- modules/networking/firewall_network_rule_collections/network_rule_collection.tf
+++ modules/networking/firewall_network_rule_collections/network_rule_collection.tf
@@ -23,12 +23,22 @@ resource "azurerm_firewall_network_rule_collection" "rule" {
     for_each = var.azurerm_firewall_network_rule_collection_definition[each.key].ruleset
 
     content {
-      name                  = rule.value.name
-      description           = try(rule.value.description, null)
-      source_addresses      = rule.value.source_addresses
-      destination_addresses = rule.value.destination_addresses
-      destination_ports     = rule.value.destination_ports
-      protocols             = rule.value.protocols
+      name             = rule.value.name
+      description      = try(rule.value.description, null)
+      source_addresses = try(rule.value.source_addresses, null)
+      source_ip_groups = try(rule.value.source_ip_groups, try(flatten([
+        for key, value in var.ip_groups : value.id
+        if contains(rule.value.source_ip_groups_keys, key)
+        ]), null)
+      )
+      destination_addresses = try(rule.value.destination_addresses, null)
+      destination_ip_groups = try(rule.value.destination_ip_groups, try(flatten([
+        for key, value in var.ip_groups : value.id
+        if contains(rule.value.destination_ip_groups_keys, key)
+        ]), null)
+      )
+      destination_ports = rule.value.destination_ports
+      protocols         = rule.value.protocols
     }
   }
 }
