afa017f35fb0fd479d3277e2ee61eb970c5b82cf
lolorol <lalesle@microsoft.com>
2021-02-27 00:21:04 +0000

    Add landingzones

---
+++ landingzones/caf_launchpad/dynamic_secrets.tf
@@ -0,0 +1,12 @@
+
+module dynamic_keyvault_secrets {
+  # source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
+  # version = "~>5.1.0"
+  source = "/tf/caf/aztfmod//modules/security/dynamic_keyvault_secrets"
+
+  for_each = try(var.dynamic_keyvault_secrets, {})
+
+  settings = each.value
+  keyvault = module.launchpad.keyvaults[each.key]
+  objects  = module.launchpad
+}
