a9090fdeff0a33ec550e8c15f4f00a5836434206
Hieu Nguyen Nhu <5441003+hieumoscow@users.noreply.github.com>
2021-02-27 15:51:51 +0800

    Updates ip_group examples

--- examples/networking/ip_group/200-firewall-ip_group/standalone/ci.sh
+++ examples/networking/ip_group/200-firewall-ip_group/standalone/ci.sh
@@ -12,11 +12,13 @@ parameter_files=$(find .. | grep .tfvars | sed 's/.*/-var-file &/' | xargs)
 # Trigger the deployment of the resources
 
 eval terraform apply ${parameter_files} \
+  -var tags='{testing_job_id='"${1}"'}' \
   -var var_folder_path=${current_folder} \
   -input=false \
   -auto-approve
 
 eval terraform destroy ${parameter_files} \
+  -var tags='{testing_job_id='"${1}"'}' \
   -var var_folder_path=${current_folder} \
   -input=false \
   -auto-approve
