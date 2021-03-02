588fe785ba0212b4a81db3ff80eab61d2ea77fe6
Arnaud Lheureux <arnaud@microsoft.com>
2021-03-02 01:09:18 +0000

    Fixing CI for standalone examples

--- examples/compute/container_groups/101-aci-rover/standalone/ci.sh
+++ examples/compute/container_groups/101-aci-rover/standalone/ci.sh
@@ -8,9 +8,11 @@ cd standalone
 terraform init
 
 terraform apply \
-  -var-file ../aks.tfvars \
-  -var-file ../networking.tfvars \
-  -var-file ../diagnostics.tfvars \
+  -var-file ../container_groups.tfvars \
+  -var-file ../global_settings.tfvars \
+  -var-file ../resource_groups.tfvars \
+  -var-file ../keyvaults.tfvars \
+  -var-file ../managed_identities.tfvars \
   -var tags='{testing_job_id='"${1}"'}' \
   -var var_folder_path=${current_folder} \
   -input=false \
@@ -18,9 +20,11 @@ terraform apply \
 
 
 terraform destroy \
-  -var-file ../aks.tfvars \
-  -var-file ../networking.tfvars \
-  -var-file ../diagnostics.tfvars \
+  -var-file ../container_groups.tfvars \
+  -var-file ../global_settings.tfvars \
+  -var-file ../resource_groups.tfvars \
+  -var-file ../keyvaults.tfvars \
+  -var-file ../managed_identities.tfvars \
   -var tags='{testing_job_id='"${1}"'}' \
   -var var_folder_path=${current_folder} \
   -input=false \
--- examples/networking/private_dns/100-private-dns-vnet-links/standalone/backend.tfc.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "aztfmod"
-
-    workspaces {
-      name = "example_standalone_private-dns_100-private-dns-vnet-links"
-    }
-  }
-}
