3c8bd1eb4ed62dfdfd77277d7bd14dbe6e386208
Ben <v-beprak@microsoft.com>
2021-03-10 09:46:16 +0800

    remove packer managed indentity module

--- examples/shared_image_gallery/100-packer_managed_identity/managed_identity.tfvars
+++
@@ -1,6 +0,0 @@
-managed_identities = {
-  packer = {
-    name               = "packer-msi"
-    resource_group_key = "packer"
-  }
-}
--- shared_image_gallery.tf
+++ shared_image_gallery.tf
@@ -42,33 +42,6 @@ resource "time_sleep" "time_delay_03" {
   destroy_duration = "60s"
 }
 
-module packer_managed_identity {
-  source   = "./modules/shared_image_gallery/packer_managed_identity"
-  for_each = try(local.shared_services.packer_managed_identity, {})
-
-  resource_group_name      = module.resource_groups[each.value.resource_group_key].name
-  location                 = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  client_config            = local.client_config
-  global_settings          = local.global_settings
-  subscription             = data.azurerm_subscription.primary.subscription_id
-  tenant_id                = data.azurerm_client_config.current.tenant_id
-  gallery_name             = module.shared_image_galleries[each.value.shared_image_gallery_destination.gallery_key].name
-  image_name               = module.image_definitions[each.value.shared_image_gallery_destination.image_key].name
-  key_vault_id             = lookup(each.value, "keyvault_key") == null ? null : module.keyvaults[each.value.keyvault_key].id
-  ssh_private_pem_key_name = module.virtual_machines[each.value.vm_key].ssh_keys.ssh_private_key_pem
-  public_ip_addresses      = local.combined_objects_public_ip_addresses
-  settings                 = each.value
-  base_tags                = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
-  depends_on = [
-    module.shared_image_galleries,
-    module.image_definitions,
-    module.virtual_machines,
-    module.keyvaults,
-    time_sleep.time_delay_01,
-    time_sleep.time_delay_01
-  ]
-}
-
 module packer_service_principal {
   source   = "./modules/shared_image_gallery/packer_service_principal"
   for_each = try(local.shared_services.packer_service_principal, {})
