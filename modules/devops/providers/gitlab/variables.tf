f4b39e2ebc678ab92fdb3ee7094a6a8e36e12b81
polatengin <enpolat@microsoft.com>
2021-03-11 14:46:22 +0300

    document formatted

--- modules/devops/providers/gitlab/variables.tf
+++ modules/devops/providers/gitlab/variables.tf
@@ -2,6 +2,7 @@ variable project {
   description = "The object representative of the gitlab project entity to update"
   type        = map
 }
+
 variable token {
   description = "Token that has permission to create-update-delete projects"
 }
