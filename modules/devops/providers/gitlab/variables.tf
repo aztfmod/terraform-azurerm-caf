23a32d362026dbd291f317090107b468b9c04339
polatengin <enpolat@microsoft.com>
2021-03-11 08:33:48 +0300

    token variable name changed

--- modules/devops/providers/gitlab/variables.tf
+++ modules/devops/providers/gitlab/variables.tf
@@ -2,6 +2,6 @@ variable project {
   description = "The object representative of the gitlab project entity to update"
   type        = map
 }
-variable gitlab_token {
+variable token {
   description = "Token that has permission to create-update-delete projects"
 }
