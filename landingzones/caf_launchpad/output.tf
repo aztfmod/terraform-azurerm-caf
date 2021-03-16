e843769955447e59895e3e1c504e7097fa54b33c
lolorol <lalesle@microsoft.com>
2021-03-16 16:04:40 +0000

    Reduce output without empty maps Add wokspace support to remote objects

--- landingzones/caf_launchpad/output.tf
+++ landingzones/caf_launchpad/output.tf
@@ -1,6 +1,12 @@
 
 output objects {
-  value     = tomap({ (var.landingzone.key) = module.launchpad })
+  value     = tomap(
+    { (var.landingzone.key) = {
+        for key, value in module.launchpad : key => value
+        if try(value, {}) != {}
+      } 
+    }
+  )
   sensitive = true
 }
 
