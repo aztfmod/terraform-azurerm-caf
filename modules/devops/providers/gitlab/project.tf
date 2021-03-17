0a6c4bedd020583a5f2cd0d443028c41fc15d025
Engin Polat <polatengin@hotmail.com>
2021-03-17 10:49:57 +0300

    gitlab project variables resource added to gitlab module resources

--- modules/devops/providers/gitlab/project.tf
+++ modules/devops/providers/gitlab/project.tf
@@ -3,3 +3,13 @@ resource "gitlab_project" "project" {
   description        = lookup(var.project, "description", "")
   visibility_level   = lookup(var.project, "visibility", "private")
 }
+
+resource "gitlab_project_variable" "variable" {
+  for_each    = lookup(var.project, "variables", {})
+
+  project   = gitlab_project.project.id
+  key       = each.key
+  value     = lookup(each.value, "value", "")
+  protected = lookup(each.value, "protected", false)
+  masked    = lookup(each.value, "masked", false)
+}
