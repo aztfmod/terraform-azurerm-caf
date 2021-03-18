a0cd34b25a0e1c267c89ebd24b8483f17ea04dae
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2021-03-18 16:26:37 -0700

    removing gitlab_projects.tf from root

--- gitlab_projects.tf
+++
@@ -1,9 +0,0 @@
-module "gitlab_projects" {
-  source          = "./modules/devops/providers/gitlab"
-  for_each        = var.gitlab_projects
-  project         = each.value
-}
-
-output gitlab_projects {
-  value = module.gitlab_projects
-}
