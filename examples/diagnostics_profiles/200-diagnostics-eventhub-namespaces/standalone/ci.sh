209225dd52537704fbfa7cca5ae8ab72bf3901ba
Ben <benhur.joel@outlook.com>
2021-03-23 17:10:33 +0000

    typo

--- examples/diagnostics_profiles/200-diagnostics-eventhub-namespaces/standalone/ci.sh
+++ examples/diagnostics_profiles/200-diagnostics-eventhub-namespaces/standalone/ci.sh
@@ -11,8 +11,7 @@ terraform apply \
   -var-file ../configuration.tfvars \
   -var-file  ../diagnostic_event_hub_namespaces.tfvars \
   -var-file  ../diagnostics_definition.tfvars \
-  -var-file  ../diagnostics_destinations.tfvars
-
+  -var-file  ../diagnostics_destinations.tfvars \
   -var tags='{testing_job_id='"${1}"'}' \
   -var var_folder_path=${current_folder} \
   -input=false \
@@ -23,7 +22,7 @@ terraform destroy \
   -var-file ../configuration.tfvars \
   -var-file  ../diagnostic_event_hub_namespaces.tfvars \
   -var-file  ../diagnostics_definition.tfvars \
-  -var-file  ../diagnostics_destinations.tfvars
+  -var-file  ../diagnostics_destinations.tfvars \
   -var tags='{testing_job_id='"${1}"'}' \
   -var var_folder_path=${current_folder} \
   -input=false \
