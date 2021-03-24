09b55b8f04925406665e9385b56a73c77e4c326d
lolorol <LaurentLesle@users.noreply.github.com>
2021-03-24 05:05:16 +0000

    Update example

--- landingzones/caf_example/module.tf
+++ landingzones/caf_example/module.tf
@@ -111,6 +111,7 @@ module "example" {
   }
 
   security = {
+    disk_encryption_sets          = var.disk_encryption_sets
     dynamic_keyvault_secrets      = var.dynamic_keyvault_secrets
     keyvault_certificate_issuers  = var.keyvault_certificate_issuers
     keyvault_certificate_requests = var.keyvault_certificate_requests
