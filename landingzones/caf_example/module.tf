3f6e36f06ae317d91e346317188ab6f27babcd7a
Abdullah Khairi <akamarulzama@microsoft.com>
2021-03-30 04:24:54 +0000

    agw - add support for manual cert

--- landingzones/caf_example/module.tf
+++ landingzones/caf_example/module.tf
@@ -117,6 +117,7 @@ module "example" {
     keyvault_certificate_issuers  = var.keyvault_certificate_issuers
     keyvault_certificate_requests = var.keyvault_certificate_requests
     keyvault_keys                 = var.keyvault_keys
+    keyvault_certificates         = var.keyvault_certificates
   }
 
   storage = {
