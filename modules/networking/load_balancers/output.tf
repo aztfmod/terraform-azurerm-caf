cd2c7d161a73db43659f336b6a20c33e33de0e32
Ben <benhur.joel@outlook.com>
2021-04-01 05:50:18 +0000

    add backend address pool output

--- modules/networking/load_balancers/output.tf
+++ modules/networking/load_balancers/output.tf
@@ -2,3 +2,10 @@ output "id" {
   value = azurerm_lb.lb.id
 }
 
+output "bap" {
+  value = {
+    for backend_address_pool_name, value in var.settings : backend_address_pool_name => {
+      id   = azurerm_lb_backend_address_pool.backend_address_pool.0.id
+      name = azurerm_lb_backend_address_pool.backend_address_pool.0.name
+    }
+  }
