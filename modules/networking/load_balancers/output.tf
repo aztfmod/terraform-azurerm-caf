ac19490b435ac43aebca4020a0c5c4b626eb0bf5
Ben <benhur.joel@outlook.com>
2021-04-01 09:51:44 +0000

    additional output attributes

--- modules/networking/load_balancers/output.tf
+++ modules/networking/load_balancers/output.tf
@@ -2,6 +2,20 @@ output "id" {
   value = azurerm_lb.lb.id
 }
 
+output "private_ip_addresses" {
+  value = azurerm_lb.lb.private_ip_addresses
+  description = "The list of private IP address assigned to the load balancer in frontend_ip_configuration blocks, if any"
+}
+
+output "private_ip_address" {
+  value = azurerm_lb.lb.private_ip_address
+  description = "The first private IP address assigned to the load balancer in frontend_ip_configuration"
+}
+
+output "frontend_ip_configuration" {
+  value = azurerm_lb.lb.frontend_ip_configuration
+}
+
 output "bap" {
   value = {
     for backend_address_pool_name, value in var.settings : backend_address_pool_name => {
@@ -10,3 +24,4 @@ output "bap" {
     }
   }
 }
+
