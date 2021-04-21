80c095ec2dd30221343d78500fe7ec8399d924a5
Jor Seng <v-jorsengtan@microsoft.com>
2021-04-21 03:22:26 +0000

    modify lighthouse to handle dynamic authorization

--- modules/security/lighthouse_definition/assignment.tf
+++ modules/security/lighthouse_definition/assignment.tf
@@ -1,4 +1,19 @@
+# resource "azurerm_lighthouse_assignment" "assignment" {
+#   scope = coalesce(
+#     try(var.settings.assignment_scope.resource_group.id, ""),
+#     try(var.resources["resource_groups"][try(var.settings.assignment_scope.resource_group.lz_key, var.client_config.landingzone_key)][var.settings.scope.resource_group.key].id, ""),
+
+#     try(format("/subscription/%s", var.resources["subscriptions"][try(var.settings.assignment_scope.subscription.lz_key, var.client_config.landingzone_key)][var.settings.scope.subscription.key].id), ""),
+#     try(var.settings.assignment_scope.subscription.id, "")
+#   )
+
+#   lighthouse_definition_id = azurerm_lighthouse_definition.definition.id
+# }
+
+
 resource "azurerm_lighthouse_assignment" "assignment" {
+  for_each = try(var.settings.assignment_scopes, {})
+
   scope = coalesce(
     try(var.settings.assignment_scope.resource_group.id, ""),
     try(var.resources["resource_groups"][try(var.settings.assignment_scope.resource_group.lz_key, var.client_config.landingzone_key)][var.settings.scope.resource_group.key].id, ""),
@@ -9,3 +24,4 @@ resource "azurerm_lighthouse_assignment" "assignment" {
 
   lighthouse_definition_id = azurerm_lighthouse_definition.definition.id
 }
+
--- modules/security/lighthouse_definition/definition.tf
+++ modules/security/lighthouse_definition/definition.tf
@@ -15,22 +15,45 @@ resource "azurerm_lighthouse_definition" "definition" {
     try(var.settings.definition_scope.subscription.id, "")
   )
 
-  authorization {
-    principal_id = coalesce(
-      try(var.settings.definition_scope.managed_identity.id, ""),
-      try(var.resources["managed_identities"][try(var.settings.definition_scope.managed_identity.lz_key, var.client_config.landingzone_key)][var.settings.definition_scope.managed_identity.key].id, ""),
+  dynamic "authorization" {
+    # verify syntax
+    for_each = try(var.settings.authorizations, {})
+    
+    
+    content {
+      principal_id = coalesce(
+      try(authorization.value.managed_identity.id, ""),
+      try(var.resources["managed_identities"][try(authorization.value.managed_identity.lz_key, var.client_config.landingzone_key)][authorization.value.managed_identity.key].id, ""),
 
-      try(var.settings.definition_scope.azuread_group.id, ""),
-      try(var.resources["azuread_groups"][try(var.settings.definition_scope.azuread_group.lz_key, var.client_config.landingzone_key)][var.settings.definition_scope.azuread_group.key].id, ""),
+      try(authorization.value.azuread_group.azuread_group.id, ""),
+      try(var.resources["azuread_groups"][try(authorization.value.azuread_group.lz_key, var.client_config.landingzone_key)][authorization.value.azuread_group.azuread_group.key].id, ""),
 
-      try(var.settings.definition_scope.azuread_user.id, ""),
-      try(var.resources["azuread_users"][try(var.settings.definition_scope.azuread_user.lz_key, var.client_config.landingzone_key)][var.settings.definition_scope.azuread_user.key].id, ""),
+      try(authorization.value.azuread_group.azuread_user.id, ""),
+      try(var.resources["azuread_users"][try(authorization.value.azuread_group.azuread_user.lz_key, var.client_config.landingzone_key)][authorization.value.azuread_group.azuread_user.key].id, ""),
 
-      try(var.settings.definition_scope.azuread_app.id, ""),
-      try(var.resources["azuread_apps"][try(var.settings.definition_scope.azuread_app.lz_key, var.client_config.landingzone_key)][var.settings.definition_scope.azuread_app.key].id, "")
+      try(authorization.value.azuread_group.azuread_app.id, ""),
+      try(var.resources["azuread_apps"][try(authorization.value.azuread_group.azuread_app.lz_key, var.client_config.landingzone_key)][authorization.value.azuread_group.azuread_app.key].id, "")
     )
-    role_definition_id     = local.role_id
-    principal_display_name = var.settings.authorization.principal_display_name
+    }
   }
 
+  # authorization {
+  #   principal_id = coalesce(
+  #     try(var.settings.definition_scope.managed_identity.id, ""),
+  #     try(var.resources["managed_identities"][try(var.settings.definition_scope.managed_identity.lz_key, var.client_config.landingzone_key)][var.settings.definition_scope.managed_identity.key].id, ""),
+
+  #     try(var.settings.definition_scope.azuread_group.id, ""),
+  #     try(var.resources["azuread_groups"][try(var.settings.definition_scope.azuread_group.lz_key, var.client_config.landingzone_key)][var.settings.definition_scope.azuread_group.key].id, ""),
+
+  #     try(var.settings.definition_scope.azuread_user.id, ""),
+  #     try(var.resources["azuread_users"][try(var.settings.definition_scope.azuread_user.lz_key, var.client_config.landingzone_key)][var.settings.definition_scope.azuread_user.key].id, ""),
+
+  #     try(var.settings.definition_scope.azuread_app.id, ""),
+  #     try(var.resources["azuread_apps"][try(var.settings.definition_scope.azuread_app.lz_key, var.client_config.landingzone_key)][var.settings.definition_scope.azuread_app.key].id, "")
+  #   )
+  #   role_definition_id     = local.role_id
+  #   principal_display_name = var.settings.authorization.principal_display_name
+  # }
+
 }
+
