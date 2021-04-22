a11fb18c144b10114e42de0fa449d0667ba3ef62
Daniel Valdivieso <vdaniel@pm.me>
2021-04-22 18:58:43 +0200

    be able to pass subnet_id from landing zones' combined objects; allow creation of fully private cache

--- redis_caches.tf
+++ redis_caches.tf
@@ -1,7 +1,8 @@
 
 module "redis_caches" {
-  source   = "./modules/redis_cache"
-  for_each = local.database.azurerm_redis_caches
+  source     = "./modules/redis_cache"
+  depends_on = [module.networking]
+  for_each   = local.database.azurerm_redis_caches
 
   tags                = try(each.value.tags, null)
   location            = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
@@ -9,6 +10,7 @@ module "redis_caches" {
   redis               = each.value.redis
   global_settings     = local.global_settings
   base_tags           = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
+  subnet_id           = try(each.value.subnet_key, null) == null ? null : try(local.combined_objects_networking[local.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, local.combined_objects_networking[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id)
 
 }
 
