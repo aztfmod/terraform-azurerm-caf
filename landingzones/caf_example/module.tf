f207175efffe8a86af2a1abb80904c2c3497b2c8
Jor Seng <v-jorsengtan@microsoft.com>
2021-04-22 07:23:19 +0000

    updated example landingzone for diagnostics variable

--- landingzones/caf_example/module.tf
+++ landingzones/caf_example/module.tf
@@ -82,9 +82,14 @@ module "example" {
   }
 
   diagnostics = {
-    diagnostic_event_hub_namespaces = var.diagnostic_event_hub_namespaces
-    diagnostic_log_analytics        = var.diagnostic_log_analytics
-    diagnostic_storage_accounts     = var.diagnostic_storage_accounts
+    diagnostics_definition          = local.diagnostics.diagnostics_definition
+    diagnostics_destinations        = local.diagnostics.diagnostics_destinations
+    storage_accounts                = local.diagnostics.storage_accounts
+    log_analytics                   = local.diagnostics.log_analytics
+    event_hub_namespaces            = local.diagnostics.event_hub_namespaces
+    diagnostic_event_hub_namespaces = try(local.diagnostics.diagnostic_event_hub_namespaces, var.diagnostic_event_hub_namespaces)
+    diagnostic_log_analytics        = try(local.diagnostics.diagnostic_log_analytics, var.diagnostic_log_analytics)
+    diagnostic_storage_accounts     = try(local.diagnostics.diagnostic_storage_accounts, var.diagnostic_storage_accounts)
   }
 
   database = {
