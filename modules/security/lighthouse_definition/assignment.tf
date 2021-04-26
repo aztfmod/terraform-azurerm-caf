ad92db075820c2a866144d5f7944ba573800ed87
Papun Senapati <papun.senapati@servian.com>
2021-04-26 01:21:13 +0000

    modifications as per requirement

--- modules/security/lighthouse_definition/assignment.tf
+++ modules/security/lighthouse_definition/assignment.tf
@@ -1,20 +1,21 @@
 resource "azurerm_lighthouse_assignment" "subscriptionassignment" {
-  for_each = try(var.settings.assignment_scopes.subscriptions, {})
-  
+  count = try(coalesce(
+    try(format("/subscriptions/%s", var.resources["subscriptions"][try(var.settings.scopes.subscription.lz_key, var.client_config.landingzone_key)][var.settings.scopes.subscription.key].id), ""),
+    try(var.settings.scopes.subscription.id, "")),"") != "" ? 1 : 0
   scope = coalesce(
-    try(format("/subscription/%s", var.resources["subscriptions"][try(each.value.lz_key, var.client_config.landingzone_key)][each.value.key].id), ""),
-    try(each.value.id, "")
-  )
-  lighthouse_definition_id = azurerm_lighthouse_definition.definition.id
+     try(format("/subscriptions/%s", var.resources["subscriptions"][try(var.settings.scopes.subscription.lz_key, var.client_config.landingzone_key)][var.settings.scopes.subscription.key].id), ""),
+     try(var.settings.scopes.subscription.id, ""))
+   lighthouse_definition_id = azurerm_lighthouse_definition.definition.id
 }
 
 resource "azurerm_lighthouse_assignment" "resourcegroupassignment" {
-  for_each = try(var.settings.assignment_scopes.resource_groups, {})
-
-  scope = coalesce(
-    try(var.resources["resource_groups"][try(each.value.lz_key, var.client_config.landingzone_key)][each.value.key].id, ""),
-    try(each.value.id, "")
-  )
-  lighthouse_definition_id = azurerm_lighthouse_definition.definition.id
+  for_each = {
+    for key, value in try(var.settings.scopes.resource_groups, {}) : key => value
+    if try(value.id, "") != "" || try(value.key, "") != "" || try(value.lz_key, "") != ""
+  }
+    scope = coalesce(
+    try(var.resources["resource_groups"][try(each.value.lz_key, var.client_config.landingzone_key)][each.value.key].id, null),
+    try(each.value.id, ""))
+    lighthouse_definition_id = azurerm_lighthouse_definition.definition.id
 }
 
