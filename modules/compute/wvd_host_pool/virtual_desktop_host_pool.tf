a80f5741cab93a107c813dd92e960f71b440a3aa
abhilash-keloth <abhi2620@gmail.com>
2021-04-28 08:08:27 +0000

    added custome rdp properties to host pool

--- modules/compute/wvd_host_pool/virtual_desktop_host_pool.tf
+++ modules/compute/wvd_host_pool/virtual_desktop_host_pool.tf
@@ -23,6 +23,7 @@ resource "azurerm_virtual_desktop_host_pool" "wvdpool" {
   load_balancer_type               = try(var.settings.load_balancer_type, null)
   personal_desktop_assignment_type = try(var.settings.personal_desktop_assignment_type, null)
   preferred_app_group_type         = try(var.settings.preferred_app_group_type, null)
+  custom_rdp_properties            = try(var.settings.custom_rdp_properties, null)
   tags                             = local.tags
 
   dynamic "registration_info" {
