6e6deb6553eaaf5cb78db1debd579dfbdbedaa47
Daniel Valdivieso <vdaniel@pm.me>
2021-05-01 22:47:47 +0200

    add example link to pipeline, use simplified structure

--- redis_caches.tf
+++ redis_caches.tf
@@ -10,8 +10,6 @@ module "redis_caches" {
   global_settings     = local.global_settings
   location            = lookup(each.value, "region", null) == null ? module.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
   redis               = each.value.redis
-  global_settings     = local.global_settings
-  base_tags           = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
   subnet_id           = try(local.combined_objects_networking[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id, null)
   resource_group_name = module.resource_groups[each.value.resource_group_key].name
   tags                = try(each.value.tags, null)
