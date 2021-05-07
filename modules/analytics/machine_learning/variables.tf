bc8fe34e9837cad5de6e1fac851358a01bc67628
Ben <benhur.joel@outlook.com>
2021-05-07 06:14:35 +0000

    edit variable file

--- modules/analytics/machine_learning/variables.tf
+++ modules/analytics/machine_learning/variables.tf
@@ -53,3 +53,5 @@ variable "base_tags" {
 variable "vnets" {
   description = "Virtual networks objects - contains all virtual networks that could potentially be used by the module."
 }
+
+variable container_registry_id {}
