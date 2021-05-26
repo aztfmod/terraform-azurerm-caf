e352103be38c6a132610dbc61ff3526e7d425e9b
Arnaud Lheureux <arnaudlh@hotmail.com>
2021-05-26 07:45:14 +0000

    AAD provider version pinning

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 2.55.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.4.0"
+    }
     azurecaf = {
       source  = "aztfmod/azurecaf"
       version = "~> 1.2.0"
