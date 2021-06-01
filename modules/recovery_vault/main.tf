cf414a352888f968913a9a4f31d1301bcfe19812
Arnaud Lheureux <arnaud@microsoft.com>
2021-06-01 11:30:44 +0000

    Review site recovery vaults

--- modules/recovery_vault/main.tf
+++ modules/recovery_vault/main.tf
@@ -13,10 +13,3 @@ terraform {
     }
   }
 }
-
-
-resource "time_sleep" "delay_create" {
-  depends_on = [azurerm_recovery_services_vault.asr]
-
-  create_duration = "60s"
-}
--- modules/storage_account/backup_container.tf
+++ modules/storage_account/backup_container.tf
@@ -1,3 +1,6 @@
+# Tested with :  AzureRM version 2.61.0
+# Ref : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/recovery_services_vault
+
 locals {
   recovery_vault = try(var.storage_account.backup, null) == null ? null : try(var.recovery_vaults[var.client_config.landingzone_key][var.storage_account.backup.vault_key], var.recovery_vaults[var.storage_account.backup.lz_key][var.storage_account.backup.vault_key])
 }
