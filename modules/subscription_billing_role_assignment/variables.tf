24f93b64339fad3ceeea3c77f248d2f6530dc083
Dave Seepersad <davesee@microsoft.com>
2021-06-03 14:03:52 -0400

    Updated remaining hardcoded Azure urls

--- modules/subscription_billing_role_assignment/variables.tf
+++ modules/subscription_billing_role_assignment/variables.tf
@@ -1,6 +1,7 @@
 variable "client_config" {}
 variable "principals" {}
 variable "settings" {}
+variable "cloud" {}
 variable "keyvaults" {}
 variable "billing_role_definition_name" {
   default = "Enrollment account subscription creator"
