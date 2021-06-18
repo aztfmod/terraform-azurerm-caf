d300b645b1ab68c43c7b5fb6767aa8a0dff86276
GlennChia <glennchia7@gmail.com>
2021-06-18 08:42:51 +0000

    feat: handle resource_group_key used in dimension

--- consumption_budgets.tf
+++ consumption_budgets.tf
@@ -2,9 +2,12 @@ module "consumption_budgets" {
   source   = "./modules/consumption_budget"
   for_each = local.shared_services.consumption_budgets
 
+  client_config = local.client_config
   resource_group_id = coalesce(
     try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].id, null),
     try(each.value.resource_group.id, null)
   )
-  settings = each.value
+  # lz_key used in dimension to reference remote state
+  resource_groups = local.combined_objects_resource_groups
+  settings        = each.value
 }
