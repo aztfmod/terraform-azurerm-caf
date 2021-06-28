cd683ebc1f3c325fd0fc4a72c126e93e9ada4045
Paul Matthews <paul.matthews@beis.gov.uk>
2021-06-28 13:09:26 +0000

    Removing setting owners on App, as when running as that App, it deletes itself.

--- modules/azuread/applications/module.tf
+++ modules/azuread/applications/module.tf
@@ -3,7 +3,8 @@ resource "azuread_application" "app" {
   display_name = var.global_settings.passthrough ? format("%s", var.settings.application_name) : format("%v-%s", try(var.global_settings.prefixes[0], ""), var.settings.application_name)
 
   owners = [
-    var.client_config.object_id
+    # Removing as it causes issue with Switch from User to Service Prinicpal
+    #var.client_config.object_id
   ]
 
   reply_urls                 = try(var.settings.reply_urls, null)
