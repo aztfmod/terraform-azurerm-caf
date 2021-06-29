0e2d017cc35b23997f0aff340bca88319d6164a2
lolorol <lalesle@microsoft.com>
2021-06-29 06:15:27 +0000

    Fix admin consent

--- modules/azuread/service_principal/scripts/grant_consent.sh
+++ modules/azuread/service_principal/scripts/grant_consent.sh
@@ -5,13 +5,21 @@ set -e
 resourceId=$(az ad sp show --id "${resourceAppId}" --query "objectId" -o tsv)
 echo " -resourceId: ${resourceId}"
 
-URI=$(echo  "https://graph.microsoft.com/beta/servicePrincipals/${resourceId}/appRoleAssignments") && echo " - uri: $URI"
+microsoft_graph_endpoint=$(az cloud show | jq -r ".endpoints.microsoftGraphResourceId")
 
-# grant consent (Application.ReadWrite.OwnedBy)
-JSON=$( jq -n \
-            --arg principalId "${principalId}" \
-            --arg resourceId "${resourceId}" \
-            --arg appRoleId "${appRoleId}" \
-        '{principalId: $principalId, resourceId: $resourceId, appRoleId: $appRoleId}' ) && echo " - body: $JSON"
+URI=$(echo  "${microsoft_graph_endpoint}v1.0/servicePrincipals/${resourceId}/appRoleAssignedTo") && echo " - uri: $URI"
 
-az rest --method POST --uri $URI --header Content-Type=application/json --body "$JSON"
+appRoleId=$(az rest --method GET --uri ${URI} \
+    --query "value[?appRoleId=='${appRoleId}' && principalId=='${principalId}' && resourceId=='${resourceId}'].appRoleId" -o tsv)
+
+if [ -z ${appRoleId} ]; then
+    JSON=$( jq -n \
+                --arg principalId "${principalId}" \
+                --arg resourceId "${resourceId}" \
+                --arg appRoleId "${appRoleId}" \
+            '{principalId: $principalId, resourceId: $resourceId, appRoleId: $appRoleId}' ) && echo " - body: $JSON"
+
+    az rest --method POST --uri $URI --header Content-Type=application/json --body "$JSON"
+else
+    echo "API permission already granted."
+fi
