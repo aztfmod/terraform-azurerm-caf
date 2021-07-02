db7372bf135ad2fb223d41e0cd20defb91b40ca2
Abdullah Khairi <akamarulzama@microsoft.com>
2021-07-02 06:44:39 +0000

    add pcredentials detail to output

--- modules/databases/mssql_managed_instance/output.tf
+++ modules/databases/mssql_managed_instance/output.tf
@@ -17,3 +17,11 @@ output "principal_id" {
   value       = lookup(azurerm_template_deployment.mssqlmi.outputs, "objectId")
   description = "SQL MI Identity Principal Id"
 }
+
+output "administratorLogin" {
+  value = var.settings.administratorLogin
+}
+
+output "administratorLoginPassword" {
+  value = try(var.settings.administratorLoginPassword, random_password.mssqlmi.0.result)
+}
