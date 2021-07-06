7cbe85057f2b90edad04ac64abeb0b7dabb03477
lolorol <lalesle@microsoft.com>
2021-07-06 04:27:19 +0000

    Add firewall policy parent child

--- modules/networking/firewall_policies/variables.tf
+++ modules/networking/firewall_policies/variables.tf
@@ -1,17 +1,9 @@
-variable "policy_settings" {
+variable "settings" {
   description = "(Required) Azure Firewall Policy Configuration"
-}
-
-variable "resource_group_name" {
-  description = "(Required) Specifies the name of the Resource Group in which the Firewall exists. Changing this forces a new resource to be created."
 }
 
 variable "global_settings" {
   description = "Global settings object (see module README.md)"
-}
-
-variable "location" {
-  description = "(Required) Specifies the supported Azure location where the resource exists"
 }
 
 variable "tags" {
@@ -19,10 +11,8 @@ variable "tags" {
   default     = {}
 }
 
-variable "base_tags" {}
-
-
-variable "name" {
+variable "resource_group" {
+  description = "(Required) A resource_group object."
 }
 
 variable "base_policy_id" {
