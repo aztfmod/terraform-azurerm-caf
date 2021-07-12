5280abb4f2a17d5570b0b983d405f9fb527a87b6
Abdullah Khairi <akamarulzama@microsoft.com>
2021-07-12 07:48:17 +0000

    WIP agw decouple subresources

---
+++ modules/networking/application_gateway_platform/locals.networking.tf
@@ -0,0 +1,40 @@
+locals {
+
+  gateway_vnet_local = try(coalesce(
+    try(var.vnets[var.client_config.landingzone_key][var.settings.vnet_key], null),
+    try(var.vnets[var.client_config.landingzone_key][var.settings.subnet.vnet_key], null)
+  ), null)
+
+  private_vnet_local = try(var.vnets[var.client_config.landingzone_key][var.settings.front_end_ip_configurations.private.vnet_key], null)
+  public_vnet_local  = try(var.vnets[var.client_config.landingzone_key][var.settings.front_end_ip_configurations.public.vnet_key], null)
+
+  gateway_vnet_remote = try(coalesce(
+    try(var.vnets[var.settings.lz_key][var.settings.vnet_key], null),
+    try(var.vnets[var.settings.subnet.lz_key][var.settings.subnet.vnet_key], null)
+  ), null)
+
+  private_vnet_remote = try(var.vnets[var.settings.front_end_ip_configurations.private.lz_key][var.settings.front_end_ip_configurations.private.vnet_key], null)
+  public_vnet_remote  = try(var.vnets[var.settings.front_end_ip_configurations.public.lz_key][var.settings.front_end_ip_configurations.public.vnet_key], null)
+
+  gateway_vnet = merge(local.gateway_vnet_local, local.gateway_vnet_remote)
+  private_vnet = merge(local.private_vnet_local, local.private_vnet_remote)
+  public_vnet  = merge(local.public_vnet_local, local.public_vnet_remote)
+
+  ip_configuration = {
+    gateway = {
+      subnet_id = local.gateway_vnet.subnets[var.settings.subnet_key].id
+    }
+    private = {
+      subnet_id = local.private_vnet.subnets[var.settings.front_end_ip_configurations.private.subnet_key].id
+      cidr      = local.private_vnet.subnets[var.settings.front_end_ip_configurations.private.subnet_key].cidr
+    }
+    public = {
+      subnet_id     = try(local.public_vnet.subnets[var.settings.front_end_ip_configurations.public.subnet_key].id, null)
+      ip_address_id = try(var.public_ip_addresses[var.client_config.landingzone_key][var.settings.front_end_ip_configurations.public.public_ip_key].id, var.public_ip_addresses[var.settings.front_end_ip_configurations.public.lz_key][var.settings.front_end_ip_configurations.public.public_ip_key].id)
+    }
+  }
+
+  private_cidr       = local.ip_configuration.private.cidr[var.settings.front_end_ip_configurations.private.subnet_cidr_index]
+  private_ip_address = cidrhost(local.private_cidr, var.settings.front_end_ip_configurations.private.private_ip_offset)
+
+}
---
+++ modules/networking/application_gateway_platform/output.tf
@@ -0,0 +1,7 @@
+output "id" {
+  value = azurerm_application_gateway.agw.id
+}
+
+output "private_ip_address" {
+  value = local.private_ip_address
+}
