bdd0afb71bad293d98cb3ebf137af83ddcf0c094
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2021-07-22 04:44:45 +0000

    Adding outputs

--- cognitive_service.tf
+++ cognitive_service.tf
@@ -8,3 +8,7 @@ module "cognitive_services_account" {
   location            = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
   settings            = each.value
 }
+
+output "cognitive_services_account" {
+  value = module.cognitive_services_account
+}
