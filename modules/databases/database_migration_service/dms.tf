eb98d07d506eba5c019f831eee5ffead65a624d5
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2021-08-18 07:50:39 +0000

    Update to name provider resource_type

--- modules/databases/database_migration_service/dms.tf
+++ modules/databases/database_migration_service/dms.tf
@@ -1,7 +1,6 @@
-#TODO replace with the right resource type
 resource "azurecaf_name" "dms" {
   name          = var.settings.name
-  resource_type = "azurerm_virtual_desktop_application_group" #azurerm_database_migration_service
+  resource_type = "azurerm_database_migration_service"
   prefixes      = var.global_settings.prefix
   random_length = var.global_settings.random_length
   clean_input   = true
