1959c726eab0e0805dc7813ad2c5c9cc0bdf4190
Swetha Sundar <swetha.sundr@gmail.com>
2021-08-22 11:38:23 +0000

    Add a new sub-module for PrivateDNSZone Virtual Network Link

---
+++ modules/networking/private_dns_vnet_link/variables.tf
@@ -0,0 +1,30 @@
+variable "global_settings" {
+  description = "Global settings object (see module README.md)"
+}
+variable "client_config" {
+  description = "Client configuration object (see module README.md)."
+}
+variable "name" {
+  description = "Name of the Virtual Network Link"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+}
+
+variable "vnet_links" {
+  default = {}
+}
+
+variable "vnets" {
+  default = {}
+}
+
+variable "base_tags" {
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = map(any)
+}
+
+variable "tags" {
+  default = {}
+}
