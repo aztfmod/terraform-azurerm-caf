1fd8328fd64679dab260665d2cfdda8232d54a46
Hassan Hassoun <mail@hassanhassoun.com>
2021-08-30 11:11:27 -0400

    Qualify redis id

--- modules/networking/private_links/endpoints/subnet/redis_cache.tf
+++ modules/networking/private_links/endpoints/subnet/redis_cache.tf
@@ -5,7 +5,7 @@ module "redis_caches" {
   global_settings     = var.global_settings
   client_config       = var.client_config
   settings            = each.value
-  resource_id         = try(var.remote_objects.redis_caches[each.value.lz_key][each.key].id, var.remote_objects.redis_caches[var.client_config.landingzone_key][each.key].id)
+  resource_id         = try(var.remote_objects.redis_caches[each.value.lz_key][each.key].redis_cache.id, var.remote_objects.redis_caches[var.client_config.landingzone_key][each.key].redis_cache.id)
   subresource_names   = ["redisCache"]
   subnet_id           = var.subnet_id
   private_dns         = var.private_dns
