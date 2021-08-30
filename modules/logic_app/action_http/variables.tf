6f62591c1cd951eb7a4e9e9058e521cadd588afd
SergioMiyama <sergio.miyama@outlook.com>
2021-08-30 05:16:02 +0000

    Remove Tags from Logic App Action Http

--- modules/logic_app/action_http/variables.tf
+++ modules/logic_app/action_http/variables.tf
@@ -7,14 +7,6 @@ variable "client_config" {
 variable "settings" {
   description = "Settings object (see module README.md)."
 }
-variable "base_tags" {
-  description = "Base tags for the resource to be inherited from the resource group."
-  type        = map(any)
-}
-variable "tags" {
-  description = "(Required) map of tags for the deployment"
-  default     = null
-}
 variable "logic_app_id" {
   description = "(Required) Specifies the ID of the Logic App Workflow"
 }
