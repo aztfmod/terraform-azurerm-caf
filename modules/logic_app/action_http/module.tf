bd4a3cd7c779f4df2498fedb26de7349a9878336
SergioMiyama <sergio.miyama@outlook.com>
2021-08-30 05:37:43 +0000

    Fix name provider for logic app actions

--- modules/logic_app/action_http/module.tf
+++ modules/logic_app/action_http/module.tf
@@ -1,6 +1,6 @@
 resource "azurecaf_name" "laah" {
   name          = var.settings.name
-  resource_type = "azurerm_logic_app_workflow"
+  resource_type = "azurerm_logic_app_action_http"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
   clean_input   = true
