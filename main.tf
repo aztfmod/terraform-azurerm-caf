646c1f50a71a896ef2144ca7745897d6f7ce1ccf
Arnaud Lheureux <arnaudlh@hotmail.com>
2021-09-03 07:32:03 +0000

    update to azurerm 2.75

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.74.0"
+      version = "~> 2.75.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
