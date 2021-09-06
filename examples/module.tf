2d7be637b1f00ccd482f5759fc14bcfc65453a5c
SergioMiyama <sergio.miyama@outlook.com>
2021-09-06 05:35:34 +0000

    Data_Factory Example

--- examples/module.tf
+++ examples/module.tf
@@ -193,6 +193,8 @@ module "example" {
     app_service_plans            = var.app_service_plans
     app_services                 = var.app_services
   }
-  data_factory = var.data_factory
+  data_factory = {
+    data_factory = var.data_factory
+  }
 
 }
