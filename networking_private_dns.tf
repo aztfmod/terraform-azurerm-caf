ba9baad73603dcf05ef31b4f109ea41cd9447676
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2021-09-14 13:15:33 +0000

    Fixing #671

--- networking_private_dns.tf
+++ networking_private_dns.tf
@@ -24,13 +24,17 @@ output "private_dns" {
 #
 
 module "private_dns_vnet_links" {
-  source   = "./modules/networking/private_dns_vnet_link"
-  for_each = try(local.networking.private_dns_vnet_links, {})
-  depends_on  = [module.private_dns]
+  source     = "./modules/networking/private_dns_vnet_link"
+  for_each   = try(local.networking.private_dns_vnet_links, {})
+  depends_on = [module.private_dns]
 
-  base_tags       = {}
-  global_settings = local.global_settings
-  client_config   = local.client_config
+  base_tags           = {}
+  global_settings     = local.global_settings
+  resource_group_name = coalesce(
+    try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].name, null),
+    try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].name, null)
+  )
+  client_config       = local.client_config
   virtual_network_id = coalesce(
     try(local.combined_objects_networking[each.value.lz_key][each.value.vnet_key].id, null),
     try(local.combined_objects_networking[local.client_config.landingzone_key][each.value.vnet_key].id, null)
