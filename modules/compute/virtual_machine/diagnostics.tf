6109e5bcb4d9b92b9fe4d3d4b84aa9d20f4694e3
lolorol <lalesle@microsoft.com>
2021-09-16 02:36:42 +0000

    Update vm custom_data

--- modules/compute/virtual_machine/diagnostics.tf
+++ modules/compute/virtual_machine/diagnostics.tf
@@ -1,6 +1,6 @@
 module "nics" {
   source   = "../../diagnostics"
-  for_each = try(var.settings.networking_interfaces,toset([]))
+  for_each = try(var.settings.networking_interfaces, toset([]))
 
   resource_id       = azurerm_network_interface.nic[each.key].id
   resource_location = azurerm_network_interface.nic[each.key].location
