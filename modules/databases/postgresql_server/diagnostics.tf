37cd7db87742520fb05101a85093caff459eb251
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2021-10-12 06:18:52 +0000

    Adding diagnostics for mysql

--- modules/databases/postgresql_server/diagnostics.tf
+++ modules/databases/postgresql_server/diagnostics.tf
@@ -4,6 +4,7 @@ module "diagnostics" {
   count  = lookup(var.settings, "diagnostic_profiles", null) == null ? 0 : 1
 
   resource_id       = azurerm_postgresql_server.postgresql.id
+  resource_location = azurerm_postgresql_server.postgresql.location
   diagnostics       = var.diagnostics
   profiles          = var.settings.diagnostic_profiles
 }
