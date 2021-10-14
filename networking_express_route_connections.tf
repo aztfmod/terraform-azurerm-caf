d55b355aa6578d7c259c41696f5388af20d7991a
lolorol <lalesle@microsoft.com>
2021-10-14 05:45:54 +0000

    Fix er connection peering

--- networking_express_route_connections.tf
+++ networking_express_route_connections.tf
@@ -6,6 +6,13 @@ module "express_route_connections" {
   settings                 = each.value
   virtual_hub_route_tables = local.combined_objects_virtual_hub_route_tables
 
+  virtual_hub_id = coalesce(
+    try(local.combined_objects_virtual_hubs[each.value.virtual_hub.lz_key][each.value.virtual_hub.key].id, null),
+    try(local.combined_objects_virtual_hubs[local.client_config.landingzone_key][each.value.virtual_hub.key].id, null),
+    try(each.value.virtual_hub.id, null),
+    try(each.value.virtual_hub_id, null)
+  )
+
   express_route_circuit_peering_id = coalesce(
     try(local.combined_objects_express_route_circuit_peerings[each.value.circuit_peering.lz_key][each.value.circuit_peering.key].id, null),
     try(each.value.express_route_circuit_peering_id, null)
