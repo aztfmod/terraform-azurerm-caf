f342c5617fe6bb918944460025b87e96ec1182e8
Solomchuk Anastasiia <SSI_ASolomchuk@qbsol.com>
2021-10-20 18:22:26 +0300

    Added 'tags' to route_filter

--- modules/networking/route_filters/modules.tf
+++ modules/networking/route_filters/modules.tf
@@ -2,7 +2,7 @@ resource "azurerm_route_filter" "rtfilter" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
-
+  tags                = local.tags
   rule {
     name        = var.rule_name
     access      = "Allow"       # The access type of the rule. The only possible value is Allow
