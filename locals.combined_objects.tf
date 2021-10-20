b84af5ff94f7ed76c57c66166021f12cb10068f7
SergioMiyama <sergio.miyama@outlook.com>
2021-10-20 08:15:00 +0000

    Fixes

--- locals.combined_objects.tf
+++ locals.combined_objects.tf
@@ -60,6 +60,7 @@ locals {
   combined_objects_recovery_vaults                     = merge(tomap({ (local.client_config.landingzone_key) = module.recovery_vaults }), try(var.remote_objects.recovery_vaults, {}))
   combined_objects_resource_groups                     = merge(tomap({ (local.client_config.landingzone_key) = local.resource_groups }), try(var.remote_objects.resource_groups, {}))
   combined_objects_storage_accounts                    = merge(tomap({ (local.client_config.landingzone_key) = module.storage_accounts }), try(var.remote_objects.storage_accounts, {}))
+  combined_objects_storage_containers                  = merge(tomap({ (local.client_config.landingzone_key) = module.storage_containers }), try(var.remote_objects.storage_containers, {}))
   combined_objects_synapse_workspaces                  = merge(tomap({ (local.client_config.landingzone_key) = module.synapse_workspaces }), try(var.remote_objects.synapse_workspaces, {}))
   combined_objects_virtual_machines                    = merge(tomap({ (local.client_config.landingzone_key) = module.virtual_machines }), try(var.remote_objects.virtual_machines, {}))
   combined_objects_virtual_machine_scale_sets          = merge(tomap({ (local.client_config.landingzone_key) = module.virtual_machine_scale_sets }), try(var.remote_objects.virtual_machine_scale_sets, {}))
