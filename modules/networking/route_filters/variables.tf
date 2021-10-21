5a8cc8623d10be9b6e5da6da9740c69e3540be9d
Solomchuk Anastasiia <SSI_ASolomchuk@qbsol.com>
2021-10-21 15:48:12 +0300

    Removed name varible in route_filter

--- modules/networking/route_filters/variables.tf
+++ modules/networking/route_filters/variables.tf
@@ -1,4 +1,4 @@
-variable "name" {}
+#variable "name" {}
 variable "resource_group_name" {
   description = "(Required) The name of the resource group where to create the resource."
   type        = string
