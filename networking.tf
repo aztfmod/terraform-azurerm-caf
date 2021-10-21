243c203b4a48b6a3607582acdd747a1b02e2f04d
Solomchuk Anastasiia <SSI_ASolomchuk@qbsol.com>
2021-10-21 13:39:30 +0300

    Ass base_tag for route_filter in networking.tf

--- networking.tf
+++ networking.tf
@@ -215,6 +215,7 @@ module "route_filters" {
   location                      = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
   rule_name                     = each.value.rule.rule_name
   rule_communities              = each.value.rule.rule_communities
+  base_tags                     = try(local.global_settings.inherit_tags, false) ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags : {}
   tags                          = try(each.value.tags, null)
 }
 #
