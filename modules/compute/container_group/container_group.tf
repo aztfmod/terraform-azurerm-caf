4e23493803faa3d641ed03e5d5c6a276cda153f1
Solomchuk Anastasiia <SSI_ASolomchuk@qbsol.com>
2021-10-21 14:05:36 +0300

    Updated image_registry_credential section in container_registry)group module

--- modules/compute/container_group/container_group.tf
+++ modules/compute/container_group/container_group.tf
@@ -21,9 +21,9 @@ resource "azurerm_container_group" "acg" {
   dynamic "image_registry_credential" {
     for_each = lookup(var.settings, "image_registry_credential", null) == null ? [] : [1]
     content {
-      server   =  each.value.image_registry_credential.server
-      username =  each.value.image_registry_credential.username
-      password =  each.value.image_registry_credential.password
+      server   =  image_registry_credential.value.server
+      username =  image_registry_credential.value.username
+      password =  image_registry_credential.value.password
     }
   }
 
