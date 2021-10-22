370f7ebdd828338280da646e8c1aa7df98387809
Solomchuk Anastasiia <SSI_ASolomchuk@qbsol.com>
2021-10-22 15:17:57 +0300

    Update

--- modules/networking/express_route_circuit_peerings/output.tf
+++ modules/networking/express_route_circuit_peerings/output.tf
@@ -1,16 +1,16 @@
 output "id" {
-  value       = azurerm_express_route_circuit_peerings.circuitpeering.id
+  value       = azurerm_express_route_circuit_peering.circuitpeering.id
   description = "ExpressRoute Circuit Peering ID"
 }
 output "azure_asn" {
-  value       = azurerm_express_route_circuit_peerings.circuitpeering.azure_asn
+  value       = azurerm_express_route_circuit_peering.circuitpeering.azure_asn
   description = "The ASN used by Azure"
 }
 output "primary_azure_port" {
-  value       = azurerm_express_route_circuit_peerings.circuitpeering.primary_azure_port
+  value       = azurerm_express_route_circuit_peering.circuitpeering.primary_azure_port
   description = "The Primary Port used by Azure for this Peering."
 }
 output "secondary_azure_port" {
-  value       = azurerm_express_route_circuit_peerings.circuitpeering.secondary_azure_port
+  value       = azurerm_express_route_circuit_peering.circuitpeering.secondary_azure_port
   description = "The Secondary Port used by Azure for this Peering."
 }
