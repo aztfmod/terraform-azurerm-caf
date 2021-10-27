d0d1b6c66a95f70f02efe6af50ca727391bd325b
Dimitri <>
2021-10-27 09:22:01 +0200

    add ability for key reference to custom script extension

--- modules/compute/virtual_machine_extensions/custom_script.tf
+++ modules/compute/virtual_machine_extensions/custom_script.tf
@@ -7,16 +7,12 @@ resource "azurerm_virtual_machine_extension" "custom_script" {
   type_handler_version       = "1.10"
   auto_upgrade_minor_version = true
 
-  settings = jsonencode(
-    {
-      "fileUris" : try(var.extension.fileuris, ""),
-      "timestamp" : try(var.extension.timestamp, "12345678")
-    }
-  )
-  protected_settings = jsonencode(local.protected_settings)
+  settings                   = jsonencode(local.settings)
+  protected_settings         = jsonencode(local.protected_settings)
 }
 
 locals {
+  # managed identity
   identity_type = try(var.extension.identity_type, "") #userassigned, systemassigned or null
   managed_local_identity = try(var.managed_identities[var.client_config.landingzone_key][var.extension.managed_identity_key].princpal_id, "")
   managed_remote_identity = try(var.managed_identities[var.extension.lz_key][var.extension.managed_identity_key].principal_id, "")
@@ -28,5 +24,15 @@ locals {
   
   command = {"commandtoexecute" : "${var.extension.commandtoexecute}"}
 
-  protected_settings = merge(local.command,local.system_assigned_id,local.user_assigned_id) 
+  protected_settings = merge(local.command,local.system_assigned_id,local.user_assigned_id)
+
+  # fileuris
+  fileuri_sa_key       = try(var.extension.fileuri_sa_key, "")
+  fileuri_sa_path      = try(var.extension.fileuri_sa_path, "")
+  fileuri_sa           = local.fileuri_sa_key != "" ? try(var.storage_accounts[var.client_config.landingzone_key][var.extension.fileuri_sa_key].primary_blob_endpoint, try(var.storage_accounts[var.extension.lz_key][var.extension.fileuri_sa_key].primary_blob_endpoint)) : null
+  fileuri_sa_full_path = "${local.filuri_sa}${local.fileuri_sa_path}"
+
+  timestamp = {"timestamp" : try(var.extension.timestamp, "12345678")}
+  fileuris = (try(var.extension.fileuris, "" == "")) ? [local.fileuri_sa_full_path] : var.extension.fileuris
+  settings = merge(local.timestamp,local.fileuris)
 }
