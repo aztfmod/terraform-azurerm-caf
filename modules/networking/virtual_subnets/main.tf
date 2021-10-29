85f14f6f1915af280cec62e9d40613008622cb08
Jor Seng <jorseng@gmail.com>
2021-10-29 10:20:59 +0000

    virtual subnet standalone

---
+++ modules/networking/virtual_subnets/main.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
---
+++ modules/networking/virtual_subnets/output.tf
@@ -0,0 +1,3 @@
+output "subnets" {
+  value = module.subnets
+}
