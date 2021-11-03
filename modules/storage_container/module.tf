792476c2da6558c29684857885a8e37a9cd19808
lolorol <lalesle@microsoft.com>
2021-11-03 03:59:18 +0000

    Clean-up

--- modules/storage_container/module.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_storage_container" "sc" {
-  name                  = var.settings.name
-  storage_account_name  = var.storage_account_name
-  container_access_type = try(var.settings.container_access_type, null)
-  metadata              = try(var.settings.metadata, null)
-}
