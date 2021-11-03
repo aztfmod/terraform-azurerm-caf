4ecd5269faaeddb92399abe886992db5597ee739
lolorol <lalesle@microsoft.com>
2021-11-03 03:49:56 +0000

    Update storage container and subscription lifecycle

--- storage_container.tf
+++ storage_container.tf
@@ -1,10 +1,8 @@
 module "storage_containers" {
-  source   = "./modules/storage_container/"
+  source   = "./modules/storage_account/container/"
   for_each = local.storage.storage_containers
 
-  global_settings = local.global_settings
-  client_config   = local.client_config
-  settings        = each.value
+  settings = each.value
 
   storage_account_name = coalesce(
     try(local.combined_objects_storage_accounts[each.value.storage_account.lz_key][each.value.storage_account.key].name, null),
@@ -12,10 +10,6 @@ module "storage_containers" {
     try(each.value.storage_account.name, null)
   )
 
-
-  combined_resources = {
-    storage_account = local.combined_objects_storage_accounts
-  }
 }
 output "storage_containers" {
   value = module.storage_containers
