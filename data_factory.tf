1b0c61a96067679fd44e88c915bae9fb8be1e0de
SergioMiyama <sergio.miyama@outlook.com>
2021-11-04 23:16:53 +0000

    Advanced Config

--- data_factory.tf
+++ data_factory.tf
@@ -11,7 +11,8 @@ module "data_factory" {
   location  = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group.key].location : local.global_settings.regions[each.value.region]
 
   resource_group_name = coalesce(
-    try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].name, null),
+    try(local.combined_objects_resource_groups[each.value.resource_group.lz_key][each.value.resource_group.key].name, null),
+    try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group.key].name, null),
     try(each.value.resource_group.name, null)
   )
   remote_objects = {
--- modules/networking/frontdoor_custom_https_configuration/module.tf
+++ modules/networking/frontdoor_custom_https_configuration/module.tf
@@ -1,26 +1,24 @@
-resource "azurecaf_name" "fdchc" {
-  name          = var.settings.name
-  resource_type = "azurerm_frontdoor" #"azurerm_frontdoor_custom_https_configuration"
-  prefixes      = var.global_settings.prefixes
-  random_length = var.global_settings.random_length
-  clean_input   = true
-  passthrough   = var.global_settings.passthrough
-  use_slug      = var.global_settings.use_slug
-}
-
 resource "azurerm_frontdoor_custom_https_configuration" "fdchc" {
-  frontend_endpoint_id              = var.settings.frontend_endpoint_id
+  frontend_endpoint_id = coalesce(
+    try(var.settings.frontend_endpoint_id, null),
+    try(var.remote_objects.frontdoor[var.settings.frontend_endpoint.lz_key][var.settings.frontend_endpoint.front_door_key].frontend_endpoints[var.settings.frontend_endpoint.name], null),
+    try(var.remote_objects.frontdoor[var.client_config.landingzone_key][var.settings.frontend_endpoint.front_door_key].frontend_endpoints[var.settings.frontend_endpoint.name], null)
+  )
+
   custom_https_provisioning_enabled = var.settings.custom_https_provisioning_enabled
 
   dynamic "custom_https_configuration" {
     for_each = try(var.settings.custom_https_configuration, null) != null ? [var.settings.custom_https_configuration] : []
 
     content {
-      certificate_source                         = custom_https_configuration.value.certificate_source
-      azure_key_vault_certificate_vault_id       = custom_https_configuration.value.azure_key_vault_certificate_vault_id
-      azure_key_vault_certificate_secret_name    = custom_https_configuration.value.azure_key_vault_certificate_secret_name
-      azure_key_vault_certificate_secret_version = custom_https_configuration.value.azure_key_vault_certificate_secret_version
+      certificate_source = try(custom_https_configuration.value.certificate_source, null)
+      azure_key_vault_certificate_vault_id = try(coalesce(
+        try(custom_https_configuration.value.keyvault.id, null),
+        try(var.remote_objects.keyvault[custom_https_configuration.value.keyvault.lz_key][custom_https_configuration.value.keyvault.key].id, null),
+        try(var.remote_objects.keyvault[var.client_config.landingzone_key][custom_https_configuration.value.keyvault.key].id, null)
+      ), null)
+      azure_key_vault_certificate_secret_name    = try(custom_https_configuration.value.keyvault.secret_name, null)
+      azure_key_vault_certificate_secret_version = try(custom_https_configuration.value.keyvault.secret_version, null)
     }
   }
-
 }
