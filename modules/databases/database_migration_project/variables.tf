f7a7bf084ab5aaaa0ff0f33a430ae57028352d43
SergioMiyama <sergio.miyama@outlook.com>
2021-11-05 04:27:43 +0000

    remote object fix

--- modules/databases/database_migration_project/variables.tf
+++ modules/databases/database_migration_project/variables.tf
@@ -10,7 +10,11 @@ variable "location" {
   description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
   type        = string
 }
-variable "vnets" {}
+
+variable "remote_objects" {
+  description = "Combined objects for virtual networks used in the module."
+  type        = map(any)
+}
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
   type        = map(any)
