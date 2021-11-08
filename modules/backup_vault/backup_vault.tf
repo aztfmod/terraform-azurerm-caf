fef0d54f1fd1a477981c3cc307d9ec1e127a3854
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-11-08 17:57:59 +0200

    Update backup_vault.tf

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -1,13 +1,13 @@
-# naming convention
-# resource "azurecaf_name" "backup_vault_name" {
-#   name          = var.settings.name
-#   resource_type = "azurerm_data_protection_backup_vault"
-#   prefixes      = var.global_settings.prefixes
-#   random_length = var.global_settings.random_length
-#   clean_input   = true
-#   passthrough   = var.global_settings.passthrough
-#   use_slug      = var.global_settings.use_slug
-# }
+naming convention
+resource "azurecaf_name" "backup_vault_name" {
+  name          = var.settings.name
+  resource_type = "azurerm_data_protection_backup_vault"
+  prefixes      = var.global_settings.prefixes
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
 resource "azurerm_data_protection_backup_vault" "backup_vault" {
   name                = azurecaf_name.backup_vault_name.result
   location            = var.location
