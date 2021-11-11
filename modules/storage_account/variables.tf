db4dfb6c1623aa6a016c051669a3d9b78b3294cf
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-11-11 15:19:23 +0200

    Update variables.tf

--- modules/storage_account/variables.tf
+++ modules/storage_account/variables.tf
@@ -4,7 +4,9 @@ variable "global_settings" {
 variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
-variable "storage_account" {}
+variable "storage_account" {
+  sensitive = true
+}
 variable "resource_group_name" {
   description = "(Required) The name of the resource group where to create the resource."
   type        = string
