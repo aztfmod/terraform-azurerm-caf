fb4f55c039eaff5be007505c89d78117c5e5b05a
SergioMiyama <sergio.miyama@outlook.com>
2021-11-12 00:59:54 +0000

    Fix

--- modules/data_factory/data_factory_integration_runtime_self_hosted/output.tf
+++ modules/data_factory/data_factory_integration_runtime_self_hosted/output.tf
@@ -2,11 +2,11 @@ output "id" {
   value       = azurerm_data_factory_integration_runtime_self_hosted.dfirsh.id
   description = "The ID of the Data Factory."
 }
-# output "auth_key_1" {
-#   value = azurerm_data_factory_integration_runtime_self_hosted.dfirsh.auth_key_1
-#   description = "The primary integration runtime authentication key."
-# }
-# output "auth_key_2" {
-#   value = azurerm_data_factory_integration_runtime_self_hosted.dfirsh.auth_key_2
-#   description = "The secondary integration runtime authentication key."
-# }
+output "auth_key_1" {
+   value = azurerm_data_factory_integration_runtime_self_hosted.dfirsh.auth_key_1
+   description = "The primary integration runtime authentication key."
+ }
+output "auth_key_2" {
+  value = azurerm_data_factory_integration_runtime_self_hosted.dfirsh.auth_key_2
+  description = "The secondary integration runtime authentication key."
+}
