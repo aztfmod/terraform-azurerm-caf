82915f97bb89763c58b6c005452bedc12f006048
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-11-12 17:13:34 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -8,7 +8,6 @@ module "backup_vaults" {
   diagnostics         = local.combined_diagnostics
   identity            = try(each.value.identity, null)
   resource_groups     = local.resource_groups
-  resource_group_name = local.resource_groups[each.value.resource_group_key].name
   location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
   base_tags           = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
 }
