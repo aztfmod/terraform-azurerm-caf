c0aabd74272b918505a489e6c115d73ee9e842e6
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-11-12 16:25:34 +0200

    Update backup_vault.tf

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -4,27 +4,27 @@ locals {
 }
 
 # naming convention
-# resource "azurecaf_name" "backup_vault_name" {
-#   name          = var.settings.name
-#   resource_type = "azurerm_data_protection_backup_vault"
-#   prefixes      = var.global_settings.prefixes
-#   random_length = var.global_settings.random_length
-#   clean_input   = true
-#   passthrough   = var.global_settings.passthrough
-#   use_slug      = var.global_settings.use_slug
-# }
-
-resource "random_string" "bckp_name" {
-#   count   = try(var.global_settings.prefix, null) == null ? 1 : 0
-  length  = 4
-  special = false
-  upper   = false
-  lower   = true
-  number  = false
+resource "azurecaf_name" "bckp" {
+  name          = var.backup_vault.name
+  resource_type = "azurerm_storage_account"
+  prefixes      = var.global_settings.prefixes
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
 }
 
+# resource "random_string" "bckp_name" {
+#   count   = try(var.global_settings.prefix, null) == null ? 1 : 0
+#   length  = 4
+#   special = false
+#   upper   = false
+#   lower   = true
+#   number  = false
+# }
+
 resource "azurerm_data_protection_backup_vault" "backup_vault" {
-  name                = random_string.bckp_name 
+  name                = azurecaf_name.bckp.result 
   location            = var.location
   resource_group_name = var.resource_group_name
   datastore_type      = try(var.datastore_type, "VaultStore")
