f934ba27ad2590309ec197ed4c3277d74a6a2281
Solomchuk Anastasiia <SSI_ASolomchuk@qbsol.com>
2021-11-16 22:57:32 +0200

    fix

--- modules/compute/virtual_machine/vm_windows.tf
+++ modules/compute/virtual_machine/vm_windows.tf
@@ -120,12 +120,11 @@ resource "azurerm_windows_virtual_machine" "vm" {
   }
 
   dynamic "additional_unattend_content" {
-    for_each = { for key, value in try(each.value.additional_unattend_content, null) : key => value}
-
+    for_each = try(each.value.additional_unattend_content[*], false) == false ? [] : [1]
     content {
 
-        content = try(value.content, try(filebase64(format("%s/%s", path.cwd,value.contentfile))))
-        setting = value.setting 
+        content = try(additional_unattend_content.value.content, try(filebase64(format("%s/%s", path.cwd,additional_unattend_content.value.contentfile))))
+        setting = additional_unattend_content.value.setting 
         
     }  
   }
