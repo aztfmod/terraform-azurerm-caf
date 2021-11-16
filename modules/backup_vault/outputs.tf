3c08d7dca21c7d99d1c38e3f1888b25308f7b930
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-11-16 16:51:22 +0200

    Update outputs.tf

--- modules/backup_vault/outputs.tf
+++ modules/backup_vault/outputs.tf
@@ -1,5 +1,34 @@
 output "id" {
-  # depends_on = [azurerm_resource_group_template_deployment.asr]
-  description = "Output the object ID"
-  value       = azurerm_recovery_services_vault.asr.id
+  description = "The ID of the Backup Vault"
+  value       = azurerm_data_protection_backup_vault.backup_vault.id
+}
+
+output "name" {
+  depends_on  = [azurerm_data_protection_backup_vault.backup_vault]
+  description = "The name of the Backup Vault"
+  value       = azurerm_data_protection_backup_vault.backup_vault.name
+}
+output "location" {
+  description = "The location of the Backup Vault"
+  value       = var.location
+}
+
+output "resource_group_name" {
+  description = "The resource group name of the Backup Vault"
+  value       = var.resource_group_name
+}
+
+output "datastore_type" {
+  description = "Output of the datastore type on the resource."
+  value       = try(var.backup_vault.datastore_type, null)
+}
+
+output "redundancy" {
+  description = "Output of the datastore type on the resource."
+  value       = try(var.backup_vault.redundancy, null)
+}
+
+output "identity" {
+  description = " An identity block, which contains the Identity information for this Backup Vault. Exports principal_id (The Principal ID for the Service Principal associated with the Identity of this Backup Vault), tenand_id (The Tenant ID for the Service Principal associated with the Identity of this Backup Vault)"
+  value       = try(azurerm_data_protection_backup_vault.backup_vault.identity, null)
 }
