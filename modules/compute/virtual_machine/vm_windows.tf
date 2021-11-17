06f908a00c3e8ebca2091114569ba183e56446b6
Solomchuk Anastasiia <SSI_ASolomchuk@qbsol.com>
2021-11-17 19:38:58 +0200

    fix

--- modules/compute/virtual_machine/vm_windows.tf
+++ modules/compute/virtual_machine/vm_windows.tf
@@ -124,7 +124,7 @@ resource "azurerm_windows_virtual_machine" "vm" {
     
       content {
 
-        content = try(additional_unattend_content.value.content, null)!= null ? null : file(format("%s/%s", path.cwd, additional_unattend_content.value.contentfile))
+        content = try(additional_unattend_content.value.content, null) != null ? null : file(format("%s/%s", path.cwd, additional_unattend_content.value.contentfile))
         setting = additional_unattend_content.value.setting
       }  
   }
