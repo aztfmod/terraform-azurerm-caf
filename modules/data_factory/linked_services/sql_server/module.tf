487cc1bb8b1d90bc71d8f661690546f678ac6846
lolorol <lalesle@microsoft.com>
2021-11-25 07:17:13 +0000

    Add remote resource group support

--- modules/data_factory/linked_services/sql_server/module.tf
+++ modules/data_factory/linked_services/sql_server/module.tf
@@ -1,6 +1,6 @@
-resource "azurecaf_name" "dataset" {
+resource "azurecaf_name" "linked" {
   name          = var.settings.name
-  resource_type = "azurerm_data_factory" #"azurerm_data_factory_linked_service_sql_server"
+  resource_type = "azurerm_data_factory_linked_service_sql_server"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
   clean_input   = true
@@ -8,7 +8,7 @@ resource "azurecaf_name" "dataset" {
   use_slug      = var.global_settings.use_slug
 }
 resource "azurerm_data_factory_linked_service_sql_server" "linked_service_sql_server" {
-  name                     = azurecaf_name.dataset.name
+  name                     = azurecaf_name.linked.name
   resource_group_name      = var.resource_group_name
   data_factory_name        = var.data_factory_name
   description              = try(var.settings.description, null)
