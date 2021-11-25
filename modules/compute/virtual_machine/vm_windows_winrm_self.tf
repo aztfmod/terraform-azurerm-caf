cce994a8a09812fb18deb95b4992f9ed10bac032
Ricky Chew <41740185+rickychew77@users.noreply.github.com>
2021-11-25 18:43:33 +0800

    remove inherited tags into winrm self-signed cert
    there is limit of 15 tags in kv cert and when there's more than 15 tags, error below shows up 
    │ Error: keyvault.BaseClient#CreateCertificate: Failure responding to request: StatusCode=400 -- Original Error: autorest/azure: Service returned an error. Status=400 Code="BadParameter" Message="Property has invalid value\r\n"

--- modules/compute/virtual_machine/vm_windows_winrm_self.tf
+++ modules/compute/virtual_machine/vm_windows_winrm_self.tf
@@ -7,7 +7,7 @@ resource "azurerm_key_vault_certificate" "self_signed_winrm" {
 
   name         = try(format("%s-winrm-cert", azurecaf_name.windows[each.key].result), format("%s-winrm-cert", azurecaf_name.legacy[each.key].result))
   key_vault_id = local.keyvault.id
-  tags         = try(merge(var.base_tags, try(each.value.tags, null)), null)
+  tags         = try(each.value.cert_tags, null)
 
   certificate_policy {
     issuer_parameters {
