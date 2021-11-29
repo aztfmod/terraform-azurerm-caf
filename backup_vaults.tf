b6677bdcaa1850fac81f765b184ba9ccada22393
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-11-29 13:56:00 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -1,14 +1,12 @@
 module "backup_vaults" {
   source   = "./modules/backup_vault"
-#   for_each = var.backup_vaults
-  for_each = local.shared_services.backup_vaults
+  for_each = var.backup_vaults
 
   global_settings     = local.global_settings
   client_config       = local.client_config
-  settings            = each.value
-#   backup_vault        = each.value
+  backup_vault        = each.value
   diagnostics         = local.combined_diagnostics
-  identity            = try(each.value.identity, null)
+  identity            = try(each.value.identity, {})
   resource_group_name = local.resource_groups[each.value.resource_group_key].name
   location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
   base_tags           = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
