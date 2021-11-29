12b2d87369d4471c271604d3e70328b9c08e6f8a
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-11-29 16:22:58 +0200

    Update backup_vault.tf

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -1,7 +1,7 @@
-# locals {
-#   # Need to update the storage tags if the environment tag is updated with the rover command line
-#   tags = lookup(var.backup_vault, "tags", null) == null ? null : lookup(var.backup_vault.tags, "environment", null) == null ? var.backup_vault.tags : merge(lookup(var.backup_vault, "tags", {}), { "environment" : var.global_settings.environment })
-# }
+locals {
+  # Need to update the storage tags if the environment tag is updated with the rover command line
+  tags = try(var.settings.tags, null) == null ? null : try(var.settings.tags.environment, null) == null ? var.settings.tags : merge(lookup(var.settings, "tags", {}), { "environment" : var.global_settings.environment })
+}
 
 # naming convention
 resource "azurecaf_name" "bckp" {
@@ -23,7 +23,7 @@ resource "azurerm_data_protection_backup_vault" "backup_vault" {
 #   redundancy          = try(var.backup_vault.redundancy, "LocallyRedundant")
   datastore_type      = try(var.settings.datastore_type, "VaultStore")
   redundancy          = try(var.settings.redundancy, "LocallyRedundant")
-  tags                = merge(var.base_tags, local.tags)
+  tags                = try(merge(var.base_tags, local.tags), {})
   
   dynamic "identity" {
     for_each = lookup(var.settings, "enable_identity", false) == false ? [] : [1]
