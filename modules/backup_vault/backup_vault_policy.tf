11aaa9e67502a2a50deb3f5ab8a1df70ae1ab5fe
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-11-29 18:24:38 +0200

    Delete backup_vault_policy.tf

--- modules/backup_vault/backup_vault_policy.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_data_protection_backup_policy_blob_storage" "backup_vault_policy" {
-  
-  name                                             = var.settings.name
-  backup_vault_name                                = azurerm_data_protection_backup_vault.backup_vault.name
-  retention_duration                               = try(var.settings.retention_duration, "P30D")
-}
