4ad5b3eb6dc1054a84308d7467a12fe820fc1e0c
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-11-29 14:21:17 +0200

    Update main.tf

--- modules/backup_vault/main.tf
+++ modules/backup_vault/main.tf
@@ -14,8 +14,8 @@ terraform {
   required_version = ">= 0.13"
 }
 
-resource "time_sleep" "delay_create" {
-  depends_on = [azurerm_data_protection_backup_vault.backup_vault]
+# resource "time_sleep" "delay_create" {
+#   depends_on = [azurerm_data_protection_backup_vault.backup_vault]
 
-  create_duration = "30s"
-}
+#   create_duration = "30s"
+# }
