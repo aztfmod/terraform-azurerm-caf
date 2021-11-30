4b572ca0460bbbe231e2b759059c22174738f7a0
SergioMiyama <sergio.miyama@outlook.com>
2021-11-30 19:55:08 +0000

    Private Links

--- modules/networking/private_links/endpoints/private_endpoint/private_endpoint.tf
+++ modules/networking/private_links/endpoints/private_endpoint/private_endpoint.tf
@@ -19,7 +19,7 @@ resource "azurerm_private_endpoint" "pep" {
   tags                = local.tags
 
   private_service_connection {
-    name                           = format("%s-%s", var.settings.private_service_connection.name, each.key)
+    name                           = format("%s-%s", var.settings.private_service_connection.name, replace(each.key, " ", "-"))
     private_connection_resource_id = var.resource_id
     is_manual_connection           = try(var.settings.private_service_connection.is_manual_connection, false)
     subresource_names              = [each.key]
@@ -27,11 +27,18 @@ resource "azurerm_private_endpoint" "pep" {
   }
 
   dynamic "private_dns_zone_group" {
-    for_each = try(var.settings.private_dns, {}) == {} ? [] : [1]
+    for_each = can(var.settings.private_dns) ? [var.settings.private_dns] : []
 
     content {
-      name                 = var.settings.private_dns.zone_group_name
-      private_dns_zone_ids = local.private_dns_zone_ids
+      name = private_dns_zone_group.value.zone_group_name
+      private_dns_zone_ids = concat(
+        flatten([
+          for key in try(private_dns_zone_group.value.keys, []) : [
+            can(private_dns_zone_group.value.lz_key) ? var.private_dns[private_dns_zone_group.value.lz_key][key].id : var.private_dns[var.client_config.landingzone_key][key].id
+          ]
+        ]),
+        try(private_dns_zone_group.value.ids, [])
+      )
     }
   }
   lifecycle {
@@ -41,10 +48,10 @@ resource "azurerm_private_endpoint" "pep" {
   }
 }
 
-locals {
-  private_dns_zone_ids = flatten([
-    for key in try(var.settings.private_dns.keys, []) : [
-      try(var.private_dns[var.client_config.landingzone_key][key].id, var.private_dns[var.settings.private_dns.lz_key][key].id)
-    ]
-  ])
-}
+# locals {
+#   private_dns_zone_ids = flatten([
+#     for key in try(var.settings.private_dns.keys, []) : [
+#       try(var.private_dns[var.client_config.landingzone_key][key].id, var.private_dns[var.settings.private_dns.lz_key][key].id)
+#     ]
+#   ])
+# }
