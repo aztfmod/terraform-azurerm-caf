818483d47c14707413953e78b4363860ea5a6761
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-01 16:37:48 +0200

    Update backup_vault.tf

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -5,8 +5,7 @@ locals {
 
 # naming convention
 resource "azurecaf_name" "bckp" {
-#   name          = var.backup_vault.name
-  name          = var.settings.name
+  name          = var.backup_vault.name
   resource_type = "azurerm_recovery_services_vault"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
@@ -19,14 +18,12 @@ resource "azurerm_data_protection_backup_vault" "backup_vault" {
   name                = azurecaf_name.bckp.result 
   location            = var.location
   resource_group_name = var.resource_group_name
-#   datastore_type      = try(var.backup_vault.datastore_type, "VaultStore")
-#   redundancy          = try(var.backup_vault.redundancy, "LocallyRedundant")
-  datastore_type      = try(var.settings.datastore_type, "VaultStore")
-  redundancy          = try(var.settings.redundancy, "LocallyRedundant")
+  datastore_type      = try(var.backup_vault.datastore_type, "VaultStore")
+  redundancy          = try(var.backup_vault.redundancy, "LocallyRedundant")
   tags                = try(merge(var.base_tags, local.tags), {})
   
   dynamic "identity" {
-    for_each = lookup(var.settings, "enable_identity", false) == false ? [] : [1]
+    for_each = lookup(var.backup_vault, "enable_identity", false) == false ? [] : [1]
 
     content {
       type = "SystemAssigned"
