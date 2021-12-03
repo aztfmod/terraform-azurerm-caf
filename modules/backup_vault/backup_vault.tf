bc834232045d2df97c75238275a08469453f7b95
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-03 17:20:18 +0200

    Update backup_vault.tf

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -39,14 +39,14 @@ module "backup_vault_policy" {
   settings = each.value
 }
   
-module "backup_vault_instance" {
-  source   = "./backup_vault_instance"
-  for_each = try(var.backup_vault.backup_vault_instances, {})
+# module "backup_vault_instance" {
+#   source   = "./backup_vault_instance"
+#   for_each = try(var.backup_vault.backup_vault_instances, {})
 
-  vault_id           = azurerm_data_protection_backup_vault.backup_vault.id
-  location           = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  storage_account_id = module.storage_accounts[each.value.storage_account_key].id
-  backup_policy_id   = azurerm_data_protection_backup_vault.backup_vault_policy.id
-  settings           = each.value
-}
+#   vault_id           = azurerm_data_protection_backup_vault.backup_vault.id
+#   location           = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+#   storage_account_id = module.storage_accounts[each.value.storage_account_key].id
+#   backup_policy_id   = azurerm_data_protection_backup_vault.backup_vault_policy.id
+#   settings           = each.value
+# }
   
