0cfd8020ac4e7ac96ab943f9cef0748f1beae020
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-03 17:04:26 +0200

    Update variables.tf

--- modules/backup_vault/backup_vault_instance/variables.tf
+++ modules/backup_vault/backup_vault_instance/variables.tf
@@ -1,5 +1,5 @@
 variable "settings" {}
 variable "vault_id" {}
 variable "location" {}
-variable "storage_account_id"
-variable "backup_policy_id"
+variable "storage_account_id" {}
+variable "backup_policy_id" {}
