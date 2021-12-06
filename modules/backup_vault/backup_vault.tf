71abc6569a5ef99c594c0f67a51667367cb97ea6
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-06 17:36:57 +0200

    Update backup_vault.tf

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -45,9 +45,9 @@ module "backup_vault_instance" {
   
 
   vault_id           = azurerm_data_protection_backup_vault.backup_vault.id
-  location           = "South Central US"
-  storage_account_id = "/subscriptions/3f4dae7c-7085-45cc-a974-39c13903344c/resourceGroups/qs-rg-launchpad-level0-gdj/providers/Microsoft.Storage/storageAccounts/qsstlevel0skk"
-  backup_policy_id   = "/subscriptions/3f4dae7c-7085-45cc-a974-39c13903344c/resourceGroups/qs-rg-launchpad-level0-gdj/providers/Microsoft.DataProtection/backupVaults/qs-bckp-level0-gvp/backupPolicies/back-policy"
+  location           = var.location
+  storage_account_id = azurerm_storage_account.stg.id
+  backup_policy_id   = azurerm_data_protection_backup_policy_blob_storage.backup_vault_policy.id 
   settings           = each.value
 }
   
