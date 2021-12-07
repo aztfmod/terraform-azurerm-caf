1314ecc1e10777ab29bc92e0b54d9852fa31bbbc
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-07 14:06:04 +0200

    Update variables.tf

--- modules/backup_vault/backup_vault_instance/variables.tf
+++ modules/backup_vault/backup_vault_instance/variables.tf
@@ -3,9 +3,9 @@ variable "location" {}
 variable "vault_id" {}
 variable "storage_account_id" {}
 variable "backup_policy_id" {}
-# variable "global_settings" {
-#   description = "Global settings object (see module README.md)"
-# }
+variable "global_settings" {
+  description = "Global settings object (see module README.md)"
+}
 # variable "client_config" {
 #   description = "Client configuration object (see module README.md)."
 # }
