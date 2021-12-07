eee2e5cb0116c9c56b02ecb18f2b05802d05dbae
Arnaud Lheureux <arnaudlh@hotmail.com>
2021-12-07 16:37:27 +0800

    caf name for netapp

--- modules/netapp/pool/pool.tf
+++ modules/netapp/pool/pool.tf
@@ -1,17 +1,16 @@
-#TODO: Naming convention resources to be implemented
-#resource "azurecaf_name" "pool" {
-#  name          = var.settings.name
-#  prefixes      = var.global_settings.prefixes
-#  resource_type = "azurerm_netapp_pool"
-#  random_length = var.global_settings.random_length
-#  clean_input   = true
-#  passthrough   = var.global_settings.passthrough
-#  use_slug      = var.global_settings.use_slug
-#}
+resource "azurecaf_name" "pool" {
+ name          = var.settings.name
+ prefixes      = var.global_settings.prefixes
+ resource_type = "azurerm_netapp_pool"
+ random_length = var.global_settings.random_length
+ clean_input   = true
+ passthrough   = var.global_settings.passthrough
+ use_slug      = var.global_settings.use_slug
+}
 
 resource "azurerm_netapp_pool" "pool" {
   # The capacity pool name must be unique for each NetApp account
-  name                = var.settings.name
+  name                = azurecaf_name.pool.result
   account_name        = var.account_name
   resource_group_name = var.resource_group_name
   location            = var.location
@@ -19,7 +18,7 @@ resource "azurerm_netapp_pool" "pool" {
   size_in_tb          = try(var.settings.size_in_tb, 4)
   tags                = merge(var.base_tags, try(var.settings.tags, {}))
   lifecycle {
-    ignore_changes = [resource_group_name, location]
+    ignore_changes = [resource_group_name, location, name]
   }
 
 }
