7837f1f815da62635250aa9879a576d310701b7a
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2021-12-07 09:33:00 +0000

    Adding global settings

--- modules/netapp/pool/pool.tf
+++ modules/netapp/pool/pool.tf
@@ -36,5 +36,5 @@ module "volumes" {
   subnet_id           = try(var.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, var.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id)
   export_policy_rule  = try(each.value.export_policy_rule, {})
   tags                = merge(var.base_tags, try(each.value.tags, {}))
-
+  global_settings     = var.global_settings
 }
