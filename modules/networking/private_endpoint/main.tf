a6f01b728a5ae99a723a187bd179620139c88172
lolorol <lalesle@microsoft.com>
2021-12-07 08:58:32 +0000

    Fix ci private endpoints

--- modules/networking/private_endpoint/main.tf
+++ modules/networking/private_endpoint/main.tf
@@ -18,6 +18,7 @@ locals {
     var.resource_groups[var.client_config.landingzone_key][var.settings.resource_group_key],
     var.resource_groups[var.settings.lz_key][var.settings.resource_group_key],
     var.resource_groups[var.client_config.landingzone_key][var.settings.resource_group.key],
-    var.resource_groups[var.settings.resource_group.lz_key][var.settings.resource_group.key]
+    var.resource_groups[var.settings.resource_group.lz_key][var.settings.resource_group.key],
+    null
   )
 }
