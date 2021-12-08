a4c65256f8962667f2eb88298526d17092b3ddf8
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-08 16:02:17 +0200

    Delete modules/backup_vault/backup_vault_policy directory

--- modules/backup_vault/backup_vault_policy/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "id" {
-  description = "The Resource Manager ID of this Backup Vault Policy."
-  value       = azurerm_data_protection_backup_policy_blob_storage.backup_vault_policy.id
-}
--- modules/backup_vault/backup_vault_policy/variables.tf
+++
@@ -1,2 +0,0 @@
-variable "settings" {}
-variable "vault_id" {}
