ae021bf826396afb338bb6d45d57abb4e22ac70d
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-08 12:50:46 +0200

    Update backup_vault_instance.tf

--- modules/backup_vault/backup_vault_instance.tf
+++ modules/backup_vault/backup_vault_instance.tf
@@ -1,10 +1,10 @@
-resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_instance" {
-#   for_each = try (var.settings.backup_vault_policies, {})
-  for_each = try(var.backup_vault.backup_vault_policies, {})
+# resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_instance" {
+# #   for_each = try (var.settings.backup_vault_policies, {})
+#   for_each = try(var.backup_vault.backup_vault_policies, {})
   
-  name               = var.settings.name
-  vault_id           = azurerm_data_protection_backup_vault.backup_vault.id
-  location           = var.location
-  storage_account_id = var.storage_account_id 
-  backup_policy_id   = module.backup_vault_policy[each.value].id 
-}
+#   name               = var.settings.name
+#   vault_id           = azurerm_data_protection_backup_vault.backup_vault.id
+#   location           = var.location
+#   storage_account_id = var.storage_account_id 
+#   backup_policy_id   = module.backup_vault_policy[each.value].id 
+# }
