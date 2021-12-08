4f9e1da2c7784484bcc8b09342056e981097b466
lolorol <lalesle@microsoft.com>
2021-12-08 12:42:00 +0000

    Update subscription validation rule

--- modules/subscriptions/variables.tf
+++ modules/subscriptions/variables.tf
@@ -1,8 +1,9 @@
 variable "settings" {
-  validation {
-    condition     = can(var.settings.subscription_id) || (can(var.settings.billing_account_name) && (can(var.settings.enrollment_account_name) || can([var.settings.billing_profile_name, var.settings.invoice_section_name])))
-    error_message = "You need to either specify an existing subscription_id, an enrollment_account_name ( Enterprise Agreement ), or a billing_profile_name and invoice_section_name ( MCA )."
-  }
+  # Commenting as current validation rule cannot verify if the var.subscription_key == "logged_in_subscription")
+  # validation {
+  #   condition     = can(var.settings.subscription_id) || (can(var.settings.billing_account_name) && (can(var.settings.enrollment_account_name) || can([var.settings.billing_profile_name, var.settings.invoice_section_name])))
+  #   error_message = "You need to either specify an existing subscription_id, an enrollment_account_name ( Enterprise Agreement ), or a billing_profile_name and invoice_section_name ( MCA )."
+  # }
 }
 variable "subscription_key" {}
 variable "client_config" {}
