82998144a3c8fbf9e8ddd20a7dcbff0464f97737
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-08 12:27:59 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -10,6 +10,7 @@ module "backup_vaults" {
   resource_group_name = local.resource_groups[each.value.resource_group_key].name
   location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
   base_tags           = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
+  storage_account_id  = try(var.storage_accounts[try(each.value.storage_account.lz_key, var.client_config.landingzone_key)][each.value.storage_account.key].id, null)
 }
 
 output "backup_vaults" {
