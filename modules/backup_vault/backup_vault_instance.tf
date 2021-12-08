b21f960bdb866f723628153ca6077f7b5963c82a
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-08 12:37:22 +0200

    Update backup_vault_instance.tf

--- modules/backup_vault/backup_vault_instance.tf
+++ modules/backup_vault/backup_vault_instance.tf
@@ -3,7 +3,7 @@ resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_in
   for_each = try(var.backup_vault.backup_vault_policies, {})
   
   name               = var.settings.name
-  vault_id           = var.vault_id
+  vault_id           = azurerm_data_protection_backup_vault.backup_vault.id
   location           = var.location
   storage_account_id = var.storage_account_id 
   backup_policy_id   = module.backup_vault_policy[each.value].id 
