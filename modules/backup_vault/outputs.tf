fe9668e553132af4f365e02a2e7b2b7d05965451
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-08 14:56:31 +0200

    Update outputs.tf

--- modules/backup_vault/outputs.tf
+++ modules/backup_vault/outputs.tf
@@ -18,10 +18,10 @@ output "resource_group_name" {
   value       = var.resource_group_name
 }
 
-output "backup_vault_policies"{
-  description = "The backup vault policies output objects as created by the backup vault policies submodule."
-  value       = module.backup_vault_policies
-}
+# output "backup_vault_policies"{
+#   description = "The backup vault policies output objects as created by the backup vault policies submodule."
+#   value       = module.backup_vault_policies
+# }
 
 output "datastore_type" {
   description = "Output of the datastore type on the resource."
