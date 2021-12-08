7925e30e7d14bee1b1340affdd691561ee682e1b
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-08 14:49:27 +0200

    Update backup_vault.tf

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -31,10 +31,10 @@ resource "azurerm_data_protection_backup_vault" "backup_vault" {
   }
 }
 
-module "backup_vault_policies" {
-  source   = "./backup_vault_policy"
-  for_each = try(var.settings.backup_vault_policies, {})
+# module "backup_vault_policies" {
+#   source   = "./backup_vault_policy"
+#   for_each = try(var.settings.backup_vault_policies, {})
 
-  vault_id = azurerm_data_protection_backup_vault.backup_vault.id
-  settings = each.value
-}
+#   vault_id = azurerm_data_protection_backup_vault.backup_vault.id
+#   settings = each.value
+# }
