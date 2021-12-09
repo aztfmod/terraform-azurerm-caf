afabd62c96eff236e057545dfc00564fdc7836e4
Rafael <rafael.fernandez@bravent.com>
2021-12-09 15:45:27 +0100

    systemidentity added

--- modules/webapps/function_app/module.tf
+++ modules/webapps/function_app/module.tf
@@ -99,12 +99,12 @@ resource "azurerm_function_app" "function_app" {
     }
   }
 
-  dynamic "identity" {
+    dynamic "identity" {
     for_each = try(var.identity, null) == null ? [] : [1]
 
     content {
-      type         = "UserAssigned"
-      identity_ids = local.managed_identities
+      type         = var.identity.type
+      identity_ids = lower(var.identity.type) == "userassigned" ? local.managed_identities : null
     }
   }
 
