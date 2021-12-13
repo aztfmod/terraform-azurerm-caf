4a8c09efe57c1f6ae66a803a2df2df06c403ee01
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-13 14:33:52 +0200

    Update backup_vault_instance.tf

--- modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
+++ modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
@@ -1,12 +1,12 @@
 resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_instance" {
-  for_each = try(var.settings.backup_vault_instances, {})
-  depends_on = [time_sleep.delay_create]
+#   for_each = try(var.settings.backup_vault_instances, {})
+#   depends_on = [time_sleep.delay_create]
 #   depends_on = [azurerm_role_assignment.for]
   
-  name               = each.value.name
-  vault_id           = azurerm_data_protection_backup_vault.backup_vault.id
+  name               = var.settings.name
+  vault_id           = var.vault_id
   location           = var.location
-  storage_account_id = lookup(each.value, "storage_account_key") == null ? null : var.storage_accounts[each.value.storage_account_key].id
-  backup_policy_id   = azurerm_data_protection_backup_policy_blob_storage.backup_vault_policy[each.value.backup_vault_policy_key].id #module.backup_vault_policy[each.value].id 
+  storage_account_id = var.storage_account_id
+  backup_policy_id   = var.backup_policy_id
 }
 
