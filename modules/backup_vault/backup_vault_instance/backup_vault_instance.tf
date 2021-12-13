f41d2a6ec0bd1761dc58eb876c467ef2390cc6a9
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-13 18:26:17 +0200

    Update backup_vault_instance.tf

--- modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
+++ modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
@@ -3,7 +3,7 @@ resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_in
 #   depends_on = [time_sleep.delay_create]
 #   depends_on = [azurerm_role_assignment.for]
   
-  name               = var.name
+  name               = var.backup_vault_instances.name
   vault_id           = var.vault_id
   location           = var.location
   storage_account_id = var.storage_account_id
