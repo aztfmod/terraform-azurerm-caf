3878970c5d3012eb89af630d406a367ee36bda92
Ykazmin <ssi_ykazim@qbsol.comn>
2021-12-13 20:07:07 +0200

    added sensitive values for storage

--- modules/storage_account/output.tf
+++ modules/storage_account/output.tf
@@ -26,7 +26,6 @@ output "primary_blob_endpoint" {
 output "primary_access_key" {
   description = "The endpoint URL for blob storage in the primary location."
   value       = azurerm_storage_account.stg.primary_access_key
-  sensitive   = true
 }
 
 output "containers" {
@@ -66,11 +65,13 @@ output "backup_container_id" {
 
 output "primary_connection_string" {
   value = try(azurerm_storage_account.stg.primary_connection_string, null)
+  sensitive   = true
 }
 
 
 output "primary_blob_connection_string" {
   value = try(azurerm_storage_account.stg.primary_blob_connection_string, null)
+  sensitive   = true
 }
 
 #output "primary_queue_endpoint" {
