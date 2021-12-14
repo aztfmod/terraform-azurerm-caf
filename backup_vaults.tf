e6c327b4df5a9871b52c233ae9891e9834e12983
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-14 17:49:18 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -17,18 +17,18 @@ output "backup_vaults" {
   value = module.backup_vaults
 }
 
-# module "backup_vault_policies" {
-#   source   = "./modules/backup_vault/backup_vault_policy"
-#   for_each = var.backup_vaults
+module "backup_vault_policies" {
+  source   = "./modules/backup_vault/backup_vault_policy"
+  for_each = var.backup_vault_policies
   
-#   settings = each.value
-#   vault_id = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id
-#   retention_duration = try(each.value.retention_duration, "P30D")
-# }
+  settings = each.value
+  vault_id = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id
+  retention_duration = try(each.value.retention_duration, "P30D")
+}
   
-# output "backup_vault_policies" {
-#   value = module.backup_vault_policies
-# }
+output "backup_vault_policies" {
+  value = module.backup_vault_policies
+}
   
 # module "backup_vault_instances" {
 #   source   = "./modules/backup_vault/backup_vault_instance"
