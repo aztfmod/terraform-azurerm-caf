1ba6ccabe3924aca0421649085b16c46261b76eb
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-14 17:06:21 +0200

    Update main.tf

--- modules/backup_vault/main.tf
+++ modules/backup_vault/main.tf
@@ -1,9 +1,9 @@
-locals {
-  module_tag = {
-    "module" = basename(abspath(path.module))
-  }
-  tags = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
-}
+# locals {
+#   module_tag = {
+#     "module" = basename(abspath(path.module))
+#   }
+#   tags = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
+# }
 
 
 terraform {
