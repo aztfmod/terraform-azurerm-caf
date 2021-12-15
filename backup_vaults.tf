f2855d98a8659f287ffd7485a2151a5d06868ce5
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-15 16:41:29 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -1,28 +1,23 @@
-# resource "time_sleep" "delay_p" {
-#   depends_on = [azurerm_role_assignment.for]
-#   for_each = var.backup_vaults
-#   create_duration = "30s"
-# } 
 
-# module "backup_vaults" {
-#   source   = "./modules/backup_vault"
-#   depends_on = [azurerm_role_assignment.for]
-#   for_each = var.backup_vaults
+module "backup_vaults" {
+  source   = "./modules/backup_vault"
+  depends_on = [azurerm_role_assignment.for]
+  for_each = var.backup_vaults
 
-#   global_settings     = local.global_settings
-#   client_config       = local.client_config
-#   backup_vault        = each.value
-#   diagnostics         = local.combined_diagnostics
-#   identity            = try(each.value.identity, {})
-#   resource_group_name = local.resource_groups[each.value.resource_group_key].name
-#   location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-#   base_tags           = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
-# #   storage_accounts    = module.storage_accounts
-# }
+  global_settings     = local.global_settings
+  client_config       = local.client_config
+  backup_vault        = each.value
+  diagnostics         = local.combined_diagnostics
+  identity            = try(each.value.identity, {})
+  resource_group_name = local.resource_groups[each.value.resource_group_key].name
+  location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  base_tags           = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
+#   storage_accounts    = module.storage_accounts
+}
 
-# output "backup_vaults" {
-#   value = module.backup_vaults
-# }
+output "backup_vaults" {
+  value = module.backup_vaults
+}
 
 # module "backup_vault_policies" {
 #   source   = "./modules/backup_vault/backup_vault_policy"
