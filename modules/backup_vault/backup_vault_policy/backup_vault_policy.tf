5fff29b7ea853b891977c8a880acac51d2221c27
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-15 18:24:31 +0200

    Update backup_vault_policy.tf

--- modules/backup_vault/backup_vault_policy/backup_vault_policy.tf
+++ modules/backup_vault/backup_vault_policy/backup_vault_policy.tf
@@ -3,4 +3,5 @@ resource "azurerm_data_protection_backup_policy_blob_storage" "backup_vault_poli
   
   name                                             = var.settings.name
   vault_id                                         = var.vault_id
-   retention_duration = try(each.value.retention_duration, "P30D")
+  retention_duration = try(each.value.retention_duration, "P30D")
+}
