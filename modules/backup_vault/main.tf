ebe2f73a2598c03b0fe570d60bae2492f7c670e6
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-15 13:29:07 +0200

    Update main.tf

--- modules/backup_vault/main.tf
+++ modules/backup_vault/main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 0.13"
 }
 
-resource "time_sleep" "delay_p" {
-  depends_on = [azurerm_role_assignment.for]
-  create_duration = "30s"
-}
+# resource "time_sleep" "delay_p" {
+#   depends_on = [azurerm_role_assignment.for]
+#   create_duration = "30s"
+# }
