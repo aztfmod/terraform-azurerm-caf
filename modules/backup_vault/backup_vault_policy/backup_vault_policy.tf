3ff7f14974b8da0c3fbc8e15c821f8e0d1c84dae
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-20 18:55:06 +0200

    Update backup_vault_policy.tf

--- modules/backup_vault/backup_vault_policy/backup_vault_policy.tf
+++ modules/backup_vault/backup_vault_policy/backup_vault_policy.tf
@@ -1,5 +1,5 @@
 resource "azurerm_data_protection_backup_policy_blob_storage" "backup_vault_policy" {
-  for_each = try(var.settings.backup_vault_policies, {})
+#   for_each = try(var.settings.backup_vault_policies, {})
   
   name                                             = var.settings.name
   vault_id                                         = var.vault_id
