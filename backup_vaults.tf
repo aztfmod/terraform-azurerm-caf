004c517cec7359f1950cf59ce5743c4f0de1c962
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-20 15:57:38 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -19,7 +19,6 @@ output "backup_vaults" {
 
 module "backup_vault_policies" {
   source   = "./modules/backup_vault/backup_vault_policy"
-  depends_on = [azurerm_role_assignment.for]
   for_each = var.backup_vaults
   
   settings = each.value
@@ -32,7 +31,7 @@ output "backup_vault_policies" {
   
 module "backup_vault_instances" {
   source   = "./modules/backup_vault/backup_vault_instance"
-  depends_on = [azurerm_role_assignment.for]
+  depends_on = [azurerm_role_assignment.for, module.backup_vault_policies]
   for_each = var.backup_vaults
 
   settings = each.value
