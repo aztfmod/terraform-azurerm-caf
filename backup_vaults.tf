0ff7fb87b5e3a9e6ce3176dbea63fad119501812
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-20 15:52:46 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -30,20 +30,20 @@ output "backup_vault_policies" {
   value = module.backup_vault_policies
 }
   
-# module "backup_vault_instances" {
-#   source   = "./modules/backup_vault/backup_vault_instance"
-#   depends_on = [azurerm_role_assignment.for]
-#   for_each = var.backup_vaults
+module "backup_vault_instances" {
+  source   = "./modules/backup_vault/backup_vault_instance"
+  depends_on = [azurerm_role_assignment.for]
+  for_each = var.backup_vaults
 
-#   settings = each.value
-#   vault_id           = module.backup_vaults[each.key].id
-#   location           = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-#   storage_account_id = try(module.storage_accounts[each.value.storage_account_key].id, null)
-#   #  backup_policy_id   = azurerm_data_protection_backup_policy_blob_storage.backup_vault_policy[each.value.backup_vault_policy_key].id
-#   backup_policy_id = try(module.backup_vault_policies[each.value.backup_vault_policy_key].id, null)
+  settings = each.value
+  vault_id           = module.backup_vaults[each.key].id
+  location           = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  storage_account_id = try(module.storage_accounts[each.value.storage_account_key].id, null)
+  #  backup_policy_id   = azurerm_data_protection_backup_policy_blob_storage.backup_vault_policy[each.value.backup_vault_policy_key].id
+  backup_policy_id = try(module.backup_vault_policies[each.value.backup_vault_policy_key].id, null)
 
-# }
+}
 
-# output "backup_vault_instances" {
-#   value = module.backup_vault_instances
-# }
+output "backup_vault_instances" {
+  value = module.backup_vault_instances
+}
