9f5337415735b7827028144d4ab0c39be2887fab
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-23 10:43:35 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -32,7 +32,7 @@ output "backup_vault_policies" {
 module "backup_vault_instances" {
   source   = "./modules/backup_vault/backup_vault_instance"
   depends_on = [azurerm_role_assignment.for, module.backup_vault_policies]
-  for_each = var.backup_vaults
+  for_each = var.backup_vault_instances
  
   settings           = each.value
   vault_id           = module.backup_vaults[each.key].id
@@ -40,10 +40,10 @@ module "backup_vault_instances" {
     try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].location, null),
     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].location, null)
   ) : local.global_settings.regions[each.value.region]
- storage_account_id = try(module.storage_accounts[each.value.storage_account_key].id, null)
+  storage_account_id = try(module.storage_accounts[each.value.storage_account_key].id, null)
 #   backup_policy_id   = lookup(each.value.backup_vault_instances, "backup_vault_policy_key") == null ? null : module.backup_vault_policies[each.value.backup_vault_policy_key].id
-  backup_policy_id = try(each.value.backup_vault_policy.id, null)
-
+#   backup_policy_id = try(each.value.backup_vault_policy.id, null)
+  vault_id           = module.backup_vault_policies[each.key].id
 }
 
 output "backup_vault_instances" {
