eaa5dfe045a27dea1c682a0cfb5ac7317ba720b6
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-24 16:21:32 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -5,7 +5,7 @@ module "backup_vaults" {
 
   global_settings     = local.global_settings
   client_config       = local.client_config
-  backup_vault        = each.value
+  settings            = each.value
   diagnostics         = local.combined_diagnostics
   identity            = try(each.value.identity, {})
   resource_group_name = local.resource_groups[each.value.resource_group_key].name
