3c37ccba8f7e8e72befb4e8c4956453a41aec711
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-24 16:54:07 +0200

    Update backup_vault_policies.tf

--- modules/backup_vault/backup_vault_policies.tf
+++ modules/backup_vault/backup_vault_policies.tf
@@ -3,5 +3,6 @@ module "backup_vault_policies" {
   for_each = try(var.settings.backup_vault_policies, {})
   
   settings = each.value
+  name     = each.value.name
   vault_id = azurerm_data_protection_backup_vault.backup_vault.id  
 }
