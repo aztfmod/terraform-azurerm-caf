3804f10670b4bb0c48e009700b5799072b1c9245
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-24 17:13:24 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -17,18 +17,18 @@ output "backup_vaults" {
   value = module.backup_vaults
 }
 
-module "backup_vault_policies" {
-  source   = "./modules/backup_vault/backup_vault_policy"
-  depends_on = [module.backup_vaults]
-  for_each = try(var.backup_vaults, {})
+# module "backup_vault_policies" {
+#   source   = "./modules/backup_vault/backup_vault_policy"
+#   depends_on = [module.backup_vaults]
+#   for_each = try(var.backup_vaults, {})
   
-  settings = each.value
-  vault_id = module.backup_vaults[each.key].id  
-}
+#   settings = each.value
+#   vault_id = module.backup_vaults[each.key].id  
+# }
   
-output "backup_vault_policies" {
-  value = module.backup_vault_policies
-}
+# output "backup_vault_policies" {
+#   value = module.backup_vault_policies
+# }
   
 # module "backup_vault_instances" {
 #   source     = "./modules/backup_vault/backup_vault_instance"
