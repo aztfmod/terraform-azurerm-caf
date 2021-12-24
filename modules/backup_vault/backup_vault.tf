6b6a5b62617e6bb638bf47e4b86926803ba1e374
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-24 16:22:28 +0200

    Update backup_vault.tf

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -1,12 +1,12 @@
 locals {
   # Need to update the storage tags if the environment tag is updated with the rover command line
-  tags = try(var.backup_vault.tags, null) == null ? null : try(var.backup_vault.tags.environment, null) == null ? var.backup_vault.tags : merge(lookup(var.backup_vault, "tags", {}), { "environment" : var.global_settings.environment })
+  tags = try(var.settings.tags, null) == null ? null : try(var.settings.tags.environment, null) == null ? var.settings.tags : merge(lookup(var.settings, "tags", {}), { "environment" : var.global_settings.environment })
 }
 
 # naming convention
 resource "azurecaf_name" "bckp" {
   
-  name          = var.backup_vault.name
+  name          = var.settings.name
   resource_type = "azurerm_recovery_services_vault"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
@@ -19,12 +19,12 @@ resource "azurerm_data_protection_backup_vault" "backup_vault" {
   name                = azurecaf_name.bckp.result 
   location            = var.location
   resource_group_name = var.resource_group_name
-  datastore_type      = try(var.backup_vault.datastore_type, "VaultStore")
-  redundancy          = try(var.backup_vault.redundancy, "LocallyRedundant")
+  datastore_type      = try(var.settings.datastore_type, "VaultStore")
+  redundancy          = try(var.settings.redundancy, "LocallyRedundant")
   tags                = try(merge(var.base_tags, local.tags), {})
   
   dynamic "identity" {
-    for_each = lookup(var.backup_vault, "enable_identity", false) == false ? [] : [1]
+    for_each = lookup(var.settings, "enable_identity", false) == false ? [] : [1]
 
     content {
       type = "SystemAssigned"
