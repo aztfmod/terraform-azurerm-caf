9efb074a20a2a188937d649183128812395eb1a6
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-28 17:04:08 +0200

    Update variables.tf

--- modules/backup_vault/backup_vault_instance/variables.tf
+++ modules/backup_vault/backup_vault_instance/variables.tf
@@ -8,12 +8,12 @@ variable "location" {
 #   }
 }
 variable "settings" {}
-variable "vault_id" {}
-variable "storage_account_id" {
-  description = "Identifier of the storage account ID to be used."
-  type        = string
-}
-variable "backup_policy_id" {
-  description = "The ID of the backup vault policy to be used."
-  type        = string
-}
+# variable "vault_id" {}
+# variable "storage_account_id" {
+#   description = "Identifier of the storage account ID to be used."
+#   type        = string
+# }
+# variable "backup_policy_id" {
+#   description = "The ID of the backup vault policy to be used."
+#   type        = string
+# }
