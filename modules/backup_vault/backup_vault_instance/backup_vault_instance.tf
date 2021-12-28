5394baa211bc491a68ee8aaf23ed6910b2c0ed2b
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-28 18:45:49 +0200

    Update backup_vault_instance.tf

--- modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
+++ modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
@@ -1,5 +1,5 @@
 resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_instance" {
-#   for_each = try(var.settings.backup_vault_instances, {})
+  for_each = try(var.settings.backup_vault_instances, {})
   
   name               = var.settings.instance_name
   vault_id           = var.vault_id
