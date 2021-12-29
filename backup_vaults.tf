4c2104e457fc60340b727c848c266b7ca73e51e0
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-29 15:57:26 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -34,7 +34,7 @@ output "backup_vault_policies" {
 module "backup_vault_instances" {
   source     = "./modules/backup_vault/backup_vault_instance"
   depends_on = [azurerm_role_assignment.for, module.backup_vault_policies]
-  for_each   = try(var.backup_vault_instances, {})
+  for_each   = try(var.backup_vaults, {})
 
   settings = each.value
   vault_id = module.backup_vaults[each.key].id
@@ -42,10 +42,10 @@ module "backup_vault_instances" {
     try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].location, null),
     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].location, null)
   ) : local.global_settings.regions[each.value.region]
-#   storage_account_id = module.storage_accounts[each.value.storage_account_key].id
-#   backup_policy_id   = module.backup_vault_policies[each.value.backup_vault_policy_key].id
-  storage_account_id = module.storage_accounts[each.key].id
-  backup_policy_id   = module.backup_vault_policies[each.key].id
+  storage_account_id = module.storage_accounts[each.value.storage_account_key].id
+  backup_policy_id   = module.backup_vault_policies[each.value.backup_vault_policy_key].id
+#   storage_account_id = module.storage_accounts[each.key].id
+#   backup_policy_id   = module.backup_vault_policies[each.key].id
 #   storage_account_id = coalesce(
 #     try(local.combined_objects_storage_accounts[each.value.storage_account.lz_key][each.value.storage_account.key].id, null),
 #     try(local.combined_objects_storage_accounts[local.client_config.landingzone_key][each.value.storage_account.key].id, null),
