d2f937aefab0784d949e70df87c87390626d39c1
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2021-12-30 15:28:29 +0200

    Update backup_vault_policies.tf

--- modules/backup_vault/backup_vault_policies.tf
+++ modules/backup_vault/backup_vault_policies.tf
@@ -1,7 +1,7 @@
-module "backup_vault_policies" {
-  source   = "./backup_vault_policy"
-  for_each = try(var.settings.backup_vault_policies, {})
+# module "backup_vault_policies" {
+#   source   = "./backup_vault_policy"
+#   for_each = try(var.settings.backup_vault_policies, {})
   
-  settings = each.value
-  vault_id = azurerm_data_protection_backup_vault.backup_vault.id  
-}
+#   settings = each.value
+#   vault_id = azurerm_data_protection_backup_vault.backup_vault.id  
+# }
