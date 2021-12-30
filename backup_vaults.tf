e72d1526e2837df7cf85dcc5ad4be002e1d71590
Roman Burdiuzha <ssi_rburdiuzha@qbsol.com>
2021-12-30 17:50:43 +0530

    fixing issues with backup_instance

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -47,7 +47,7 @@ module "backup_vault_instances" {
     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].location, null)
   ) : local.global_settings.regions[each.value.region]
   storage_accounts = local.combined_objects_storage_accounts
-  backup_policy = local.backup_vault_policies.id[each.key]
+  backup_policy = local.backup_vault_policies.[each.key]
   #storage_account_id = module.storage_accounts[each.value.storage_account_key].id
   #backup_policy_id   = module.backup_vault_policies[each.value.backup_vault_policy_key].id
   #   storage_account_id = module.storage_accounts[each.key].id
