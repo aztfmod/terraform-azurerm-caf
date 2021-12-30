c1986cc0be159af05a16980ba9ac51c4bc3a0d01
Roman Burdiuzha <ssi_rburdiuzha@qbsol.com>
2021-12-30 17:18:46 +0530

    fixing issues with backup_instance

--- modules/backup_vault/backup_vault_policy/backup_vault_policy.tf
+++ modules/backup_vault/backup_vault_policy/backup_vault_policy.tf
@@ -1,15 +1,15 @@
-# resource "azurerm_data_protection_backup_policy_blob_storage" "backup_vault_policy" {
-#   for_each = try(var.settings.backup_vault_policies, {})
-  
-#   name               = each.value.name
-#   vault_id           = var.vault_id
-#   retention_duration = try(each.value.retention_duration, "P30D")
-# }
-
-
 resource "azurerm_data_protection_backup_policy_blob_storage" "backup_vault_policy" {
+  for_each = try(var.settings.backup_vault_policies, {})
   
-  name               = var.settings.policy_name
+  name               = each.value.name
   vault_id           = var.vault_id
-  retention_duration = try(var.settings.retention_duration, "P30D")
+  retention_duration = try(each.value.retention_duration, "P30D")
 }
+
+
+# resource "azurerm_data_protection_backup_policy_blob_storage" "backup_vault_policy" {
+  
+#   name               = var.settings.policy_name
+#   vault_id           = var.vault_id
+#   retention_duration = try(var.settings.retention_duration, "P30D")
+# }
