a16ffb6f462b70b543ce4d043b36498618df738e
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-05 16:18:26 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -49,10 +49,10 @@ module "backup_vault_instances" {
     try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].location, null),
     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].location, null)
   ) : local.global_settings.regions[each.value.region]
-  storage_accounts = module.storage_accounts
-  backup_policy = local.backup_vault_policies[each.key]
-  #storage_account_id = module.storage_accounts[each.value.storage_account_key].id
-  #backup_policy_id   = module.backup_vault_policies[each.value.backup_vault_policy_key].id
+#   storage_accounts = module.storage_accounts #Romans item
+#   backup_policy = local.backup_vault_policies[each.key] ##Romans item
+  storage_account_id = module.storage_accounts[each.value.storage_account_key].id
+  backup_policy_id   = module.backup_vault_policies[each.value.backup_vault_policy_key].id
   #   storage_account_id = module.storage_accounts[each.key].id
   #   backup_policy_id   = module.backup_vault_policies[each.key].id
   #   storage_account_id = coalesce(
