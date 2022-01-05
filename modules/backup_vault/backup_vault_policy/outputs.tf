64fff87c93d1c93b7de90ff1a5b2d8788d9d869a
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-05 13:40:31 +0200

    Update outputs.tf

--- modules/backup_vault/backup_vault_policy/outputs.tf
+++ modules/backup_vault/backup_vault_policy/outputs.tf
@@ -1,4 +1,4 @@
-output "id" {
+output "backup_vault_policies" {
   description = "The ID of the Backup Vault Policy"
-  value       = azurerm_data_protection_backup_policy_blob_storage.backup_vault_policy.id
+  value       = azurerm_data_protection_backup_policy_blob_storage.backup_vault_policy
 }
