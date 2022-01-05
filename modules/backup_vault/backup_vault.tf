e9c7b4ecf682dfed3f25158ec5e061e45c4078b8
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-05 15:40:51 +0200

    Update backup_vault.tf

--- modules/backup_vault/backup_vault.tf
+++ modules/backup_vault/backup_vault.tf
@@ -1,7 +1,7 @@
-locals {
-  # Need to update the storage tags if the environment tag is updated with the rover command line
-  tags = try(var.settings.tags, null) == null ? null : try(var.settings.tags.environment, null) == null ? var.settings.tags : merge(lookup(var.settings, "tags", {}), { "environment" : var.global_settings.environment })
-}
+# locals {
+#   # Need to update the storage tags if the environment tag is updated with the rover command line
+#   tags = try(var.settings.tags, null) == null ? null : try(var.settings.tags.environment, null) == null ? var.settings.tags : merge(lookup(var.settings, "tags", {}), { "environment" : var.global_settings.environment })
+# }
 
 # naming convention
 resource "azurecaf_name" "bckp" {
