a0a2de9fa9200fba5e3d139a0385b67a68c6616c
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-11 11:29:09 +0200

    Update variables.tf

--- modules/backup_vault/variables.tf
+++ modules/backup_vault/variables.tf
@@ -10,7 +10,7 @@ variable "location" {
 variable "global_settings" {
   description = "Global settings object (see module README.md)"
 }
-variable "resource_groups" {}
+# variable "resource_groups" {}
 variable "resource_group_name" {
   description = "(Required) The name of the resource group where to create the resource."
   type        = string
