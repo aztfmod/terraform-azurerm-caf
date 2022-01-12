0aeec4e888ffa03195f8d6fac6e7a76d5e8624dd
dimitrifc <83534599+dimitrifc@users.noreply.github.com>
2022-01-12 09:33:12 +0100

    add dependency for packer_service_principal and packer_build modules to vm and vmss

--- virtual_machines.tf
+++ virtual_machines.tf
@@ -9,7 +9,9 @@ module "virtual_machines" {
     module.keyvault_access_policies_azuread_apps,
     module.proximity_placement_groups,
     module.network_security_groups,
-    module.storage_account_blobs
+    module.storage_account_blobs,
+    module.packer_service_principal,
+    module.packer_build
   ]
   for_each = local.compute.virtual_machines
 
