717ea249312648a417166bfa2ac1d6fd2593c14f
Jor Seng <jorseng@gmail.com>
2022-01-12 06:21:33 +0000

    udpate example module

--- examples/module.tf
+++ examples/module.tf
@@ -135,6 +135,9 @@ module "example" {
   }
   messaging = {
     signalr_services = var.signalr_services
+    servicebus_namespaces = var.servicebus_namespaces
+    servicebus_topics     = var.servicebus_topics
+    servicebus_queues     = var.servicebus_queues
   }
   networking = {
     application_gateway_applications                        = var.application_gateway_applications
@@ -195,11 +198,6 @@ module "example" {
     keyvault_keys                 = var.keyvault_keys
     keyvault_certificates         = var.keyvault_certificates
     lighthouse_definitions        = var.lighthouse_definitions
-  }
-  servicebus = {
-    servicebus_namespaces = var.servicebus_namespaces
-    servicebus_topics     = var.servicebus_topics
-    servicebus_queues     = var.servicebus_queues
   }
   shared_services = {
     consumption_budgets        = var.consumption_budgets
