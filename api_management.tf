1b02ea5f5b8ca08b658c0a0018a886b95e8e8628
SergioMiyama <sergio.miyama@outlook.com>
2022-01-16 23:22:48 +0000

    fix

--- api_management.tf
+++ api_management.tf
@@ -32,8 +32,8 @@ module "api_management_api" {
   settings        = each.value
 
   api_management_name = coalesce(
-    try(local.combined_objects_api_management_api[each.value.api.lz_key][each.value.api.key].name, null),
-    try(local.combined_objects_api_management_api[local.client_config.landingzone_key][each.value.api.key].name, null),
+    try(local.combined_objects_api_management[each.value.api.lz_key][each.value.api.key].name, null),
+    try(local.combined_objects_api_management[local.client_config.landingzone_key][each.value.api.key].name, null),
     try(each.value.api.name, null)
   )
 
@@ -53,6 +53,36 @@ output "api_management_api" {
   value = module.api_management_api
 }
 
+module "api_management_logger" {
+  source   = "./modules/apim/api_management_logger"
+  for_each = local.apim.api_management_logger
+
+  global_settings = local.global_settings
+  client_config   = local.client_config
+  settings        = each.value
+
+  resource_group_name = coalesce(
+    try(local.combined_objects_resource_groups[each.value.resource_group.lz_key][each.value.resource_group.key].name, null),
+    try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group.key].name, null),
+    try(each.value.resource_group.name, null)
+  )
+
+  api_management_name = coalesce(
+    try(local.combined_objects_api_management[each.value.api_management.lz_key][each.value.api_management.key].name, null),
+    try(local.combined_objects_api_management[local.client_config.landingzone_key][each.value.api_management.key].name, null),
+    try(each.value.api_management.name, null)
+  )
+
+
+  remote_objects = {
+    resource_group       = local.combined_objects_resource_groups
+    api_management       = local.combined_objects_api_management
+    application_insights = local.combined_objects_application_insights
+  }
+}
+output "api_management_logger" {
+  value = module.api_management_logger
+}
 module "api_management_api_diagnostic" {
   source   = "./modules/apim/api_management_api_diagnostic"
   for_each = local.apim.api_management_api_diagnostic
@@ -92,36 +122,6 @@ module "api_management_api_diagnostic" {
 }
 output "api_management_api_diagnostic" {
   value = module.api_management_api_diagnostic
-}
-module "api_management_logger" {
-  source   = "./modules/apim/api_management_logger"
-  for_each = local.apim.api_management_logger
-
-  global_settings = local.global_settings
-  client_config   = local.client_config
-  settings        = each.value
-
-  resource_group_name = coalesce(
-    try(local.combined_objects_resource_groups[each.value.resource_group.lz_key][each.value.resource_group.key].name, null),
-    try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group.key].name, null),
-    try(each.value.resource_group.name, null)
-  )
-
-  api_management_name = coalesce(
-    try(local.combined_objects_api_management[each.value.api_management.lz_key][each.value.api_management.key].name, null),
-    try(local.combined_objects_api_management[local.client_config.landingzone_key][each.value.api_management.key].name, null),
-    try(each.value.api_management.name, null)
-  )
-
-
-  remote_objects = {
-    resource_group       = local.combined_objects_resource_groups
-    api_management       = local.combined_objects_api_management
-    application_insights = local.combined_objects_application_insights
-  }
-}
-output "api_management_logger" {
-  value = module.api_management_logger
 }
 module "api_management_api_operation" {
   source   = "./modules/apim/api_management_api_operation"
