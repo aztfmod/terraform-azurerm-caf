8ab3b5a8b90e4d18a7847c21e4fdd2b6806a443f
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-18 13:32:25 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -44,19 +44,36 @@ output "backup_vault_policies" {
 
 module "backup_vault_instances" {
   source     = "./modules/backup_vault/backup_vault_instance"
-  depends_on = [azurerm_role_assignment.for, module.backup_vault_policies]
-  for_each   = try(var.backup_vaults, {})
-
+  for_each   = try(var.backup_vault_instances, {}) 
+  
+  client_config       = local.client_config
+  global_settings     = local.global_settings
   settings = each.value
-  vault_id = module.backup_vaults[each.key].id
+  vault_id = module.backup_vaults[each.value.backup_vault_key].id
   location = lookup(each.value, "region", null) == null ? coalesce(
     try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].location, null),
     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].location, null)
-  ) : local.global_settings.regions[each.value.region]
-  storage_accounts = module.storage_accounts #Romans item
-  backup_policy    = local.backup_vault_policies[each.key] #Romans item
+  ) : local.global_settings.regions[each.value.region]  
+    }
+  storage_account_id = module.storage_accounts[each.value.storage_account_key].id
+  backup_policy_id = module.backup_vault_policies[each.value.backup_vault_policy_key].id
 }
+  
+# module "backup_vault_instances" {
+#   source     = "./modules/backup_vault/backup_vault_instance"
+#   depends_on = [azurerm_role_assignment.for, module.backup_vault_policies]
+#   for_each   = try(var.backup_vaults, {})
 
+#   settings = each.value
+#   vault_id = module.backup_vaults[each.key].id
+#   location = lookup(each.value, "region", null) == null ? coalesce(
+#     try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].location, null),
+#     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].location, null)
+#   ) : local.global_settings.regions[each.value.region]
+#   storage_accounts = module.storage_accounts #Romans item
+#   backup_policy    = local.backup_vault_policies[each.key] #Romans item
+# }
+  
 output "backup_vault_instances" {
   value = module.backup_vault_instances
 }
