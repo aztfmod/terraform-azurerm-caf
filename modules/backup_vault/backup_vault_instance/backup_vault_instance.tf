ac1e50a7d1528d7f1c5898bdde3a6c04873795b2
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-18 13:47:39 +0200

    Update backup_vault_instance.tf

--- modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
+++ modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
@@ -1,9 +1,18 @@
-resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_instance" {
-  for_each = try(var.settings.backup_vault_instances, {})
+# resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_instance" {
+#   for_each = try(var.settings.backup_vault_instances, {})
   
-  name               = each.value.instance_name
+#   name               = each.value.instance_name
+#   vault_id           = var.vault_id
+#   location           = var.location
+#   storage_account_id = var.storage_accounts[each.value.storage_account_key].id #Romans item
+#   backup_policy_id   = var.backup_policy.id[each.value.backup_vault_policy_key].id #Romans item
+# }
+
+resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_instance" {
+  
+  name               = var.settings.instance_name
   vault_id           = var.vault_id
   location           = var.location
-  storage_account_id = var.storage_accounts[each.value.storage_account_key].id #Romans item
-  backup_policy_id   = var.backup_policy.id[each.value.backup_vault_policy_key].id #Romans item
+  storage_account_id = var.storage_account_id
+  backup_policy_id   = var.backup_policy_id
 }
