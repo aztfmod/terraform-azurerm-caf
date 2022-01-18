850173c059d7009415d8bf014c412b6abee3a92f
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-18 14:24:46 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -54,7 +54,6 @@ module "backup_vault_instances" {
     try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].location, null),
     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].location, null)
   ) : local.global_settings.regions[each.value.region]  
-    }
   storage_account_id = module.storage_accounts[each.value.storage_account_key].id
   backup_policy_id = module.backup_vault_policies[each.value.backup_vault_policy_key].id  
 }
