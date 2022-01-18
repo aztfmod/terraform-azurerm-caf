81f38c22e992f831f44323e462f7bff754967e5e
Nick Metz <nick.metz@volocopter.com>
2022-01-18 15:07:03 +0000

    Update provider version to 2.89.0

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.88.1"
+      version = "~> 2.89.0"
       configuration_aliases = [
         azurerm.vhub
       ]
