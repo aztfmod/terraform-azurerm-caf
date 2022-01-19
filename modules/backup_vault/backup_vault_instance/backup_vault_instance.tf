8a6b667716fa8c6ea4435102a5c59870dba6fc60
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-19 11:51:25 +0200

    Update backup_vault_instance.tf

--- modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
+++ modules/backup_vault/backup_vault_instance/backup_vault_instance.tf
@@ -13,6 +13,6 @@ resource "azurerm_data_protection_backup_instance_blob_storage" "backup_vault_in
   name               = var.settings.instance_name
   vault_id           = var.vault_id
   location           = var.location
-  storage_account_id = var.storage_account_id
-  backup_policy_id   = var.backup_policy_id
+  storage_account_id = var.storage_accounts[each.value.storage_account_key].id #Romans item
+  backup_policy_id   = var.var.backup_policy.id[each.value.backup_vault_policy_key].id #Romans item
 }
