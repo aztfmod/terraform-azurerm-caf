e006da9a6fbc26d21a141cf29c00c17104fc0197
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-19 13:52:26 +0200

    Update backup_vaults.tf

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -52,10 +52,8 @@ module "backup_vault_instances" {
     try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].location, null),
     try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].location, null)
   ) : local.global_settings.regions[each.value.region]
-  storage_accounts = module.storage_accounts #Romans item
-  backup_policy = local.backup_vault_policies[each.key] #Romans item
-#   storage_account_id = lookup(each.value, "storage_account_key") == null ? null : module.storage_accounts[each.value.storage_account_key].id  
-#   backup_policy_id   = lookup(each.value, "backup_vault_policy_key") == null ? null : module.backup_vault_policies[each.value.backup_vault_policy_key].id 
+  storage_account_id = lookup(each.value, "storage_account_key") == null ? null : module.storage_accounts[each.value.storage_account_key].id  
+  backup_policy_id   = lookup(each.value, "backup_vault_policy_key") == null ? null : module.backup_vault_policies[each.value.backup_vault_policy_key].id 
 }
   
 # module "backup_vault_instances" {
