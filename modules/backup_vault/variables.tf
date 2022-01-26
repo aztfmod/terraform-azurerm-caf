8fead1b674ab345794f903e5f23ae31e3e2e8863
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-26 16:43:52 +0200

    Update variables.tf

--- modules/backup_vault/variables.tf
+++ modules/backup_vault/variables.tf
@@ -1,11 +1,6 @@
 variable "location" {
   description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
   type        = string
-  
-#   validation {
-#     condition     = contains(["southcentralus", "centralus"], var.location)
-#     error_message = "Allowed values are southcentralus, centralus."
-#   }
 }
 variable "global_settings" {
   description = "Global settings object (see module README.md)"
