8eb31254b7da97e49d7ac65f41fbc5c0d320ee77
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-01-26 16:43:14 +0200

    Update variables.tf

--- modules/backup_vault/backup_vault_instance/variables.tf
+++ modules/backup_vault/backup_vault_instance/variables.tf
@@ -1,11 +1,6 @@
 variable "location" {
   description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
   type        = string
-  
-#   validation {
-#     condition     = contains(["southcentralus", "centralus"], var.location)
-#     error_message = "Allowed values are southcentralus, centralus."
-#   }
 }
 variable "settings" {}
 variable "vault_id" {}
@@ -17,6 +12,3 @@ variable "backup_policy_id" {
   description = "The ID of the backup vault policy to be used."
   type        = string
 }
-# variable "storage_accounts" {}
-# variable "backup_policy" {}
-
