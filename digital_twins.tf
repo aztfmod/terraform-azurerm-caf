3a7b76590df918282ec5d738e7c04533777e4a1d
Rafael <rafael.fernandez@bravent.com>
2022-01-26 17:05:23 +0100

    adt eventgrid and servicebus endpoints added

--- digital_twins.tf
+++ digital_twins.tf
@@ -1,19 +1,19 @@
 # Digital Twins Instaces
-module "digital_twins_instance" {
+module "digital_twins_instances" {
   source = "./modules/iot/digital_twins/digital_twins_instance"
 
   for_each = local.iot.digital_twins_instances
 
-  resource_group_name        = local.resource_groups[each.value.resource_group_key].name
-  name            = each.value.name
-  location                   = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
-  base_tags                  = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
-  tags                       = try(each.value.tags, null)
-  global_settings            = local.global_settings  
+  resource_group_name = local.resource_groups[each.value.resource_group_key].name
+  name                = each.value.name
+  location            = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
+  base_tags           = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
+  tags                = try(each.value.tags, null)
+  global_settings     = local.global_settings
   client_config       = local.client_config
 }
 
-output "digital_twins_instances" {
+output "digital_twins_instance" {
   value = module.digital_twins_instances
 
 }
@@ -24,34 +24,49 @@ module "digital_twins_endpoint_eventhub" {
 
   for_each = local.iot.digital_twins_endpoint_eventhubs
 
-  name            = each.value.name
-  digital_twins_id  = try(each.value.lz_key, null) == null ? local.combined_objects_digital_twins_instances[local.client_config.landingzone_key][each.value.digital_twins_instance_key].id : local.combined_objects_digital_twins_instances[each.value.lz_key][each.value.digital_twins_instance_key].id
-  eventhub_primary_connection_string  = module.event_hub_auth_rules[each.value.event_hub_auth_rules_key].primary_connection_string
-  eventhub_secondary_connection_string  = module.event_hub_auth_rules[each.value.event_hub_auth_rules_key].primary_connection_string
+  name                                 = each.value.name
+  digital_twins_id                     = try(each.value.lz_key, null) == null ? local.combined_objects_digital_twins_instances[local.client_config.landingzone_key][each.value.digital_twins_instance_key].id : local.combined_objects_digital_twins_instances[each.value.lz_key][each.value.digital_twins_instance_key].id
+  eventhub_primary_connection_string   = module.event_hub_auth_rules[each.value.event_hub_auth_rules_key].primary_connection_string
+  eventhub_secondary_connection_string = module.event_hub_auth_rules[each.value.event_hub_auth_rules_key].primary_connection_string
+  global_settings     = local.global_settings
+  client_config       = local.client_config
   depends_on = [
-    module.digital_twins_instance
+    module.digital_twins_instances
   ]
 }
 
+module "digital_twins_endpoint_eventgrid" {
+  source = "./modules/iot/digital_twins/digital_twins_endpoint_eventgrid"
 
-#resource "azurerm_digital_twins_endpoint_eventhub" "example" {
-#  name                                 = "example-EH"
-#  digital_twins_id                     = azurerm_digital_twins_instance.example.id
-#  eventhub_primary_connection_string   = azurerm_eventhub_authorization_rule.example.primary_connection_string
-#  eventhub_secondary_connection_string = azurerm_eventhub_authorization_rule.example.secondary_connection_string
-#}
+  for_each = local.iot.digital_twins_endpoint_eventgrids
 
-#resource "azurerm_digital_twins_endpoint_eventgrid" "example" {
-#  name                                 = "example-EG"
-#  digital_twins_id                     = azurerm_digital_twins_instance.example.id
-#  eventgrid_topic_endpoint             = azurerm_eventgrid_topic.example.endpoint
-#  eventgrid_topic_primary_access_key   = azurerm_eventgrid_topic.example.primary_access_key
-#  eventgrid_topic_secondary_access_key = azurerm_eventgrid_topic.example.secondary_access_key
-#}
+  name                                 = each.value.name
+  digital_twins_id                     = try(each.value.lz_key, null) == null ? local.combined_objects_digital_twins_instances[local.client_config.landingzone_key][each.value.digital_twins_instance_key].id : local.combined_objects_digital_twins_instances[each.value.lz_key][each.value.digital_twins_instance_key].id
+  eventgrid_topic_endpoint             = module.eventgrid_topic[each.value.eventgrid_topic_key].endpoint
+  eventgrid_topic_primary_access_key   = module.eventgrid_topic[each.value.eventgrid_topic_key].primary_access_key
+  eventgrid_topic_secondary_access_key = module.eventgrid_topic[each.value.eventgrid_topic_key].secondary_access_key
+  global_settings     = local.global_settings
+  client_config       = local.client_config
 
-#resource "azurerm_digital_twins_endpoint_servicebus" "example" {
-#  name                                   = "example-EndpointSB"
-#  digital_twins_id                       = azurerm_digital_twins_instance.example.id
-#  servicebus_primary_connection_string   = azurerm_servicebus_topic_authorization_rule.example.primary_connection_string
-#  servicebus_secondary_connection_string = azurerm_servicebus_topic_authorization_rule.example.secondary_connection_string
-#}
+  depends_on = [
+    module.digital_twins_instances
+  ]
+}
+
+module "digital_twins_endpoint_servicebus" {
+  source = "./modules/iot/digital_twins/digital_twins_endpoint_servicebus"
+
+  for_each = local.iot.digital_twins_endpoint_servicebuses
+
+  name                                   = each.value.name
+  digital_twins_id                       = try(each.value.lz_key, null) == null ? local.combined_objects_digital_twins_instances[local.client_config.landingzone_key][each.value.digital_twins_instance_key].id : local.combined_objects_digital_twins_instances[each.value.lz_key][each.value.digital_twins_instance_key].id
+  servicebus_primary_connection_string   = module.servicebus_topic_authorization_rules[each.value.servicebus_topic_authorization_rules_key].primary_connection_string
+  servicebus_secondary_connection_string = module.servicebus_topic_authorization_rule[each.value.servicebus_topic_authorization_rules_key].secondary_connection_string
+  global_settings     = local.global_settings
+  client_config       = local.client_config
+
+  depends_on = [
+    module.digital_twins_instances
+  ]
+}
+
--- modules/iot/digital_twins/digital_twins_endpoint_servicebus/variables.tf
+++ modules/iot/digital_twins/digital_twins_endpoint_servicebus/variables.tf
@@ -1,9 +1,11 @@
 variable "global_settings" {
   description = "Global settings object (see module README.md)"
 }
+
 variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
+
 variable "name" {
   description = "(Required) Name of the Digital Twins Endpoint Eventhub"
 }
@@ -12,10 +14,10 @@ variable "digital_twins_id" {
   description = "(Required) ID of the Digital Twins"
 }
 
-variable "eventhub_primary_connection_string" {
-  description = "Event Hub primary connection string from authorization rule"
+variable "servicebus_primary_connection_string" {
+  description = "Servicebus Primary Connection String"
 }
 
-variable "eventhub_secondary_connection_string" {
-  description = "Event Hub secondary connection string from authorization rule"
+variable "servicebus_secondary_connection_string" {
+  description = "Servicebus Secondary Connection String"
 }
