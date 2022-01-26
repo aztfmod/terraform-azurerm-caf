c4a7f2bda0d03b79c8613835745cf4e9b57d29b5
Rafael <rafael.fernandez@bravent.com>
2022-01-26 13:09:05 +0100

    naming convention added to endpoint_eventhub

--- modules/iot/digital_twins/digital_twins_endpoint_eventhub/module.tf
+++ modules/iot/digital_twins/digital_twins_endpoint_eventhub/module.tf
@@ -1,5 +1,15 @@
+# naming convention
+resource "azurecaf_name" "adteh" {
+  name          = var.name
+  resource_type = "azurerm_digital_twins_endpoint_eventhub"
+  prefixes      = var.global_settings.prefixes
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
 # Per options https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/digital_twins_endpoint_eventhub
-resource "azurerm_digital_twins_endpoint_eventhub" "ev" {
+resource "azurerm_digital_twins_endpoint_eventhub" "adtehv" {
   name                                 = var.name
   digital_twins_id                     = var.digital_twins_id
   eventhub_primary_connection_string   = var.eventhub_primary_connection_string
