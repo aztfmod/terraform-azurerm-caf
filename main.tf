b3c2f86ff2698b1e8b4ebe545e473bc9c0b5d429
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-02-03 18:55:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.84.0"
+      version = "~> 2.91.0"
       configuration_aliases = [
         azurerm.vhub
       ]
