71d06c154640da9b8bec9ef958a5a5e3b035163b
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2022-02-03 18:45:55 +0200

    Update configuration.tf

--- modules/databases/mysql_flexible_server/configuration.tf
+++ modules/databases/mysql_flexible_server/configuration.tf
@@ -8,10 +8,8 @@ resource "azurerm_mysql_flexible_server_configuration" "mysql" {
   depends_on = [time_sleep.server_configuration]
   for_each   = try(var.settings.mysql_configurations, {})
 
-  name        = each.value.name
-  server_name = azurerm_mysql_flexible_server.mysql.name
-  value       = each.value.value
+  name                = each.value.name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  value               = each.value.value
   resource_group_name = var.resource_group.name
 }
-
-
