dd71ab57cced74d8305c7490c52df2e5d5c5f677
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-02-16 14:37:06 +0200

    Delete modules/networking/express_route_circuit_peerings directory

--- modules/networking/express_route_circuit_peerings/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "settings" {}
-variable "resource_group_name" {
-  description = "(Required) The name of the resource group where to create the resource."
-  type        = string
-}
-variable "express_route_circuit_name" {}
-variable "route_filter_id" {}
