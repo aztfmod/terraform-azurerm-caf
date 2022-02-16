dddaf0bffb16e0f33f6f58b2fe357413c8a5f5d2
yuri-shpik <93662208+yuri-shpik@users.noreply.github.com>
2022-02-16 14:37:29 +0200

    Delete modules/networking/network_profiles directory

--- modules/networking/network_profiles/output.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = azurerm_network_profile.netprof.id
-}
-
-output "container_network_interface_ids" {
-  value = azurerm_network_profile.netprof.container_network_interface_ids
-}
