85d89b84ab1378ca65c1a59b6637a238c96af244
hyperfocus1337 <hyperfocus@hotmail.com>
2022-02-18 12:09:06 +0100

    Remove strings, certificateStoreName is required

--- modules/compute/virtual_machine_scale_set_extensions/key_vault.tf
+++ modules/compute/virtual_machine_scale_set_extensions/key_vault.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_machine_scale_set_extension" "keyvault" {
   settings = jsonencode({
     "secretsManagementSettings" : {
       "pollingIntervalInS" : try(var.extension.secretsManagementSettings.pollingIntervalInS, "3600")
-      "certificateStoreName" : try(var.extension.secretsManagementSettings.certificateStoreName, "")
+      "certificateStoreName" : try(var.extension.secretsManagementSettings.certificateStoreName)
       "linkOnRenewal" : try(var.extension.secretsManagementSettings.linkOnRenewal, false)
       "certificateStoreLocation" : try(var.extension.secretsManagementSettings.certificateStoreLocation, "")
       "requireInitialSync" : try(var.extension.secretsManagementSettings.requireInitialSync, true)
