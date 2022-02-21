970efb18e45cdfad5aa9700d9dd1ef08a7f54e81
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2022-02-21 06:18:02 +0000

    Update for azurerm_virtual_desktop_host_pool_registration_info

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.96"
+      version = "~> 2.97"
       configuration_aliases = [
         azurerm.vhub
       ]
