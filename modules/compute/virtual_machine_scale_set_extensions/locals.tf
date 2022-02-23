7a204959be44355b636873b420a5593a5b5fbf1b
hyperfocus1337 <hyperfocus@hotmail.com>
2022-02-23 11:37:40 +0100

    Remove separate locals.tf

--- modules/compute/virtual_machine_scale_set_extensions/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  managed_local_identity  = try(var.managed_identities[var.client_config.landingzone_key][var.extension.managed_identity_key].principal_id, "")
-  managed_remote_identity = try(var.managed_identities[var.extension.lz_key][var.extension.managed_identity_key].principal_id, "")
-  provided_identity       = try(var.extension.managed_identity_id, "")
-  managed_identity        = try(coalesce(local.managed_local_identity, local.managed_remote_identity, local.provided_identity), "")
-}
