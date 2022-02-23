4670e4f6fa91fa787b1562d8425e42801228dfaa
onlyillusion <onlyilussion@gmail.com>
2022-02-23 18:23:24 +0000

    Fix azurecaf name

--- modules/databases/mysql_flexible_server/database.tf
+++ modules/databases/mysql_flexible_server/database.tf
@@ -4,12 +4,23 @@ resource "time_sleep" "database_configuration" {
   create_duration = "120s"
 }
 
+resource "azurecaf_name" "mysql_flexible_database" {
+  for_each = var.settings.mysql_databases
+
+  name          = each.value.name
+  resource_type = "azurerm_mysql_flexible_server_database"
+  prefixes      = var.global_settings.prefixes
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
 
 resource "azurerm_mysql_flexible_database" "mysql" {
   depends_on = [time_sleep.database_configuration]
   for_each   = try(var.settings.mysql_databases, {})
 
-  name        = each.value.name
+  name        = azurecaf_name.mysql_flexible_database[each.key].result
   server_name = azurerm_mysql_flexible_server.mysql.name
   collation   = try(each.value.collation, "utf8_unicode_ci")
   charset     = try(each.value.charset, "utf8")
