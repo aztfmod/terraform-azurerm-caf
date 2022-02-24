1fe6e648a7d562a8abf76d6ac83fbbc6fed3193f
hyperfocus1337 <hyperfocus@hotmail.com>
2022-02-24 09:10:19 +0100

    Dynamic os type with local

--- modules/compute/virtual_machine_scale_set_extensions/application_health.tf
+++ modules/compute/virtual_machine_scale_set_extensions/application_health.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_machine_scale_set_extension" "HealthExtension" {
   for_each                     = var.extension_name == "microsoft_azure_health_extension" ? toset(["enabled"]) : toset([])
   name                         = "HealthExtension"
   publisher                    = "Microsoft.ManagedServices"
-  type                         = "extensions"
+  type                         = local.application_health_extension_type
   virtual_machine_scale_set_id = var.virtual_machine_scale_set_id
   type_handler_version         = try(var.extension.type_handler_version, "1.0")
   auto_upgrade_minor_version   = try(var.extension.auto_upgrade_minor_version, true)
@@ -22,4 +22,8 @@ resource "azurerm_virtual_machine_scale_set_extension" "HealthExtension" {
     "numberOfProbes" : try(var.extension.settings.requestPath, "1.0")
     }
   )
+}
+
+locals {
+  application_health_extension_type = var.virtual_machine_scale_set_os_type == "linux" ? "ApplicationHealthLinux" : "ApplicationHealthWindows"
 }
