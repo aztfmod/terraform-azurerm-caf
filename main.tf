58c844137f09f035764564a70c8ee2fb8b8fba48
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2022-02-25 04:34:08 +0000

    Will 2.98 be the one?

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.97"
+      version = "~> 2.98"
       configuration_aliases = [
         azurerm.vhub
       ]
