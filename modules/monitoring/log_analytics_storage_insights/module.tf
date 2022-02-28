c0b8e8d8ee1892a9e2bdce10a09e8a6d5ba4a0e3
SergioMiyama <76666459+SergioMiyama@users.noreply.github.com>
2022-02-28 04:34:08 +0000

    Correct Naming Provider Added

--- modules/monitoring/log_analytics_storage_insights/module.tf
+++ modules/monitoring/log_analytics_storage_insights/module.tf
@@ -1,7 +1,7 @@
 
 resource "azurecaf_name" "lasi" {
   name          = var.settings.name
-  resource_type = "azurerm_data_factory" #"azurerm_log_analytics_storage_insights"
+  resource_type = "azurerm_log_analytics_storage_insights"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
   clean_input   = true
