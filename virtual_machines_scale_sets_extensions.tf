36f27fcb4b08e39b2163ce1d768576ff1d9b3f23
SergioMiyama <76666459+SergioMiyama@users.noreply.github.com>
2022-03-03 11:44:23 +0000

    VMSS Extensions

--- virtual_machines_scale_sets_extensions.tf
+++ virtual_machines_scale_sets_extensions.tf
@@ -29,3 +29,33 @@ module "vmss_extension_microsoft_azure_domainjoin" {
   extension_name               = "microsoft_azure_domainJoin"
   keyvaults                    = local.combined_objects_keyvaults
 }
+
+
+module "vmss_extension_microsoft_monitoring_agent" {
+  source     = "./modules/compute/virtual_machine_scale_set_extensions"
+  for_each = {
+    for key, value in try(local.compute.virtual_machine_scale_sets, {}) : key => value
+    if try(value.virtual_machine_scale_set_extensions.microsoft_monitoring_agent, null) != null
+  }
+
+  client_config                     = local.client_config
+  virtual_machine_scale_set_id      = module.virtual_machine_scale_sets[each.key].id
+  extension                         = each.value.virtual_machine_scale_set_extensions.microsoft_monitoring_agent
+  extension_name                    = "microsoft_monitoring_agent"
+  virtual_machine_scale_set_os_type = module.virtual_machine_scale_sets[each.key].os_type
+  log_analytics_workspaces          = local.combined_objects_log_analytics
+}
+
+module "vmss_extension_dependency_agent" {
+  source     = "./modules/compute/virtual_machine_scale_set_extensions"
+  for_each = {
+    for key, value in try(local.compute.virtual_machine_scale_sets, {}) : key => value
+    if try(value.virtual_machine_scale_set_extensions.dependency_agent, null) != null
+  }
+  
+  client_config                     = local.client_config
+  virtual_machine_scale_set_id      = module.virtual_machine_scale_sets[each.key].id
+  extension                         = each.value.virtual_machine_scale_set_extensions.dependency_agent
+  extension_name                    = "dependency_agent"
+  virtual_machine_scale_set_os_type = module.virtual_machine_scale_sets[each.key].os_type
+}
