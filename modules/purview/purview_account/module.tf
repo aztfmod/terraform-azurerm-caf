271fca4963674ce8b0e76c1d02a491b1dbed3d52
Arnaud Lheureux <arnaudlh@hotmail.com>
2022-03-03 00:12:27 +0000

    Update with resource type for provider

--- modules/purview/purview_account/module.tf
+++ modules/purview/purview_account/module.tf
@@ -1,6 +1,6 @@
 resource "azurecaf_name" "pva" {
   name          = var.settings.name
-  resource_type = "azurerm_data_factory" #"azurerm_purview_account"
+  resource_type = "azurerm_purview_account"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
   clean_input   = true
