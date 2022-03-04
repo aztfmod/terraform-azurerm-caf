ae23a73f3a68db41ff868f6bbc9e84650785c402
hyperfocus1337 <hyperfocus@hotmail.com>
2022-03-04 12:51:57 +0100

    Add module to new location

--- virtual_machines_scale_sets_extensions.tf
+++ virtual_machines_scale_sets_extensions.tf
@@ -59,3 +59,20 @@ module "vmss_extension_dependency_agent" {
   extension_name                    = "dependency_agent"
   virtual_machine_scale_set_os_type = module.virtual_machine_scale_sets[each.key].os_type
 }
+
+module "vmss_extension_keyvault_extension" {
+  source = "./modules/compute/virtual_machine_scale_set_extensions"
+
+  depends_on = [module.example]
+
+  for_each = {
+    for key, value in try(var.virtual_machine_scale_sets, {}) : key => value
+    if try(value.virtual_machine_scale_set_extensions.microsoft_azure_keyvault, null) != null
+  }
+
+  client_config                = module.example.client_config
+  virtual_machine_scale_set_id = module.example.virtual_machine_scale_sets[each.key].id
+  extension                    = each.value.virtual_machine_scale_set_extensions.microsoft_azure_keyvault
+  extension_name               = "microsoft_azure_keyvault"
+  managed_identities           = tomap({ (var.landingzone.key) = module.example.managed_identities })
+}
