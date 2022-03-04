cf148742e66a06dbf951fbbe65d16bdc2874be7c
Arnaud Lheureux <arnaudlh@hotmail.com>
2022-03-04 09:54:32 +0000

    Refactor function

--- function_app.tf
+++ function_app.tf
@@ -8,12 +8,8 @@ module "function_apps" {
   dynamic_app_settings = try(each.value.dynamic_app_settings, {})
   app_settings         = try(each.value.app_settings, null)
   combined_objects     = local.dynamic_app_settings_combined_objects
-  resource_group_name = coalesce(
-    try(local.combined_objects_resource_groups[each.value.resource_group.lz_key][each.value.resource_group.key].name, null),
-    try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group.key].name, null),
-    try(local.combined_objects_resource_groups[local.client_config.landingzone_key][each.value.resource_group_key].name, null),
-    try(each.value.resource_group.name, null)
-  )
+  resource_group_name  = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(local.client_config.landingzone_key, each.value.resource_group.lz_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+
   location                   = lookup(each.value, "region", null) == null ? local.resource_groups[each.value.resource_group_key].location : local.global_settings.regions[each.value.region]
   app_service_plan_id        = try(each.value.lz_key, null) == null ? local.combined_objects_app_service_plans[local.client_config.landingzone_key][each.value.app_service_plan_key].id : local.combined_objects_app_service_plans[each.value.lz_key][each.value.app_service_plan_key].id
   settings                   = each.value.settings
@@ -28,8 +24,9 @@ module "function_apps" {
   #                 null
   #                 )
   global_settings = local.global_settings
-  base_tags       = try(local.global_settings.inherit_tags, false) ? local.resource_groups[each.value.resource_group_key].tags : {}
-  tags            = try(each.value.tags, null)
+  base_tags       = try(local.global_settings.inherit_tags, false) ? local.combined_objects_resource_groups[try(local.client_config.landingzone_key, each.value.resource_group.lz_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags : {}
+
+  tags = try(each.value.tags, null)
 
   remote_objects = {
     subnets = try(local.combined_objects_networking[try(each.value.settings.lz_key, local.client_config.landingzone_key)][each.value.settings.vnet_key].subnets, null)
