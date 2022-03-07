d7772caf8832f9a8955d94da57dcdf8ac10f13ce
Arnaud Lheureux <arnaudlh@hotmail.com>
2022-03-07 03:50:29 +0000

    Update for storage_container locator

--- storage_container.tf
+++ storage_container.tf
@@ -3,13 +3,7 @@ module "storage_containers" {
   for_each = local.storage.storage_containers
 
   settings = each.value
-
-  storage_account_name = coalesce(
-    try(local.combined_objects_storage_accounts[each.value.storage_account.lz_key][each.value.storage_account.key].name, null),
-    try(local.combined_objects_storage_accounts[local.client_config.landingzone_key][each.value.storage_account.key].name, null),
-    try(each.value.storage_account.name, null)
-  )
-
+  storage_account_name = can(each.value.storage_account.name) ? each.value.storage_account.name : local.combined_objects_storage_accounts[try(each.value.storage_account.lz_key,local.client_config.landingzone_key)][try(each.value.storage_account.key,each.value.storage_account_key)].name
 }
 output "storage_containers" {
   value = module.storage_containers
