bbf39fa1f8f636df4c2bca6cb4a5165f3851f764
Arnaud Lheureux <arnaudlh@hotmail.com>
2022-03-08 02:25:59 +0000

    Fix refactoring regression on dns_zones

--- networking_dns_zones.tf
+++ networking_dns_zones.tf
@@ -28,9 +28,9 @@ module "dns_zone_records" {
 
   base_tags           = {}
   client_config       = local.client_config
-  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+  resource_group_name = can(each.value.dns_zone.resource_group_name) ? each.value.dns_zone.resource_group_name : local.combined_objects_dns_zones[try(each.value.dns_zone.lz_key, local.client_config.landingzone_key)][each.value.dns_zone.key].resource_group_name
   records             = each.value.records
-  zone_name           = try(each.value.dns_zone.name, null) == null ? local.combined_objects_dns_zones[lookup(each.value.dns_zone, "lz_key", local.client_config.landingzone_key)][each.value.dns_zone.key].name : each.value.dns_zone.name
+  zone_name           = can(each.value.dns_zone.name) ? each.value.dns_zone.name : local.combined_objects_dns_zones[try(each.value.dns_zone.lz_key, local.client_config.landingzone_key)][each.value.dns_zone.key].name
 
   resource_ids = {
     public_ip_addresses = local.combined_objects_public_ip_addresses
