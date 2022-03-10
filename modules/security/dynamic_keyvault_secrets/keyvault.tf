3f7a0ebf8b05caced759781758e9f91d1dacd966
Anas Almohana <anas.almohana@tietoevry.com>
2022-03-10 08:46:15 +0100

    add dynamic value to dynamic kv secret

--- modules/security/dynamic_keyvault_secrets/keyvault.tf
+++ modules/security/dynamic_keyvault_secrets/keyvault.tf
@@ -15,7 +15,7 @@ module "secret_value" {
   source = "./secret"
   for_each = {
     for key, value in var.settings : key => value
-    if try(value.value, null) != null && try(value.value, null) != ""
+    if try(value.value, null) != null && try(value.value, null) != "" && try(value.value, null) != "dynamic"
   }
 
   name        = each.value.secret_name
@@ -34,4 +34,17 @@ module "secret_immutable" {
   value = can(each.value.output_key) && (can(each.value.resource_key) || can(each.value.attribute_key)) ? lookup(lookup(var.objects[each.value.output_key], try(each.value.resource_key, ""), var.objects[each.value.output_key]), each.value.attribute_key, null) : each.value.value
   # for future generations: double lookup because each.value.resource_key is optional
   keyvault_id = var.keyvault.id
+}
+
+module "secret_dynamic" {
+  source = "./secret_dynamic"
+  for_each = {
+    for key, value in var.settings : key => value
+    if try(value.value, null) == "dynamic"
+  }
+
+  name        = each.value.secret_name
+  value       = each.value.value
+  keyvault_id = var.keyvault.id
+  config      = each.value.config
 }
