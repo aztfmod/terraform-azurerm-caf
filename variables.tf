637be80ba58d00e537ab205fe3b965854a271c26
SergioMiyama <76666459+SergioMiyama@users.noreply.github.com>
2022-03-10 08:12:54 +0000

    Fixes

--- variables.tf
+++ variables.tf
@@ -334,9 +334,9 @@ variable "keyvault_certificate_issuers" {
 #   default = {}
 # }
 
-# variable "app_config" {
-#   default = {}
-# }
+variable "app_config" {
+  default = {}
+}
 
 # variable "local_network_gateways" {
 #   default = {}
