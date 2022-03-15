21402d4ae7e1be783db37b02ca4dc3b8c2300528
Antony Croft <antony.croft@irwinmitchell.com>
2022-03-15 09:54:23 +0000

    No azurecaf naming scheme for logic apps standard, so have to use logic app workflow instead

--- modules/logic_app/standard/module.tf
+++ modules/logic_app/standard/module.tf
@@ -1,6 +1,6 @@
 resource "azurecaf_name" "logic_app_standard_name" {
   name          = var.settings.name
-  resource_type = "azurerm_logic_app_standard"
+  resource_type = "azurerm_logic_app_workflow" #No azurecaf name for standard logic apps yet :-/
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
   clean_input   = true
