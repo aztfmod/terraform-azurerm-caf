c08b5d3054b7bc511946a63a449dde921076c2a1
Jor Seng <jorseng@gmail.com>
2022-03-16 11:20:15 +0000

    add test

--- modules/monitoring/monitor_autoscale_settings/main.tf
+++ modules/monitoring/monitor_autoscale_settings/main.tf
@@ -5,3 +5,15 @@ terraform {
     }
   }
 }
+
+locals {
+  target_resource_id = coalesce(
+    try(var.remote_objects.virtual_machine_scale_sets[var.client_config.landingzone_key][var.settings.vmss_key].id,null), # for backward compatibility
+
+    try(var.remote_objects.virtual_machine_scale_sets[var.settings.target_resource.lz_key][var.settings.target_resource.vmss_key].id,null),
+    try(var.remote_objects.virtual_machine_scale_sets[var.client_config.landingzone_key][var.settings.target_resource.vmss_key].id,null),
+    
+    try(var.remote_objects.app_service_plans[var.settings.target_resource.lz_key][var.settings.target_resource.app_service_plan_key].id,null),
+    try(var.remote_objects.app_service_plans[var.client_config.landingzone_key][var.settings.target_resource.app_service_plan_key].id,null),
+  )
+}
