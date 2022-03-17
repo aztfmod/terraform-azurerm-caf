8b664a03af5d02e11be7eb56a65e961bd65e42ba
Arnaud Lheureux <arnaudlh@hotmail.com>
2022-03-17 11:25:33 +0000

    Update ACR and MSSQL composition

--- modules/compute/container_registry/private_endpoint.tf
+++ modules/compute/container_registry/private_endpoint.tf
@@ -12,10 +12,5 @@ module "private_endpoint" {
   resource_group_name = var.resource_groups[try(each.value.resource_group.lz_key, var.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
   resource_id         = azurerm_container_registry.acr.id
   settings            = each.value
-
-  subnet_id = coalesce(
-    try(each.value.subnet_id, null),
-    try(var.vnets[var.client_config.landingzone_key][each.value.vnet_key].subnets[each.value.subnet_key].id, null),
-    try(var.vnets[each.value.lz_key][each.value.vnet_key].subnets[each.value.subnet_key].id, null)
-  )
+  subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : var.vnets[try(each.value.lz_key,var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
 }
