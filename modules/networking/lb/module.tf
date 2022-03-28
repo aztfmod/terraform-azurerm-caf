a839e6dbbba45afa4951abd2f59bcfe3e7ae713e
LaurentLesle <lalesle@microsoft.com>
2022-03-28 08:40:49 +0000

    Update load balancer

--- modules/networking/lb/module.tf
+++ modules/networking/lb/module.tf
@@ -17,25 +17,18 @@ resource "azurerm_lb" "lb" {
   dynamic "frontend_ip_configuration" {
     for_each = try(var.settings.frontend_ip_configuration, null) != null ? [var.settings.frontend_ip_configuration] : []
     content {
-      name              = try(frontend_ip_configuration.value.name, null)
-      availability_zone = try(frontend_ip_configuration.value.availability_zone, null)
-      subnet_id = try(
-        var.remote_objects.virtual_network[frontend_ip_configuration.value.subnet.lz_key][frontend_ip_configuration.value.subnet.vnet_key].subnets[frontend_ip_configuration.value.subnet.key].id,
-        var.remote_objects.virtual_network[var.client_config.landingzone_key][frontend_ip_configuration.value.subnet.vnet_key].subnets[frontend_ip_configuration.value.subnet.key].id,
-        frontend_ip_configuration.value.subnet.id,
-        null
-      )
+      name                                               = try(frontend_ip_configuration.value.name, null)
       gateway_load_balancer_frontend_ip_configuration_id = try(frontend_ip_configuration.value.gateway_load_balancer_frontend_ip_configuration_id, null)
       private_ip_address                                 = try(frontend_ip_configuration.value.private_ip_address, null)
       private_ip_address_allocation                      = try(frontend_ip_configuration.value.private_ip_address_allocation, null)
       private_ip_address_version                         = try(frontend_ip_configuration.value.private_ip_address_version, null)
-      public_ip_address_id = try(
-        var.remote_objects.public_ip_addresses[frontend_ip_configuration.value.public_ip_address.lz_key][frontend_ip_configuration.value.public_ip_address.key].id,
-        var.remote_objects.public_ip_addresses[var.client_config.landingzone_key][frontend_ip_configuration.value.public_ip_address.key].id,
-        frontend_ip_configuration.value.public_ip_address.id,
-        null
-      )
-      public_ip_prefix_id = try(frontend_ip_configuration.value.public_ip_prefix_id, null)
+      public_ip_prefix_id                                = try(frontend_ip_configuration.value.public_ip_prefix_id, null)
+      zones                                              = can(frontend_ip_configuration.value.zones) ? frontend_ip_configuration.value.zones : try(frontend_ip_configuration.value.availability_zone, null)
+      # TODO: availability_zone kept for smooth migration to 3.0
+
+      public_ip_address_id = can(frontend_ip_configuration.value.public_ip_address.id) || can(frontend_ip_configuration.value.public_ip_address.key) ? try(frontend_ip_configuration.value.public_ip_address.id, var.remote_objects.public_ip_addresses[try(frontend_ip_configuration.value.public_ip_address.lz_key, var.client_config.landingzone_key)][frontend_ip_configuration.value.public_ip_address.key].id) : null
+      subnet_id            = can(frontend_ip_configuration.value.subnet.id) || can(frontend_ip_configuration.value.subnet.key) ? try(frontend_ip_configuration.value.subnet.id, var.remote_objects.virtual_network[try(frontend_ip_configuration.value.subnet.lz_key, var.client_config.landingzone_key)][frontend_ip_configuration.value.subnet.vnet_key].subnets[frontend_ip_configuration.value.subnet.key].id) : null
+
     }
   }
   sku      = try(var.settings.sku, null)
