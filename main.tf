388673dcfcac761501923eac0d409bc9b13cafda
Cheng Wang <chengwang@microsoft.com>
2022-04-06 17:05:10 +1000

    update azuread provider to 2.0.0 to support argument

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 1.4.0"
+      version = "~> 2.0.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
