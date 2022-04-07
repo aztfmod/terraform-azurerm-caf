ab81f6de3ab0d315de6bb33301ca14bebacb9234
Hamad Riaz <hriaz@mobizinc.com>
2022-04-06 18:12:04 -0700

    Added try block

--- modules/storage_account/file_share/file_share.tf
+++ modules/storage_account/file_share/file_share.tf
@@ -5,14 +5,16 @@ resource "azurerm_storage_share" "fs" {
   name                 = var.settings.name
   storage_account_name = var.storage_account_name
   quota                = try(var.settings.quota, null)
-  enabled_protocol     = try(var.settings.enabled_protocol, null)
   metadata             = try(var.settings.metadata, null)
-    dynamic "acl" {
+  enabled_protocol     = try(var.settings.enabled_protocol, "SMB")
+
+  dynamic "acl" {
     for_each = try(var.settings.acl, {})
     content {
       id = acl.value.id
       
       dynamic "access_policy" {
+        for_each = try(var.settings.access_policy, {})
         content {
           permissions = access_policy.value.permissions
           start = try(access_policy.value.start, null)
@@ -27,10 +29,9 @@ resource "azurerm_storage_share" "fs" {
 resource "azurerm_backup_protected_file_share" "fs_backup" {
   for_each = try(var.settings.backups, null) != null ? toset(["enabled"]) : toset([])
 
-  resource_group_name       = try(var.recovery_vault.resource_group_name, var.resource_group_name)
+  resource_group_name       = var.recovery_vault.resource_group_name
   recovery_vault_name       = var.recovery_vault.name
   source_storage_account_id = var.storage_account_id
   source_file_share_name    = azurerm_storage_share.fs.name
   backup_policy_id          = var.recovery_vault.backup_policies.file_shares[var.settings.backups.policy_key].id
 }
-
