5348de02b6205b16c13f2caa6ba5fc0d5ea24b17
Simon Schneider <simon.schneider@volocopter.com>
2022-04-20 17:23:19 +0000

    Add managed disk support for azure backup vault

---
+++ modules/backup_vault/backup_vault_policy_disk/backup_vault_policy.tf
@@ -0,0 +1,19 @@
+resource "azurerm_data_protection_backup_policy_disk" "backup_vault_policy" {
+  name     = var.settings.policy_name
+  vault_id = var.vault_id
+
+  backup_repeating_time_intervals = var.settings.backup_repeating_time_intervals
+  default_retention_duration      = try(var.settings.retention_duration, "P30D")
+
+  dynamic "retention_rule" {
+    for_each = try(var.settings.retention_rules, {})
+    content {
+      name     = retention_rule.value.name
+      duration = retention_rule.value.duration
+      priority = retention_rule.value.priority
+      criteria {
+        absolute_criteria = try(retention_rule.value.absolute_criteria, null)
+      }
+    }
+  }
+}
