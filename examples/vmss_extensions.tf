485da316e3bc8c71c44e02008aa17ce33f2cb26a
tschwarz01 <thosch@microsoft.com>
2022-04-21 14:50:53 -0500

    Add Data Factory Self-Hosted Runtime via CustomScriptExtension for Virtual Machine Scale Sets

--- examples/vmss_extensions.tf
+++ examples/vmss_extensions.tf
@@ -32,3 +32,24 @@ module "vmss_extension_custom_scriptextension" {
   storage_accounts                  = tomap({ (var.landingzone.key) = module.example.storage_accounts })
   virtual_machine_scale_set_os_type = module.example.virtual_machine_scale_sets[each.key].os_type
 }
+
+
+module "vmss_extension_custom_script_data_factory_self_hosted_runtime" {
+  source     = "../modules/compute/virtual_machine_scale_set_extensions"
+  depends_on = [module.example]
+
+  for_each = {
+    for key, value in try(var.virtual_machine_scale_sets, {}) : key => value
+    if try(value.virtual_machine_scale_set_extensions.data_factory_self_hosted_integration_runtime, null) != null
+  }
+
+  client_config                     = module.example.client_config
+  virtual_machine_scale_set_id      = module.example.virtual_machine_scale_sets[each.key].id
+  extension                         = each.value.virtual_machine_scale_set_extensions.data_factory_self_hosted_integration_runtime
+  extension_name                    = "data_factory_self_hosted_integration_runtime"
+  managed_identities                = tomap({ (var.landingzone.key) = module.example.managed_identities })
+  storage_accounts                  = tomap({ (var.landingzone.key) = module.example.storage_accounts })
+  virtual_machine_scale_set_os_type = module.example.virtual_machine_scale_sets[each.key].os_type
+  keyvaults                         = tomap({ (var.landingzone.key) = module.example.keyvaults })
+  keyvault_id                       = module.example.keyvaults["kv1"].id
+}
