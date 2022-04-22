9accae8e08c2e6cd2b3b356a699ed60f804b1cd0
Arnaud Lheureux <arnaudlh@hotmail.com>
2022-04-22 00:43:04 +0000

    Additionnal refactoring

--- modules/compute/virtual_machine_scale_set/vmss_linux.tf
+++ modules/compute/virtual_machine_scale_set/vmss_linux.tf
@@ -292,13 +292,9 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss_autoscaled" {
       network_security_group_id     = try(network_interface.value.network_security_group_id, null)
 
       ip_configuration {
-        name    = azurecaf_name.linux_nic[network_interface.key].result
-        primary = try(network_interface.value.primary, false)
-        subnet_id = coalesce(
-          try(network_interface.value.subnet_id, null),
-          try(var.vnets[var.client_config.landingzone_key][network_interface.value.vnet_key].subnets[network_interface.value.subnet_key].id, null),
-          try(var.vnets[network_interface.value.lz_key][network_interface.value.vnet_key].subnets[network_interface.value.subnet_key].id, null)
-        )
+        name                                         = azurecaf_name.linux_nic[network_interface.key].result
+        primary                                      = try(network_interface.value.primary, false)
+        subnet_id                                    = can(network_interface.value.subnet_id) ? network_interface.value.subnet_id : var.vnets[try(network_interface.value.lz_key, var.client_config.landingzone_key)][network_interface.value.vnet_key].subnets[network_interface.value.subnet_key].id
         load_balancer_backend_address_pool_ids       = try(local.load_balancer_backend_address_pool_ids, null)
         application_gateway_backend_address_pool_ids = try(local.application_gateway_backend_address_pool_ids, null)
         application_security_group_ids               = try(local.application_security_group_ids, null)
