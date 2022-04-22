e4fc7012b66960d16b8cc36ebe1ce941ca0c1532
Simon Schneider <simon.schneider@volocopter.com>
2022-04-22 14:04:17 +0000

    remove defaults

--- modules/backup_vault/backup_vault_policy_disk/backup_vault_policy.tf
+++ modules/backup_vault/backup_vault_policy_disk/backup_vault_policy.tf
@@ -3,7 +3,7 @@ resource "azurerm_data_protection_backup_policy_disk" "backup_vault_policy" {
   vault_id = var.vault_id
 
   backup_repeating_time_intervals = var.settings.backup_repeating_time_intervals
-  default_retention_duration      = try(var.settings.retention_duration, "P30D")
+  default_retention_duration      = var.settings.retention_duration
 
   dynamic "retention_rule" {
     for_each = try(var.settings.retention_rules, {})
