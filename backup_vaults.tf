b5fd9c24f9193ed7802678c8c8e47ff0110e5b26
Simon Schneider <simon.schneider@volocopter.com>
2022-04-26 12:55:29 +0000

    FMT

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -20,24 +20,24 @@ output "backup_vaults" {
 
 module "backup_vault_policies_blob_storage" {
   source = "./modules/backup_vault/backup_vault_policy_blob_storage"
-  for_each   = {
+  for_each = {
     for key, value in local.data_protection.backup_vault_policies : key => value
     if value.type == "blob_storage"
   }
 
   settings = each.value
-  vault_id = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id 
+  vault_id = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id
 }
 
 module "backup_vault_policies_disk" {
   source = "./modules/backup_vault/backup_vault_policy_disk"
-  for_each   = {
+  for_each = {
     for key, value in local.data_protection.backup_vault_policies : key => value
     if value.type == "disk"
   }
 
   settings = each.value
-  vault_id = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id 
+  vault_id = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id
 }
 
 locals {
@@ -51,32 +51,32 @@ output "backup_vault_policies" {
 module "backup_vault_instances_blob_storage" {
   source     = "./modules/backup_vault/backup_vault_instance_blob_storage"
   depends_on = [azurerm_role_assignment.for]
-  for_each   = {
+  for_each = {
     for key, value in local.data_protection.backup_vault_instances : key => value
     if value.type == "blob_storage"
   }
-  
-  settings = each.value
-  vault_id = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id
-  location = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
-  storage_account_id = lookup(each.value, "storage_account_key") == null ? null : module.storage_accounts[each.value.storage_account_key].id  
-  backup_policy_id   = lookup(each.value, "backup_vault_policy_key") == null ? null : local.backup_vault_policies[each.value.backup_vault_policy_key].id 
+
+  settings           = each.value
+  vault_id           = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id
+  location           = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
+  storage_account_id = lookup(each.value, "storage_account_key") == null ? null : module.storage_accounts[each.value.storage_account_key].id
+  backup_policy_id   = lookup(each.value, "backup_vault_policy_key") == null ? null : local.backup_vault_policies[each.value.backup_vault_policy_key].id
 }
 
 module "backup_vault_instances_disk" {
   source     = "./modules/backup_vault/backup_vault_instance_disk"
   depends_on = [azurerm_role_assignment.for]
-  for_each   = {
+  for_each = {
     for key, value in local.data_protection.backup_vault_instances : key => value
     if value.type == "disk"
   }
-  
-  settings = each.value
-  vault_id = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id 
-  location = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
-  backup_policy_id   = lookup(each.value, "backup_vault_policy_key") == null ? null : local.backup_vault_policies[each.value.backup_vault_policy_key].id 
+
+  settings                     = each.value
+  vault_id                     = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id
+  location                     = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
+  backup_policy_id             = lookup(each.value, "backup_vault_policy_key") == null ? null : local.backup_vault_policies[each.value.backup_vault_policy_key].id
   snapshot_resource_group_name = can(each.value.snapshot_resource_group.name) || can(each.value.snapshot_resource_group_name) ? try(each.value.snapshot_resource_group_name.name, each.value.snapshot_resource_group_name) : local.combined_objects_resource_groups[try(each.value.snapshot_resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.snapshot_resource_group_key, each.value.snapshot_resource_group.key)].name
-  disk_id = try(each.value.disk.os_disk, false) == true ?  try(local.combined_objects_virtual_machines[try(each.value.disk.lz_key, local.client_config.landingzone_key)][each.value.disk.vm_key].os_disk_id) : try(local.combined_objects_virtual_machines[try(each.value.disk.lz_key, local.client_config.landingzone_key)][each.value.disk.vm_key].data_disks[each.value.disk.disk_key])
+  disk_id                      = try(each.value.disk.os_disk, false) == true ? try(local.combined_objects_virtual_machines[try(each.value.disk.lz_key, local.client_config.landingzone_key)][each.value.disk.vm_key].os_disk_id) : try(local.combined_objects_virtual_machines[try(each.value.disk.lz_key, local.client_config.landingzone_key)][each.value.disk.vm_key].data_disks[each.value.disk.disk_key])
 }
 
 locals {
