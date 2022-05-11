b2029b1087142b8a23c5a660f35fb3df286759fe
Stanislaw Wozniak <swozniak@evops.eu>
2022-05-11 11:56:38 +0100

    Making rule collection group dependent of Azure Firewall in order for DNat rules to work
    Without this dependency, azure firewall IP configurations are configured after NAT rules which causes an error

--- networking_firewall.tf
+++ networking_firewall.tf
@@ -1,7 +1,6 @@
 module "azurerm_firewalls" {
   depends_on = [
-    module.azurerm_firewall_policies,
-    module.azurerm_firewall_policy_rule_collection_groups
+    module.azurerm_firewall_policies
   ]
   source   = "./modules/networking/firewall"
   for_each = local.networking.azurerm_firewalls
