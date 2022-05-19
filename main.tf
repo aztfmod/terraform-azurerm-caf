307d067d83f321de78686eb51c16ff1c5b651306
lolorol <LaurentLesle@users.noreply.github.com>
2022-05-19 16:19:09 +0000

    Update azapi to 0.2.0

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
     }
     azapi = {
       source  = "azure/azapi"
-      version = "~> 0.1.1"
+      version = "~> 0.2.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
