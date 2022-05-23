9c5c716ea3b1aebcca51b7fd09106ec15f7dfbe2
Arnaud Lheureux <arnaudlh@hotmail.com>
2022-05-23 09:44:14 +0000

    Fixes #1156

--- modules/storage_account/diagnostics.tf
+++ modules/storage_account/diagnostics.tf
@@ -1,9 +1,9 @@
 module "diagnostics" {
-  source = "../../diagnostics"
-  count  = try(var.settings.diagnostic_profiles, null) == null ? 0 : 1
+  source = "../diagnostics"
+  count  = var.diagnostic_profiles == {} ? 0 : 1
 
   resource_id       = azurerm_storage_account.stg.id
   resource_location = azurerm_storage_account.stg.location
   diagnostics       = var.diagnostics
-  profiles          = try(var.settings.diagnostic_profiles, {})
+  profiles          = try(var.diagnostic_profiles, {})
 }
