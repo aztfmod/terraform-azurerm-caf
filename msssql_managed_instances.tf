f88769c12c2fb79b459b26ff14497b78b640733e
lolorol <LaurentLesle@users.noreply.github.com>
2022-05-27 09:34:57 +0000

    Update resource_id

--- msssql_managed_instances.tf
+++ msssql_managed_instances.tf
@@ -13,6 +13,7 @@ module "mssql_managed_instances" {
   depends_on = [module.routes]
 
   global_settings = local.global_settings
+  client_config   = local.client_config
   settings        = each.value
   subnet_id       = can(each.value.networking.subnet_id) ? each.value.networking.subnet_id : local.combined_objects_networking[try(each.value.networking.lz_key, local.client_config.landingzone_key)][each.value.networking.vnet_key].subnets[each.value.networking.subnet_key].id
 
@@ -29,6 +30,7 @@ module "mssql_managed_instances_secondary" {
   depends_on = [module.routes]
 
   global_settings     = local.global_settings
+  client_config       = local.client_config
   settings            = each.value
   location            = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
