6dec0cc3742fa79b0fbd499edbad3a62c004619a
hyperfocus1337 <hyperfocus@hotmail.com>
2022-06-17 09:14:46 +0200

    Add local.client_config to vw-debug.tf

--- vw-debug.tf
+++ vw-debug.tf
@@ -3,10 +3,11 @@ resource "null_resource" "debug" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-    command = "echo $VARIABLE1 >> debug.json; echo $VARIABLE2 >> debug.json; cat debug.json"
+    command = "echo $VARIABLE1 >> debug.json; echo $VARIABLE2 >> debug.json; echo $VARIABLE3 >> debug.json; cat debug.json"
     environment = {
       VARIABLE1 = jsonencode(try(local.combined_objects_networking, ""))
       VARIABLE2 = jsonencode(try(local.combined_objects_virtual_subnets, ""))
+      VARIABLE3 = jsonencode(try(local.client_config, ""))
     }
   }
 }
