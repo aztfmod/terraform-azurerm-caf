8666f9305913cf1d027f425dbe768594699d25e8
hyperfocus1337 <hyperfocus@hotmail.com>
2022-06-17 08:16:26 +0200

    Add local.combined_objects_virtual_subnets to mysql_servers.tf

--- mysql_servers.tf
+++ mysql_servers.tf
@@ -24,10 +24,11 @@ module "mysql_servers" {
   storage_accounts    = module.storage_accounts
   azuread_groups      = module.azuread_groups
   vnets               = local.combined_objects_networking
-  subnet_id           = can(each.value.subnet_id) || can(each.value.vnet_key) == false ? try(each.value.subnet_id, null) : local.combined_objects_networking[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
+  subnet_id           = can(each.value.subnet_id) || can(each.value.vnet_key) == false ? try(each.value.subnet_id, null) : try(local.combined_objects_virtual_subnets[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.subnet_key].id, local.combined_objects_networking[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id)
   private_endpoints   = try(each.value.private_endpoints, {})
   resource_groups     = try(each.value.private_endpoints, {}) == {} ? null : local.resource_groups
   private_dns         = local.combined_objects_private_dns
+  virtual_subnets     = local.combined_objects_virtual_subnets
   diagnostics         = local.combined_diagnostics
   diagnostic_profiles = try(each.value.diagnostic_profiles, {})
 }
