088091c84b387726d724ed851fa71878342dd919
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2022-06-21 14:42:42 +0800

    Update data_explorer.tf
    Co-authored-by: lolorol <LaurentLesle@users.noreply.github.com>

--- data_explorer.tf
+++ data_explorer.tf
@@ -47,7 +47,7 @@ module "kusto_attached_database_configurations" {
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
   cluster_name        = can(each.value.cluster_name) ? each.value.cluster_name : local.combined_objects_kusto_clusters[try(each.value.kusto_cluster.destination.lz_key, local.client_config.landingzone_key)][each.value.kusto_cluster.destination.key].name
   cluster_resource_id = can(each.value.cluster_resource_id) ? each.value.cluster_resource_id : local.combined_objects_kusto_clusters[try(each.value.kusto_cluster.source.lz_key, local.client_config.landingzone_key)][each.value.kusto_cluster.source.key].id
-  database_name       = can(each.value.kusto_database.name) ? each.value.kusto_database.name : local.combined_objects_kusto_databases[try(each.value.kusto_database.lz_key, local.client_config.landingzone_key)][each.value.kusto_database.key].name
+  database_name       = can(each.value.database_name) ? each.value.database_name : local.combined_objects_kusto_databases[try(each.value.kusto_database.lz_key, local.client_config.landingzone_key)][each.value.kusto_database.key].name
 }
 
 output "kusto_attached_database_configurations" {
