263b0f2d0f267bbce1fbcde639c112a90bb753da
dimitrifc <>
2022-06-22 10:39:21 +0200

    add aadlogin vm extension

---
+++ modules/compute/virtual_machine_extensions/AADLogin.tf
@@ -0,0 +1,15 @@
+resource "azurerm_virtual_machine_extension" "AADLogin" {
+  for_each                   = var.extension_name == "AADLogin" ? toset(["enabled"]) : toset([])
+  name                       = "AADLogin"
+  virtual_machine_id         = var.virtual_machine_id
+  publisher                  = local.publisher
+  type                       = local.type
+  #type_handler_version       = local.type_handler_version
+  auto_upgrade_minor_version = true
+}
+
+locals {
+  publisher            = "Microsoft.Azure.ActiveDirectory"
+  #type_handler_version = var.virtual_machine_os_type == "linux" ? "2.1" : "1.10"
+  type                 = var.virtual_machine_os_type == "linux" ? "AADSSHLoginForLinux" : "AADLoginForWindows"
+}
