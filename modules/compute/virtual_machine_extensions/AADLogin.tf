b12a96c4b471fae9077c5bf2594f4540f0a09c4f
dimitrifc <>
2022-06-22 11:42:49 +0200

    rename aadlogin locals

--- modules/compute/virtual_machine_extensions/AADLogin.tf
+++ modules/compute/virtual_machine_extensions/AADLogin.tf
@@ -2,14 +2,14 @@ resource "azurerm_virtual_machine_extension" "AADLogin" {
   for_each                   = var.extension_name == "AADLogin" ? toset(["enabled"]) : toset([])
   name                       = "AADLogin"
   virtual_machine_id         = var.virtual_machine_id
-  publisher                  = local.publisher
-  type                       = local.type
+  publisher                  = local.aadlogin_publisher
+  type                       = local.aadlogin_type
   #type_handler_version       = local.type_handler_version
   auto_upgrade_minor_version = true
 }
 
 locals {
-  publisher            = "Microsoft.Azure.ActiveDirectory"
+  aadlogin_publisher            = "Microsoft.Azure.ActiveDirectory"
   #type_handler_version = var.virtual_machine_os_type == "linux" ? "2.1" : "1.10"
-  type                 = var.virtual_machine_os_type == "linux" ? "AADSSHLoginForLinux" : "AADLoginForWindows"
+  aadlogin_type                 = var.virtual_machine_os_type == "linux" ? "AADSSHLoginForLinux" : "AADLoginForWindows"
 }
