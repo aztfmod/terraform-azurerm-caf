8d88403ad71a067d2aba152ae56c153170d79326
LaurentLesle <lalesle@microsoft.com>
2022-06-24 02:00:31 +0000

    Add new attributes and support passthrough

--- modules/networking/firewall_policy_rule_collection_groups/firewall_policy_rule_collection_groups.tf
+++ modules/networking/firewall_policy_rule_collection_groups/firewall_policy_rule_collection_groups.tf
@@ -9,7 +9,10 @@ resource "azurecaf_name" "polgroup" {
 }
 
 resource "azurecaf_name" "application_rule" {
-  for_each = try(var.policy_settings.application_rule_collections, {})
+  for_each = {
+    for key, value in try(var.policy_settings.application_rule_collections, {}) : key => value
+    if try(var.policy_settings.passthrough, false) == false
+  }
 
   name          = each.value.name
   resource_type = "azurerm_firewall_application_rule_collection"
@@ -21,7 +24,10 @@ resource "azurecaf_name" "application_rule" {
 }
 
 resource "azurecaf_name" "network_rule" {
-  for_each = try(var.policy_settings.network_rule_collections, {})
+  for_each = {
+    for key, value in try(var.policy_settings.network_rule_collections, {}) : key => value
+    if try(var.policy_settings.passthrough, false) == false
+  }
 
   name          = each.value.name
   resource_type = "azurerm_firewall_network_rule_collection"
@@ -33,7 +39,10 @@ resource "azurecaf_name" "network_rule" {
 }
 
 resource "azurecaf_name" "nat_rule" {
-  for_each = try(var.policy_settings.nat_rule_collections, {})
+  for_each = {
+    for key, value in try(var.policy_settings.nat_rule_collections, {}) : key => value
+    if try(var.policy_settings.passthrough, false) == false
+  }
 
   name          = each.value.name
   resource_type = "azurerm_firewall_nat_rule_collection"
@@ -46,7 +55,7 @@ resource "azurecaf_name" "nat_rule" {
 
 
 resource "azurerm_firewall_policy_rule_collection_group" "polgroup" {
-  name               = azurecaf_name.polgroup.result
+  name               = try(var.policy_settings.passthrough, false) ? var.policy_settings.name : azurecaf_name.polgroup.result
   priority           = var.policy_settings.priority
   firewall_policy_id = var.firewall_policy_id
 
@@ -54,7 +63,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "polgroup" {
     for_each = try(var.policy_settings.application_rule_collections, {})
 
     content {
-      name     = azurecaf_name.application_rule[application_rule_collection.key].result
+      name     = try(var.policy_settings.passthrough, false) ? application_rule_collection.value.name : azurecaf_name.application_rule[application_rule_collection.key].result
       priority = application_rule_collection.value.priority
       action   = application_rule_collection.value.action
 
@@ -63,21 +72,26 @@ resource "azurerm_firewall_policy_rule_collection_group" "polgroup" {
 
         content {
           name             = rule.value.name
+          description      = try(rule.value.description, null)
           source_addresses = try(rule.value.source_addresses, null)
           source_ip_groups = try(rule.value.source_ip_groups, try(flatten([
             for key, value in var.ip_groups : value.id
             if contains(rule.value.source_ip_groups_keys, key)
             ]), null)
           )
+          destination_addresses = try(rule.value.destination_addresses, null)
+          destination_urls      = try(rule.value.destination_urls, null)
           destination_fqdn_tags = try(rule.value.destination_fqdn_tags, null)
           destination_fqdns     = try(rule.value.destination_fqdns, null)
+          terminate_tls         = try(rule.value.terminate_tls, null)
+          web_categories        = try(rule.value.web_categories, null)
 
           dynamic "protocols" {
             for_each = try(rule.value.protocols, {})
 
             content {
               type = protocols.value.type
-              port = try(protocols.value.port, null)
+              port = protocols.value.port
             }
           }
         }
@@ -89,7 +103,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "polgroup" {
     for_each = try(var.policy_settings.network_rule_collections, {})
 
     content {
-      name     = azurecaf_name.network_rule[network_rule_collection.key].result
+      name     = try(var.policy_settings.passthrough, false) ? network_rule_collection.value.name : azurecaf_name.network_rule[network_rule_collection.key].result
       priority = network_rule_collection.value.priority
       action   = network_rule_collection.value.action
 
@@ -122,7 +136,7 @@ resource "azurerm_firewall_policy_rule_collection_group" "polgroup" {
     for_each = try(var.policy_settings.nat_rule_collections, {})
 
     content {
-      name     = azurecaf_name.nat_rule[nat_rule_collection.key].result
+      name     = try(var.policy_settings.passthrough, false) ? nat_rule_collection.value.name : azurecaf_name.nat_rule[nat_rule_collection.key].result
       priority = nat_rule_collection.value.priority
       action   = nat_rule_collection.value.action
 
@@ -143,13 +157,12 @@ resource "azurerm_firewall_policy_rule_collection_group" "polgroup" {
             var.public_ip_addresses[rule.value.destination_address_public_ip_key].ip_address
           )
           translated_port    = rule.value.translated_port
-          translated_address = rule.value.translated_address
+          translated_address = try(rule.value.translated_address, null)
+          translated_fqdn    = try(rule.value.translated_fqdn, null)
           protocols          = rule.value.protocols
         }
       }
     }
   }
 
-
 }
-
