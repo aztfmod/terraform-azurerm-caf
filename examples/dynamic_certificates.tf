cc0c6215a9e43cf786c112445fad3c252c7ff529
Nick Metz <nick.metz@volocopter.com>
2022-07-27 10:17:20 +0000

    remove for loop

--- examples/dynamic_certificates.tf
+++ examples/dynamic_certificates.tf
@@ -3,12 +3,7 @@ module "dynamic_keyvault_certificates" {
   source = "../modules/security/dynamic_keyvault_certificates"
 
   # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git//modules/security/dynamic_keyvault_certificates?ref=master"
-
-  for_each = {
-    for keyvault_key, secrets in try(var.dynamic_keyvault_certificates, {}) : keyvault_key => {
-      for key, value in secrets : key => value
-    }
-  }
+  for_each = try(var.dynamic_keyvault_certificates, {})
 
   settings = each.value
   keyvault = module.example.keyvaults[each.key]
