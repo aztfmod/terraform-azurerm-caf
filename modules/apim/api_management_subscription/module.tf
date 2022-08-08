65f19c593b31dcf7b8db6e6727658e56dfca45f5
Nick Metz <nick.metz@volocopter.com>
2022-08-08 11:13:54 +0200

    Add product reference to subscription + tests

--- modules/apim/api_management_subscription/module.tf
+++ modules/apim/api_management_subscription/module.tf
@@ -2,7 +2,7 @@ resource "azurerm_api_management_subscription" "apim" {
   api_management_name = var.api_management_name
   display_name        = var.settings.display_name
   resource_group_name = var.resource_group_name
-  product_id          = try(var.settings.product_id, null)
+  product_id          = try(var.product_id, null)
   user_id             = try(var.settings.user_id, null)
   api_id              = try(var.settings.api_id, null)
   primary_key         = try(var.settings.primary_key, null)
