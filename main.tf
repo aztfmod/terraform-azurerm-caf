8224a3ab8a89e0614290468aab8671228de1506e
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2022-08-17 01:40:56 +0000

    Update to azurerm 3.9.0

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.8.0"
+      version = "~> 3.9.0"
       configuration_aliases = [
         azurerm.vhub
       ]
