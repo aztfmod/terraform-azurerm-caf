61f398188078fc02c768edd0d7ed8c5be982c965
Bastian Karstaedt <2082580+bkarstaedt@users.noreply.github.com>
2022-09-05 12:36:38 +0000

    feat: adding support for 'static_site_custom_domain' within 'static_site' module

--- modules/webapps/static_site/output.tf
+++ modules/webapps/static_site/output.tf
@@ -12,3 +12,12 @@ output "api_key" {
   value       = azurerm_static_site.static_site.api_key
   description = "The API key of this Static Web App, which is used for later interacting with this Static Web App from other clients, e.g. GitHub Action."
 }
+
+output "custom_domain" {
+  value = {
+    for key, value in try(var.custom_domains, {}) : key => {
+      id = azurerm_static_site_custom_domain.custom_domains[key].id
+      validation_token = azurerm_static_site_custom_domain.custom_domains[key].validation_token
+    }
+  }
+}
