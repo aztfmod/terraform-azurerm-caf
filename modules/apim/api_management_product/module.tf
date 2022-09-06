7206780be66606624f3255d1e46eb738f4b0a535
Nick Metz <nick.metz@ptvgroup.com>
2022-09-06 10:04:18 +0200

    Update modules/apim/api_management_product/module.tf
    Co-authored-by: Arnaud Lheureux <arnaudlh@users.noreply.github.com>

--- modules/apim/api_management_product/module.tf
+++ modules/apim/api_management_product/module.tf
@@ -5,7 +5,7 @@ resource "azurerm_api_management_product" "apim" {
   product_id            = var.settings.product_id
   description           = try(var.settings.description, null)
   approval_required     = try(var.settings.approval_required, null)
-  subscription_required = try(var.settings.subscription_required, false)
+  subscription_required = try(var.settings.subscription_required, null)
   published             = try(var.settings.published, false)
   subscriptions_limit   = try(var.settings.subscriptions_limit, null)
   terms                 = try(var.settings.terms, null)
