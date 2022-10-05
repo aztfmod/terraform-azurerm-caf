ace917962857fceb508d9ef154318f1e5302693d
hyperfocus1337 <hyperfocus@hotmail.com>
2022-10-05 10:29:52 +0200

    Update to .json

--- debug.tf
+++ debug.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "debug" {
   provisioner "local-exec" {
-    command = "echo $VARIABLE1 > debug.txt ; echo $VARIABLE2 >> debug.txt ; echo $VARIABLE3 >> debug.txt"
+    command = "echo $VARIABLE1 > debug.json ; echo $VARIABLE2 >> debug.json ; echo $VARIABLE3 >> debug.json"
     environment = {
       VARIABLE1 = jsonencode(local.combined_objects_networking)
       VARIABLE2 = jsonencode(local.client_config)
