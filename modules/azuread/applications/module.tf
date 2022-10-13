5b532ca907ba6d711f1b2eb8fe0698eb07bb9926
hyperfocus1337 <hyperfocus@hotmail.com>
2022-10-13 08:24:07 +0200

    Add ability to set owners of SP manually

--- modules/azuread/applications/module.tf
+++ modules/azuread/applications/module.tf
@@ -3,7 +3,8 @@ resource "azuread_application" "app" {
   display_name = var.global_settings.passthrough ? format("%s", var.settings.application_name) : format("%v-%s", try(var.global_settings.prefixes[0], ""), var.settings.application_name)
 
   owners = [
-    var.client_config.object_id
+    var.client_config.object_id,
+    var.settings.owners
   ]
 
   reply_urls                 = try(var.settings.reply_urls, null)
