2c855397c670f93638029e589cfd89f293562e6f
Jose Luis Carrillo Aguilar <joselcaguilar@users.noreply.github.com>
2022-10-13 12:04:01 +0200

    Application Gateway bugs + adding force_firewall_policy_association (#1385)
    * frontend-ports commands fixed
    
    * Minor fix
    
    * frontend_ports_replica time_sleep
    
    * time_sleep removed
    
    * Dependency rollback
    
    * Adding probe_key to the http_settings
    
    * probe http_settings fix
    
    * ssl_cert wait for http_settings
    
    * WAF Policies are now a dependency for AGW WAFv2
    
    * Using coalesce
    
    * Adding force_firewall_policy_association
    
    * force_firewall_policy_association also added to the application_gateway
    
    * SSL_CERT value fixed on http_listener.tf
    
    * probe is optional in http_settings
    
    * ssl_cert can be null
    
    * front_end_ports agw reference in http_listener fixed
    
    * AKS Dependency added with the application gateways
    
    * frontend_ports rollback in the listener
    
    * Adding ssl_certs as application_gateway_platform output
    
    * ssl_cert statement in http_listener.tf must be in the opposite way
    
    * ssl_cert name or id can be passed in http_listener.tf
    
    * output ssl_certificates var name changed
    
    * var.settings.ssl_certs not always exists for agw platforms output
    
    * SSL_CERT value fixed
    
    * coalesce function removed
    
    * Conditional changed for force_firewall_policy_association
    
    * firewall_policy_id logic aligned between agw and agw_platform
    
    * using can function in firewall_policy_id
    
    * Try from right statement in firewall_policy_id removed
    
    * try function removed from firewall_policy_id statement
    
    * SKU name changed to Standard_v2 for examples 301 and 302 in agw

--- modules/networking/application_gateway/application_gateway.tf
+++ modules/networking/application_gateway/application_gateway.tf
@@ -31,10 +31,11 @@ resource "azurerm_application_gateway" "agw" {
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  zones              = try(var.settings.zones, null)
-  enable_http2       = try(var.settings.enable_http2, true)
-  tags               = try(local.tags, null)
-  firewall_policy_id = can(var.settings.firewall_policy_id) || can(var.settings.waf_policy.key) == false ? try(var.settings.firewall_policy_id, null) : var.application_gateway_waf_policies[try(var.settings.waf_policy.lz_key, var.client_config.landingzone_key)][var.settings.waf_policy.key].id
+  zones                             = try(var.settings.zones, null)
+  enable_http2                      = try(var.settings.enable_http2, true)
+  tags                              = try(local.tags, null)
+  firewall_policy_id                = can(var.settings.firewall_policy_id) == true ? var.settings.firewall_policy_id : (can(var.settings.waf_policy.key) == true ? var.application_gateway_waf_policies[try(var.settings.waf_policy.lz_key, var.client_config.landingzone_key)][var.settings.waf_policy.key].id : null)
+  force_firewall_policy_association = can(var.settings.firewall_policy_id) == false && can(var.settings.waf_policy.key) == false ? false : true
 
   sku {
     name     = var.sku_name
