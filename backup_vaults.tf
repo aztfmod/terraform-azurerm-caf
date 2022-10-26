4e9c61bf4696135c3c7ec23c303726cb364943f7
Nick Metz <nick.metz@volocopter.com>
2022-10-26 08:56:36 +0200

    remove duplicates

--- backup_vaults.tf
+++ backup_vaults.tf
@@ -46,21 +46,6 @@ locals {
   backup_vault_policies = merge(module.backup_vault_policies_blob_storage, module.backup_vault_policies_disk)
 }
 
-module "backup_vault_policies_disk" {
-  source = "./modules/backup_vault/backup_vault_policy_disk"
-  for_each = {
-    for key, value in local.data_protection.backup_vault_policies : key => value
-    if value.type == "disk"
-  }
-
-  settings = each.value
-  vault_id = lookup(each.value, "backup_vault_key") == null ? null : module.backup_vaults[each.value.backup_vault_key].id
-}
-
-locals {
-  backup_vault_policies = merge(module.backup_vault_policies_blob_storage, module.backup_vault_policies_disk)
-}
-
 output "backup_vault_policies" {
   value = local.backup_vault_policies
 }
