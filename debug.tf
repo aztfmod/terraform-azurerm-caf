cb3e67fadaa8d48e88f3020bbebed5a348a7ec06
hyperfocus1337 <hyperfocus@hotmail.com>
2022-10-26 16:20:41 +0200

    Add local.combined_objects_virtual_subnets to debug

--- debug.tf
+++ debug.tf
@@ -3,8 +3,9 @@ resource "null_resource" "debug" {
     command = "echo $VARIABLE1 > debug.json ; echo $VARIABLE2 >> debug.json ; echo $VARIABLE3 >> debug.json"
     environment = {
       VARIABLE1 = jsonencode(local.combined_objects_networking)
-      VARIABLE2 = jsonencode(local.client_config)
+      VARIABLE2 = jsonencode(local.combined_objects_virtual_subnets)
       VARIABLE3 = jsonencode(local.compute.virtual_machines)
+      VARIABLE3 = jsonencode(client_config)
     }
   }
 }
