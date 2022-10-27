e723c13cfe02db81462d1d2e20e738048e02e65f
hyperfocus1337 <hyperfocus@hotmail.com>
2022-10-27 09:23:55 +0200

    Fix debug

--- debug.tf
+++ debug.tf
@@ -1,11 +1,11 @@
 resource "null_resource" "debug" {
   provisioner "local-exec" {
-    command = "echo $VARIABLE1 > debug.json ; echo $VARIABLE2 >> debug.json ; echo $VARIABLE3 >> debug.json"
+    command = "echo $VARIABLE1 > debug.json ; echo $VARIABLE2 >> debug.json ; echo $VARIABLE3 >> debug.json; echo $VARIABLE4 >> debug.json"
     environment = {
       VARIABLE1 = jsonencode(local.combined_objects_networking)
       VARIABLE2 = jsonencode(local.combined_objects_virtual_subnets)
       VARIABLE3 = jsonencode(local.compute.virtual_machines)
-      VARIABLE3 = jsonencode(client_config)
+      VARIABLE4 = jsonencode(local.client_config)
     }
   }
 }
