fc9a1bc16259acea8cc857ac582b4b404163cb4a
Rob Long <Robert.Long@lv.com>
2022-10-27 15:47:09 +0100

    Add Private Endpoint support for Databricks

--- databricks.tf
+++ databricks.tf
@@ -11,6 +11,9 @@ module "databricks_workspaces" {
   vnets               = local.combined_objects_networking
   location            = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+  resource_groups     = local.combined_objects_resource_groups
+  private_dns         = local.combined_objects_private_dns
+  private_endpoints   = try(each.value.private_endpoints, {})
 }
 
 output "databricks_workspaces" {
