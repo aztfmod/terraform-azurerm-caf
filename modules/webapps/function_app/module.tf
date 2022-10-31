c34c0fc5a0159ebf2f65ccf46e57c87bf342305d
Alexandre Ferri <alexandre.ferri@planetpayment.com>
2022-10-31 16:02:21 +0100

    Add another lifecycle to function apps

--- modules/webapps/function_app/module.tf
+++ modules/webapps/function_app/module.tf
@@ -17,7 +17,10 @@ resource "azurerm_function_app" "function_app" {
       # When integrating a CI/CD pipeline and expecting to run from a deployed package in Azure
       # you must seed your app settings as part of terraform code for function app to be successfully deployed.
       # Important Default key pairs: ("WEBSITE_RUN_FROM_PACKAGE" = "", "FUNCTIONS_WORKER_RUNTIME" = "node"
-      app_settings
+      app_settings,
+      tags["hidden-link: /app-insights-instrumentation-key"],
+      tags["hidden-link: /app-insights-resource-id"],
+      tags["hidden-link: /app-insights-conn-string"]
     ]
   }
   name                = azurecaf_name.plan.result
