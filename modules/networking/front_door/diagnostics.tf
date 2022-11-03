aa5c3087a50f44e916d2e4f0a5205769cff78f35
Hein Tonny Køien <hein.koien@blinq.no>
2022-11-03 09:39:05 +0000

    blinQ: Update diganostic module for azure frontdoor to use global_settings.default_region as location. Azure Frontdoor is a global service and the azurerm object don't provide an argument for location. Also, caf frontdoor module is implemented with support for resource_group_name as parameter (resource_group key not required), this make it "impossible" to grab the location by code from the correct resource_group with 100% certenty. RG only store the metadata anyway.

--- modules/networking/front_door/diagnostics.tf
+++ modules/networking/front_door/diagnostics.tf
@@ -4,7 +4,9 @@ module "diagnostics" {
   count  = lookup(var.settings, "diagnostic_profiles", null) == null ? 0 : 1
 
   resource_id       = azurerm_frontdoor.frontdoor.id
-  # resource_location = azurerm_frontdoor.frontdoor.location       // blinQ: Argument not supported for frontdore
+  # blinQ: Updated resource location to global_settings.default_region as Azure Frontdoor is a global service and the azurerm object don't provide an argument for location. Also, caf frontdoor module is implemented with support for resource_group_name as reference (resource_group key not required), this make it "impossible" to grab the location by code from the correct resource_group with 100% certenty (rg only store the metadata anyway)
+  # resource_location = azurerm_frontdoor.frontdoor.location
+  resource_location = var.global_settings.default_region
   diagnostics       = var.diagnostics
   profiles          = var.settings.diagnostic_profiles
 }
