b97dc4b7641a022506bd2c42de64e37b80905370
Arnaud Lheureux <arnaudlh@hotmail.com>
2022-11-11 07:57:38 +0000

    Iterate on #1395

--- modules/logic_app/standard/main.tf
+++ modules/logic_app/standard/main.tf
@@ -7,29 +7,10 @@ terraform {
 }
 
 locals {
-  resource_group = coalesce(
-    try(var.resource_groups[var.client_config.landingzone_key][var.settings.resource_group_key], null),
-    try(var.resource_groups[var.client_config.landingzone_key][var.settings.resource_group.key], null),
-    try(var.resource_groups[var.settings.lz_key][var.settings.resource_group_key], null),
-    try(var.resource_groups[var.settings.resource_group.lz_key][var.settings.resource_group.key], null)
-  )
-
-  storage_account = coalesce(
-    try(var.storage_accounts[var.client_config.landingzone_key][var.settings.storage_account_key], null),
-    try(var.storage_accounts[var.client_config.landingzone_key][var.settings.storage_account.key], null),
-    try(var.storage_accounts[var.settings.lz_key][var.settings.storage_account_key], null),
-    try(var.storage_accounts[var.settings.storage_account.lz_key][var.settings.storage_account.key], null)
-  )
-
-  app_service_plan = coalesce(
-  try(var.app_service_plans[var.client_config.landingzone_key][var.settings.app_service_plan_key], null),
-  try(var.app_service_plans[var.client_config.landingzone_key][var.settings.app_service_plan.key], null),
-  try(var.app_service_plans[var.settings.lz_key][var.settings.app_service_plan_key], null),
-  try(var.app_service_plans[var.settings.app_service_plan.lz_key][var.settings.app_service_plan.key], null)
-  )  
-
-  app_settings = try(var.app_settings, null)
-
-  subnet_id = try(var.subnets[try(var.settings.vnet_integration.lz_key, var.client_config.landingzone_key)][var.settings.vnet_integration.vnet_key].subnets[var.settings.vnet_integration.subnet_key].id, null)
+  resource_group   = var.resource_groups[try(var.settings.lz_key, var.settings.resource_group.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group.key, var.settings.resource_group_key)]
+  storage_account  = var.storage_accounts[try(var.settings.lz_key, var.settings.storage_account.lz_key, var.client_config.landingzone_key)][try(var.settings.storage_account.key, var.settings.storage_account_key)]
+  app_service_plan = var.app_service_plans[try(var.settings.app_service_plan.lz_key, var.settings.lz_key, var.client_config.landingzone_key)][try(var.settings.app_service_plan.key, var.settings.app_service_plan_key)]
+  app_settings     = try(var.app_settings, null)
+  subnet_id        = can(var.settings.vnet_integration.subnet_id) || can(var.settings.vnet_integration.subnet_key) == false ? try(var.settings.vnet_integration.subnet_id, null) : var.subnets[try(var.settings.vnet_integration.lz_key, var.client_config.landingzone_key)][var.settings.vnet_integration.vnet_key].subnets[var.settings.vnet_integration.subnet_key].id
 
 }
