35d2f3c2eeccbeb390b5bc47a6f282f89a9329d6
Adam Cain <72016306+adamcaino@users.noreply.github.com>
2022-11-11 08:03:33 +0000

    Add Logic App Standard with vnet integration (#1395)
    Co-authored-by: Arnaud Lheureux <arnaudlh@users.noreply.github.com>

--- modules/logic_app/standard/main.tf
+++ modules/logic_app/standard/main.tf
@@ -12,5 +12,4 @@ locals {
   app_service_plan = var.app_service_plans[try(var.settings.app_service_plan.lz_key, var.settings.lz_key, var.client_config.landingzone_key)][try(var.settings.app_service_plan.key, var.settings.app_service_plan_key)]
   app_settings     = try(var.app_settings, null)
   subnet_id        = can(var.settings.vnet_integration.subnet_id) || can(var.settings.vnet_integration.subnet_key) == false ? try(var.settings.vnet_integration.subnet_id, null) : var.subnets[try(var.settings.vnet_integration.lz_key, var.client_config.landingzone_key)][var.settings.vnet_integration.vnet_key].subnets[var.settings.vnet_integration.subnet_key].id
-
 }
