c6f89f2cd8fa09e8dd05a9a304d6be0513a815f7
Simon Soulie <simon.soulie@avanade.com>
2022-11-29 14:44:09 +0100

    Checks for null on dynamic prop + managed ids

--- modules/analytics/synapse/workspace.tf
+++ modules/analytics/synapse/workspace.tf
@@ -24,36 +24,31 @@ resource "azurerm_synapse_workspace" "ws" {
   tags                                 = local.tags
 
   dynamic "aad_admin" {
-    for_each = try(var.settings.aad_admin, {}) == {} ? [] : [1]
-
+    for_each = try(var.settings.aad_admin, null) != null ? [var.settings.aad_admin] : []
     content {
-      login     = var.settings.aad_admin.login
-      object_id = var.settings.aad_admin.object_id
-      tenant_id = var.settings.aad_admin.tenant_id
+      login     = try(aad_admin.value.login, null)
+      object_id = try(aad_admin.value.object_id, null)
+      tenant_id = try(aad_admin.value.tenant_id, null)
     }
   }
-  
+
+  dynamic "sql_aad_admin" {
+    for_each = try(var.settings.sql_aad_admin, null) != null ? [var.settings.sql_aad_admin] : []
+    content {
+      login     = try(sql_aad_admin.value.login, null)
+      object_id = try(sql_aad_admin.value.object_id, null)
+      tenant_id = try(sql_aad_admin.value.tenant_id, null)
+    }
+  }
+
   dynamic "identity" {
-    for_each = try(var.settings.identity, null) == null ? [] : [1]
-
+    for_each = can(var.settings.identity) ? [var.settings.identity] : []
     content {
-      type         = var.settings.identity.type
-      identity_ids = var.settings.identity.ids
+      type         = identity.value.type
+      identity_ids = concat(local.managed_identities, try(identity.value.identity_ids, []))
     }
   }
   
-  dynamic "azure_devops_repo" {
-    for_each = try(var.settings.azure_devops_repo, {}) == {} ? [] : [1]
-
-    content {
-      account_name    = try(var.settings.azure_devops_repo.account_name, null)
-      branch_name     = try(var.settings.azure_devops_repo.branch_name, null)
-      project_name    = try(var.settings.azure_devops_repo.project_name, null)
-      repository_name = try(var.settings.azure_devops_repo.repository_name, null)
-      root_folder     = try(var.settings.azure_devops_repo.root_folder, null)
-    }
-  }
-
   dynamic "customer_managed_key" {
     for_each = try(var.settings.customer_managed_key_versionless_id, null) == null ? [] : [1]
 
@@ -61,21 +56,29 @@ resource "azurerm_synapse_workspace" "ws" {
       key_versionless_id = try(var.settings.customer_managed_key_versionless_id, null)
     }
   }
-
-  dynamic "github_repo" {
-    for_each = try(var.settings.github_repo, {}) == {} ? [] : [1]
-
+  dynamic "azure_devops_repo" {
+    for_each = try(var.settings.azure_devops_repo, null) != null ? [var.settings.azure_devops_repo] : []
     content {
-      account_name    = try(var.settings.github_repo.account_name, null)
-      branch_name     = try(var.settings.github_repo.branch_name, null)
-      repository_name = try(var.settings.github_repo.repository_name, null)
-      root_folder     = try(var.settings.github_repo.root_folder, null)
-      git_url         = try(var.settings.github_repo.git_url, null)
+      account_name    = try(azure_devops_repo.value.account_name, null)
+      branch_name     = try(azure_devops_repo.value.branch_name, null)
+      last_commit_id  = try(azure_devops_repo.value.last_commit_id, null)
+      project_name    = try(azure_devops_repo.value.project_name, null)
+      repository_name = try(azure_devops_repo.value.repository_name, null)
+      root_folder     = try(azure_devops_repo.value.root_folder, null)
+      tenant_id       = try(azure_devops_repo.value.tenant_id, null)
     }
   }
-
-}
-
+  dynamic "github_repo" {
+    for_each = try(var.settings.github_repo, null) != null ? [var.settings.github_repo] : []
+    content {
+      account_name    = try(github_repo.value.account_name, null)
+      branch_name     = try(github_repo.value.branch_name, null)
+      last_commit_id  = try(github_repo.value.last_commit_id, null)
+      repository_name = try(github_repo.value.repository_name, null)
+      root_folder     = try(github_repo.value.root_folder, null)
+      git_url         = try(github_repo.value.git_url, null)
+    }
+  }
 # Generate sql server random admin password if not provided in the attribute administrator_login_password
 resource "random_password" "sql_admin" {
   count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
