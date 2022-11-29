9616a92de41a0196a8c2d242efcaacaa47f1fd4c
Palaniappan Chockalingam <pchockal@in.ibm.com>
2022-11-29 11:02:07 +0530

    added main.tf terraform provider

--- modules/networking/route_tables/main.tf
+++ modules/networking/route_tables/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.99"
+    }
+  }
+}
+
 locals {
   module_tag = {
     "module" = basename(abspath(path.module))
