722831a8d65763c6d6762adc2d3531182316b29e
Simon Soulie <simon.soulie@avanade.com>
2022-11-29 17:28:17 +0100

    corrected brackets

--- modules/analytics/synapse/workspace.tf
+++ modules/analytics/synapse/workspace.tf
@@ -56,32 +56,6 @@ resource "azurerm_synapse_workspace" "ws" {
       key_versionless_id = try(var.settings.customer_managed_key_versionless_id, null)
     }
   }
-  # Generate sql server random admin password if not provided in the attribute administrator_login_password
-  resource "random_password" "sql_admin" {
-    count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
-
-    length           = 128
-    special          = true
-    upper            = true
-    numeric          = true
-    override_special = "$#%"
-  }
-
-  # Store the generated password into keyvault for password rotation support
-  resource "azurerm_key_vault_secret" "sql_admin_password" {
-    count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
-
-    name         = format("%s-synapse-sql-admin-password", azurerm_synapse_workspace.ws.name)
-    value        = random_password.sql_admin.0.result
-    key_vault_id = var.keyvault_id
-
-    lifecycle {
-      ignore_changes = [
-        value
-      ]
-    }
-  }
-
 
   dynamic "azure_devops_repo" {
     for_each = try(var.settings.azure_devops_repo, null) != null ? [var.settings.azure_devops_repo] : []
@@ -107,58 +81,79 @@ resource "azurerm_synapse_workspace" "ws" {
       git_url         = try(github_repo.value.git_url, null)
     }
   }
+}
+# Generate sql server random admin password if not provided in the attribute administrator_login_password
+resource "random_password" "sql_admin" {
+  count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
 
+  length           = 128
+  special          = true
+  upper            = true
+  numeric          = true
+  override_special = "$#%"
+}
 
+# Store the generated password into keyvault for password rotation support
+resource "azurerm_key_vault_secret" "sql_admin_password" {
+  count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
 
+  name         = format("%s-synapse-sql-admin-password", azurerm_synapse_workspace.ws.name)
+  value        = random_password.sql_admin.0.result
+  key_vault_id = var.keyvault_id
 
-
-  resource "azurerm_key_vault_secret" "sql_admin" {
-    count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
-
-    name         = format("%s-synapse-sql-admin-username", azurerm_synapse_workspace.ws.name)
-    value        = var.settings.sql_administrator_login
-    key_vault_id = var.keyvault_id
-  }
-
-  resource "azurerm_key_vault_secret" "synapse_name" {
-    count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
-
-    name         = format("%s-synapse-name", azurerm_synapse_workspace.ws.name)
-    value        = azurerm_synapse_workspace.ws.name
-    key_vault_id = var.keyvault_id
-  }
-
-  resource "azurerm_key_vault_secret" "synapse_rg_name" {
-    count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
-
-    name         = format("%s-synapse-resource-group-name", azurerm_synapse_workspace.ws.name)
-    value        = var.resource_group_name
-    key_vault_id = var.keyvault_id
-  }
-
-  # for backwards compatibility to create single firewall rule
-  # Ref : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/synapse_firewall_rule
-  # Tested with : AzureRm 2.57.0
-  resource "azurerm_synapse_firewall_rule" "wrkspc_firewall" {
-    count = try(var.settings.workspace_firewall, null) == null ? 0 : 1
-
-    name                 = var.settings.workspace_firewall.name
-    synapse_workspace_id = azurerm_synapse_workspace.ws.id
-    start_ip_address     = var.settings.workspace_firewall.start_ip
-    end_ip_address       = var.settings.workspace_firewall.end_ip
-  }
-
-  # supports adding multiple synapse firewall rules
-  # Ref : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/synapse_firewall_rule
-  # Tested with : AzureRm 2.57.0
-  resource "azurerm_synapse_firewall_rule" "wrkspc_firewalls" {
-    for_each = try(var.settings.workspace_firewalls, {})
-
-    # use key as firewall name if name attribute not defined
-    name                 = try(each.value.name, each.key)
-    synapse_workspace_id = azurerm_synapse_workspace.ws.id
-    # start_ip and end_ip must be specified in each individual workspace_firewall_rule
-    start_ip_address = each.value.start_ip
-    end_ip_address   = each.value.end_ip
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
   }
 }
+resource "azurerm_key_vault_secret" "sql_admin" {
+  count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
+
+  name         = format("%s-synapse-sql-admin-username", azurerm_synapse_workspace.ws.name)
+  value        = var.settings.sql_administrator_login
+  key_vault_id = var.keyvault_id
+}
+
+resource "azurerm_key_vault_secret" "synapse_name" {
+  count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
+
+  name         = format("%s-synapse-name", azurerm_synapse_workspace.ws.name)
+  value        = azurerm_synapse_workspace.ws.name
+  key_vault_id = var.keyvault_id
+}
+
+resource "azurerm_key_vault_secret" "synapse_rg_name" {
+  count = try(var.settings.sql_administrator_login_password, null) == null ? 1 : 0
+
+  name         = format("%s-synapse-resource-group-name", azurerm_synapse_workspace.ws.name)
+  value        = var.resource_group_name
+  key_vault_id = var.keyvault_id
+}
+
+# for backwards compatibility to create single firewall rule
+# Ref : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/synapse_firewall_rule
+# Tested with : AzureRm 2.57.0
+resource "azurerm_synapse_firewall_rule" "wrkspc_firewall" {
+  count = try(var.settings.workspace_firewall, null) == null ? 0 : 1
+
+  name                 = var.settings.workspace_firewall.name
+  synapse_workspace_id = azurerm_synapse_workspace.ws.id
+  start_ip_address     = var.settings.workspace_firewall.start_ip
+  end_ip_address       = var.settings.workspace_firewall.end_ip
+}
+
+# supports adding multiple synapse firewall rules
+# Ref : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/synapse_firewall_rule
+# Tested with : AzureRm 2.57.0
+resource "azurerm_synapse_firewall_rule" "wrkspc_firewalls" {
+  for_each = try(var.settings.workspace_firewalls, {})
+
+  # use key as firewall name if name attribute not defined
+  name                 = try(each.value.name, each.key)
+  synapse_workspace_id = azurerm_synapse_workspace.ws.id
+  # start_ip and end_ip must be specified in each individual workspace_firewall_rule
+  start_ip_address = each.value.start_ip
+  end_ip_address   = each.value.end_ip
+}
+
