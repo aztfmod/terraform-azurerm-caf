08ee8637528469186a15bd576e69061c42bbe0e4
Francisco Robles Martín <f.robles.martin@pm.me>
2023-01-20 10:20:20 +0100

    feat: add API Management Product Policy resource

--- modules/apim/api_management_product/output.tf
+++ modules/apim/api_management_product/output.tf
@@ -7,3 +7,9 @@ output "product_id" {
   value       = azurerm_api_management_product.apim.product_id
   description = "The Product ID of the API Management Product."
 }
+
+output "policy_id" {
+  value       = azurerm_api_management_product_policy.apim.id
+  description = "The ID of the API Management Product Policy."
+}
+
