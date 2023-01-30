9f8b5332dd8927581f87a9926ba2c6e12c526dbe
Nick Metz <nick.metz@volocopter.com>
2023-01-30 08:56:37 +0100

    Add value_template to dynamic secrets to customize secrets

--- modules/security/dynamic_keyvault_secrets/secret_dynamic/variables.tf
+++ modules/security/dynamic_keyvault_secrets/secret_dynamic/variables.tf
@@ -2,9 +2,5 @@ variable "name" {}
 variable "value" {}
 variable "keyvault_id" {}
 variable "config" {
-  default = {
-    length           = 16
-    special          = true
-    override_special = "_!@"
-  }
+  default = {}
 }
