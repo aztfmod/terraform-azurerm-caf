b636921d2e5552ec8ca5f83e339b5fdc7e1245f3
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-02-23 07:05:21 +0000

    Update for case sensitivity

--- modules/networking/load_balancers/load_balancers.tf
+++ modules/networking/load_balancers/load_balancers.tf
@@ -12,7 +12,7 @@ resource "azurerm_lb" "lb" {
   name                = azurecaf_name.lb_name.result
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = var.settings.sku #Accepted values are Basic and Standard. Defaults to Basic
+  sku                 = title(var.settings.sku) #Accepted values are Basic and Standard. Defaults to Basic
 
   dynamic "frontend_ip_configuration" {
     for_each = try(var.settings.frontend_ip_configurations, {})
