8a3580f8af56b881a929a77a1fdd59a0bf2e3d8a
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-02-23 08:06:24 +0000

    Update to PIP

--- modules/networking/public_ip_addresses/variables.tf
+++ modules/networking/public_ip_addresses/variables.tf
@@ -86,14 +86,14 @@ variable "tags" {
 }
 
 variable "zones" {
-  description = "(Optional) The availability zone to allocate the Public IP in. Possible values are 1, 2, 3. Defaults to null."
-  type        = list(any)
-  default     = null
+  description = "The availability zone to allocate the Public IP in. Possible values are 1, 2, 3. Defaults to null."
+  type        = list(string)
+  nullable    = true
 
-  # validation {
-  #   condition     = contains(["1", "2", "3"], var.zones)
-  #   error_message = "Provide an allowed value as defined in https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip#availability_zone."
-  # }
+  validation {
+    condition     = setunion(["1", "2", "3"], var.zones == null ? [""] : var.zones) == toset(["1", "2", "3"])
+    error_message = "Update behavior to mandate specifying the zones in order to avoid accidental resources destructions. https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip#zone."
+  }
 }
 
 variable "diagnostics" {
