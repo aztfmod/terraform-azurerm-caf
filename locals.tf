6c1aa171c72d6e20c144ca421ac106482ef038bf
lolorol <laurent.lesle@gmail.com>
2023-02-24 02:20:46 +0000

    Updating azuread apps and sp

--- locals.tf
+++ locals.tf
@@ -22,7 +22,7 @@ locals {
   }
 
   client_config = var.client_config == {} ? {
-    client_id               = data.azurerm_client_config.current.client_id
+    client_id               = data.azuread_client_config.current.client_id
     landingzone_key         = var.current_landingzone_key
     logged_aad_app_objectId = local.object_id
     logged_user_objectId    = local.object_id
@@ -316,7 +316,7 @@ locals {
     vpn_sites                                               = try(var.networking.vpn_sites, {})
   }
 
-  object_id = coalesce(var.logged_user_objectId, var.logged_aad_app_objectId, try(data.azurerm_client_config.current.object_id, null), try(data.azuread_service_principal.logged_in_app.0.object_id, null))
+  object_id = coalesce(var.logged_user_objectId, var.logged_aad_app_objectId, try(data.azuread_client_config.current.object_id, null), try(data.azuread_service_principal.logged_in_app.0.object_id, null))
 
   security = {
     disk_encryption_sets                = try(var.security.disk_encryption_sets, {})
