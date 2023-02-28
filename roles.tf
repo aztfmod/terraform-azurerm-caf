71c6501e5e355eef38f5373e9ba9d30cedcb4495
Simon Schneider <simon.schneider@volocopter.com>
2023-02-28 12:34:49 +0000

    add service roles for wvd objects

--- roles.tf
+++ roles.tf
@@ -154,6 +154,10 @@ locals {
     subscriptions                              = local.combined_objects_subscriptions
     synapse_workspaces                         = local.combined_objects_synapse_workspaces
     virtual_subnets                            = local.combined_objects_virtual_subnets
+    wvd_application_groups                     = local.combined_objects_wvd_application_groups
+    wvd_applications                           = local.combined_objects_wvd_applications
+    wvd_host_pools                             = local.combined_objects_wvd_host_pools
+    wvd_workspaces                             = local.combined_objects_wvd_workspaces
     log_analytics                              = local.current_objects_log_analytics
   }
 
