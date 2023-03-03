d98ed2a7da3a2f5135aaf690b6359a2f4ee4359c
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-03 10:01:17 +0000

    Update VMSS

--- virtual_machines_scale_sets.tf
+++ virtual_machines_scale_sets.tf
@@ -20,7 +20,6 @@ module "virtual_machine_scale_sets" {
   availability_sets                = local.combined_objects_availability_sets
   application_gateways             = local.combined_objects_application_gateways
   application_security_groups      = local.combined_objects_application_security_groups
-  base_tags                        = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
   boot_diagnostics_storage_account = try(local.combined_diagnostics.storage_accounts[each.value.boot_diagnostics_storage_account_key].primary_blob_endpoint, {})
   client_config                    = local.client_config
   diagnostics                      = local.combined_diagnostics
@@ -38,8 +37,8 @@ module "virtual_machine_scale_sets" {
   recovery_vaults                  = local.combined_objects_recovery_vaults
   settings                         = each.value
   vnets                            = local.combined_objects_networking
-  location                         = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
-  resource_group_name              = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+  resource_group                   = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
+  base_tags                        = local.global_settings.inherit_tags
 }
 
 
