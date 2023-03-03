7660e92b576e56482cfcd97a1379f62e948200d6
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-03 10:01:43 +0000

    Update RG

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -6,3 +6,16 @@ terraform {
   }
 
 }
+
+locals {
+
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    try(var.settings.tags, null)
+    ) : merge(
+    try(var.settings.tags,
+    null)
+  )
+
+
+}
