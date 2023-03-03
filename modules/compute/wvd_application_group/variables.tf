08f37cce4afac1fb6eb14abb75086aa7ccbf0c9c
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-03 10:00:50 +0000

    Update WVD

--- modules/compute/wvd_application_group/variables.tf
+++ modules/compute/wvd_application_group/variables.tf
@@ -1,10 +1,12 @@
 variable "settings" {}
 variable "global_settings" {}
-variable "location" {}
-variable "resource_group_name" {
-  description = "Name of the existing resource group to deploy the virtual machine"
+variable "resource_group" {
+  description = "Resource group object"
 }
-variable "base_tags" {}
+variable "base_tags" {
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = bool
+}
 variable "wvd_host_pools" {
   default = {}
 }
