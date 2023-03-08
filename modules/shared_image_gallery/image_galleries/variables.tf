72688ba6b3004842165f509027b4123f2e158d58
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-08 06:20:32 +0000

    Update shared image gallery

--- modules/shared_image_gallery/image_galleries/variables.tf
+++ modules/shared_image_gallery/image_galleries/variables.tf
@@ -1,8 +1,11 @@
-
-variable "resource_group_name" {}
-variable "location" {}
 variable "diagnostics" {}
 variable "client_config" {}
 variable "global_settings" {}
 variable "settings" {}
-variable "base_tags" {}
+variable "resource_group" {
+  description = "Resource group object"
+}
+variable "base_tags" {
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = bool
+}
--- modules/shared_image_gallery/packer_build/variables.tf
+++ modules/shared_image_gallery/packer_build/variables.tf
@@ -1,12 +1,16 @@
-variable "resource_group_name" {}
 variable "build_resource_group_name" {
   default = {}
 }
-variable "location" {}
 variable "client_config" {}
 variable "global_settings" {}
 variable "settings" {}
-variable "base_tags" {}
+variable "resource_group" {
+  description = "Resource group object"
+}
+variable "base_tags" {
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = bool
+}
 variable "gallery_name" {}
 variable "image_name" {}
 variable "key_vault_id" {}
