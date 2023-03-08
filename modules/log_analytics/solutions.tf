eac0e5edefc21d3c3ffc70af8e366c3ba2260550
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-08 06:22:48 +0000

    Update storage account and diagnostocs

--- modules/log_analytics/solutions.tf
+++ modules/log_analytics/solutions.tf
@@ -2,8 +2,8 @@ resource "azurerm_log_analytics_solution" "solution" {
   for_each = lookup(var.log_analytics, "solutions_maps", {})
 
   solution_name         = each.key
-  location              = var.location
-  resource_group_name   = var.resource_group_name
+  location              = local.location
+  resource_group_name   = local.resource_group_name
   workspace_resource_id = azurerm_log_analytics_workspace.law.id
   workspace_name        = azurerm_log_analytics_workspace.law.name
   tags                  = local.tags
