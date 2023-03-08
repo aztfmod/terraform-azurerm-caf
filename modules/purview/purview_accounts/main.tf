a875bc1d8bb1d55a2e2d171aec98348661af7b3b
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-08 06:18:28 +0000

    Update purview

--- modules/purview/purview_accounts/main.tf
+++ modules/purview/purview_accounts/main.tf
@@ -6,5 +6,15 @@ terraform {
   }
 }
 locals {
-  tags = merge(var.base_tags, try(var.settings.tags, null))
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    try(var.resource_group.tags, null),
+    try(var.settings.tags, null)
+    ) : merge(
+    try(var.settings.tags,
+    null)
+  )
+
+  location            = var.resource_group.location
+  resource_group_name = var.resource_group.name
 }
