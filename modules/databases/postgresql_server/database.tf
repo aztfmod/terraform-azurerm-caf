a632ef45284aff4e2ae12c13e8eb6e1d616e94e6
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-08 06:21:20 +0000

    Upgrade postgres

--- modules/databases/postgresql_server/database.tf
+++ modules/databases/postgresql_server/database.tf
@@ -7,7 +7,7 @@ resource "azurerm_postgresql_database" "postgresql_database" {
   for_each = var.settings.postgresql_databases
 
   name                = each.value.name
-  resource_group_name = var.resource_group_name
+  resource_group_name = local.resource_group_name
   server_name         = azurerm_postgresql_server.postgresql.name
   charset             = each.value.charset
   collation           = each.value.collation
