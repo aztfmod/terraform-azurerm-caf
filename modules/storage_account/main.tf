8a9a12776ade8c8fa5c3dee5305eedb7b6edf8c5
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-08 06:22:48 +0000

    Update storage account and diagnostocs

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -6,3 +6,17 @@ terraform {
   }
 
 }
+
+locals {
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    try(var.resource_group.tags, null),
+    try(var.storage_account.tags, null)
+    ) : merge(
+    try(var.storage_account.tags,
+    null)
+  )
+
+  location            = var.resource_group.location
+  resource_group_name = var.resource_group.name
+}
