f219abb727337142622c902123e20db9e69d5dd9
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-08 06:19:14 +0000

    Update redis

--- modules/redis_cache/variables.tf
+++ modules/redis_cache/variables.tf
@@ -1,13 +1,3 @@
-variable "resource_group_name" {
-  description = "(Required) The name of the resource group where to create the resource."
-  type        = string
-}
-
-variable "location" {
-  description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
-  type        = string
-}
-
 variable "tags" {
   description = "(Required) Map of tags to be applied to the resource"
   type        = map(any)
@@ -23,11 +13,6 @@ variable "subnet_id" {
 
 variable "global_settings" {
   description = "Global settings object (see module README.md)"
-}
-
-variable "base_tags" {
-  description = "Base tags for the resource to be inherited from the resource group."
-  type        = map(any)
 }
 
 variable "diagnostic_profiles" {
@@ -47,4 +32,11 @@ variable "private_dns" {
 }
 variable "client_config" {
   description = "Client configuration object (see module README.md)."
+}
+variable "resource_group" {
+  description = "Resource group object"
+}
+variable "base_tags" {
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = bool
 }
