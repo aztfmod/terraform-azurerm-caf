1d24a7bc7f329bbda22ed45af3048f7e1afa8064
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-10 10:13:09 +0000

    Update mariadb and private endpoint

--- modules/databases/mariadb_server/variables.tf
+++ modules/databases/mariadb_server/variables.tf
@@ -5,14 +5,6 @@ variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
 variable "settings" {}
-variable "resource_group_name" {
-  description = "(Required) The name of the resource group where to create the resource."
-  type        = string
-}
-variable "location" {
-  description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
-  type        = string
-}
 variable "keyvault_id" {}
 variable "storage_accounts" {}
 variable "azuread_groups" {}
@@ -26,9 +18,20 @@ variable "diagnostic_profiles" {
 variable "diagnostics" {
   default = {}
 }
+variable "location" {
+  description = "location of the resource if different from the resource group."
+  default     = null
+}
+variable "resource_group_name" {
+  description = "Resource group object to deploy the virtual machine"
+  default     = null
+}
+variable "resource_group" {
+  description = "Resource group object to deploy the virtual machine"
+}
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
-  type        = map(any)
+  type        = bool
 }
 variable "private_dns" {
   default = {}
