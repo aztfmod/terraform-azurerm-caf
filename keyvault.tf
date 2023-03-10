8d84b39dd528c27227d3e4546c9cccf8ae06f50c
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-10 09:53:13 +0000

    Update AKV

--- keyvault.tf
+++ keyvault.tf
@@ -11,8 +11,11 @@ module "keyvaults" {
   azuread_groups     = local.combined_objects_azuread_groups
   managed_identities = local.combined_objects_managed_identities
   private_dns        = local.combined_objects_private_dns
-  resource_group     = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
-  base_tags          = local.global_settings.inherit_tags
+
+  base_tags           = local.global_settings.inherit_tags
+  resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
+  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
+  location            = try(local.global_settings.regions[each.value.region], null)
 }
 
 #
