9bb341019c232d23d4c21b2490a846cb7bc54ae8
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-10 10:13:09 +0000

    Update mariadb and private endpoint

--- modules/networking/private_links/endpoints/main.tf
+++ modules/networking/private_links/endpoints/main.tf
@@ -8,8 +8,15 @@ terraform {
 }
 
 locals {
-  module_tag = {
-    "module" = basename(abspath(path.module))
-  }
-  tags = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    # try(local.resource_group.tags, null),
+    try(var.settings.tags, null)
+    ) : merge(
+    try(var.settings.tags,
+    null)
+  )
+
+  # location            = coalesce(var.location, var.resource_group.location)
+  # resource_group = var.resource_groups[try(each.value.lz_key, var.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
 }
--- modules/networking/private_links/endpoints/private_endpoint/main.tf
+++ modules/networking/private_links/endpoints/private_endpoint/main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 locals {
-  module_tag = {
-    "module" = basename(abspath(path.module))
-  }
-  tags = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    try(var.settings.tags, null)
+  ) : try(var.settings.tags, null)
 }
