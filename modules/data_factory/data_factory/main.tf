94553cc151e32477912678336cf15954627a91d1
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-10 10:17:12 +0000

    Update ADF

--- modules/data_factory/data_factory/main.tf
+++ modules/data_factory/data_factory/main.tf
@@ -7,10 +7,11 @@ terraform {
 
 }
 locals {
-  module_tag = {
-    "module" = basename(abspath(path.module))
-  }
-  tags                = merge(var.base_tags, local.module_tag, try(var.tags, null))
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    try(var.settings.tags, null)
+  ) : try(var.settings.tags, null)
+
   location            = can(var.settings.location) ? var.settings.location : var.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group.key, var.settings.resource_group_key)].location
   resource_group_name = can(var.settings.resource_group_name) || can(var.settings.resource_group.name) ? try(var.settings.resource_group_name, var.settings.resource_group.name) : var.resource_groups[try(var.settings.lz_key, var.settings.resource_group.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group_key, var.settings.resource_group.key)].name
 }
--- modules/data_factory/data_factory/variables.tf
+++ modules/data_factory/data_factory/variables.tf
@@ -18,7 +18,7 @@ variable "resource_groups" {
 }
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
-  type        = map(any)
+  type        = bool
 }
 variable "settings" {}
 variable "diagnostic_profiles" {
