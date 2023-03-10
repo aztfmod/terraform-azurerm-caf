a3736d1df3fe1d65b2337a377cb1965c906b5b66
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-10 09:53:13 +0000

    Update AKV

--- modules/security/keyvault/variables.tf
+++ modules/security/keyvault/variables.tf
@@ -21,8 +21,16 @@ variable "diagnostics" {
 variable "private_dns" {
   default = {}
 }
+variable "location" {
+  description = "location of the resource if different from the resource group."
+  default     = null
+}
+variable "resource_group_name" {
+  description = "Resource group object to deploy the virtual machine"
+  default     = null
+}
 variable "resource_group" {
-  description = "Resource group object"
+  description = "Resource group object to deploy the virtual machine"
 }
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
