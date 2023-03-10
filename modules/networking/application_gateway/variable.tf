b2644c6705ffce628e1fe3ea6fdabae990071800
lolorol <LaurentLesle@users.noreply.github.com>
2023-03-10 02:09:32 +0000

    Fix tags app_gateway

--- modules/networking/application_gateway/variable.tf
+++ modules/networking/application_gateway/variable.tf
@@ -6,13 +6,16 @@ variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
 variable "diagnostics" {}
-variable "resource_group_name" {
-  description = "(Required) The name of the resource group where to create the resource."
-  type        = string
-}
 variable "location" {
-  description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
-  type        = string
+  description = "(Required) Resource Location"
+  default     = null
+}
+variable "resource_group_name" {
+  description = "Resource group object to deploy the virtual machine"
+  default     = null
+}
+variable "resource_group" {
+  description = "Resource group object to deploy the virtual machine"
 }
 variable "public_ip_addresses" {
   default = {}
@@ -49,7 +52,7 @@ variable "sku_tier" {
 
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
-  type        = map(any)
+  type        = bool
 }
 variable "private_dns" {
   default = {}
