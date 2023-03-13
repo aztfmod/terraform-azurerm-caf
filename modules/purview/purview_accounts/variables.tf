c5292a6790216c9c51b07f1161f374dc28a553fe
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-13 11:29:41 +0000

    Update purview

--- modules/purview/purview_accounts/variables.tf
+++ modules/purview/purview_accounts/variables.tf
@@ -11,8 +11,16 @@ variable "remote_objects" {
   description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
   default     = {}
 }
+variable "location" {
+  description = "location of the resource if different from the resource group."
+  default     = null
+}
+variable "resource_group_name" {
+  description = "Resource group object to deploy the virtual machine"
+  default     = null
+}
 variable "resource_group" {
-  description = "Resource group object"
+  description = "Resource group object to deploy the virtual machine"
 }
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
