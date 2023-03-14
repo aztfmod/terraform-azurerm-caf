8868a12ce08a9bf60bfd538deaa2c6758f5e5c89
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-14 11:18:06 +0000

    Update PPG

--- proximity_placement_groups.tf
+++ proximity_placement_groups.tf
@@ -7,8 +7,12 @@ module "proximity_placement_groups" {
   global_settings = local.global_settings
   client_config   = local.client_config
   name            = each.value.name
-  resource_group  = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
-  base_tags       = local.global_settings.inherit_tags
+  tags            = try(each.value.tags, null)
+
+  base_tags           = local.global_settings.inherit_tags
+  resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
+  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
+  location            = try(local.global_settings.regions[each.value.region], null)
 }
 
 
