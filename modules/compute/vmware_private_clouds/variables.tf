23a64d7d57216e61b7d65c7f72d8a088562cf445
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-14 11:20:09 +0000

    Update vmware

--- modules/compute/vmware_private_clouds/variables.tf
+++ modules/compute/vmware_private_clouds/variables.tf
@@ -4,8 +4,16 @@ variable "global_settings" {
 variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
+variable "location" {
+  description = "location of the resource if different from the resource group."
+  default     = null
+}
+variable "resource_group_name" {
+  description = "Resource group object to deploy the virtual machine"
+  default     = null
+}
 variable "resource_group" {
-  description = "Resource group object"
+  description = "Resource group object to deploy the virtual machine"
 }
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
