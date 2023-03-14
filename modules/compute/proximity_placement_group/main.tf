365f1696c10fb6589d406f674548ef75e070f81a
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-14 11:18:06 +0000

    Update PPG

--- modules/compute/proximity_placement_group/main.tf
+++ modules/compute/proximity_placement_group/main.tf
@@ -9,18 +9,12 @@ terraform {
 
 
 locals {
-  module_tag = {
-    "module" = basename(abspath(path.module))
-  }
   tags = var.base_tags ? merge(
     var.global_settings.tags,
     try(var.resource_group.tags, null),
     try(var.tags, null)
-    ) : merge(
-    try(var.tags,
-    null)
-  )
+  ) : try(var.tags, null)
 
-  location            = var.resource_group.location
-  resource_group_name = var.resource_group.name
+  location            = coalesce(var.location, var.resource_group.location)
+  resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
 }
--- modules/compute/proximity_placement_group/variables.tf
+++ modules/compute/proximity_placement_group/variables.tf
@@ -9,8 +9,16 @@ variable "tags" {
 }
 variable "name" {}
 
+variable "location" {
+  description = "location of the resource if different from the resource group."
+  default     = null
+}
+variable "resource_group_name" {
+  description = "Resource group object to deploy the virtual machine"
+  default     = null
+}
 variable "resource_group" {
-  description = "Resource group object"
+  description = "Resource group object to deploy the virtual machine"
 }
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
