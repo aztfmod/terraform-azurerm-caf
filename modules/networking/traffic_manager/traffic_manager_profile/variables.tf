452ada725e0a88833daf0d6d4099eb1e185f81c1
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-14 11:19:28 +0000

    Update traffic manager

--- modules/networking/traffic_manager/traffic_manager_profile/variables.tf
+++ modules/networking/traffic_manager/traffic_manager_profile/variables.tf
@@ -4,8 +4,12 @@ variable "global_settings" {
 variable "settings" {
   default = {}
 }
+variable "resource_group_name" {
+  description = "Resource group object to deploy the virtual machine"
+  default     = null
+}
 variable "resource_group" {
-  description = "Resource group object"
+  description = "Resource group object to deploy the virtual machine"
 }
 variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
