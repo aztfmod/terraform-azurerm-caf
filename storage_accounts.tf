b0713b21c0a7308bf4d94c7bd864205206efef03
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-14 11:21:04 +0000

    Update SA

--- storage_accounts.tf
+++ storage_accounts.tf
@@ -15,10 +15,13 @@ module "storage_accounts" {
   private_dns               = local.combined_objects_private_dns
   private_endpoints         = try(each.value.private_endpoints, {})
   recovery_vaults           = local.combined_objects_recovery_vaults
-  resource_group            = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
-  base_tags                 = local.global_settings.inherit_tags
   storage_account           = each.value
   vnets                     = local.combined_objects_networking
+
+  base_tags           = local.global_settings.inherit_tags
+  resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
+  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
+  location            = try(local.global_settings.regions[each.value.region], null)
 }
 
 output "storage_accounts" {
