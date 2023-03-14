5ee160a506c9d6b015e4f3fe24ae3450b22637e0
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-14 11:20:40 +0000

    Update wvd

--- wvd_workspaces.tf
+++ wvd_workspaces.tf
@@ -4,10 +4,13 @@ module "wvd_workspaces" {
 
   global_settings     = local.global_settings
   settings            = each.value
-  resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
-  base_tags           = local.global_settings.inherit_tags
   diagnostics         = local.combined_diagnostics
   diagnostic_profiles = try(each.value.diagnostic_profiles, {})
+
+  base_tags           = local.global_settings.inherit_tags
+  resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
+  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
+  location            = try(local.global_settings.regions[each.value.region], null)
 }
 
 output "wvd_workspaces" {
