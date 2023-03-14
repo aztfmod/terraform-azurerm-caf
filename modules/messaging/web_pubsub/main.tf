2cb00c1f3c690e3171604bb8a2d25dd5719dc6ca
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2023-03-14 11:21:28 +0000

    Update webpubsub

--- modules/messaging/web_pubsub/main.tf
+++ modules/messaging/web_pubsub/main.tf
@@ -7,10 +7,12 @@ terraform {
 }
 
 locals {
-  module_tag = {
-    "module" = basename(abspath(path.module))
-  }
-  tags      = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
-  location  = can(var.settings.location) ? var.settings.location : var.remote_objects.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][var.settings.resource_group.key].location
-  base_tags = try(var.global_settings.inherit_tags, false) ? var.remote_objects.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][var.settings.resource_group.key].tags : {}
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    try(var.resource_group.tags, null),
+    try(var.settings.tags, null)
+  ) : try(var.settings.tags, null)
+
+  location            = coalesce(var.location, var.resource_group.location)
+  resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
 }
