41a1a87ec717d7c94e7bc2c5fae183084cd0eb2f
Nick Metz <nick.metz@volocopter.com>
2023-03-24 09:14:13 +0100

    Add outputs

--- aadb2c.tf
+++ aadb2c.tf
@@ -8,3 +8,7 @@ module "aadb2c_directory" {
   resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
 }
+
+output "aadb2c_directory" {
+  value = module.aadb2c_directory
+}
