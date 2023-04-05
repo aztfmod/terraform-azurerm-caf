4205b43158312ee5cef40f3996c196f9c9410318
Rajasekhar <rajasekhar.ketham@gmail.com>
2023-04-05 10:33:12 +0000

    initial commit - private dns resolvers

---
+++ modules/networking/private-dns-resolvers/variables.tf
@@ -0,0 +1,24 @@
+variable "global_settings" {
+  description = "Global settings object (see module README.md)"
+}
+variable "client_config" {
+  description = "Client configuration object (see module README.md)."
+}
+
+variable "settings" {
+}
+
+variable "resource_group_name" {
+}
+variable "resource_group" {
+}
+
+variable "location" {
+}
+
+variable "virtual_network_id" {
+}
+variable "inherit_tags" {
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = bool
+}
