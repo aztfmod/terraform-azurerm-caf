e3d99ec3c921d928ae94e3908d0c87a8b9f4d834
jorseng <34156375+jorseng@users.noreply.github.com>
2023-04-11 02:43:08 +0000

    add azapi to cert-issuer

--- keyvault_certificate_issuers.tf
+++ keyvault_certificate_issuers.tf
@@ -10,7 +10,8 @@ module "keyvault_certificate_issuers" {
   global_settings = local.global_settings
   settings        = each.value
   keyvault_id     = can(each.value.keyvault_id) ? each.value.keyvault_id : local.combined_objects_keyvaults[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.keyvault_key].id
-  password        = try(data.azurerm_key_vault_secret.certificate_issuer_password[each.key].value, each.value.cert_issuer_password)
+  # password        = try(data.azurerm_key_vault_secret.certificate_issuer_password[each.key].value, each.value.cert_issuer_password)
+  password        = can(each.value.cert_secret_name) ? jsondecode(data.azapi_resource.certificate_issuer_password.output).properties.0.value : data.azurerm_key_vault_secret.certificate_issuer_password[0].value
 }
 
 data "azurerm_key_vault_secret" "certificate_issuer_password" {
@@ -22,6 +23,19 @@ data "azurerm_key_vault_secret" "certificate_issuer_password" {
 
   name         = var.security.dynamic_keyvault_secrets[each.value.keyvault_key][each.value.cert_password_key].secret_name
   key_vault_id = can(each.value.key_vault_id) ? each.value.key_vault_id : local.combined_objects_keyvaults[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.keyvault_key].id
+}
+
+data "azapi_resource" "certificate_issuer_password" {
+  depends_on = [module.dynamic_keyvault_secrets]
+  for_each = {
+    for key, value in local.security.keyvault_certificate_issuers : key => value
+    if try(value.cert_secret_name, null) != null
+  }
+
+  type      = "Microsoft.KeyVault/vaults/secrets@2022-07-01"
+  parent_id = can(each.value.key_vault_id) ? each.value.key_vault_id : local.combined_objects_keyvaults[try(each.value.lz_key, local.client_config.landingzone_key)][each.value.keyvault_key].id
+  name      = each.value.cert_secret_name
+
 }
 
 output "keyvault_certificate_issuers" {
