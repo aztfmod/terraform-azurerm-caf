ed901e15ad546333425d78495e30aa35e6cfa5b5
Simon Schneider <simon.schneider@volocopter.com>
2023-04-19 11:43:00 +0000

    add caf names and ativate features

--- modules/iot/iot_hub_dps/iot_hub_dps.tf
+++ modules/iot/iot_hub_dps/iot_hub_dps.tf
@@ -11,13 +11,12 @@ resource "azurecaf_name" "iothdps" {
 }
 
 resource "azurerm_iothub_dps" "iothubdps" {
-  name                   = azurecaf_name.iothdps.result
-  resource_group_name    = var.resource_group_name
-  location               = var.location 
-  allocation_policy      = try(var.settings.allocation_policy, null)
-  
-  # requires azurerm provider >= 2.97.0
-  # public_network_access_enabled   = try(var.settings.public_network_access_enabled, true)
+  name                = azurecaf_name.iothdps.result
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_policy   = try(var.settings.allocation_policy, null)
+
+  public_network_access_enabled = try(var.settings.public_network_access_enabled, true)
 
   sku {
     name     = var.settings.sku.name
@@ -27,24 +26,23 @@ resource "azurerm_iothub_dps" "iothubdps" {
   dynamic "linked_hub" {
     for_each = lookup(var.settings, "linked_hub", {}) == {} ? [] : [1]
     content {
-      connection_string         = var.settings.linked_hub.connection_string 
-      location                  = var.settings.linked_hub.location 
-      apply_allocation_policy   = try(var.settings.linked_hub.apply_allocation_policy, null)
-      allocation_weight         = try(var.settings.linked_hub.allocation_weight, null)
-      hostname                  = try(var.settings.linked_hub.hostname, null)
+      connection_string       = var.settings.linked_hub.connection_string
+      location                = var.settings.linked_hub.location
+      apply_allocation_policy = try(var.settings.linked_hub.apply_allocation_policy, null)
+      allocation_weight       = try(var.settings.linked_hub.allocation_weight, null)
+      hostname                = try(var.settings.linked_hub.hostname, null)
     }
   }
 
-  # requires azurerm provider >= 2.99.0
-  # dynamic "ip_filter_rule" {
-  #   for_each = lookup(var.settings, "ip_filter_rule", {}) == {} ? [] : [1]
-  #   content {
-  #     name             = var.settings.ip_filter_rule.name 
-  #     ip_mask          = var.settings.ip_filter_rule.ip_mask
-  #     action           = var.settings.ip_filter_rule.action
-  #     target           = try(var.settings.ip_filter_rule.target, null)
-  #   }
-  # }
+  dynamic "ip_filter_rule" {
+    for_each = lookup(var.settings, "ip_filter_rule", {}) == {} ? [] : [1]
+    content {
+      name    = var.settings.ip_filter_rule.name
+      ip_mask = var.settings.ip_filter_rule.ip_mask
+      action  = var.settings.ip_filter_rule.action
+      target  = try(var.settings.ip_filter_rule.target, null)
+    }
+  }
 
   tags = merge(local.tags, lookup(var.settings, "tags", {}))
 }
