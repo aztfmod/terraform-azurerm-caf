6aef4a89ec5424b30ddce3230a1a1b27cc97aeb3
Simon Schneider <simon.schneider@volocopter.com>
2023-04-19 11:43:00 +0000

    add caf names and ativate features

--- modules/iot/iot_hub_dps/output.tf
+++ modules/iot/iot_hub_dps/output.tf
@@ -3,13 +3,13 @@ output "id" {
 }
 
 output "device_provisioning_host_name" {
-  value = azurerm_iothub_dps.iothubdps.device_provisioning_host_name 
+  value = azurerm_iothub_dps.iothubdps.device_provisioning_host_name
 }
 
 output "id_scope" {
-  value = azurerm_iothub_dps.iothubdps.id_scope 
+  value = azurerm_iothub_dps.iothubdps.id_scope
 }
 
 output "service_operations_host_name" {
-  value = azurerm_iothub_dps.iothubdps.service_operations_host_name 
+  value = azurerm_iothub_dps.iothubdps.service_operations_host_name
 }
