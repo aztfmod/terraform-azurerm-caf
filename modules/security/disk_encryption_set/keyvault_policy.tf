f9c2b9ca40193542dfe964b0f20f77981288afb3
Shanoor HASSANALY <17402800+shanoor@users.noreply.github.com>
2023-04-24 11:40:11 +0200

    Added option to use a custom key id for disk encryption set

--- modules/security/disk_encryption_set/keyvault_policy.tf
+++ modules/security/disk_encryption_set/keyvault_policy.tf
@@ -1,4 +1,5 @@
 resource "azurerm_key_vault_access_policy" "des" {
+  count        = try(var.keyvault_id, null) == null ? 0 : 1
   key_vault_id = var.keyvault_id
 
   tenant_id = azurerm_disk_encryption_set.encryption_set.identity.0.tenant_id
