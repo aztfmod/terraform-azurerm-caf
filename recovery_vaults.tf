e8a6a09cf9e52201eb22c4092773e1f94808b40c
shanoor <17402800+shanoor@users.noreply.github.com>
2023-04-26 17:14:30 +0200

    fix keyvault_policy

--- recovery_vaults.tf
+++ recovery_vaults.tf
@@ -14,7 +14,8 @@ module "recovery_vaults" {
   base_tags         = local.global_settings.inherit_tags
 
   # support for CMK encryption
-  key_id = can(each.value.encryption.key_id) ? each.value.encryption.key_id : try(local.combined_objects_keyvault_keys[try(each.value.encryption.keyvault_key.lz_key, local.client_config.landingzone_key)][try(each.value.encryption.key_vault_key_key, each.value.encryption.key_vault_key.key)].id, null)
+  # key_id = can(each.value.encryption.key_id) ? each.value.encryption.key_id : try(local.combined_objects_keyvault_keys[try(each.value.encryption.keyvault_key.lz_key, local.client_config.landingzone_key)][try(each.value.encryption.key_vault_key_key, each.value.encryption.key_vault_key.key)].id, null)
+  key_id = try(local.combined_objects_keyvault_keys[try(each.value.encryption.keyvault_key.lz_key, local.client_config.landingzone_key)][try(each.value.encryption.key_vault_key_key, each.value.encryption.key_vault_key.key)].id, null)
   # try(local.combined_objects_keyvault_keys[try(each.value.encryption.keyvault_key.lz_key, local.client_config.landingzone_key)][each.value.encryption.keyvault_key.key].id, null)
   # keyvault_id = can(each.value.encryption) ? (can(each.value.encryption.key_id) ? null : local.combined_objects_keyvaults[try(each.value.encryption.keyvault.lz_key, local.client_config.landingzone_key)][each.value.encryption.keyvault.key].id) : null
 }
