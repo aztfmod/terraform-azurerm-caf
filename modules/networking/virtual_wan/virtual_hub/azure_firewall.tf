cc9a4b0ee9791788cf5cba1155c355867cb269de
lolorol <LaurentLesle@users.noreply.github.com>
2023-05-04 06:28:22 +0000

    Fix legacy firewall in vhub

--- modules/networking/virtual_wan/virtual_hub/azure_firewall.tf
+++ modules/networking/virtual_wan/virtual_hub/azure_firewall.tf
@@ -11,47 +11,30 @@ resource "azurecaf_name" "virtualhub_fw" {
   use_slug      = var.global_settings.use_slug
 }
 
-moved {
-  from = azurerm_template_deployment.arm_template_vhub_firewall
-  to   = azurerm_resource_group_template_deployment.arm_template_vhub_firewall
-}
 
 # As per https://docs.microsoft.com/en-us/azure/templates/microsoft.network/2019-09-01/azurefirewalls
 resource "azurerm_resource_group_template_deployment" "arm_template_vhub_firewall" {
   count               = try(var.virtual_hub_config.deploy_firewall, false) ? 1 : 0
-  name                = azurecaf_name.virtualhub_fw.0.result
+  name                = "arm_template_vhub_firewall"
   resource_group_name = var.resource_group_name
 
   template_content = file("${path.module}/arm_template_vhub_firewall.json")
 
   parameters_content = jsonencode(
     {
-      "vwan_id"  = azurerm_virtual_hub.vwan_hub.id,
-      "name"     = var.virtual_hub_config.firewall_name,
-      "location" = var.location,
-      "Tier"     = "Standard",
+      vwan_id  = {
+        value = azurerm_virtual_hub.vwan_hub.id
+      }
+      name     = {
+        value = var.virtual_hub_config.firewall_name
+      }
+      location = {
+        value = var.location
+      }
+      Tier     = {
+        value = "Standard"
+      }
     }
   )
   deployment_mode = "Incremental"
-}
-
-
-resource "null_resource" "arm_template_vhub_firewall" {
-  count = try(var.virtual_hub_config.deploy_firewall, false) ? 1 : 0
-
-  triggers = {
-    resource_id = jsondecode(azurerm_resource_group_template_deployment.arm_template_vhub_firewall[0].output_content).resourceID
-  }
-
-  provisioner "local-exec" {
-    command     = format("%s/scripts/destroy_resource.sh", path.module)
-    when        = destroy
-    interpreter = ["/bin/bash"]
-    on_failure  = continue
-
-    environment = {
-      RESOURCE_IDS = self.triggers.resource_id
-    }
-  }
-
 }
