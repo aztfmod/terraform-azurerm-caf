3ec2e7d93af563b918e29750d49c761dc1797e1b
lolorol <lalesle@microsoft.com>
2023-05-19 07:15:04 +0000

    Fix failover group

--- modules/databases/mssql_managed_instance_v1/failover_group/variables.tf
+++ modules/databases/mssql_managed_instance_v1/failover_group/variables.tf
@@ -4,7 +4,6 @@ variable "global_settings" {
 variable "settings" {}
 variable "managed_instance" {
   description = "(Required) The primary SQL Managed Instance object which will be replicated using a SQL Instance Failover Group. Changing this forces a new SQL Instance Failover Group to be created."
-  type        = string
 }
 variable "partner_managed_instance_id" {
   description = "(Required) ID of the SQL Managed Instance which will be replicated to. Changing this forces a new resource to be created."
