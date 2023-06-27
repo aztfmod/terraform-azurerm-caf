cb1a725d4fab4dd61331ea88882734ce2e5a0de2
lolorol <laurent.lesle@gmail.com>
2023-06-27 00:03:45 +0000

    Add vm extension for azdo and tfcloud + precond check

--- modules/compute/virtual_machine_extensions/generic_extensions.tf
+++ modules/compute/virtual_machine_extensions/generic_extensions.tf
@@ -4,12 +4,27 @@ resource "azurerm_virtual_machine_extension" "genext" {
     if var.extension_name == "generic_extension"
   }
 
-  virtual_machine_id   = var.virtual_machine_id
-  name                 = try(each.value.name, null)
-  publisher            = try(each.value.publisher, null)
-  type                 = try(each.value.type, null)
-  type_handler_version = try(each.value.type_handler_version, null)
+  virtual_machine_id          = var.virtual_machine_id
+  name                        = try(each.value.name, null)
+  publisher                   = try(each.value.publisher, null)
+  type                        = try(each.value.type, null)
+  type_handler_version        = try(each.value.type_handler_version, null)
+  auto_upgrade_minor_version  = try(each.value.auto_upgrade_minor_version, null)
+  automatic_upgrade_enabled   = try(each.value.automatic_upgrade_enabled, null)
+  failure_suppression_enabled = try(each.value.failure_suppression_enabled, null)
 
   settings           = try(each.value.settings, null)
   protected_settings = try(each.value.protected_settings, null)
+
+  lifecycle {
+    precondition {
+      condition = anytrue(
+        [
+          for status in jsondecode(data.azapi_resource_action.azurerm_virtual_machine_status.output).statuses : "true"
+          if status.code == "PowerState/running"
+        ]
+      )
+      error_message = format("The virtual machine (%s) must be in running state to be able to deploy or modify the vm extension.", var.virtual_machine_id)
+    }
+  }
 }
