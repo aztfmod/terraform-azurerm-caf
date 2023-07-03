1c443dc18848dda4a9453dd9a77341fbefafd151
Kévin DELMONT <kevin.delmont@visiativ.com>
2023-07-03 11:33:53 +0200

    Add powerbi embedded module

--- locals.tf
+++ locals.tf
@@ -446,4 +446,6 @@ locals {
     iot_security_device_group           = try(var.iot.iot_security_device_group, {})
     iot_central_application             = try(var.iot.iot_central_application, {})
   }
+  
+  powerbi_embedded = try(var.powerbi_embedded, {})  
 }
