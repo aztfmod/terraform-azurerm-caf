44781837ec9ce4e4ff1f2cc6a0041829c551739c
shanoor <17402800+shanoor@users.noreply.github.com>
2023-07-17 15:22:02 +0200

    Added support for UserAssigned msi to disk encryption set

--- modules/security/disk_encryption_set/disk_encryption_set.tf
+++ modules/security/disk_encryption_set/disk_encryption_set.tf
@@ -10,7 +10,14 @@ resource "azurerm_disk_encryption_set" "encryption_set" {
   encryption_type           = try(var.settings.encryption_type, null)
 
   identity {
-    type = "SystemAssigned"
+    type = try(var.settings.identity.type, "SystemAssigned")
+
+    # if type contains UserAssigned, `identity_ids` is mandatory
+    identity_ids = try(regex("UserAssigned", var.settings.identity.type), null) != null ? flatten([
+      for managed_identity in var.settings.identity.managed_identities : [
+        var.managed_identities[try(managed_identity.lz_key, var.client_config.landingzone_key)][managed_identity.key].id
+      ]
+    ]) : null
   }
   tags = merge(var.base_tags, try(var.settings.tags, null))
 }
