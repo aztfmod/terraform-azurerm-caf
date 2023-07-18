99c9d5c89114d28b1f2e485344a655bd32d1a999
shanoor <17402800+shanoor@users.noreply.github.com>
2023-07-18 09:23:07 +0200

    Added userassigned msi to recovery vaults

--- modules/recovery_vault/variables.tf
+++ modules/recovery_vault/variables.tf
@@ -23,3 +23,4 @@ variable "base_tags" {
   description = "Base tags for the resource to be inherited from the resource group."
   type        = bool
 }
+variable "managed_identities" {}
--- recovery_vaults.tf
+++ recovery_vaults.tf
@@ -1,21 +1,20 @@
-
 module "recovery_vaults" {
   source   = "./modules/recovery_vault"
   for_each = local.shared_services.recovery_vaults
 
-  global_settings   = local.global_settings
-  client_config     = local.client_config
-  settings          = each.value
-  diagnostics       = local.combined_diagnostics
-  identity          = try(each.value.identity, null)
-  vnets             = try(local.combined_objects_networking, {})
-  private_endpoints = try(each.value.private_endpoints, {})
-  private_dns       = local.combined_objects_private_dns
-  resource_group    = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
-  base_tags         = local.global_settings.inherit_tags
+  global_settings    = local.global_settings
+  client_config      = local.client_config
+  settings           = each.value
+  diagnostics        = local.combined_diagnostics
+  identity           = try(each.value.identity, null)
+  vnets              = try(local.combined_objects_networking, {})
+  private_endpoints  = try(each.value.private_endpoints, {})
+  private_dns        = local.combined_objects_private_dns
+  resource_group     = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)]
+  base_tags          = local.global_settings.inherit_tags
+  managed_identities = local.combined_objects_managed_identities
 }
 
 output "recovery_vaults" {
   value = module.recovery_vaults
-
 }
