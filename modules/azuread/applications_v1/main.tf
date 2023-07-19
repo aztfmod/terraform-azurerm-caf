d9042821d5025eaa7c5d8e20993391ab211e87a0
James Leone <james.leone@ibm.com>
2023-07-19 17:17:25 +0000

    formatting

--- modules/azuread/applications_v1/main.tf
+++ modules/azuread/applications_v1/main.tf
@@ -1,9 +1,8 @@
 terraform {
-  
+
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      
+      source = "hashicorp/azurerm"
     }
     azuread = {
       source = "hashicorp/azuread"
--- modules/azuread/service_principal/main.tf
+++ modules/azuread/service_principal/main.tf
@@ -1,5 +1,4 @@
 terraform {
-
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
