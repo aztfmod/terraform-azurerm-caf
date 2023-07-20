3614f4ce65ce24fd054ac9d8028faf74c9d021e1
Janik Muenk <janik.muenk@adesso.de>
2023-07-20 10:52:05 +0000

    fix(keyvault): adding compatibility for resourcegroup_key + lz_key style references for compatibility purposes

--- keyvault.tf
+++ keyvault.tf
@@ -14,7 +14,7 @@ module "keyvaults" {
   private_dns        = local.combined_objects_private_dns
 
   base_tags           = local.global_settings.inherit_tags
-  resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
+  resource_group      = local.combined_objects_resource_groups[try(try(each.value.resource_group.lz_key,each.value.lz_key), local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
   location            = try(local.global_settings.regions[each.value.region], null)
 }
