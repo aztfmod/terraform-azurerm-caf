57dcccda1945391579dbb9f15d6e60d9c4a525df
Mykhailo Danyliuk <mykhailo.danyliuk@ryan.com>
2023-07-21 15:19:57 +0300

    Add Private Link endpoints for the App Configuration module

--- app_config.tf
+++ app_config.tf
@@ -7,6 +7,8 @@ module "app_config" {
   combined_objects    = local.dynamic_app_config_combined_objects
   global_settings     = local.global_settings
   settings            = each.value
+  vnets               = local.combined_objects_networking
+  private_dns         = local.combined_objects_private_dns
   base_tags           = local.global_settings.inherit_tags
   resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
