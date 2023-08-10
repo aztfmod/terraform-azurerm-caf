4356a1b23ded2be7a4c706e471044c168f8540cb
shanoor <17402800+shanoor@users.noreply.github.com>
2023-08-10 15:47:23 +0200

    Fixed custom script location priority (remote must be first)

--- modules/compute/virtual_machine_extensions/custom_script.tf
+++ modules/compute/virtual_machine_extensions/custom_script.tf
@@ -69,10 +69,9 @@ locals {
   protected_settings = merge(local.map_command, local.system_assigned_id, local.user_assigned_id)
 
   # fileuris
-  fileuri_sa_key  = try(var.extension.fileuri_sa_key, "")
-  fileuri_sa_path = try(var.extension.fileuri_sa_path, "")
-  fileuri_sa = local.fileuri_sa_key != "" ? try(var.storage_accounts[var.client_config.landingzone_key][var.extension.fileuri_sa_key].primary_blob_endpoint,
-  var.storage_accounts[var.extension.lz_key][var.extension.fileuri_sa_key].primary_blob_endpoint) : ""
+  fileuri_sa_key       = try(var.extension.fileuri_sa_key, "")
+  fileuri_sa_path      = try(var.extension.fileuri_sa_path, "")
+  fileuri_sa           = local.fileuri_sa_key != "" ? try(var.storage_accounts[var.extension.lz_key][var.extension.fileuri_sa_key].primary_blob_endpoint, var.storage_accounts[var.client_config.landingzone_key][var.extension.fileuri_sa_key].primary_blob_endpoint) : ""
   fileuri_sa_full_path = "${local.fileuri_sa}${local.fileuri_sa_path}"
   fileuri_sa_defined   = try(var.extension.fileuris, "")
   fileuris             = local.fileuri_sa_defined == "" ? [local.fileuri_sa_full_path] : var.extension.fileuris
