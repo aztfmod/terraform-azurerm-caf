7923cd755cee046542fe5ef0cefc6cc179f11907
Muhammad Hamza <m.hamzajaved04@gmail.com>
2023-08-21 18:30:39 +0500

    Added owners argument for spn

--- modules/azuread/applications/module.tf
+++ modules/azuread/applications/module.tf
@@ -113,6 +113,9 @@ resource "azuread_service_principal" "app" {
   application_id               = azuread_application.app.application_id
   app_role_assignment_required = try(var.settings.app_role_assignment_required, false)
   tags                         = try(var.settings.tags, null)
+  owners = [
+    var.client_config.object_id
+  ]
 }
 
 resource "azuread_service_principal_password" "pwd" {
