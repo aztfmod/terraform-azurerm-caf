2d5b95671e5a06dcfd1395ae27d6da08e4bd114f
shanoor <17402800+shanoor@users.noreply.github.com>
2023-08-22 11:08:26 +0200

    Update modules/recovery_vault/recovery_vault.tf
    Co-authored-by: Arnaud Lheureux <arnaudlh@users.noreply.github.com>

--- modules/recovery_vault/recovery_vault.tf
+++ modules/recovery_vault/recovery_vault.tf
@@ -36,7 +36,7 @@ resource "azurerm_recovery_services_vault" "asr" {
 
     content {
       key_id                            = var.settings.encryption.key_id
-      infrastructure_encryption_enabled = try(var.settings.encryption.infrastructure_encryption_enabled, true)
+      infrastructure_encryption_enabled = try(var.settings.encryption.infrastructure_encryption_enabled, null)
       use_system_assigned_identity      = can(var.settings.encryption.user_assigned_identity) ? false : true
       user_assigned_identity_id         = var.managed_identities[try(var.settings.encryption.user_assigned_identity.lz_key, var.client_config.landingzone_key)][var.settings.encryption.user_assigned_identity.key].id
     }
