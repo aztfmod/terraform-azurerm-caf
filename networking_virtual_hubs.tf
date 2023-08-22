a3ebc4ed8cd5105413d4aa5a0b9053ffbf3223d8
brangaadmin <61812164+brangaadmin@users.noreply.github.com>
2023-08-22 08:59:07 +0530

    changed the modified location

--- networking_virtual_hubs.tf
+++ networking_virtual_hubs.tf
@@ -18,7 +18,7 @@ module "virtual_hubs" {
   client_config       = local.client_config
   global_settings     = local.global_settings
   public_ip_addresses = local.combined_objects_public_ip_addresses
-  location            = try(local.global_settings.regions[each.value.region], null)
+  location            = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
   tags                = try(local.global_settings.inherit_tags, false) ? merge(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][each.value.resource_group.key].tags, try(each.value.tags, null)) : {}
   virtual_hub_config  = each.value
