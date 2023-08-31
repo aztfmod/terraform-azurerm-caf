1d5184d311aa8cb52c250fe31ffdd3719fea1cdc
Simon Jackson <simon.jackson@alltimetechnologies.co.uk>
2023-08-31 14:22:32 +0100

    masq errors produced

--- disk_encryption_sets.tf
+++ disk_encryption_sets.tf
@@ -13,7 +13,7 @@ module "disk_encryption_sets" {
   base_tags           = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
   key_vault_key_id    = can(each.value.key_vault_key_id) || can(each.value.key_vault_key.id) ? try(each.value.key_vault_key_id, each.value.key_vault_key.id) : local.combined_objects_keyvault_keys[try(each.value.keyvault_key.lz_key, local.client_config.landingzone_key)][try(each.value.key_vault_key_key, each.value.key_vault_key.key)].id
   keyvault_id         = can(each.value.key_vault_key_id) ? null : local.combined_objects_keyvaults[try(each.value.keyvault.lz_key, local.client_config.landingzone_key)][each.value.keyvault.key].id
-  managed_identities  = local.combined_objects_managed_identities
+  #managed_identities  = local.combined_objects_managed_identities
 }
 
 module "disk_encryption_sets_external" {
@@ -30,7 +30,7 @@ module "disk_encryption_sets_external" {
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
   base_tags           = try(local.global_settings.inherit_tags, false) ? try(local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags, {}) : {}
   key_vault_key_id    = can(each.value.key_vault_key_id) ? each.value.key_vault_key_id : each.value.key_vault_key.id
-  managed_identities  = local.combined_objects_managed_identities
+  #managed_identities  = local.combined_objects_managed_identities
 }
 
 
