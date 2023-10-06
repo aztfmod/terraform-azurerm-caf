d9335a737f386bcecc92e842dace939c46c1c074
serco-karleveleigh <71755319+serco-karleveleigh@users.noreply.github.com>
2023-10-06 14:59:03 +0100

    fix aad_auth

--- modules/monitoring/monitor_action_group/monitor_action_group.tf
+++ modules/monitoring/monitor_action_group/monitor_action_group.tf
@@ -123,12 +123,12 @@ resource "azurerm_monitor_action_group" "this" {
       use_common_alert_schema = try(webhook_receiver.value.use_common_alert_schema, false)
 
       dynamic "aad_auth" {
-        # for_each = try(webhook_receiver.value.aad_auth, null) == null ? [] : [1]
-        for_each = try(webhook_receiver.value.aad_auth, {})
+        for_each = try(webhook_receiver.value.aad_auth, null) == null ? [] : [1]
+
         content {
-          object_id      = aad_auth.value.object_id
-          identifier_uri = try(aad_auth.value.identifier_uri, null)
-          tenant_id      = try(aad_auth.value.tenant_id, null)
+          object_id      = aad_auth.object_id
+          identifier_uri = try(aad_auth.identifier_uri, null)
+          tenant_id      = try(aad_auth.tenant_id, null)
         }
       }
     }
