b38f8a7f854007e4ce42399c59fae39ff7411d62
thanhlcao <thanhlcao@kms.com.vn>
2023-10-22 14:06:35 +0700

    update code

--- modules/recovery_vault/backup_policies_vm_workload.tf
+++ modules/recovery_vault/backup_policies_vm_workload.tf
@@ -15,20 +15,20 @@ resource "azurerm_backup_policy_vm_workload" "sql" {
     policy_type = each.value.policy_type
 
     dynamic "backup" {
-      for_each = lookup(each.value, "retention_daily", null) == "Daily" ? [1] : []
+      for_each = lookup(each.value, each.value.backup.frequency, null) == "Daily" ? [1] : []
 
       content {
-        frequency            = "Daily"
+        frequency            = each.value.backup.frequency
         frequency_in_minutes = each.value.backup.frequency_in_minutes
         time                 = each.value.backup.time
       }
     }
 
     dynamic "backup" {
-      for_each = lookup(each.value, "retention_daily", null) == "Weekly" ? [1] : []
+      for_each = lookup(each.value, each.value.backup.frequency, null) == "Weekly" ? [1] : []
 
       content {
-        frequency            = "Weekly"
+        frequency            = each.value.backup.frequency
         frequency_in_minutes = each.value.backup.frequency_in_minutes
         time                 = each.value.backup.time
         weekdays             = each.value.backup.weekdays
