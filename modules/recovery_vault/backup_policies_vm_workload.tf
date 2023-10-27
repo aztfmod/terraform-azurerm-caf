e6b29d3aaa0b9ed0e55974d5e9a077854453f490
thanhlcao <thanhlcao@kms.com.vn>
2023-10-27 14:16:09 +0700

    separate SQLDatabase and SAPHana

--- modules/recovery_vault/backup_policies_vm_workload.tf
+++ modules/recovery_vault/backup_policies_vm_workload.tf
@@ -4,7 +4,7 @@ resource "azurerm_backup_policy_vm_workload" "sql" {
   name                = each.value.name
   resource_group_name = local.resource_group_name
   recovery_vault_name = azurerm_recovery_services_vault.asr.name
-  workload_type       = each.value.workload_type
+  workload_type       = "SQLDataBase"
 
   settings {
     time_zone           = each.value.timezone
@@ -21,7 +21,7 @@ resource "azurerm_backup_policy_vm_workload" "sql" {
         frequency            = try(each.value.backup.frequency, null)
         frequency_in_minutes = try(each.value.backup.frequency_in_minutes, null)
         time                 = try(each.value.backup.time, null)
-        weekdays             = try(each.value.weekdays.time, null)
+        weekdays             = try(each.value.backup.weekdays, null)
       }
     }
 
@@ -48,9 +48,9 @@ resource "azurerm_backup_policy_vm_workload" "sql" {
       content {
         count       = each.value.retention_monthly.count
         format_type = each.value.retention_monthly.format_type
-        monthdays   = try(each.value.backup.monthdays, null)
-        weekdays    = try(each.value.backup.weekdays, null)
-        weeks       = try(each.value.backup.weeks, null)
+        monthdays   = try(each.value.retention_monthly.monthdays, null)
+        weekdays    = try(each.value.retention_monthly.weekdays, null)
+        weeks       = try(each.value.retention_monthly.weeks, null)
       }
     }
 
@@ -61,9 +61,88 @@ resource "azurerm_backup_policy_vm_workload" "sql" {
         count       = each.value.retention_yearly.count
         format_type = each.value.retention_yearly.format_type
         months      = each.value.retention_yearly.months
-        monthdays   = try(each.value.backup.monthdays, null)
-        weekdays    = try(each.value.weekdays.monthdays, null)
-        weeks       = try(each.value.weeks.monthdays, null)
+        monthdays   = try(each.value.retention_yearly.monthdays, null)
+        weekdays    = try(each.value.retention_yearly.weekdays, null)
+        weeks       = try(each.value.retention_yearly.weeks, null)
+      }
+    }
+
+    dynamic "simple_retention" {
+      for_each = lookup(each.value, "simple_retention", null) == null ? [] : [1]
+
+      content {
+        count = each.value.simple_retention.count
+      }
+    }
+  }
+}
+
+resource "azurerm_backup_policy_vm_workload" "saphana" {
+  for_each = try(var.settings.backup_policies.saphana, {})
+
+  name                = each.value.name
+  resource_group_name = local.resource_group_name
+  recovery_vault_name = azurerm_recovery_services_vault.asr.name
+  workload_type       = "SAPHanaDatabase"
+
+  settings {
+    time_zone           = each.value.timezone
+    compression_enabled = each.value.compression_enabled
+  }
+
+  protection_policy {
+    policy_type = each.value.policy_type
+
+    dynamic "backup" {
+      for_each = lookup(each.value, "backup", null) == null ? [] : [1]
+
+      content {
+        frequency            = try(each.value.backup.frequency, null)
+        frequency_in_minutes = try(each.value.backup.frequency_in_minutes, null)
+        time                 = try(each.value.backup.time, null)
+        weekdays             = try(each.value.backup.weekdays, null)
+      }
+    }
+
+    dynamic "retention_daily" {
+      for_each = lookup(each.value, "retention_daily", null) == null ? [] : [1]
+
+      content {
+        count = each.value.retention_daily.count
+      }
+    }
+
+    dynamic "retention_weekly" {
+      for_each = lookup(each.value, "retention_weekly", null) == null ? [] : [1]
+
+      content {
+        count    = each.value.retention_weekly.count
+        weekdays = each.value.retention_weekly.weekdays
+      }
+    }
+
+    dynamic "retention_monthly" {
+      for_each = lookup(each.value, "retention_monthly", null) == null ? [] : [1]
+
+      content {
+        count       = each.value.retention_monthly.count
+        format_type = each.value.retention_monthly.format_type
+        monthdays   = try(each.value.retention_monthly.monthdays, null)
+        weekdays    = try(each.value.retention_monthly.weekdays, null)
+        weeks       = try(each.value.retention_monthly.weeks, null)
+      }
+    }
+
+    dynamic "retention_yearly" {
+      for_each = lookup(each.value, "retention_yearly", null) == null ? [] : [1]
+
+      content {
+        count       = each.value.retention_yearly.count
+        format_type = each.value.retention_yearly.format_type
+        months      = each.value.retention_yearly.months
+        monthdays   = try(each.value.retention_yearly.monthdays, null)
+        weekdays    = try(each.value.retention_yearly.weekdays, null)
+        weeks       = try(each.value.retention_yearly.weeks, null)
       }
     }
 
