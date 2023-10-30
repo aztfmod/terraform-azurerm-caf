c8141bf6af21cf4f21247785dd3b08bcde925ef5
Magnus Longva <magnus@longva.email>
2023-10-30 16:45:57 +0100

    remove outcommented block

--- modules/roles/custom_roles/module.tf
+++ modules/roles/custom_roles/module.tf
@@ -14,16 +14,6 @@ resource "azurecaf_name" "custom_role" {
   use_slug      = local.global_settings.use_slug
 }
 
-# resource "azurecaf_name" "custom_role" {
-#   name          = var.custom_role.name
-#   resource_type = "azurerm_resource_group"
-#   #TODO: need to be changed to appropriate resource (no caf reference for now)
-#   prefixes      = var.global_settings.prefixes
-#   random_length = var.global_settings.random_length
-#   clean_input   = true
-#   passthrough   = var.global_settings.passthrough
-#   use_slug      = var.global_settings.use_slug
-# }
 
 resource "azurerm_role_definition" "custom_role" {
   name = azurecaf_name.custom_role.result
