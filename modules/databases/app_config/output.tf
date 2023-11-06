bcc52b880c66839e56771a8d7e5a804256e656a2
shanoor <17402800+shanoor@users.noreply.github.com>
2023-11-06 15:45:09 +0100

    Added userassigned identies support, custom private endpoint RG, reworked app_config_key

--- modules/databases/app_config/output.tf
+++ modules/databases/app_config/output.tf
@@ -17,3 +17,23 @@ output "rbac_id" {
   value       = try(azurerm_app_configuration.config.identity[0].principal_id, null)
   description = "The rbac_id of the App Config for role assignments."
 }
+
+output "primary_read_key_connection_string" {
+  value       = azurerm_app_configuration.config.primary_read_key
+  description = "The Connection String for the primary read access key - comprising of the Endpoint, ID and Secret."
+}
+
+output "primary_write_key_connection_string" {
+  value       = azurerm_app_configuration.config.primary_write_key
+  description = "The Connection String for the primary write access key - comprising of the Endpoint, ID and Secret."
+}
+
+output "secondary_read_key_connection_string" {
+  value       = azurerm_app_configuration.config.secondary_read_key
+  description = "The Connection String for the secondary read access key - comprising of the Endpoint, ID and Secret."
+}
+
+output "secondary_write_key_connection_string" {
+  value       = azurerm_app_configuration.config.secondary_write_key
+  description = "The Connection String for the secondary write access key - comprising of the Endpoint, ID and Secret."
+}
--- modules/databases/app_config/settings/app_config_setting.tf
+++ modules/databases/app_config/settings/app_config_setting.tf
@@ -1,28 +1,11 @@
-resource "azurecaf_name" "settings" {
-  name          = format("app-config-%s", var.config_name)
-  resource_type = "azurerm_template_deployment"
-  prefixes      = var.global_settings.prefixes
-  random_length = var.global_settings.random_length
-  clean_input   = true
-  passthrough   = var.global_settings.passthrough
-  use_slug      = var.global_settings.use_slug
-}
+resource "azurerm_app_configuration_key" "config" {
+  for_each = var.config_settings
 
-# create app config settings
-resource "azurerm_resource_group_template_deployment" "settings" {
-  name                = "settings"
-  resource_group_name = var.resource_group_name
-
-  template_content = file(local.arm_filename)
-
-  parameters_content = jsonencode(local.parameters_content)
-
-  deployment_mode = "Incremental"
-
-  timeouts {
-    create = "1h"
-    update = "1h"
-    delete = "1h"
-    read   = "5m"
-  }
+  configuration_store_id = var.app_config_id
+  key                    = each.value.key
+  label                  = try(each.value.label, null)
+  # if value is a keyvault reference, set the correct type, set value to null and set vault_key_reference
+  type                = try(each.value.vault_key, null) == null ? "kv" : "vault"
+  value               = try(each.value.vault_key, null) == null ? each.value.value : null
+  vault_key_reference = try(each.value.vault_key, null) == null ? null : "${var.keyvaults[try(each.value.vault_key.keyvault.lz_key, var.client_config.landingzone_key)][each.value.vault_key.keyvault.key].vault_uri}secrets/${each.value.vault_key.secret_name}"
 }
