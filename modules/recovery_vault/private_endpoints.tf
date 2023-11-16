b965324808fd5ffca7b569ffd063c3b0749eda45
Marco Mansi <mmansi@gemeentewestland.nl>
2023-11-16 17:58:10 +0100

    removed unused parameter subresource_names

--- modules/recovery_vault/private_endpoints.tf
+++ modules/recovery_vault/private_endpoints.tf
@@ -14,7 +14,6 @@ module "private_endpoint" {
   resource_group_name = local.resource_group_name
   subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : var.vnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
   settings            = each.value
-  subresource_names   = ["AzureSiteRecovery"]
   global_settings     = var.global_settings
   tags                = local.tags
   base_tags           = var.base_tags
