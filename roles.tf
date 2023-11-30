e268e80ca3233b4aea9111a0f33fb55517d66ed5
shanoor <17402800+shanoor@users.noreply.github.com>
2023-11-30 18:43:20 +0100

    Fixed key mapping

--- roles.tf
+++ roles.tf
@@ -229,7 +229,7 @@ locals {
           ]
         ]
       ]
-    ) : format("%s_%s_%s_%s", mapping.object_id_resource_type, mapping.scope_key_resource, replace(mapping.role_definition_name, " ", "_"), mapping.object_id_key_resource) => mapping
+    ) : format("%s_%s_%s_%s_%s", mapping.object_id_resource_type, mapping.scope_key_resource, replace(mapping.role_definition_name, " ", "_"), coalesce(mapping.object_id_lz_key, local.client_config.landingzone_key), mapping.object_id_key_resource) => mapping
   }
 }
 
