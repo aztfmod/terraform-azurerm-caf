7058bc8b07fbb8896ca2c7d64c0a3b94d211104d
janik.muenk <janik.muenk@adesso.de>
2023-12-01 10:57:13 +0000

    feat(dynamic-secrets): Adding missing outputs

---
+++ modules/security/dynamic_keyvault_secrets/secret/outputs.tf
@@ -0,0 +1,18 @@
+output "secret" {
+  value = azurerm_key_vault_secret.secret
+}
+output "id" {
+  value = azurerm_key_vault_secret.secret.id
+}
+output "resource_id" {
+  value = azurerm_key_vault_secret.secret.resource_id
+}
+output "resource_versionless_id" {
+  value = azurerm_key_vault_secret.secret.resource_versionless_id
+}
+output "version" {
+  value = azurerm_key_vault_secret.secret.version
+}
+output "versionless_id" {
+  value = azurerm_key_vault_secret.secret.versionless_id
+}
