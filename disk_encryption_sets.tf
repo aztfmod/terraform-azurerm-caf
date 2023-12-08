9edf529bf004732b16ae136ae95e1eb99eb3e54b
Olivier Comeau <ocomeau@artm.quebec>
2023-12-08 13:35:23 -0500

    fix(disk-encryption-sets): Correction du depends_on pour utiliser le premier de la liste

--- disk_encryption_sets.tf
+++ disk_encryption_sets.tf
@@ -1,7 +1,7 @@
 module "disk_encryption_sets" {
   source = "./modules/security/disk_encryption_set"
 
-  depends_on = [time_sleep.azurerm_role_assignment_for]
+  depends_on = [time_sleep.azurerm_role_assignment_for.0]
 
   for_each = {
     for key, value in local.security.disk_encryption_sets : key => value
@@ -22,7 +22,7 @@ module "disk_encryption_sets" {
 module "disk_encryption_sets_external" {
   source = "./modules/security/disk_encryption_set_external"
 
-  depends_on = [time_sleep.azurerm_role_assignment_for]
+  depends_on = [time_sleep.azurerm_role_assignment_for.0]
 
   for_each = {
     for key, value in local.security.disk_encryption_sets : key => value
