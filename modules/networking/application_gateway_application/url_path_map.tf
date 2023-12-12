2720bda7fb675bc0300ded05364572824c1cf36c
khairi <akamarulzama@microsoft.com>
2023-12-12 09:26:44 +0000

    fix urlpathmap issue when update backendpool

--- modules/networking/application_gateway_application/url_path_map.tf
+++ modules/networking/application_gateway_application/url_path_map.tf
@@ -20,9 +20,13 @@ resource "null_resource" "set_url_path_map" {
       APPLICATION_GATEWAY_ID   = var.application_gateway.id
       NAME                     = each.value.name
       PATHS                    = each.value.paths
+      DEFAULT_ADDRESS_POOL     = try(var.settings.backend_pools[each.value.default_backend_pool_key].name, var.settings.backend_pools[each.value.backend_pool_key].name)
       ADDRESS_POOL             = var.settings.backend_pools[each.value.backend_pool_key].name
+      DEFAULT_HTTP_SETTINGS    = try(var.settings.http_settings[each.value.default_http_settings_key].name, var.settings.http_settings[each.value.http_settings_key].name)
       HTTP_SETTINGS            = var.settings.http_settings[each.value.http_settings_key].name
+      DEFAULT_REDIRECT_CONFIG  = try(each.value.default_redirect_config, each.value.redirect_config, null)
       REDIRECT_CONFIG          = try(each.value.redirect_config, null)
+      DEFAULT_REWRITE_RULE_SET = try(var.settings.rewrite_rule_sets[each.value.default_rewrite_rule_set_key].name, var.settings.rewrite_rule_sets[each.value.rewrite_rule_set_key].name, null)
       REWRITE_RULE_SET         = try(var.settings.rewrite_rule_sets[each.value.rewrite_rule_set_key].name, null)
       RULE_NAME                = try(each.value.rule_name, null)
       WAF_POLICY               = try(each.value.waf_policy, null)
