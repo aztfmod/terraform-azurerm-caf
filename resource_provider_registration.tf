15eff8615fca1b42b733e9c67139ef1a2bb85d2e
Olivier Comeau <ocomeau@artm.quebec>
2023-12-14 19:36:24 +0000

    fix(resource_provider_registration): Ajuster la condition pour rendre le contenu de feature optionnel

--- resource_provider_registration.tf
+++ resource_provider_registration.tf
@@ -3,12 +3,11 @@ resource "azurerm_resource_provider_registration" "default" {
   name     = each.value.name
 
   dynamic "feature" {
-    for_each = can(try(each.value.feature_name, null)) != null ? [1] : []
+    for_each = try(each.value.feature_name, null) != null ? [1] : []
 
     content {
       name       = try(each.value.feature_name, null)
       registered = try(each.value.registered, true)
     }
   }
-
 }
