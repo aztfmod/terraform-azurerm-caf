f1d4b4224e92d467a28468d247c0b89b584263a0
Olivier Comeau <ocomeau@artm.quebec>
2023-12-14 19:18:13 +0000

    fix(resource_provider_registration): Rendre la section feature optionnel

--- resource_provider_registration.tf
+++ resource_provider_registration.tf
@@ -2,8 +2,13 @@ resource "azurerm_resource_provider_registration" "default" {
   for_each = var.resource_provider_registration
   name     = each.value.name
 
-  feature {
-    name       = each.value.feature_name
-    registered = each.value.registered
+  dynamic "feature" {
+    for_each = can(try(each.value.feature_name, null)) != null ? [1] : []
+
+    content {
+      name       = try(each.value.feature_name, null)
+      registered = try(each.value.registered, true)
+    }
   }
+
 }
