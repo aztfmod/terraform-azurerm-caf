81237857fe75761c09461c18ee3fbd541fb8266f
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-24 23:25:48 +0000

    no rg tags

--- modules/analytics/machine_learning/main.tf
+++ modules/analytics/machine_learning/main.tf
@@ -5,7 +5,6 @@ locals {
   # tags = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
   tags = var.base_tags ? merge(
     var.global_settings.tags,
-    try(var.resource_group.tags, null),
     try(var.settings.tags, null)
   ) : try(var.settings.tags, null)
 
