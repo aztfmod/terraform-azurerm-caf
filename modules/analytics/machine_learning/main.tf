3a95c2f479f1094b70409c645c5563e0b55e800d
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-24 23:11:30 +0000

    fmt

--- modules/analytics/machine_learning/main.tf
+++ modules/analytics/machine_learning/main.tf
@@ -8,23 +8,13 @@ locals {
     try(var.resource_group.tags, null),
     try(var.settings.tags, null)
   ) : try(var.settings.tags, null)
-  
+
   resource_group = coalesce(
     try(var.resource_groups[var.client_config.landingzone_key][var.settings.resource_group_key], null),
     try(var.resource_groups[var.settings.lz_key][var.settings.resource_group_key], null),
     try(var.resource_groups[var.client_config.landingzone_key][var.settings.resource_group.key], null),
     try(var.resource_groups[var.settings.resource_group.lz_key][var.settings.resource_group.key], null)
   )
-}
-locals {
-  tags = var.base_tags ? merge(
-    var.global_settings.tags,
-    try(var.resource_group.tags, null),
-    try(var.settings.tags, null)
-  ) : try(var.settings.tags, null)
-
-  location            = coalesce(var.location, var.resource_group.location)
-  resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
 }
 
 terraform {
