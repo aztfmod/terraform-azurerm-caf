7e5c0e0ecd365787b5738337aa251112ae0661d7
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-28 21:42:37 +0000

    amend vars

--- modules/cognitive_services/cognitive_services_account/variables.tf
+++ modules/cognitive_services/cognitive_services_account/variables.tf
@@ -1,3 +1,8 @@
+variable "base_tags" {
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = bool
+}
+
 variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
@@ -11,13 +16,25 @@ variable "location" {
   type        = string
 }
 
+variable "private_dns" {
+  default = {}
+}
+
 variable "private_endpoints" {
   default = {}
 }
 
 variable "resource_group_name" {
-  description = "Name of the existing resource group to deploy the virtual machine"
+  description = "Name of the existing resource group to deploy to"
   type        = string
 }
 
+variable "resource_groups" {}
+
 variable "settings" {}
+
+variable "tags" = {
+  var.tags
+}
+
+variable "vnets" {}
