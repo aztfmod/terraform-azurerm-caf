a6c1b5b05512c2fa4d12aed4520ef4052d6d7a78
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-28 22:00:18 +0000

    remove vars

--- modules/cognitive_services/cognitive_services_account/main.tf
+++ modules/cognitive_services/cognitive_services_account/main.tf
@@ -8,11 +8,7 @@ terraform {
 
 locals {
   tags = var.base_tags ? merge(
-    local.module_tag,
     var.global_settings.tags,
     try(var.settings.tags, null)
   ) : try(var.settings.tags, null)
-
-  location            = coalesce(var.location, var.resource_group.location)
-  resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
 }
