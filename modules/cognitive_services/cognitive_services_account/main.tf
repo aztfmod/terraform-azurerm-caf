8f2f190198c3cd56d513844d662b216006df6f8c
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-28 21:49:15 +0000

    add locals

--- modules/cognitive_services/cognitive_services_account/main.tf
+++ modules/cognitive_services/cognitive_services_account/main.tf
@@ -5,3 +5,14 @@ terraform {
     }
   }
 }
+
+locals {
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    try(var.resource_group.tags, null),
+    try(var.settings.tags, null)
+  ) : try(var.settings.tags, null)
+
+  location            = coalesce(var.location, var.resource_group.location)
+  resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
+}
