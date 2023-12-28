f2c0212a5acba4716ff16a10fdb77a464a235993
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-28 21:30:53 +0000

    pass thru pe

--- cognitive_service.tf
+++ cognitive_service.tf
@@ -4,8 +4,9 @@ module "cognitive_services_account" {
 
   client_config       = local.client_config
   global_settings     = local.global_settings
-  resource_group_name = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
   location            = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
+  private_endpoints   = try(each.value.private_endpoints, {})
+  resource_group_name = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
   settings            = each.value
 }
 
