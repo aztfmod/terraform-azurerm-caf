48b729b12a1ea6d3aed3f86b51d44a9244911b7a
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-29 03:47:07 +0000

    remove output

--- modules/cognitive_services/search_service/output.tf
+++ modules/cognitive_services/search_service/output.tf
@@ -2,8 +2,3 @@ output "id" {
   description = "The ID of the Search Service"
   value       = azurerm_search_service.search.id
 }
-
-output "endpoint" {
-  description = "The endpoint used to connect to the Search Service"
-  value       = azurerm_search_service.search.endpoint
-}
