d7d77eb3f21eed254ab72f290931d40084bbe45b
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-29 04:00:16 +0000

    change rg var

--- modules/cognitive_services/search_service/search_service.tf
+++ modules/cognitive_services/search_service/search_service.tf
@@ -2,7 +2,7 @@ resource "azurerm_search_service" "search" {
   location                      = var.location
   name                          = var.settings.name
   public_network_access_enabled = var.public_network_access_enabled
-  resource_group_name           = var.resource_group_name
+  resource_group_name           = local.resource_group_name
   sku                           = var.settings.sku
   tags                          = local.tags
 }
