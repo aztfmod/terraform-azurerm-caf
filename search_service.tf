cd886eb84a5f369892ead0ea391fee066b699ab0
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-29 18:21:19 +0000

    revert rg changes

--- search_service.tf
+++ search_service.tf
@@ -8,9 +8,9 @@ module "azurerm_search_service" {
   location            = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
   private_endpoints   = try(each.value.private_endpoints, {})
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
+  resource_groups     = local.combined_objects_resource_groups
   settings            = each.value
   vnets               = local.combined_objects_networking
-  # resource_groups     = local.combined_objects_resource_groups
 }
 
 output "search_service" {
