f64786ffc1c8f3c82c6348b4550ca4dfff8e0825
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-29 04:00:16 +0000

    change rg var

--- modules/cognitive_services/search_service/main.tf
+++ modules/cognitive_services/search_service/main.tf
@@ -11,4 +11,6 @@ locals {
     var.global_settings.tags,
     try(var.settings.tags, null)
   ) : try(var.settings.tags, null)
+
+  resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
 }
