b3b43b8df39d60edf80eb26b42a70eed2e111a58
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-29 17:39:15 +0000

    update

--- modules/cognitive_services/search_service/search_service.tf
+++ modules/cognitive_services/search_service/search_service.tf
@@ -1,8 +1,8 @@
 resource "azurerm_search_service" "search" {
   location                      = var.location
   name                          = var.settings.name
-  public_network_access_enabled = var.public_network_access_enabled
-  resource_group_name           = local.resource_group_name
+  public_network_access_enabled = var.settings.public_network_access_enabled
+  resource_group_name           = var.resource_group_name
   sku                           = var.settings.sku
   tags                          = local.tags
 }
--- search_service.tf
+++ search_service.tf
@@ -7,10 +7,10 @@ module "azurerm_search_service" {
   global_settings     = local.global_settings
   location            = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
   private_endpoints   = try(each.value.private_endpoints, {})
-  resource_group_name = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
-  resource_groups     = local.combined_objects_resource_groups
+  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
   settings            = each.value
   vnets               = local.combined_objects_networking
+  # resource_groups     = local.combined_objects_resource_groups
 }
 
 output "search_service" {
