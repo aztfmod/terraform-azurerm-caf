5b758da997d7d4e54f1ea6c03f1c007784266c14
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-29 20:19:32 +0000

    add ss pe subnet tf

--- modules/networking/private_links/endpoints/subnet/search_service.tf
+++ modules/networking/private_links/endpoints/subnet/search_service.tf
@@ -1,7 +1,7 @@
-module "cognitive_services_accounts" {
+module "search_services" {
   source = "../private_endpoint"
   for_each = {
-    for key, value in try(var.private_endpoints.cognitive_services_accounts, {}) : key => value
+    for key, value in try(var.private_endpoints.search_services, {}) : key => value
     if can(value.lz_key) == false
   }
   base_tags           = var.base_tags
@@ -11,16 +11,16 @@ module "cognitive_services_accounts" {
   name                = try(each.value.name, each.key)
   private_dns         = var.private_dns
   resource_group_name = try(var.resource_groups[each.value.resource_group_key].name, var.vnet_resource_group_name)
-  resource_id         = can(each.value.resource_id) ? each.value.resource_id : var.remote_objects.cognitive_services_accounts[var.client_config.landingzone_key][each.key].id
+  resource_id         = can(each.value.resource_id) ? each.value.resource_id : var.remote_objects.search_services[var.client_config.landingzone_key][each.key].id
   settings            = each.value
   subnet_id           = var.subnet_id
   subresource_names   = ["searchService"]
 }
 
-module "cognitive_services_accounts_remote" {
+module "search_services_remote" {
   source = "../private_endpoint"
   for_each = {
-    for key, value in try(var.private_endpoints.cognitive_services_accounts, {}) : key => value
+    for key, value in try(var.private_endpoints.search_services, {}) : key => value
     if can(value.lz_key)
   }
   base_tags           = var.base_tags
@@ -30,7 +30,7 @@ module "cognitive_services_accounts_remote" {
   name                = try(each.value.name, each.key)
   private_dns         = var.private_dns
   resource_group_name = try(var.resource_groups[each.value.resource_group_key].name, var.vnet_resource_group_name)
-  resource_id         = var.remote_objects.cognitive_services_accounts[each.value.lz_key][each.key].id
+  resource_id         = var.remote_objects.search_services[each.value.lz_key][each.key].id
   settings            = each.value
   subnet_id           = var.subnet_id
   subresource_names   = ["searchService"]
