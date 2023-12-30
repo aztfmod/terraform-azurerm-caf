ef6343b0ef90778cba5b3cf41d5b952d208a5751
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-30 13:07:39 +0000

    rationalise vars

--- modules/analytics/analysis_services_server/variables.tf
+++ modules/analytics/analysis_services_server/variables.tf
@@ -1,23 +1,19 @@
-# variable "base_tags" {
-#   description = "Base tags for the resource to be inherited from the resource group."
-#   type        = bool
-# }
+variable "base_tags" {
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = bool
+}
 
-# variable "global_settings" {
-#   description = "Global settings object (see module README.md)"
-# }
+variable "global_settings" {
+  description = "Global settings object (see module README.md)"
+}
 
 # variable "resource_group_name" {}
 
 
-
-
-
-
-# variable "location" {
-#   description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
-#   type        = string
-# }
+variable "location" {
+  description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
+  type        = string
+}
 
 # variable "private_dns" {
 #   default = {}
@@ -31,11 +27,13 @@
 #   default = {}
 # }
 
-# variable "resource_group_name" {
-#   description = "Name of the existing resource group to deploy to"
-#   type        = string
-# }
+variable "resource_group_name" {
+  description = "Name of the existing resource group to deploy to"
+  type        = string
+}
 
 # variable "resource_groups" {}
+
+variable "settings" {}
 
 # variable "vnets" {}
