f1f525a84d33207767425ec3d2b1c07dfbc2eeb6
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-30 16:34:58 +0000

    try if

--- modules/analytics/analysis_services_server/analysis_services_server.tf
+++ modules/analytics/analysis_services_server/analysis_services_server.tf
@@ -9,7 +9,7 @@ resource "azurerm_analysis_services_server" "as_server" {
 
   dynamic "ipv4_firewall_rule" {
     # for_each = try(var.settings.ipv4_firewall_rules, null) != null ? [var.settings.ipv4_firewall_rules] : []
-    for_each = can(var.settings.ipv4_firewall_rules) ? [var.settings.ipv4_firewall_rules] : []
+    for_each = var.settings.ipv4_firewall_rules != null ? [var.settings.ipv4_firewall_rules] : []
     # for_each = var.settings.ipv4_firewall_rules
 
     content {
