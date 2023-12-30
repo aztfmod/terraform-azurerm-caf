9204b745085a67331ce3b4fb16e6cf5cb7cb0ac0
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-30 15:42:38 +0000

    tidy

--- modules/analytics/analysis_services_server/analysis_services_server.tf
+++ modules/analytics/analysis_services_server/analysis_services_server.tf
@@ -8,9 +8,7 @@ resource "azurerm_analysis_services_server" "as_server" {
   tags                    = local.tags
 
   dynamic "ipv4_firewall_rule" {
-    # for_each = try(var.settings.ipv4_firewall_rules, null) != null ? [var.settings.ipv4_firewall_rules] : []
-    # for_each = local.ipv4_firewall_rules
-    for_each = var.settings.ipv4_firewall_rules
+    for_each = try(var.settings.ipv4_firewall_rules, null) != null ? [var.settings.ipv4_firewall_rules] : []
 
     content {
       name        = ipv4_firewall_rule.value.name
@@ -19,18 +17,3 @@ resource "azurerm_analysis_services_server" "as_server" {
     }
   }
 }
-
-# locals {
-#   ipv4_firewall_rules = [
-#     {
-#       name        = "ClientIPAddress03"
-#       range_start = "217.22.14.10"
-#       range_end   = "217.22.14.10"
-#     },
-#     {
-#       name        = "ClientIPAddress04"
-#       range_start = "51.140.74.14"
-#       range_end   = "51.140.74.14"
-#     },
-#   ]
-# }
