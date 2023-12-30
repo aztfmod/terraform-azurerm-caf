30a7a8f9d1fa6b8955e2ec34393cdca2dd15bd98
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2023-12-30 15:17:41 +0000

    hardcode list test

--- modules/analytics/analysis_services_server/analysis_services_server.tf
+++ modules/analytics/analysis_services_server/analysis_services_server.tf
@@ -8,7 +8,8 @@ resource "azurerm_analysis_services_server" "as_server" {
   tags                    = local.tags
 
   dynamic "ipv4_firewall_rule" {
-    for_each = try(var.settings.ipv4_firewall_rules, null) != null ? [var.settings.ipv4_firewall_rules] : []
+    # for_each = try(var.settings.ipv4_firewall_rules, null) != null ? [var.settings.ipv4_firewall_rules] : []
+    for_each = local.ipv4_firewall_rules
 
     content {
       name        = ipv4_firewall_rule.value.name
@@ -16,4 +17,19 @@ resource "azurerm_analysis_services_server" "as_server" {
       range_end   = try(ipv4_firewall_rule.value.range_end, null)
     }
   }
+}
+
+locals {
+  ipv4_firewall_rules = [
+    {
+      name        = "ClientIPAddress03"
+      range_start = "217.22.14.10"
+      range_end   = "217.22.14.10"
+    },
+    {
+      name        = "ClientIPAddress04"
+      range_start = "51.140.74.14"
+      range_end   = "51.140.74.14"
+    },
+  ]
 }
