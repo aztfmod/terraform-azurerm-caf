f0f059e1b6693d4307db08eedc86c188b9b8db60
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2024-01-01 19:45:37 +0000

    tidy

--- modules/analytics/machine_learning/main.tf
+++ modules/analytics/machine_learning/main.tf
@@ -10,7 +10,6 @@ locals {
   module_tag = {
     "module" = basename(abspath(path.module))
   }
-  # tags = merge(var.base_tags, local.module_tag, try(var.settings.tags, null))
   tags = var.base_tags ? merge(
     local.module_tag,
     var.global_settings.tags,
