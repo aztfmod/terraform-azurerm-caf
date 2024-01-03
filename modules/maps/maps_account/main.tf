1faf0453823be43d6410a3d8ceb5b21e5a6374f4
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2024-01-03 20:27:20 +0000

    add map module tag

--- modules/maps/maps_account/main.tf
+++ modules/maps/maps_account/main.tf
@@ -7,6 +7,10 @@ terraform {
 }
 
 locals {
+  module_tag = {
+    "module" = basename(abspath(path.module))
+  }
+
   tags = var.base_tags ? merge(
     var.global_settings.tags,
     try(var.resource_group.tags, null),
