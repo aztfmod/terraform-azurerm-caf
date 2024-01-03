d3362aefb36262279a5b83f0331a4d8f49a0e470
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2024-01-03 15:19:39 +0000

    add tags to cog

--- modules/cognitive_services/cognitive_services_account/main.tf
+++ modules/cognitive_services/cognitive_services_account/main.tf
@@ -7,7 +7,13 @@ terraform {
 }
 
 locals {
+  module_tag = {
+    "module" = basename(abspath(path.module))
+  }
+
   tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    local.module_tag,
     var.global_settings.tags,
     try(var.settings.tags, null)
   ) : try(var.settings.tags, null)
