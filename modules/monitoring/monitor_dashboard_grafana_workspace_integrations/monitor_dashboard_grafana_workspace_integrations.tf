d2fc6b865fcfe2ca64edf77d105f39eb79367bc5
Olivier Comeau <ocomeau@artm.quebec>
2024-01-18 20:25:33 +0000

    fix(monitor_dashboard_grafana_workspace_integrations): Remove data and use only vars from configurations

--- modules/monitoring/monitor_dashboard_grafana_workspace_integrations/monitor_dashboard_grafana_workspace_integrations.tf
+++ modules/monitoring/monitor_dashboard_grafana_workspace_integrations/monitor_dashboard_grafana_workspace_integrations.tf
@@ -1,26 +1,6 @@
-data "azapi_resource" "dashboard" {
-  type                   = "Microsoft.Dashboard/grafana@2022-08-01"
-  resource_id            = can(var.settings.monitor_dashboard_grafana.id) || can(var.settings.monitor_dashboard_grafana_id) ? try(var.settings.monitor_dashboard_grafana.id, var.settings.monitor_dashboard_grafana_id) : try(var.monitor_dashboard_grafana[try(var.settings.monitor_dashboard_grafana.lz_key, var.client_config.landingzone_key)][try(var.settings.monitor_dashboard_grafana_key, var.settings.monitor_dashboard_grafana.key)].id)
-  response_export_values = ["properties.grafanaIntegrations.azureMonitorWorkspaceIntegrations"]
-}
-
-locals {
-  azapi_data = try(data.azapi_resource.dashboard, {})
-
-  workspaces = distinct(
-    concat(
-      [
-        for ws in length(local.azapi_data) != 0 ? jsondecode(local.azapi_data.output).properties.grafanaIntegrations.azureMonitorWorkspaceIntegrations : [] : {
-          resource_id = ws.azureMonitorWorkspaceResourceId
-        }
-      ],
-      var.settings.monitor_dashboard_grafana.workspaces
-    )
-  )
-}
-
-
 resource "azapi_update_resource" "dashboard" {
+  count = length(var.settings.monitor_dashboard_grafana.workspaces) > 0 ? 1 : 0
+
   type        = "Microsoft.Dashboard/grafana@2022-08-01"
   resource_id = can(var.settings.monitor_dashboard_grafana.id) || can(var.settings.monitor_dashboard_grafana_id) ? try(var.settings.monitor_dashboard_grafana.id, var.settings.monitor_dashboard_grafana_id) : try(var.monitor_dashboard_grafana[try(var.settings.monitor_dashboard_grafana.lz_key, var.client_config.landingzone_key)][try(var.settings.monitor_dashboard_grafana_key, var.settings.monitor_dashboard_grafana.key)].id)
 
@@ -28,7 +8,7 @@ resource "azapi_update_resource" "dashboard" {
     properties = {
       grafanaIntegrations = {
         azureMonitorWorkspaceIntegrations = [
-          for workspace_key, workspace in local.workspaces : {
+          for workspace_key, workspace in var.settings.monitor_dashboard_grafana.workspaces : {
             azureMonitorWorkspaceResourceId = workspace.resource_id
           }
         ]
