8a2782c230ed733a34629ba4bb48617867011789
Olivier Comeau <ocomeau@artm.quebec>
2024-01-18 16:05:05 +0000

    fix(monitor-dashboard-grafana-workspace-integrations): Fix resource fecth in different subs

--- modules/monitoring/monitor_dashboard_grafana_workspace_integrations/monitor_dashboard_grafana_workspace_integrations.tf
+++ modules/monitoring/monitor_dashboard_grafana_workspace_integrations/monitor_dashboard_grafana_workspace_integrations.tf
@@ -1,20 +1,24 @@
-data "azurerm_dashboard_grafana" "dashboard" {
-  name                = can(var.settings.monitor_dashboard_grafana.id) || can(var.settings.monitor_dashboard_grafana_id) ? try(var.settings.monitor_dashboard_grafana.id, var.settings.monitor_dashboard_grafana_id) : try(var.monitor_dashboard_grafana[try(var.settings.monitor_dashboard_grafana.lz_key, var.client_config.landingzone_key)][try(var.settings.monitor_dashboard_grafana_key, var.settings.monitor_dashboard_grafana.key)].name)
-  resource_group_name = can(var.settings.monitor_dashboard_grafana.id) || can(var.settings.monitor_dashboard_grafana_id) ? try(var.settings.monitor_dashboard_grafana.id, var.settings.monitor_dashboard_grafana_id) : try(var.monitor_dashboard_grafana[try(var.settings.monitor_dashboard_grafana.lz_key, var.client_config.landingzone_key)][try(var.settings.monitor_dashboard_grafana_key, var.settings.monitor_dashboard_grafana.key)].resource_group_name)
+data "azapi_resource" "dashboard" {
+  type                   = "Microsoft.Dashboard/grafana@2022-08-01"
+  resource_id            = can(var.settings.monitor_dashboard_grafana.id) || can(var.settings.monitor_dashboard_grafana_id) ? try(var.settings.monitor_dashboard_grafana.id, var.settings.monitor_dashboard_grafana_id) : try(var.monitor_dashboard_grafana[try(var.settings.monitor_dashboard_grafana.lz_key, var.client_config.landingzone_key)][try(var.settings.monitor_dashboard_grafana_key, var.settings.monitor_dashboard_grafana.key)].id)
+  response_export_values = ["properties.grafanaIntegrations.azureMonitorWorkspaceIntegrations"]
 }
 
 locals {
+  azapi_data = try(data.azapi_resource.dashboard, {})
+
   workspaces = distinct(
     concat(
       [
-        for ws in data.azurerm_dashboard_grafana.dashboard.azure_monitor_workspace_integrations : {
-          resource_id = ws.resource_id
+        for ws in length(local.azapi_data) != 0 ? jsondecode(local.azapi_data.output).properties.grafanaIntegrations.azureMonitorWorkspaceIntegrations : [] : {
+          resource_id = ws.azureMonitorWorkspaceResourceId
         }
       ],
       var.settings.monitor_dashboard_grafana.workspaces
     )
   )
 }
+
 
 resource "azapi_update_resource" "dashboard" {
   type        = "Microsoft.Dashboard/grafana@2022-08-01"
