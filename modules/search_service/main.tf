395edab3c334b840e9bf27d4d12579429a7ff24e
dramac-planet <maciej.drabik@weareplanet.com>
2024-02-01 17:39:25 +0000

    update search service module

---
+++ modules/search_service/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+    }
+  }
+
+}
+
+locals {
+  tags = var.base_tags ? merge(
+    var.global_settings.tags,
+    try(var.resource_group.tags, null),
+    try(var.settings.tags, null)
+  ) : try(var.settings.tags, null)
+
+  location            = coalesce(var.location, var.resource_group.location)
+  resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
+}
--- search_service.tf
+++ search_service.tf
@@ -1,14 +1,38 @@
-module "search_services" {
-  source   = "./modules/ai/search_services"
+module "search_service" {
+  source   = "./modules/search_service"
   for_each = local.search_services.search_services
 
   client_config       = local.client_config
   global_settings     = local.global_settings
-  resource_group_name = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
+  resource_group      = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)]
+  resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : null
   location            = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
   settings            = each.value
+
+  base_tags           = local.global_settings.inherit_tags
+  identity            = try(each.value.identity, null)
+  private_endpoints   = try(each.value.private_endpoints, {})
+  private_dns         = local.combined_objects_private_dns
+  vnets               = local.combined_objects_networking
+  virtual_subnets     = local.combined_objects_virtual_subnets
 }
 
-output "search_services" {
-  value = module.search_services
+output "search_service" {
+  value = module.search_service
 }
+
+
+# module "search_services" {
+#   source   = "./modules/ai/search_services"
+#   for_each = local.search_services.search_services
+
+#   client_config       = local.client_config
+#   global_settings     = local.global_settings
+#   resource_group_name = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
+#   location            = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
+#   settings            = each.value
+# }
+
+# output "search_services" {
+#   value = module.search_services
+# }
