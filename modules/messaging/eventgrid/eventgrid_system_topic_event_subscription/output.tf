10130fa45b2e7edaaf7762403af411e65d6b0d98
serco-karleveleigh <71755319+serco-karleveleigh@users.noreply.github.com>
2024-02-09 18:30:29 +0000

    second attempt at output

--- modules/messaging/eventgrid/eventgrid_system_topic_event_subscription/output.tf
+++ modules/messaging/eventgrid/eventgrid_system_topic_event_subscription/output.tf
@@ -1,5 +1,5 @@
 output "id" {
-  value       = azurerm_eventgrid_event_subscription.egstes.id
-  description = "The ID of the EventGrid Event Subscription."
+  value       = azurerm_eventgrid_system_topic_event_subscription.egstes.id
+  description = "The ID of the EventGrid System Topic Event Subscription."
 }
 
