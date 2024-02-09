02bf1fa172d51c9d32a457a1f86183d9d905fa49
serco-karleveleigh <71755319+serco-karleveleigh@users.noreply.github.com>
2024-02-09 18:25:36 +0000

    fix module output

--- modules/messaging/eventgrid/eventgrid_system_topic_event_subscription/output.tf
+++ modules/messaging/eventgrid/eventgrid_system_topic_event_subscription/output.tf
@@ -1,5 +1,5 @@
 output "id" {
-  value       = azurerm_eventgrid_event_subscription.eges.id
+  value       = azurerm_eventgrid_event_subscription.egstes.id
   description = "The ID of the EventGrid Event Subscription."
 }
 
