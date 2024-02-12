a82e0ec3bcc77d075f3824f8f2eed7def8708e4c
serco-karleveleigh <71755319+serco-karleveleigh@users.noreply.github.com>
2024-02-12 11:51:40 +0000

    fix caf name to genera;

--- modules/messaging/eventgrid/eventgrid_system_topic/module.tf
+++ modules/messaging/eventgrid/eventgrid_system_topic/module.tf
@@ -1,6 +1,6 @@
 resource "azurecaf_name" "egst" {
   name          = var.settings.name
-  resource_type = "azurerm_eventgrid_system_topic"
+  resource_type = "general"
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
   clean_input   = true
--- modules/messaging/eventgrid/eventgrid_system_topic_event_subscription/module.tf
+++ modules/messaging/eventgrid/eventgrid_system_topic_event_subscription/module.tf
@@ -1,15 +1,15 @@
 
-# resource "azurecaf_name" "egstes" {
-#   name          = var.settings.name
-#   resource_type = "azurerm_eventgrid_event_subscription"
-#   prefixes      = var.global_settings.prefixes
-#   random_length = var.global_settings.random_length
-#   clean_input   = true
-#   passthrough   = var.global_settings.passthrough
-#   use_slug      = var.global_settings.use_slug
-# }
+resource "azurecaf_name" "egstes" {
+  name          = var.settings.name
+  resource_type = "general"
+  prefixes      = var.global_settings.prefixes
+  random_length = var.global_settings.random_length
+  clean_input   = true
+  passthrough   = var.global_settings.passthrough
+  use_slug      = var.global_settings.use_slug
+}
 resource "azurerm_eventgrid_system_topic_event_subscription" "egstes" {
-  name                 = var.settings.name
+  name                 = azurecaf_name.egstes.result
   resource_group_name  = can(var.settings.resource_group.name) ? var.settings.resource_group.name : var.remote_objects.resource_group[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][var.settings.resource_group.key].name
   system_topic         = can(var.settings.eventgrid_system_topic.name) ? var.settings.eventgrid_system_topic.name : var.remote_objects.eventgrid_system_topic[try(var.settings.eventgrid_system_topic.lz_key, var.client_config.landingzone_key)][var.settings.eventgrid_system_topic.key].name
 
