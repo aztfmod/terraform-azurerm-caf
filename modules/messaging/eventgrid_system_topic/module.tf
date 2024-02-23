81aa79b3db855329f412a4aa6fe4d8c529c57fed
Najeeb Vayal Valappil <najeeb.vayal-valappil@adesso.de>
2024-02-23 14:03:04 +0100

    fix - remote_object.all introduces unexpected drift

--- modules/messaging/eventgrid_system_topic/module.tf
+++ modules/messaging/eventgrid_system_topic/module.tf
@@ -9,8 +9,8 @@ resource "azurecaf_name" "egst" {
 }
 resource "azurerm_eventgrid_system_topic" "egst" {
   name                = azurecaf_name.egst.result
-  resource_group_name = coalesce(try(var.settings.resource_group.name, null), try(var.remote_objects.all.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][var.settings.resource_group.key].name, null), try(var.remote_objects.all[var.settings.topic.resource_type][try(var.settings.topic.lz_key, var.client_config.landingzone_key)][var.settings.topic.resource_key].resource_group_name, null))
-  location            = coalesce(try(var.settings.location, null), try(var.remote_objects.all.resource_groups[try(var.settings.resource_group.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group.key, var.settings.resource_group_key)].location, null), try(var.remote_objects.all[var.settings.topic.resource_type][try(var.settings.topic.lz_key, var.client_config.landingzone_key)][var.settings.topic.resource_key].location, null), "global")
+  resource_group_name = local.resource_group_name
+  location            = local.location
 
   dynamic "identity" {
     for_each = can(var.settings.identity) ? [var.settings.identity] : []
@@ -21,7 +21,7 @@ resource "azurerm_eventgrid_system_topic" "egst" {
     }
   }
 
-  source_arm_resource_id = coalesce(try(var.settings.topic_arm_id, null), try(var.remote_objects.all[var.settings.topic.resource_type][try(var.settings.topic.lz_key, var.client_config.landingzone_key)][var.settings.topic.resource_key].id, null))
+  source_arm_resource_id = coalesce(try(var.settings.topic.resource_id, null), try(var.remote_objects[var.settings.topic.resource_type][try(var.settings.topic.lz_key, var.client_config.landingzone_key)][var.settings.topic.resource_key].id, null))
   topic_type             = var.settings.topic_type
   tags                   = local.tags
 
