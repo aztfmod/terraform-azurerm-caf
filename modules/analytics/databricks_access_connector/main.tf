5e5530b4b327b07fb6c8b1616eba8a219f7aacb7
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2024-03-07 00:00:14 +0200

    Update main.tf

--- modules/analytics/databricks_access_connector/main.tf
+++ modules/analytics/databricks_access_connector/main.tf
@@ -6,7 +6,7 @@ locals {
   ) : try(var.settings.tags, null)
 
   
-  resource_group      = var.resource_groups[try(var.settings.lz_key, var.settings.resource_group.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group.key, var.settings.resource_group_key)]
+  resource_group  = var.resource_groups[try(var.settings.lz_key, var.settings.resource_group.lz_key, var.client_config.landingzone_key)][try(var.settings.resource_group.key, var.settings.resource_group_key)]
 }
 
 terraform {
