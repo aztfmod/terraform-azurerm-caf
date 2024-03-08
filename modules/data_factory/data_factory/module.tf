ac5968fff9c1d1c051aaf0c69264c659911f5bfa
Tjomme Vergauwen <42241062+TjommeVergauwen@users.noreply.github.com>
2024-03-08 13:30:11 +0100

    #990: Update module.tf
    global_parameter should be processed as list of objects

--- modules/data_factory/data_factory/module.tf
+++ modules/data_factory/data_factory/module.tf
@@ -25,7 +25,7 @@ resource "azurerm_data_factory" "df" {
     }
   }
   dynamic "global_parameter" {
-    for_each = try(var.settings.global_parameter, null) != null ? [var.settings.global_parameter] : []
+    for_each = try(var.settings.global_parameter, null) != null ? var.settings.global_parameter : {}
 
     content {
       name  = global_parameter.value.name
