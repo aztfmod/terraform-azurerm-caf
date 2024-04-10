8f720ec10cb9954bd15f35bcfc22851b3dd72179
Prakash Paruchuri <prakash.paruchuri@microland.com>
2024-04-10 15:46:13 +0100

    fix tags variable

--- modules/networking/route_servers/main.tf
+++ modules/networking/route_servers/main.tf
@@ -2,5 +2,5 @@ locals {
   module_tag = {
     "module" = basename(abspath(path.module))
   }
-  tags = merge(var.global_settings.tags, local.module_tag, var.tags, try(var.route_server.tags, null))
+  tags = merge(var.global_settings.tags, local.module_tag, var.tags, try(var.settings.tags, null))
 }
