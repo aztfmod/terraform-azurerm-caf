8c42477a8b9dd45a76f7eb6206231793175f7c03
Prakash Paruchuri <prakash.paruchuri@microland.com>
2024-04-10 16:32:35 +0100

    cleanup

--- modules/networking/route_servers/route_servers.tf
+++ modules/networking/route_servers/route_servers.tf
@@ -4,7 +4,7 @@ resource "azurerm_route_server" "route_server" {
   location                         = var.location
   name                             = var.settings.name
   public_ip_address_id             = try(var.settings.public_ip_address_id, null) != null ? var.settings.public_ip_address_id : (lookup(var.settings.public_ip_address, "lz_key", null) == null ? var.public_ip_addresses[var.client_config.landingzone_key][var.settings.public_ip_address.public_ip_address_key].id : var.public_ip_addresses[var.settings.public_ip_address.lz_key][var.settings.public_ip_address.public_ip_address_key].id)
-  resource_group_name              = var.resource_group_name #check
+  resource_group_name              = var.resource_group_name
   sku                              = try(var.settings.sku, "Standard")
   subnet_id                        = try(var.settings.subnet_id, null) != null ? var.settings.subnet_id : (lookup(var.settings.subnet, "lz_key", null) == null ? var.virtual_networks[var.client_config.landingzone_key][var.settings.subnet.vnet_key].subnets[var.settings.subnet.subnet_key].id : var.virtual_networks[var.settings.subnet.lz_key][var.settings.subnet.vnet_key].subnets[var.settings.subnet.subnet_key].id)
   tags                             = local.tags
