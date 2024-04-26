ca6d4a293ca57cb77970c738fb8cc5f12c63ec31
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2024-04-26 15:27:43 +0100

    added disable_local_auth to cog serv

--- modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
+++ modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
@@ -9,10 +9,11 @@ resource "azurecaf_name" "service" {
 }
 
 resource "azurerm_cognitive_account" "service" {
-  name                = azurecaf_name.service.result
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  disable_local_auth  = var.settings.disable_local_auth
   kind                = var.settings.kind
+  location            = var.location
+  name                = azurecaf_name.service.result
+  resource_group_name = var.resource_group_name
   sku_name            = var.settings.sku_name
 
   qna_runtime_endpoint = var.settings.kind == "QnAMaker" ? var.settings.qna_runtime_endpoint : try(var.settings.qna_runtime_endpoint, null)
