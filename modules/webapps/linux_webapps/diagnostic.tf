9fb6eb496f28a7df69c1fab8c841b825c4f8a239
Danny Hudson <87715999+ml-dannyhudson@users.noreply.github.com>
2024-06-06 11:45:34 +0100

    correct resource ref

--- modules/webapps/linux_webapps/diagnostic.tf
+++ modules/webapps/linux_webapps/diagnostic.tf
@@ -2,7 +2,7 @@ module "diagnostics" {
   source = "../../diagnostics"
   count  = var.diagnostic_profiles == null ? 0 : 1
 
-  resource_id       = azurerm_app_service.app_service.id
+  resource_id       = azurerm_linux_web_app.linux_web_apps.id
   resource_location = local.location
   diagnostics       = var.diagnostics
   profiles          = var.diagnostic_profiles
