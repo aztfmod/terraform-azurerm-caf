52529719c3452355a44ce5eb58a963c3ab53f6cf
janik.muenk <janik.muenk@adesso.de>
2024-06-20 14:16:24 +0000

    fix: fmt

--- billing_invoice_section.tf
+++ billing_invoice_section.tf
@@ -2,10 +2,10 @@ module "invoice_section" {
   source     = "./modules/billing/invoice_sections"
   for_each   = var.invoice_sections
 
-    global_settings     = local.global_settings
+  global_settings     = local.global_settings
   client_config       = local.client_config
   settings            = each.value
-    base_tags = local.global_settings.inherit_tags
+  base_tags = local.global_settings.inherit_tags
 }
 
 output "invoice_sections" {
