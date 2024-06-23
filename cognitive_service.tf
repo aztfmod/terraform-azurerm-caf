e342bcf740bea672d31008d6cc2243e3845118d1
joseph@direct-ex.co.il <joseph@direct-ex.co.il>
2024-06-23 20:30:23 +0300

    Add Identity block

--- cognitive_service.tf
+++ cognitive_service.tf
@@ -7,6 +7,8 @@ module "cognitive_services_account" {
   resource_group_name = local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].name
   location            = lookup(each.value, "region", null) == null ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].location : local.global_settings.regions[each.value.region]
   settings            = each.value
+
+  managed_identities = local.combined_objects_managed_identities
 }
 
 output "cognitive_services_account" {
