5ab5b835df4839dcc7e2e00510cdcd33299194df
Kévin DELMONT <kevin.delmont@visiativ.com>
2024-06-27 09:46:29 +0200

    Add cosmosdb output

--- modules/databases/cosmos_dbs/output.tf
+++ modules/databases/cosmos_dbs/output.tf
@@ -6,6 +6,38 @@ output "primary_key" {
   value = azurerm_cosmosdb_account.cosmos_account.primary_key
 }
 
+output "primary_sql_connection_string" {
+  value = azurerm_cosmosdb_account.cosmos_account.primary_sql_connection_string
+}
+
+output "secondary_sql_connection_string" {
+  value = azurerm_cosmosdb_account.cosmos_account.secondary_sql_connection_string
+}
+
+output "primary_readonly_sql_connection_string" {
+  value = azurerm_cosmosdb_account.cosmos_account.primary_readonly_sql_connection_string
+}
+
+output "secondary_readonly_sql_connection_string" {
+  value = azurerm_cosmosdb_account.cosmos_account.secondary_readonly_sql_connection_string
+}
+
+output "primary_mongodb_connection_string" {
+  value = azurerm_cosmosdb_account.cosmos_account.primary_mongodb_connection_string
+}
+
+output "secondary_mongodb_connection_string" {
+  value = azurerm_cosmosdb_account.cosmos_account.secondary_mongodb_connection_string
+}
+
+output "primary_readonly_mongodb_connection_string" {
+  value = azurerm_cosmosdb_account.cosmos_account.primary_readonly_mongodb_connection_string
+}
+
+output "secondary_readonly_mongodb_connection_string" {
+  value = azurerm_cosmosdb_account.cosmos_account.secondary_readonly_mongodb_connection_string
+}
+
 output "endpoint" {
   value = azurerm_cosmosdb_account.cosmos_account.endpoint
 }
