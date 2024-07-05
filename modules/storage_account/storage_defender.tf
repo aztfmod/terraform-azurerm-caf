bae8752c2de72e0ee447e3de2b9c4b52cf6ca96d
shanoor <17402800+shanoor@users.noreply.github.com>
2024-07-05 14:53:22 +0200

    Update storage defender

--- modules/storage_account/storage_defender.tf
+++ modules/storage_account/storage_defender.tf
@@ -2,8 +2,8 @@ resource "azurerm_security_center_storage_defender" "defender" {
   count = can(var.storage_account.defender) ? 1 : 0
 
   storage_account_id                          = azurerm_storage_account.stg.id
-  override_subscription_settings_enabled      = try(var.storage_account.defender.override_subscription_settings, false)
-  malware_scanning_on_upload_enabled          = try(var.storage_account.defender.malware_scanning_on_upload, false)
-  malware_scanning_on_upload_cap_gb_per_month = try(var.storage_account.defender.malware_scanning_on_upload_cap_gb_per_month, -1)
-  sensitive_data_discovery_enabled            = try(var.storage_account.defender.sensitive_data_discovery_enabled, false)
+  override_subscription_settings_enabled      = try(var.storage_account.defender.override_subscription_settings, null)
+  malware_scanning_on_upload_enabled          = try(var.storage_account.defender.malware_scanning_on_upload, null)
+  malware_scanning_on_upload_cap_gb_per_month = try(var.storage_account.defender.malware_scanning_on_upload_cap_gb_per_month, null)
+  sensitive_data_discovery_enabled            = try(var.storage_account.defender.sensitive_data_discovery_enabled, null)
 }
