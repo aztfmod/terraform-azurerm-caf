6268054d4d284a3cea67b54a48f6ab585ba4937d
shanoor <17402800+shanoor@users.noreply.github.com>
2024-07-05 12:20:28 +0200

    Update modules/storage_account/storage_defender.tf
    Co-authored-by: Arnaud Lheureux <arnaudlh@users.noreply.github.com>

--- modules/storage_account/storage_defender.tf
+++ modules/storage_account/storage_defender.tf
@@ -3,7 +3,7 @@ resource "azurerm_security_center_storage_defender" "defender" {
 
   storage_account_id                          = azurerm_storage_account.stg.id
   override_subscription_settings_enabled      = try(var.storage_account.defender.override_subscription_settings, null)
-  malware_scanning_on_upload_enabled          = try(var.storage_account.defender.malware_scanning_on_upload, false)
+  malware_scanning_on_upload_enabled          = try(var.storage_account.defender.malware_scanning_on_upload, null)
   malware_scanning_on_upload_cap_gb_per_month = try(var.storage_account.defender.malware_scanning_on_upload_cap_gb_per_month, -1)
   sensitive_data_discovery_enabled            = try(var.storage_account.defender.sensitive_data_discovery_enabled, false)
 }
