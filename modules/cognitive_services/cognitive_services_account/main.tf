5439d3f35d018e223ac59ada174f152dca731558
shanoor <17402800+shanoor@users.noreply.github.com>
2024-08-13 15:42:25 +0200

    Enhanced cognitive services: private endpoint support, diagnostic settings, more outputs (#27)

--- modules/cognitive_services/cognitive_services_account/main.tf
+++ modules/cognitive_services/cognitive_services_account/main.tf
@@ -7,6 +7,7 @@ terraform {
 }
 
 locals {
+  location = coalesce(var.location, var.resource_group.location)
   tags = var.base_tags ? merge(
     var.global_settings.tags,
     try(var.resource_group.tags, null),
