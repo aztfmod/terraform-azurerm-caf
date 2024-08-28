c99c4d15c4e09bdb0f5cfac7e918942b1714b621
Alexandre Chatelain <alexandre@chatelain.io>
2024-08-28 14:50:48 -0400

    fix: cyclic error

--- keyvault_keys.tf
+++ keyvault_keys.tf
@@ -1,5 +1,5 @@
 module "keyvault_keys" {
-  depends_on = [module.keyvaults, module.keyvault_access_policies, time_sleep.azurerm_role_assignment_for.0]
+  depends_on = [module.keyvaults, module.keyvault_access_policies]
 
   source = "./modules/security/keyvault_key"
 
