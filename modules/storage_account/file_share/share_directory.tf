77add0a673a60466782e1e894c6caa797b607ffc
Rafael Fernández <rafael.dominguez@avanade.com>
2024-10-29 16:43:39 +0100

    refactor(storage_account): remove unused variable and update file_share_directory module

--- modules/storage_account/file_share/share_directory.tf
+++ modules/storage_account/file_share/share_directory.tf
@@ -2,6 +2,5 @@ module "file_share_directory" {
   source   = "../file_share_directory"
   for_each = try(var.settings.directories, {})
   storage_share_id     = azurerm_storage_share.fs.id
-  share_name           = azurerm_storage_share.fs.name
   settings             = each.value
 }
