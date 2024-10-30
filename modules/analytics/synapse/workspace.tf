22d5f7e6fe37201a8e1163aa758722e393ba0835
Rafael Fernández <rafael.dominguez@avanade.com>
2024-10-30 10:50:47 +0100

    feat(synapse): refactor AAD admin settings and add storage account type to upgrade hashicorp/azurerm to  4.7.0
    This commit includes changes to the Azure Synapse Terraform modules.
    
    1. The Azure Active Directory (AAD) admin settings have been refactored and moved from the workspace module to a new separate module called `workspace_aad_admin`. This change provides better modularity and isolation of concerns.
    
    2. A new attribute `storage_account_type` has been added to the `sql_pool` resource with a default value of "GRS" (Geo-Redundant Storage). This allows users to specify the type of storage account to be used by the SQL pool in Azure Synapse.

--- modules/analytics/synapse/workspace.tf
+++ modules/analytics/synapse/workspace.tf
@@ -28,15 +28,7 @@ resource "azurerm_synapse_workspace" "ws" {
     type = "SystemAssigned"
   }
 
-  dynamic "aad_admin" {
-    for_each = try(var.settings.aad_admin, null) != null ? [var.settings.aad_admin] : []
 
-    content {
-      login     = try(aad_admin.value.login, null)
-      object_id = try(aad_admin.value.object_id, null)
-      tenant_id = try(aad_admin.value.tenant_id, null)
-    }
-  }
 
   dynamic "azure_devops_repo" {
     for_each = try(var.settings.azure_devops_repo, null) != null ? [var.settings.azure_devops_repo] : []
@@ -151,3 +143,12 @@ resource "azurerm_synapse_firewall_rule" "wrkspc_firewalls" {
   end_ip_address   = each.value.end_ip
 }
 
+
+
+resource "azurerm_synapse_workspace_aad_admin" "wrkspc_aad_admin" {
+  for_each = try(var.settings.aad_admin, null) != null ? { for k, v in [var.settings.aad_admin] : k => v } : {}
+  synapse_workspace_id = azurerm_synapse_workspace.ws.id
+  login     = try(each.value.login, null)
+  object_id = try(each.value.object_id, null)
+  tenant_id = try(each.value.tenant_id, null)
+}
