25e16c80fce0a7dcba8b23e8114f7bcdb2b9e155
Mohammed Hadiulla <mohammedhadiulla7@gmail.com>
2024-11-01 19:05:45 +0100

    added backup in azurerm_cosmosdb_account

--- modules/databases/cosmos_dbs/cosmosdb_account.tf
+++ modules/databases/cosmos_dbs/cosmosdb_account.tf
@@ -56,6 +56,19 @@ resource "azurerm_cosmosdb_account" "cosmos_account" {
       name = capabilities.value
     }
   }
+
+  dynamic "backup" {
+    for_each = try(var.settings.backup, null) != null ? [var.settings.backup] : []
+
+    content {
+      type                = backup.value.type
+      tier                = try(backup.value.tier, null)
+      interval_in_minutes = try(backup.value.interval_in_minutes, null)
+      retention_in_hours  = try(backup.value.retention_in_hours, null)
+      storage_redundancy  = try(backup.value.storage_redundancy, null)
+    }
+  }
+
   dynamic "restore" {
     for_each = try(var.settings.restore, null) != null ? [var.settings.restore] : []
     content {
@@ -71,5 +84,3 @@ resource "azurerm_cosmosdb_account" "cosmos_account" {
     }
   }
 }
-
-
