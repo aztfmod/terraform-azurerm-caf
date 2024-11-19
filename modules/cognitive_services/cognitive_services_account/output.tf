11e7fad319f2596a4b478e21f225fea9f412aee7
kevindelmont <133667252+kevindelmont@users.noreply.github.com>
2024-11-19 17:38:59 +0100

    Fixed deployment_id output

--- modules/cognitive_services/cognitive_services_account/output.tf
+++ modules/cognitive_services/cognitive_services_account/output.tf
@@ -19,5 +19,5 @@ output "identity" {
 
 output "deployment_id" {
   description = "The ID of the Deployment for Azure Cognitive Services Account."
-  value       = azurerm_cognitive_deployment.id
+  value       = try(azurerm_cognitive_deployment.deployment[0].id, null)
 }
